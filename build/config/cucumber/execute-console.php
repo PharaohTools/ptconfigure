a5b492442c5c9ea38b2a3146513d82f88580fb5d
David Amanshia <phpengine@hotmail.co.uk>
2012-12-29 21:45:20 +0000

    added executor changes and config for phpunit to ignore views folder

--- build/config/cucumber/execute-console.php
+++ build/config/cucumber/execute-console.php
@@ -1,16 +1,21 @@
 <?php
 
-cukeCiExecutor::execute();
+cukeConsoleExecutor::execute();
 
-class cukeCiExecutor {
+class cukeConsoleExecutor {
 
     public static function execute(){
         self::setWorkingDirectory();
+        self::startRuby();
         self::performTests(); }
 
     private function setWorkingDirectory(){
         $scriptLocation = dirname(__FILE__).'/../../tests/';
         chdir($scriptLocation); }
+
+    private function startRuby(){
+        $starter = 'rvm use ~/.rvm/bin/ruby';
+        self::executeAndOutput($starter); }
 
     private function performTests(){
         $command = 'cucumber';
