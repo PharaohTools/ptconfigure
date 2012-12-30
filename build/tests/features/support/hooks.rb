0800f60cc70b7309f10dcabdbc0fc2cbcab4dbf1
David Amanshia <phpengine@hotmail.co.uk>
2012-12-30 16:14:10 +0000

    try to get cucumber in too

--- build/tests/features/support/hooks.rb
+++ build/tests/features/support/hooks.rb
@@ -3,7 +3,7 @@
 After do |scenario|
   if scenario.failed?
     name =  scenario.name.gsub(' ', '_').downcase.gsub(/[^a-z0-9_]/i, '')
-    path = "./build/reports/cucumber/screenshots/#{name}.png"
+    path = "../../reports/cucumber/html/screenshots/#{name}.png"
     page.driver.browser.save_screenshot path
     embed "screenshots/#{name}.png", "image/png"
   end
