a69f1ae3945c45fb9dbf9ac088a9bd72d1c517c1
David Amanshia <phpengine@hotmail.co.uk>
2013-03-11 05:01:02 +0000

    db for php

--- src/Model/DBConfigureDataGCFW2.php
+++ src/Model/DBConfigureDataGCFW2.php
@@ -6,6 +6,11 @@ class DBConfigureDataGCFW2 extends Base {
 
     private $settingsFileLocation = 'src/Core'; // no trail slash
     private $settingsFileName = 'Database.php';
+    private $settingsFileReplacements ;
+
+    public function __construct(){
+        $this->setReplacements();
+    }
 
     public function getProperty($property) {
         return $this->$property;
@@ -13,6 +18,16 @@ class DBConfigureDataGCFW2 extends Base {
 
     public function __call($var1, $var2){
         return null();
+    }
+
+    private function setReplacements(){
+
+        $this->settingsFileReplacements = array(
+            '$this->dbName ='=>'$this->dbName = "****DB NAME****"',
+            '$this->dbUser ='=>'$this->dbUser = "****DB USER****"',
+            '$this->dbPass ='=>'$this->dbPass = "****DB PASS****"',
+            '$this->dbHost ='=>'$this->dbHost = "****DB HOST****"');
+
     }
 
 }
