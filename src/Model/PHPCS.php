67784f624d3b0713a5e5322d529360c4efc92250
Beeby Dave C <beebydave@beebydave-VirtualBox.(none)>
2013-03-24 18:16:07 +0000

    added php cs

---
+++ src/Model/PHPCS.php
@@ -0,0 +1,23 @@
+<?php
+
+Namespace Model;
+
+class PHPCS extends BasePHPApp {
+
+  public function __construct() {
+    parent::__construct();
+    $this->autopilotDefiner = "PHPCS";
+    $this->fileSources = array(
+      array(
+        "https://github.com/phpengine/boxboss-phpcs.git",
+        "phpcs",
+        null // can be null for none
+      )
+    );
+    $this->programNameMachine = "phpcs"; // command and app dir name
+    $this->programNameFriendly = "PHP CSniffer"; // 12 chars
+    $this->programNameInstaller = "PHP Code Sniffer";
+    $this->programExecutorTargetPath = 'PHP_CodeSniffer-1.5.0RC1/CodeSniffer.php';
+  }
+
+}
