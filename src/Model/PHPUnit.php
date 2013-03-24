489fb5b95301051468c2d5d717d9ee98e1b3d2aa
David Amanshia <phpengine@hotmail.co.uk>
2013-03-24 17:37:15 +0000

    PHP Unit is installing correctly. Only App Available so far

--- src/Model/PHPUnit.php
+++ src/Model/PHPUnit.php
@@ -5,11 +5,11 @@ Namespace Model;
 class PHPUnit extends BasePHPApp {
 
   public function __construct() {
+    parent::__construct();
     $this->autopilotDefiner = "PHPUnit35";
     $this->fileSource = "http://github.com/phpengine/boxboss-source-phpunit";
     $this->programNameMachine = "phpunit"; // command to be used on command line
     $this->programNameFriendly = " PHP Unit ! "; // 12 chars
-    parent::__construct();
   }
 
 }
