5ceecb70500f1b51805b7cfb404fb09f08c72410
golden <phpengine@hotmail.co.uk>
2013-04-28 11:58:11 +0100

    jenkins updatable correctly

--- src/Model/Jenkins.php
+++ src/Model/Jenkins.php
@@ -7,7 +7,12 @@ class Jenkins extends BaseLinuxApp {
   public function __construct() {
     parent::__construct();
     $this->autopilotDefiner = "Jenkins";
-    $this->installCommands = array( "apt-get install -y jenkins" );
+    $this->installCommands = array(
+      "cd /tmp" ,
+      "wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -",
+      "echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list",
+      "apt-get update -y",
+      "apt-get install -y jenkins" );
     $this->uninstallCommands = array( "apt-get remove -y jenkins" );
     $this->programDataFolder = "/var/lib/jenkins"; // command and app dir name
     $this->programNameMachine = "jenkins"; // command and app dir name
