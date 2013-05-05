9a4f7f37367be319f3cfd3e8bd84faba5f09618b
David Amanshia <phpengine@hotmail.co.uk>
2013-05-05 09:50:52 +0100

    new models, 3 unfinished and standard tools includes zip

--- src/Model/StandardTools.php
+++ src/Model/StandardTools.php
@@ -7,8 +7,8 @@ class StandardTools extends BaseLinuxApp {
   public function __construct() {
     parent::__construct();
     $this->autopilotDefiner = "StandardTools";
-    $this->installCommands = array( "apt-get clean", "apt-get update", "apt-get install -y curl vim drush" );
-    $this->uninstallCommands = array( "apt-get clean", "apt-get update", "apt-get remove -y curl vim drush" );
+    $this->installCommands = array( "apt-get clean", "apt-get update", "apt-get install -y curl vim drush zip" );
+    $this->uninstallCommands = array( "apt-get clean", "apt-get update", "apt-get remove -y curl vim drush zip" );
     $this->programDataFolder = "/opt/StandardTools"; // command and app dir name
     $this->programNameMachine = "standardtools"; // command and app dir name
     $this->programNameFriendly = "Std. Tools!!"; // 12 chars
