515eabb2d995c86918a45a7e371abc455c9b6bec
David Amanshia <phpengine@hotmail.co.uk>
2013-07-04 12:38:53 +0100

    index page autodetects installed modules to display their info

--- src/Modules/Index/Model/Index.php
+++ src/Modules/Index/Model/Index.php
@@ -5,25 +5,16 @@ Namespace Model;
 class Index extends Base {
 
   public function findAllModuleNames(){
-
-    $allInfoObjects = AutoLoader::getInfoObjects() ;
-    /*
-    $aliases = array("co"=>"checkout", "hosteditor"=>"hostEditor", "he"=>"hostEditor", "host"=>"hostEditor",
-        "vhostEditor"=>"VHostEditor", "vhosteditor"=>"VHostEditor", "vhc"=>"VHostEditor", "cuke"=>"cukeConf",
-        "cukeconf"=>"cukeConf", "proj"=>"project", "db"=>"database");
-    */
-    $aliases = array();
+    $allInfoObjects = \Core\AutoLoader::getInfoObjects() ;
+    $moduleNames = array();
     foreach ($allInfoObjects as $infoObject) {
-        $aliases = array_merge( $aliases, $infoObject->routeAliases() ); }
-
-
-
-    $array_keys = array_keys($infoObject->routesAvailable()) ;
-    $miniRay = array();
-    $miniRay["command"] = $array_keys[0];
-    $miniRay["name"] = $infoObject->name ;
-
-    return $this->performDBDrop();
+        $array_keys = array_keys($infoObject->routesAvailable()) ;
+        $miniRay = array();
+        $miniRay["command"] = $array_keys[0];
+        $miniRay["name"] = $infoObject->name ;
+        $miniRay["hidden"] = $infoObject->hidden ;
+        $moduleNames[] = $miniRay; }
+    return $moduleNames;
   }
 
 }
