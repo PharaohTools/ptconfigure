6f47bc4d5a0abda1fd590489f748b9282ad240e2
David Amanshia <phpengine@hotmail.co.uk>
2013-09-18 23:56:55 +0100

    Added invoke to clep

---
+++ src/Modules/GitLab/info.GitLab.php
@@ -0,0 +1,51 @@
+<?php
+
+Namespace Info;
+
+class GitLabInfo extends Base {
+
+    public $hidden = false;
+
+    public $name = "GitLab";
+
+    public function __construct() {
+      parent::__construct();
+    }
+
+    public function routesAvailable() {
+      return array( "GitLab" =>  array_merge(parent::routesAvailable(), array("install") ) );
+    }
+
+    public function routeAliases() {
+      return array("gitlab"=>"GitLab", "git-lab"=>"GitLab");
+    }
+
+    public function autoPilotVariables() {
+      return array(
+        "GitLab" => array(
+          "GitLab" => array(
+            "programDataFolder" => "", // command and app dir name
+            "programNameMachine" => "gitlab", // command and app dir name
+            "programNameFriendly" => "!Git Lab!!", // 12 chars
+            "programNameInstaller" => "Git Lab",
+          )
+        )
+      );
+    }
+
+    public function helpDefinition() {
+      $help = <<<"HELPDATA"
+  This command allows you to install a full Git Lab installation on to a server
+  The dependencies for GitLab are also installed.
+
+  GitLab, gitlab, git-lab
+
+        - install
+        Installs the latest version of GitLab on a system
+        example: cleopatra gitlab install
+
+HELPDATA;
+      return $help ;
+    }
+
+}
--- src/Modules/RubyRVM/info.RubyRVM.php
+++ src/Modules/RubyRVM/info.RubyRVM.php
@@ -37,13 +37,18 @@ class RubyRVMInfo extends Base {
 
   public function helpDefinition() {
     $help = <<<"HELPDATA"
-  This command allows you to install Ruby RVM
+  This command allows you to install Ruby RVM.
 
   RubyRVM, rubyrvm, ruby-rvm, rubyRVM
 
         - install
         Installs Ruby RVM
         example: cleopatra ruby-rvm install
+
+  Ruby is installed the recommended per-user way. To use ruby after the install
+  first run "source ~/.rvm/scripts/rvm" to get access to the Ruby install for
+  your user, then "rvm install 1.9.3" (to install, specify version as needed)
+  then "rvm use 1.9.3" (to select your default version for the session)
 
 HELPDATA;
     return $help ;
