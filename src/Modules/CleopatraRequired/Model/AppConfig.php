5b119a2b02e57da0e5064d2150f5f2d6c822a269
Golden <golden@golden-lappy.(none)>
2013-09-22 12:55:10 +0100

    fixed appconfig to create file if needed and use correct name

--- src/Modules/CleopatraRequired/Model/AppConfig.php
+++ src/Modules/CleopatraRequired/Model/AppConfig.php
@@ -5,12 +5,16 @@ Namespace Model;
 class AppConfig {
 
 
-    private static function checkIsDHProject() {
-        return file_exists('dhproj');
+    private static function checkSettingsExistOrCreateIt() {
+        if (file_exists(dirname(__FILE__).'../../../cleovars')) {
+            return true; }
+        else {
+            touch(dirname(__FILE__).'../../../cleovars');
+            return true; }
     }
 
     public static function setProjectVariable($variable, $value, $listAdd=null) {
-        if (self::checkIsDHProject()) {
+        if (self::checkSettingsExistOrCreateIt()) {
             $appConfigArray = self::loadDHProjectFile();
             if ( $listAdd == true ) { $appConfigArray[$variable][] = $value ; }
             else { $appConfigArray[$variable] = $value ; }
@@ -19,23 +23,23 @@ class AppConfig {
 
     public static function getProjectVariable($variable) {
         $value = null;
-        if (self::checkIsDHProject()) {
+        if (self::checkSettingsExistOrCreateIt()) {
             $appConfigArray = self::loadDHProjectFile();
             $value = (isset($appConfigArray[$variable])) ? $appConfigArray[$variable] : null ; }
         return $value;
     }
 
     private static function loadDHProjectFile() {
-        $appConfigArraySeialized = file_get_contents('dhproj');
-        $decoded = unserialize($appConfigArraySeialized);
+        $appConfigArraySerialized = file_get_contents(dirname(__FILE__).'../../../cleovars');
+        $decoded = unserialize($appConfigArraySerialized);
         $appConfigArray = (is_object($decoded)) ? $decoded : array() ;
         return new \ArrayObject($appConfigArray);
     }
 
     private static function saveDHProjectFile($appConfigArray) {
         $appConfigObject = new \ArrayObject($appConfigArray);
-        $appConfigObjectSeialized = serialize($appConfigObject);
-        file_put_contents('dhproj', $appConfigObjectSeialized);
+        $appConfigObjectSerialized = serialize($appConfigObject);
+        file_put_contents(dirname(__FILE__).'../../../cleovars', $appConfigObjectSerialized);
     }
 
 }
