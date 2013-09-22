0e1ab10880172bc685ee152a7607fae0f1e81882
Golden <golden@golden-lappy.(none)>
2013-09-22 12:09:39 +0100

    fixed mysql server to install silently properly

--- src/Modules/MysqlServer/Model/MysqlServer.php
+++ src/Modules/MysqlServer/Model/MysqlServer.php
@@ -4,16 +4,31 @@ Namespace Model;
 
 class MysqlServer extends BaseLinuxApp {
 
-  public function __construct($params) {
-    parent::__construct($params);
-    $this->autopilotDefiner = "MysqlServer";
-    $this->installCommands = array( "apt-get install -y mysql-client mysql-server" );
-    $this->uninstallCommands = array( "apt-get remove -y mysql-client mysql-server" );
-    $this->programDataFolder = "/opt/MysqlServer"; // command and app dir name
-    $this->programNameMachine = "mysqlserver"; // command and app dir name
-    $this->programNameFriendly = "MySQL Server!"; // 12 chars
-    $this->programNameInstaller = "MySQL Server";
-    $this->initialize();
-  }
+    public function __construct($params) {
+        parent::__construct($params);
+        $newRootPass = $this->getNewRootPass();
+        $this->autopilotDefiner = "MysqlServer";
+        $this->installCommands = array(
+            "echo mysql-server mysql-server/root_password password $newRootPass | sudo debconf-set-selections",
+            "echo mysql-server mysql-server/root_password_again password $newRootPass | sudo debconf-set-selections",
+            "apt-get install -y mysql-client mysql-server"
+        );
+        $this->uninstallCommands = array( "apt-get remove -y mysql-client mysql-server" );
+        $this->programDataFolder = "/opt/MysqlServer"; // command and app dir name
+        $this->programNameMachine = "mysqlserver"; // command and app dir name
+        $this->programNameFriendly = "MySQL Server!"; // 12 chars
+        $this->programNameInstaller = "MySQL Server";
+        $this->initialize();
+    }
+
+    private function getNewRootPass() {
+        if (isset($this->params["mysql-root-pass"])) {
+            $newRootPass = $this->params["mysql-root-pass"] ; }
+        else if (AppConfig::getProjectVariable("mysql_default_root_pass") != "") {
+            $newRootPass = AppConfig::getProjectVariable("mysql_default_root_pass") ; }
+        else {
+            $newRootPass = "cleopatra" ; }
+        return $newRootPass;
+    }
 
 }
