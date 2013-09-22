1c38c9765c4f942da86182870fb76257e20aa962
Golden <golden@golden-lappy.(none)>
2013-09-22 19:40:16 +0100

    fixed some uninstalls

--- src/Modules/RubySystem/Model/RubyRVM.php
+++ src/Modules/RubySystem/Model/RubyRVM.php
@@ -9,7 +9,8 @@ class RubySystem extends BaseLinuxApp {
     $this->autopilotDefiner = "RubySystem";
     $this->installCommands = array(
       "apt-get install -y ruby" );
-    $this->uninstallCommands = array( "" );
+    $this->uninstallCommands = array(
+      "apt-get remove -y ruby" );
     $this->programDataFolder = "/opt/rubysystem";
     $this->programNameMachine = "ruby"; // command and app dir name
     $this->programNameFriendly = "Ruby System!"; // 12 chars
