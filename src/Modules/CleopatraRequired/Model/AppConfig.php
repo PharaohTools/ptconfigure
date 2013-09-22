417b43b103593b2b5ff05dd61a4eec42cb48b894
Golden <golden@golden-lappy.(none)>
2013-09-22 19:21:32 +0100

    models correctly report their own status from cleo config

--- src/Modules/CleopatraRequired/Model/AppConfig.php
+++ src/Modules/CleopatraRequired/Model/AppConfig.php
@@ -6,18 +6,43 @@ class AppConfig {
 
 
     private static function checkSettingsExistOrCreateIt() {
-        if (file_exists(dirname(__FILE__).'../../../cleovars')) {
-            return true; }
-        else {
-            touch(dirname(__FILE__).'../../../cleovars');
-            return true; }
+        if (!file_exists(dirname(__FILE__).DIRECTORY_SEPARATOR.'../../../cleovars')) {
+            touch(dirname(__FILE__).DIRECTORY_SEPARATOR.'../../../cleovars'); }
+        return true;
     }
 
-    public static function setProjectVariable($variable, $value, $listAdd=null) {
+    public static function setProjectVariable($variable, $value, $listAdd=null, $listAddKey=null) {
         if (self::checkSettingsExistOrCreateIt()) {
             $appConfigArray = self::loadDHProjectFile();
-            if ( $listAdd == true ) { $appConfigArray[$variable][] = $value ; }
+            if ( $listAdd == true && $listAddKey==null ) {
+                if (!in_array($value, $appConfigArray[$variable])) {
+                    $appConfigArray[$variable][] = $value ; } }
+            else if ( $listAdd == true && $listAddKey!=null ) {
+                $appConfigArray[$variable][$listAddKey] = $value ; }
             else { $appConfigArray[$variable] = $value ; }
+            self::saveDHProjectFile( $appConfigArray ) ; }
+    }
+
+    /*
+     *  to delete a value from an array with keys call deleteProjectVariable($variable, $key)
+     *  to delete a value from an array without keys call deleteProjectVariable($variable, "any", $value)
+     *  to delete a plain variable call deleteProjectVariable($variable)
+     *
+     */
+    public static function deleteProjectVariable($variable, $key=null, $value=null) {
+        if (self::checkSettingsExistOrCreateIt()) {
+            $appConfigArray = self::loadDHProjectFile();
+            if ( isset($key) ) {
+                // if variable is array without keys, delete entry by value
+                if ($key=="any" && isset($value)) {
+                    for ($i = 0; $i<count($appConfigArray[$variable]); $i++) {
+                        if ($appConfigArray[$variable][$i] == $value) {
+                            unset($appConfigArray[$variable][$i]) ; } } }
+                // if variable is array with keys, delete entry by key
+                else if (isset($appConfigArray[$variable][$key]) && !isset($value)) {
+                    unset($appConfigArray[$variable][$key]) ; } }
+            else {
+                unset($appConfigArray[$variable]) ; }
             self::saveDHProjectFile( $appConfigArray ) ; }
     }
 
@@ -25,21 +50,20 @@ class AppConfig {
         $value = null;
         if (self::checkSettingsExistOrCreateIt()) {
             $appConfigArray = self::loadDHProjectFile();
-            $value = (isset($appConfigArray[$variable])) ? $appConfigArray[$variable] : null ; }
+            $value = (isset($appConfigArray[$variable])) ? $appConfigArray[$variable] : array() ; }
         return $value;
     }
 
     private static function loadDHProjectFile() {
-        $appConfigArraySerialized = file_get_contents(dirname(__FILE__).'../../../cleovars');
+        $appConfigArraySerialized = file_get_contents(dirname(__FILE__).DIRECTORY_SEPARATOR.'../../../cleovars');
         $decoded = unserialize($appConfigArraySerialized);
-        $appConfigArray = (is_object($decoded)) ? $decoded : array() ;
-        return new \ArrayObject($appConfigArray);
+        return $decoded ;
     }
 
     private static function saveDHProjectFile($appConfigArray) {
-        $appConfigObject = new \ArrayObject($appConfigArray);
-        $appConfigObjectSerialized = serialize($appConfigObject);
-        file_put_contents(dirname(__FILE__).'../../../cleovars', $appConfigObjectSerialized);
+        $appConfigObjectSerialized = serialize($appConfigArray);
+        file_put_contents(dirname(__FILE__).DIRECTORY_SEPARATOR.'../../../cleovars', $appConfigObjectSerialized);
+        chmod(dirname(__FILE__).DIRECTORY_SEPARATOR.'../../../cleovars', 0777);
     }
 
 }
