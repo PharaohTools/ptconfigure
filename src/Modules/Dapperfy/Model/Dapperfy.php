f27d9c9d47080e2c9c6330256a20d55a1243638b
David Amanshia <phpengine@hotmail.co.uk>
2013-09-26 17:44:22 +0100

    Other menial dapper mods

--- src/Modules/Dapperfy/Model/Dapperfy.php
+++ src/Modules/Dapperfy/Model/Dapperfy.php
@@ -5,14 +5,11 @@ Namespace Model;
 class Dapperfy extends Base {
 
     private $environments ;
-    private $replacements ;
-    private $environmentSpecificReplacements ;
-    private $crossEnvironmentReplacements ;
+    private $environmentReplacements ;
 
     public function askWhetherToDapperfy() {
         if ($this->askToScreenWhetherToDapperfy() != true) { return false; }
-        $this->setCrossEnvironmentReplacements() ;
-        $this->setEnvironmentSpecificReplacements() ;
+        $this->setEnvironmentReplacements() ;
         $this->getEnvironments() ;
         $this->doDapperfy() ;
         return true;
@@ -23,86 +20,31 @@ class Dapperfy extends Base {
       return self::askYesOrNo($question, true);
     }
 
-    public function setEnvironmentSpecificReplacements() {
-      $this->environmentSpecificReplacements = array(
-        "****projectContainerDirectory****",
-        "****gitCheckoutProjectOriginRepo****",
-        "****gitCheckoutProjectCustomBranch****",
-        "****virtualHostEditorAdditionURL****",
-        "****virtualHostEditorAdditionIp****",
-        "****dbIp****",
-        "****dbAppUserName****",
-        "****dbAppUserPass****",
-        "****dbName****",
-        "****dbRootUserName****",
-        "****dbRootUserPass****",
-      );
-    }
+    public function setEnvironmentReplacements() {
 
-    public function setCrossEnvironmentReplacements() {
-      $this->crossEnvironmentReplacements = array(
-        "****dbPlatform****",
-      );
+      $this->environmentReplacements =
+          array( "dapper" => array(
+              array("var"=>"dap_proj_cont_dir", "friendly_text"=>"Project Container directory, (inc slash)"),
+              array("var"=>"dap_git_repo_url", "friendly_text"=>"Git Repo URL"),
+              array("var"=>"dap_git_custom_branch", "friendly_text"=>"Git Custom Branch"),
+              array("var"=>"dap_apache_vhost_url", "friendly_text"=>"Apache VHost URL (Don't Include http://)"),
+              array("var"=>"dap_apache_vhost_ip", "friendly_text"=>"Apache VHost Hostname/IP"),
+              array("var"=>"dap_version_num_revisions", "friendly_text"=>"How many revisions to keep"),
+              array("var"=>"dap_db_platform", "friendly_text"=>"DB Platform"),
+              array("var"=>"dap_db_ip_address", "friendly_text"=>"DB IP Address"),
+              array("var"=>"dap_db_app_user_name", "friendly_text"=>"DB App User Name (Will be created if not existing)"),
+              array("var"=>"dap_db_app_user_pass", "friendly_text"=>"DB App User Pass"),
+              array("var"=>"dap_db_name", "friendly_text"=>"DB Name (Will be created if not existing)"),
+              array("var"=>"dap_db_admin_user_name", "friendly_text"=>"DB Admin User Name"),
+              array("var"=>"dap_db_admin_user_pass", "friendly_text"=>"DB Admin User Pass"),
+          ) );
+
     }
 
     public function getEnvironments() {
-      $i = 0 ;
-      $crossEnvironmentValues = array();
-      echo "These questions apply to all environments: \n";
-      foreach ($this->crossEnvironmentReplacements as $replacementQuestion) {
-        $crossEnvironmentValues[$replacementQuestion] = self::askForInput("Value for: ".$replacementQuestion); }
-
-      $allProjectEnvs = \Model\AppConfig::getProjectVariable("environments");
-      if (count($allProjectEnvs) > 0) {
-        $question = 'Dapperfy This?';
-        $useProjEnvs = self::askYesOrNo($question, true);
-        if ($useProjEnvs == true ) {
-          $this->environments = $allProjectEnvs;
-          return; } }
-
-      $more_servers = true;
-      while ($more_servers == true) {
-        if (count($this->environments)==0) {
-          $this->populateAnEnvironment($i, $crossEnvironmentValues);}
-        else {
-          $question = 'Do you want to add another environment?';
-          $add_another_server = self::askYesOrNo($question);
-          if ($add_another_server == true) {
-            $this->populateAnEnvironment($i, $crossEnvironmentValues); }
-          else {
-            $more_servers = false; } }
-        $i++; }
-
-    }
-
-    private function populateAnEnvironment($i, $crossEnvironmentValues) {
-      echo "Environment Number $i: \n";
-      $this->environments[$i]["****CURRENT_ENVIRONMENT_NAME****"] = self::askForInput("Value for: Name", true);
-      $this->environments[$i]["****CURRENT_ENVIRONMENT_TEMP_DIR****"] = self::askForInput("Value for: Temp Dir", true);
-      $this->environments[$i]["****CURRENT_ENVIRONMENT_NUMBER_REVISIONS****"] = self::askForInput("Value for: Number Revisions", true);
-      $this->environments[$i]["****CURRENT_ENVIRONMENT_SERVERS_ARRAY_TEXT****"] = $this->getServerArrayText($this->getServers());
-      foreach ($this->environmentSpecificReplacements as $replacementQuestion) {
-        $this->environments[$i][$replacementQuestion] = self::askForInput("Value for: ".$replacementQuestion); }
-      foreach ($this->crossEnvironmentReplacements as $replacementQuestion) {
-        $this->environments[$i][$replacementQuestion] = $crossEnvironmentValues[$replacementQuestion]; }
-    }
-
-    public function getServers() {
-      $servers = array();
-      $serverAttributes = array("target", "user", "password");
-      $keepGoing = true ;
-      $question = 'Enter Servers - this is an array of entries';
-      while ($keepGoing == true) {
-        $tinierArray = array();
-        echo $question."\n";
-        foreach ($serverAttributes as $questionTarget) {
-          $miniQuestion = 'Enter '.$questionTarget.' ?';
-          $tinierArray[$questionTarget] = self::askForInput($miniQuestion, true); }
-        $servers[] = $tinierArray;
-        $keepGoingQuestion = 'Add Another Server? (Y/N)';
-        $keepGoingResult = self::askForInput($keepGoingQuestion, true);
-        $keepGoing = ($keepGoingResult == "Y" || $keepGoingResult == "y") ? true : false ; }
-      return $servers;
+        $environmentConfigModel = new EnvironmentConfig();
+        $environmentConfigModel->askWhetherToEnvironmentConfig($this->environmentReplacements) ;
+        $this->environments = $environmentConfigModel->environments ;
     }
 
     public function getServerArrayText($serversArray) {
@@ -119,31 +61,12 @@ class Dapperfy extends Base {
     private function doDapperfy() {
       $templatesDir = str_replace("Model", "Templates", dirname(__FILE__) ) ;
       $templates = scandir($templatesDir);
-      $this->writeEnvsToProjectFile();
       foreach ($this->environments as $environment) {
         foreach ($templates as $template) {
           if (!in_array($template, array(".", ".."))) {
             $fileData = $this->loadFile($templatesDir.DIRECTORY_SEPARATOR.$template);
             $fileData = $this->dataChange($fileData, $environment);
-            $this->saveFile($template, $environment["****CURRENT_ENVIRONMENT_NAME****"], $fileData); } } }
-    }
-
-    private function checkIsDHProject() {
-      return file_exists('dhproj');
-    }
-
-    private function writeEnvsToProjectFile(){
-      if ($this->checkIsDHProject()){
-        \Model\AppConfig::setProjectVariable("environments", $this->environments); }
-    }
-
-    private function deleteEnvsFromProjectFile(){
-      if ($this->checkIsDHProject()){
-        $allProjectVHosts = \Model\AppConfig::getProjectVariable("environments");
-        for ($i = 0; $i<=count($allProjectVHosts) ; $i++ ) {
-          if (isset($allProjectVHosts[$i]) && in_array($allProjectVHosts[$i], $this->vHostForDeletion)) {
-            unset($allProjectVHosts[$i]); } }
-        \Model\AppConfig::setProjectVariable("environments", $allProjectVHosts); }
+            $this->saveFile($template, $environment["any-app"]["gen_env_name"], $fileData); } } }
     }
 
     private function loadFile($fileToLoad) {
@@ -161,8 +84,13 @@ class Dapperfy extends Base {
     }
 
     private function dataChange($fileData, $environment){
-      $newFileData = strtr($fileData, $environment);
-      return $newFileData ;
+        foreach ($environment as $var_group_key => $var_group_val) {
+            foreach ($var_group_val as $var_key => $var_val) {
+                if (is_array($var_val) && $var_group_key=="servers") {
+                    $fileData = str_replace('****gen_srv_array_text****', $this->getServerArrayText($var_group_val), $fileData); }
+                else {
+                    $fileData = str_replace('****'.$var_key.'****', $var_val, $fileData);  } } }
+        return $fileData ;
     }
 
 }
