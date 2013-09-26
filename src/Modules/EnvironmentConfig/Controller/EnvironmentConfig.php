c4016aee5eec6a95b716899b5cd193903682b0ff
David Amanshia <phpengine@hotmail.co.uk>
2013-09-26 17:43:12 +0100

    Added Environment config module

---
+++ src/Modules/EnvironmentConfig/Controller/EnvironmentConfig.php
@@ -0,0 +1,23 @@
+<?php
+
+Namespace Controller ;
+
+class EnvironmentConfig extends Base {
+
+    public function execute($pageVars) {
+        $isHelp = parent::checkForHelp($pageVars) ;
+        if ( is_array($isHelp) ) {
+          return $isHelp; }
+        $action = $pageVars["route"]["action"];
+
+        $environmentConfigModel = new \Model\EnvironmentConfig();
+
+        if ($action=="configure" || $action=="config") {
+          $this->content["result"] = $environmentConfigModel->askWhetherToEnvironmentConfig();
+          return array ("type"=>"view", "view"=>"environmentConfig", "pageVars"=>$this->content); }
+
+        $this->content["messages"][] = "Invalid Project Action";
+        return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content);
+    }
+
+}
