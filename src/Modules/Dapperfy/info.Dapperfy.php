d092fa9f0ca90c26a4d44bbe2c1ac72ceec49729
Golden <golden@golden-lappy.(none)>
2013-09-26 17:44:22 +0100

    Other menial dapper mods

--- src/Modules/Dapperfy/info.Dapperfy.php
+++ src/Modules/Dapperfy/info.Dapperfy.php
@@ -39,25 +39,6 @@ class DapperfyInfo extends Base {
 
         The start of the command will be dapperstrano autopilot execute :
 
-        cap deploy               # Deploys your project.
-        cap deploy:check         # Test deployment dependencies.
-        cap deploy:cleanup       # Clean up old releases.
-        cap deploy:cold          # Deploys and starts a 'cold' application.
-        cap deploy:migrations    # Deploy and run pending migrations.
-        cap deploy:pending       # Displays the commits since your last deploy.
-        cap deploy:pending:diff  # Displays the `diff' since your last deploy.
-        cap deploy:rollback      # Rolls back to a previous version and restarts.
-        cap deploy:rollback:code # Rolls back to the previously deployed version.
-        cap deploy:start         # Blank task exists as a hook into which to install ...
-        cap deploy:stop          # Blank task exists as a hook into which to install ...
-        cap deploy:symlink       # Updates the symlink to the most recently deployed ...
-        cap deploy:update        # Copies your project and updates the symlink.
-        cap deploy:update_code   # Copies your project to the remote servers.
-        cap deploy:upload        # Copy files to the currently deployed version.
-        cap deploy:web:disable   # Present a maintenance page to visitors.
-        cap deploy:web:enable    # Makes the application web-accessible again.
-        cap invoke               # Invoke a single command on the remote servers.
-        cap shell                # Begin an interactive Capistrano session.
 
 HELPDATA;
       return $help ;
