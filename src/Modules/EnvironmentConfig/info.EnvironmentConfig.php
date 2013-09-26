3d8a9d02f0e4ac38c2fa3c81ba311359e4ab35d5
David Amanshia <phpengine@hotmail.co.uk>
2013-09-26 17:37:25 +0100

    Added Environment conifguratino module

---
+++ src/Modules/EnvironmentConfig/info.EnvironmentConfig.php
@@ -0,0 +1,43 @@
+<?php
+
+Namespace Info;
+
+class EnvironmentConfigInfo extends Base {
+
+    public $hidden = false;
+
+    public $name = "Environment Configuration - Configure Environments for a project";
+
+    public function __construct() {
+      parent::__construct();
+    }
+
+    public function routesAvailable() {
+      return array( "EnvironmentConfig" =>  array_merge(parent::routesAvailable(), array("configure", "config") ) );
+    }
+
+    public function routeAliases() {
+      return array("environmentconfig"=>"EnvironmentConfig", "environment-config"=>"EnvironmentConfig",
+        "envconfig"=>"EnvironmentConfig", "env-config"=>"EnvironmentConfig");
+    }
+
+    public function helpDefinition() {
+      $help = <<<"HELPDATA"
+  This command is part of a default Module and provides you with a method by which you can
+  configure environments for your project from the command line. Currently compliant with
+  both Dapperstrano and Cleopatra.
+
+
+  EnvironmentConfig, environmentconfig, environment-config, envconfig, env-config
+
+        - configure
+        Configure the environments for your project to use
+        example: dapperstrano envconfig configure
+        example: cleopatra envconfig configure
+
+
+HELPDATA;
+      return $help ;
+    }
+
+}
