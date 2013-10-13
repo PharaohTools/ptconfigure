d0b9a18ee798bcdc3339294c1cf24d1374d40fb9
David Amanshia <phpengine@hotmail.co.uk>
2013-10-13 13:31:59 +0100

    system detection debugging multiple os

--- src/Modules/ApacheModules/Controller/ApacheModules.php
+++ src/Modules/ApacheModules/Controller/ApacheModules.php
@@ -7,6 +7,7 @@ class ApacheModules extends Base {
     public function execute($pageVars) {
 
         $thisModel = $this->getModelAndCheckDependencies("ApacheModules", $pageVars) ;
+        var_dump($thisModel) ;
         if (!is_object($thisModel)) {
             $this->content["messages"][] = $thisModel ;
             return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content); }
