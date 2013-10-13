eee8c90f2f24a75422f6f44b0054d6dd928ae233
Golden <golden@golden-lappy.(none)>
2013-10-13 14:00:20 +0100

    fixed markdown and started compatible modules function for index

--- src/Modules/Index/Model/Index.php
+++ src/Modules/Index/Model/Index.php
@@ -4,17 +4,30 @@ Namespace Model;
 
 class Index extends Base {
 
-  public function findAllModuleNames(){
-    $allInfoObjects = \Core\AutoLoader::getInfoObjects() ;
-    $moduleNames = array();
-    foreach ($allInfoObjects as $infoObject) {
-        $array_keys = array_keys($infoObject->routesAvailable()) ;
-        $miniRay = array();
-        $miniRay["command"] = $array_keys[0];
-        $miniRay["name"] = $infoObject->name ;
-        $miniRay["hidden"] = $infoObject->hidden ;
-        $moduleNames[] = $miniRay; }
-    return $moduleNames;
-  }
+    public function findAllModuleNames(){
+        $allInfoObjects = \Core\AutoLoader::getInfoObjects() ;
+        $moduleNames = array();
+        foreach ($allInfoObjects as $infoObject) {
+            $array_keys = array_keys($infoObject->routesAvailable()) ;
+            $miniRay = array();
+            $miniRay["command"] = $array_keys[0];
+            $miniRay["name"] = $infoObject->name ;
+            $miniRay["hidden"] = $infoObject->hidden ;
+            $moduleNames[] = $miniRay; }
+        return $moduleNames;
+    }
+
+    public function findAllCompatibleModuleNames(){
+        $allInfoObjects = \Core\AutoLoader::getInfoObjects() ;
+        $moduleNames = array();
+        foreach ($allInfoObjects as $infoObject) {
+            $array_keys = array_keys($infoObject->routesAvailable()) ;
+            $miniRay = array();
+            $miniRay["command"] = $array_keys[0];
+            $miniRay["name"] = $infoObject->name ;
+            $miniRay["hidden"] = $infoObject->hidden ;
+            $moduleNames[] = $miniRay; }
+        return $moduleNames;
+    }
 
 }
