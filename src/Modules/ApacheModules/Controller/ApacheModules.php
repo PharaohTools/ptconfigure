53a129f08b4215ee4c54b5e761a940ca22e6509f
Golden <golden@golden-lappy.(none)>
2013-10-13 13:17:14 +0100

    system detection debugging multiple os

--- src/Modules/ApacheModules/Controller/ApacheModules.php
+++ src/Modules/ApacheModules/Controller/ApacheModules.php
@@ -7,6 +7,10 @@ class ApacheModules extends Base {
     public function execute($pageVars) {
 
         $thisModel = $this->getModelAndCheckDependencies("ApacheModules", $pageVars) ;
+        if (!is_object($thisModel)) {
+            $this->content["messages"][] = array_merge($thisModel, $this->content["messages"] ) ;
+            return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content); }
+
         $isDefaultAction = parent::checkDefaultActions($pageVars, array(), $thisModel) ;
         if ( is_array($isDefaultAction) ) { return $isDefaultAction; }
 
