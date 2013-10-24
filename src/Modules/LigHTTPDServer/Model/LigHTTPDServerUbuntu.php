3b3ffbcc24e3d645f919a2e404721ccf3f9bddb0
Golden <golden@golden-lappy.(none)>
2013-10-24 22:11:37 +0100

    lighttpd wworking for ubuntu

--- src/Modules/LigHTTPDServer/Model/LigHTTPDServerUbuntu.php
+++ src/Modules/LigHTTPDServer/Model/LigHTTPDServerUbuntu.php
@@ -17,8 +17,8 @@ class LigHTTPDServerUbuntu extends BaseLinuxApp {
   public function __construct($params) {
     parent::__construct($params);
     $this->autopilotDefiner = "LigHTTPDServer";
-    $this->installCommands = array("apt-get install -y lighthttpd");
-    $this->uninstallCommands = array("apt-get remove -y lighthttpd");
+    $this->installCommands = array("apt-get install -y lighttpd");
+    $this->uninstallCommands = array("apt-get remove -y lighttpd");
     $this->programDataFolder = "/opt/LigHTTPDServer"; // command and app dir name
     $this->programNameMachine = "lighttpdserver"; // command and app dir name
     $this->programNameFriendly = "LigHTTPD Server!"; // 12 chars
