6f9c39358ae3ac0b845f118cc650fbbff8e0e737
David Amanshia <phpengine@hotmail.co.uk>
2013-10-28 20:13:49 +0000

    php configuration

---
+++ src/Modules/PHPConf/info.PHPConf.php
@@ -0,0 +1,51 @@
+<?php
+
+Namespace Info;
+
+class PHPConfInfo extends Base {
+
+    public $hidden = false;
+
+    public $name = "PHP Conf - Install a PHP Configuration";
+
+    public function __construct() {
+      parent::__construct();
+    }
+
+    public function routesAvailable() {
+      return array( "PHPConf" =>  array_merge(parent::routesAvailable(), array() ) );
+    }
+
+    public function routeAliases() {
+        return array("php-configuration"=>"PHPConf", "php-configure"=>"PHPConf", "php-conf"=>"PHPConf",
+            "phpconf"=>"PHPConf");
+    }
+
+    public function autoPilotVariables() {
+      return array(
+        "PHPConf" => array(
+          "PHPConf" => array(
+            "programDataFolder" => "/etc/phpconf/", // command and app dir name
+            "programNameMachine" => "phpconf", // command and app dir name
+            "programNameFriendly" => "PHP Conf.", // 12 chars
+            "programNameInstaller" => "PHP Conf",
+          )
+        )
+      );
+    }
+
+    public function helpDefinition() {
+      $help = <<<"HELPDATA"
+  This command is part of Core and provides you  with a method by which you can install Apache HTTP Server
+
+  PHPConf, php-configure, php-configuration, php-conf, phpconf
+
+        - install
+        Installs a configuration for PHP
+        example: cleopatra phpconf install
+
+HELPDATA;
+      return $help ;
+    }
+
+}
