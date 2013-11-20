15c035edcf2b8ac8e35715ebfeaddcd7d496df78
Golden Dave <dave@theboss.com>
2013-11-20 12:15:49 +0000

    intro modified

--- src/Modules/Index/Views/IndexView.tpl.php
+++ src/Modules/Index/Views/IndexView.tpl.php
@@ -7,13 +7,9 @@ This tool helps with setting up boxes. Its intended to get any box in your stand
 up and running quickly. It's not meant to be an exhaustive list of installs for everything you'll ever need to
 install (obviously)
 
-Can be used to set up a Development Client, Development Server, Testing Server, or Production Server in minutes
+Can be used to set up a Development Client, Development Server, Testing Server, or Production Server in minutes.
 
-... Staging/UAT is not missing from the list because in "Software my box has installed" terms it should be the
-same as Production.
-
-Furthermore, especially for Production, this is intended to be a quick solution to get you up and running and I
-do not and would never recommend going into Production without checking things for yourself.
+Zero configuration across multiple Operating Systems.
 
 -------------------------------------------------------------
 
