ed4e42f2a3aadc550e28a78f5887e1bbc422a770
Golden Dave <dave@theboss.com>
2013-11-20 14:19:48 +0000

    intro text mods

--- src/Modules/Index/Controller/Index.php
+++ src/Modules/Index/Controller/Index.php
@@ -7,6 +7,7 @@ class Index extends Base {
     public function execute($pageVars) {
       $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
       // if we don't have an object, its an array of errors
+      $this->content = $pageVars ;
       if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
       $this->content["modulesInfo"] = $thisModel->findModuleNames($pageVars["route"]["extraParams"]);
       return array ("type"=>"view", "view"=>"index", "pageVars"=>$this->content);
