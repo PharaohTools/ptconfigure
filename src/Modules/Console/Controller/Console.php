482b3449c7a60acef7f4a1fbe8d090b5c630ae34
Golden Dave <dave@theboss.com>
2014-02-17 19:08:18 +0000

    Console module works nicely

---
+++ src/Modules/Console/Controller/Console.php
@@ -0,0 +1,26 @@
+<?php
+
+Namespace Controller ;
+
+class Console extends Base {
+
+    public function execute($pageVars) {
+
+        $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
+        // if we don't have an object, its an array of errors
+        if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+
+        $action = $pageVars["route"]["action"];
+
+        if ($action=="help") {
+            $helpModel = new \Model\Help();
+            $this->content["helpData"] = $helpModel->getHelpData($pageVars["route"]["control"]);
+            return array ("type"=>"view", "view"=>"help", "pageVars"=>$this->content); }
+
+        if ($action=="log") {
+            $this->content["genCreateResult"] = $thisModel->askInstall();
+            return array ("type"=>"view", "view"=>"AppInstall", "pageVars"=>$this->content); }
+
+    }
+
+}
