ccd9ada4d11e77eeb1b9f5628cc2412b814d6411
David Amanshia <phpengine@hotmail.co.uk>
2014-02-20 00:58:50 +0000

    Consolu module updated modulr for any platform at al

--- src/Modules/Console/Model/ConsoleAll.php
+++ src/Modules/Console/Model/ConsoleAll.php
@@ -2,13 +2,13 @@
 
 Namespace Model;
 
-class ConsoleUbuntu extends BaseLinuxApp {
+class ConsoleAll extends BaseLinuxApp {
 
     // Compatibility
-    public $os = array("Linux") ;
-    public $linuxType = array("Debian") ;
-    public $distros = array("Ubuntu") ;
-    public $versions = array("12.04", "12.10") ;
+    public $os = array("any") ;
+    public $linuxType = array("any") ;
+    public $distros = array("any") ;
+    public $versions = array("any") ;
     public $architectures = array("any") ;
 
     // Model Group
