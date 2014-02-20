38b18fe4db92a5b197dc47e6c1ad13d17296f9ef
David Amanshia <phpengine@hotmail.co.uk>
2014-02-20 00:18:39 +0000

    Firewall module working

---
+++ src/Modules/Firewall/Controller/Firewall.php
@@ -0,0 +1,48 @@
+<?php
+
+Namespace Controller ;
+
+class Firewall extends Base {
+
+    public function execute($pageVars) {
+
+        $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
+        // if we don't have an object, its an array of errors
+        if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+
+        $action = $pageVars["route"]["action"];
+
+        if ($action=="help") {
+            $helpModel = new \Model\Help();
+            $this->content["helpData"] = $helpModel->getHelpData($pageVars["route"]["control"]);
+            return array ("type"=>"view", "view"=>"help", "pageVars"=>$this->content); }
+
+        if (in_array($action, array("enable", "disable", "allow", "deny", "reject", "limit", "delete", "insert", "reset") )) {
+            $this->content["result"] = $thisModel->askAction($action);
+            $this->content["appName"] = $thisModel->programNameInstaller ;
+            return array ("type"=>"view", "view"=>"firewall", "pageVars"=>$this->content); }
+
+        /*
+         *
+ enable                          enables the firewall
+ disable                         disables the firewall
+ allow ARGS                      add allow rule
+ deny ARGS                       add deny rule
+ reject ARGS                     add reject rule
+ limit ARGS                      add limit rule
+ delete RULE|NUM                 delete RULE
+ insert NUM RULE                 insert RULE at NUM
+ reset                           reset firewall
+
+ default ARG                     set default policy
+ logging LEVEL                   set logging to LEVEL
+ status                          show firewall status
+ status numbered                 show firewall status as numbered list of RULES
+ status verbose                  show verbose firewall status
+ show ARG                        show firewall report
+ version                         display version information
+         *
+         */
+    }
+
+}
---
+++ src/Modules/Firewall/info.Firewall.php
@@ -0,0 +1,86 @@
+<?php
+
+Namespace Info;
+
+class FirewallInfo extends Base {
+
+    public $hidden = false;
+
+    public $name = "Add, Remove or Modify Firewalls";
+
+    public function __construct() {
+      parent::__construct();
+    }
+
+    public function routesAvailable() {
+        // return array( "Firewall" =>  array_merge(parent::routesAvailable(), array() ) );
+        return array( "Firewall" =>  array_merge(
+            array("help", "status", "enable", "disable", "allow", "deny", "reject", "limit", "delete", "insert", "reset")
+        ) );
+    }
+
+    public function routeAliases() {
+      return array("firewall"=>"Firewall");
+    }
+
+    public function autoPilotVariables() {
+      return array(
+        "Firewall" => array(
+          "Firewall" => array(
+            "programDataFolder" => "", // command and app dir name
+            "programNameMachine" => "firewall", // command and app dir name
+            "programNameFriendly" => "    Firewall    ", // 12 chars
+            "programNameInstaller" => "Firewall",
+          )
+        )
+      );
+    }
+
+    public function helpDefinition() {
+      $help = <<<"HELPDATA"
+  This command allows you to modify create or modify firewalls
+
+  Firewall, firewall
+         disable allow deny reject", "limit", "delete", "insert", "reset"
+        - enable
+        Enable system firewall
+        example: cleopatra firewall enable
+
+        - disable
+        Disable system firewall
+        example: cleopatra firewall disable
+
+        - allow
+        Allow a Firewall rule
+        example: cleopatra firewall allow --firewall-rule="ssh/tcp"
+
+        - deny
+        Deny a Firewall rule
+        example: cleopatra firewall deny --firewall-rule="ssh/tcp"
+
+        - reject
+        Reject a Firewall rule
+        example: cleopatra firewall reject --firewall-rule="ssh/tcp"
+
+        - limit
+        Limit a Firewall rule. ufw will deny connections if an IP address has attempted
+        to initiate 6 or more connections in the last 30 seconds.
+        example: cleopatra firewall limit --firewall-rule="ssh/tcp"
+
+        - delete
+        Delete a Firewall rule.
+        example: cleopatra firewall delete --firewall-rule="ssh/tcp"
+
+        - insert
+        Insert a Firewall rule.
+        example: cleopatra firewall insert --firewall-rule="ssh/tcp"
+
+        - reset
+        Reset a Firewall rule.
+        example: cleopatra firewall reset --firewall-rule="ssh/tcp"
+
+HELPDATA;
+      return $help ;
+    }
+
+}
