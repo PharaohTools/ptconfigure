05020adb35cd98d7962f4177ad9b08e8c828b00f
Golden Dave <dave@theboss.com>
2014-03-12 16:59:15 +0000

    python centos 5.8 model and ubuntu modl

--- src/Modules/Python/Model/PythonUbuntu.php
+++ src/Modules/Python/Model/PythonUbuntu.php
@@ -14,16 +14,16 @@ class PythonUbuntu extends BaseLinuxApp {
     // Model Group
     public $modelGroup = array("Default") ;
 
-  public function __construct($params) {
-    parent::__construct($params);
-    $this->autopilotDefiner = "Python";
-    $this->installCommands = array("apt-get install -y python python-docutils");
-    $this->uninstallCommands = array("apt-get remove -y python python-docutils");
-    $this->programDataFolder = "";
-    $this->programNameMachine = "python"; // command and app dir name
-    $this->programNameFriendly = "!Python!!"; // 12 chars
-    $this->programNameInstaller = "Python";
-    $this->initialize();
-  }
+    public function __construct($params) {
+        parent::__construct($params);
+        $this->autopilotDefiner = "Python";
+        $this->installCommands = array("apt-get install -y python python-docutils");
+        $this->uninstallCommands = array("apt-get remove -y python python-docutils");
+        $this->programDataFolder = "";
+        $this->programNameMachine = "python"; // command and app dir name
+        $this->programNameFriendly = "!Python!!"; // 12 chars
+        $this->programNameInstaller = "Python";
+        $this->initialize();
+    }
 
 }
