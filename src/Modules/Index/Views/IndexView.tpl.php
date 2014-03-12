09b920f8c629f23c5cef191f5cd0bca6ba1d65b8
David Amanshia <phpengine@hotmail.co.uk>
2014-03-12 13:46:55 +0000

    updated index filer text

--- src/Modules/Index/Views/IndexView.tpl.php
+++ src/Modules/Index/Views/IndexView.tpl.php
@@ -1,15 +1,15 @@
 Cleopatra by Golden Contact Computing
 -------------------
 
-About:
------------------
-This tool helps with setting up boxes. Its intended to get any box in your standard main environments to be
-up and running quickly. It's not meant to be an exhaustive list of installs for everything you'll ever need to
-install (obviously)
+The ultimate Configuration Management and Systems Automation package in PHP.
 
-Can be used to set up a Development Client, Development Server, Testing Server, or Production Server in minutes.
+Can be used to set up a Development Client, Development Server, Testing Servers, SCM Servers
+or Production Application Servers in minutes, out of the box, with Zero configuration across multiple Operating Systems.
 
-Zero configuration across multiple Operating Systems.
+You can also create far more complex or niche systems completely configured by code across platforms.
+
+Using Convention over Configuration, a lot of common Configuration Management tasks can be completed with little or
+no extra implementation work.
 
 -------------------------------------------------------------
 
@@ -17,6 +17,7 @@ Available Commands:
 ---------------------------------------
 
 <?php
+
 foreach ($pageVars["modulesInfo"] as $moduleInfo) {
   if ($moduleInfo["hidden"] != true) {
     echo $moduleInfo["command"].' - '.$moduleInfo["name"]."\n";
