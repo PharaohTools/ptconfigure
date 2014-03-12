2c6bede6bc57790c7c3bfc085f83c42fda28f5b7
Golden Dave <dave@theboss.com>
2014-03-12 13:46:15 +0000

    Cleofy using templating module

--- src/Modules/Cleofy/Model/CleofyUbuntu.php
+++ src/Modules/Cleofy/Model/CleofyUbuntu.php
@@ -30,8 +30,9 @@ class CleofyUbuntu extends Base {
     }
 
     public function askToScreenWhetherToCleofy() {
-      $question = 'Cleofy This?';
-      return self::askYesOrNo($question, true);
+        if (isset($this->params["yes"]) && $this->params["yes"]==true) { return true ; }
+        $question = 'Cleofy This?';
+        return self::askYesOrNo($question, true);
     }
 
     public function setEnvironmentReplacements() {
@@ -65,33 +66,15 @@ class CleofyUbuntu extends Base {
       foreach ($this->environments as $environment) {
         foreach ($templates as $template) {
           if (!in_array($template, array(".", ".."))) {
-            $fileData = $this->loadFile($templatesDir.DIRECTORY_SEPARATOR.$template);
-            $fileData = $this->dataChange($fileData, $environment);
-            $this->saveFile($template, $environment["any-app"]["gen_env_name"], $fileData); } } }
-    }
-
-    private function loadFile($fileToLoad) {
-      $command = 'cat '.$fileToLoad;
-      $fileData = self::executeAndLoad($command);
-      return $fileData ;
-    }
-
-    private function saveFile($fileName, $environmentName, $fileData) {
-      $newFileName = str_replace("environment", $environmentName, $fileName ) ;
-      $autosDir = getcwd().DIRECTORY_SEPARATOR.'build'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'cleopatra'.DIRECTORY_SEPARATOR.'autopilots';
-      if (!file_exists($autosDir)) { mkdir ($autosDir, 0777, true); }
-      if (!file_exists(getcwd().DIRECTORY_SEPARATOR.'src')) { mkdir (getcwd().DIRECTORY_SEPARATOR.'src', 0777, true); }
-      return file_put_contents($autosDir.DIRECTORY_SEPARATOR.$newFileName, $fileData);
-    }
-
-    private function dataChange($fileData, $environment){
-        foreach ($environment as $var_group_key => $var_group_val) {
-            foreach ($var_group_val as $var_key => $var_val) {
-                if (is_array($var_val) && $var_group_key=="servers") {
-                    $fileData = str_replace('****gen_srv_array_text****', $this->getServerArrayText($var_group_val), $fileData); }
-                else {
-                    $fileData = str_replace('****'.$var_key.'****', $var_val, $fileData);  } } }
-        return $fileData ;
+              $templatorFactory = new \Model\Templating();
+              $templator = $templatorFactory->getModel($this->params);
+              $newFileName = str_replace("environment", $environment["any-app"]["gen_env_name"], $template ) ;
+              $autosDir = getcwd().DIRECTORY_SEPARATOR.'build'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'cleopatra'.DIRECTORY_SEPARATOR.'autopilots';
+              $targetLocation = $autosDir.DIRECTORY_SEPARATOR.$newFileName ;
+              $templator->template(
+                  file_get_contents($templatesDir.DIRECTORY_SEPARATOR.$template),
+                  array( "gen_srv_array_text" => $this->getServerArrayText($environment["servers"]) ),
+                  $targetLocation ); } } }
     }
 
 }
