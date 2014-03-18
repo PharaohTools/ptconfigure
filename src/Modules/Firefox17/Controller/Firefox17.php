643fa95bd249a22b0b1550e86d8480637350d266
David Amanshia <phpengine@hotmail.co.uk>
2014-03-18 13:01:37 +0000

    updated firefox 17 controller into using model factory

--- src/Modules/Firefox17/Controller/Firefox17.php
+++ src/Modules/Firefox17/Controller/Firefox17.php
@@ -4,14 +4,4 @@ Namespace Controller ;
 
 class Firefox17 extends Base {
 
-    public function execute($pageVars) {
-
-        $thisModel = new \Model\Firefox17($pageVars["route"]["extraParams"]);
-        $isDefaultAction = parent::checkDefaultActions($pageVars, array(), $thisModel) ;
-        if ( is_array($isDefaultAction) ) { return $isDefaultAction; }
-
-        $this->content["messages"][] = "Invalid Action";
-        return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content);
-    }
-
 }
