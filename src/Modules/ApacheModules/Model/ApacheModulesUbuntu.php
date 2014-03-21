8d243aefb113d01c9b3ff1eb1ee218b505a8b972
David Amanshia <phpengine@hotmail.co.uk>
2014-03-21 16:59:55 +0000

    apache modules model using improved install steps, not just bash

--- src/Modules/ApacheModules/Model/ApacheModulesUbuntu.php
+++ src/Modules/ApacheModules/Model/ApacheModulesUbuntu.php
@@ -14,66 +14,70 @@ class ApacheModulesUbuntu extends BaseLinuxApp {
     // Model Group
     public $modelGroup = array("Default") ;
 
-  public function __construct($params) {
-    parent::__construct($params);
-    $this->autopilotDefiner = "ApacheModules";
-    $this->installCommands = array(
-        "apt-get clean", "apt-get update",
-        "apt-get install -y libxml2-dev",
-        "a2enmod rewrite",
-        "a2enmod deflate",
-        "a2enmod ssl",
-        "apt-get install -y libapache2-mod-proxy-html",
-        "a2enmod proxy",
-        "a2enmod proxy_http",
-        "apt-get install -y libapache2-mod-php5",
-        "a2enmod php5",
-        "service apache2 restart" );
-    $this->uninstallCommands = array(
-        "apt-get clean", "apt-get update",
-        "apt-get remove -y libxml2-dev",
-        "a2dismod rewrite",
-        "a2dismod deflate",
-        "a2dismod ssl",
-        "a2dismod proxy",
-        "a2dismod proxy_http",
-        "apt-get remove -y libapache2-mod-proxy-html",
-        "a2dismod php5",
-        "apt-get remove -y libapache2-mod-php5",
-        "service apache2 restart" );
-    $this->programDataFolder = "/opt/ApacheModules"; // command and app dir name
-    $this->programNameMachine = "apachemodules"; // command and app dir name
-    $this->programNameFriendly = "Apache Mods!"; // 12 chars
-    $this->programNameInstaller = "Apache Modules";
-    $this->initialize();
-  }
+    public function __construct($params) {
+        parent::__construct($params);
+        $this->autopilotDefiner = "ApacheModules";
+        $this->installCommands = array(
+            array("method"=> array("object" => $this, "method" => "packageAddDeps", "params" => array("libxml2-dev")) ),
+            array("command"=> "a2enmod rewrite" ),
+            array("command"=> "a2enmod deflate" ),
+            array("command"=> "a2enmod ssl" ),
+            array("method"=> array("object" => $this, "method" => "packageAddDeps", "params" => array("libapache2-mod-proxy-html")) ),
+            array("command"=> "a2enmod proxy" ),
+            array("command"=> "a2enmod proxy_http" ),
+            array("method"=> array("object" => $this, "method" => "packageAddDeps", "params" => array("libapache2-mod-php5")) ),
+            array("command"=> "a2enmod php5" ),
+            array("method"=> array("object" => $this, "method" => "apacheRestart", "params" => array())) );
+        $this->uninstallCommands = array(
+            array("method"=> array("object" => $this, "method" => "packageRemoveDeps", "params" => array("libxml2-dev")) ),
+            array("command"=> "a2dismod rewrite" ),
+            array("command"=> "a2dismod deflate" ),
+            array("command"=> "a2dismod ssl" ),
+            array("method"=> array("object" => $this, "method" => "packageRemoveDeps", "params" => array("libapache2-mod-proxy-html")) ),
+            array("command"=> "a2dismod proxy" ),
+            array("command"=> "a2dismod proxy_http" ),
+            array("method"=> array("object" => $this, "method" => "packageRemoveDeps", "params" => array("libapache2-mod-php5")) ),
+            array("command"=> "a2dismod php5" ),
+            array("method"=> array("object" => $this, "method" => "apacheRestart", "params" => array())) );
+        $this->programDataFolder = "/opt/ApacheModules"; // command and app dir name
+        $this->programNameMachine = "apachemodules"; // command and app dir name
+        $this->programNameFriendly = "Apache Mods!"; // 12 chars
+        $this->programNameInstaller = "Apache Modules";
+        $this->initialize();
+    }
 
     public function askStatus() {
         $modsTextCmd = 'apachectl -t -D DUMP_MODULES';
         $modsText = $this->executeAndLoad($modsTextCmd) ;
-
-        $modsToCheck = array(
-            "http_module",
-            "deflate_module",
-            "php5_module",
-            "proxy_module",
-            "proxy_html_module",
-            "proxy_http_module",
-            "rewrite_module",
-            "ssl_module"
-        ) ;
-
+        $modsToCheck = array( "http_module", "deflate_module", "php5_module", "proxy_module", "proxy_html_module",
+            "proxy_http_module", "rewrite_module", "ssl_module" ) ;
         $consoleFactory = new \Model\Console();
         $console = $consoleFactory->getModel($this->params);
         $passing = true ;
-
         foreach ($modsToCheck as $modToCheck) {
             if (!strstr($modsText, $modToCheck)) {
                 $console->log("Apache Module {$modToCheck} does not exist.") ;
                 $passing = false ; } }
-
         return $passing ;
+    }
 
+    public function packageAddDeps($package) {
+        $packageFactory = new PackageManager();
+        $packageManager = $packageFactory->getModel($this->params) ;
+        $packageManager->performPackageEnsure("Apt", $package, $this);
+    }
+
+    public function packageRemoveDeps($package) {
+        $packageFactory = new PackageManager();
+        $packageManager = $packageFactory->getModel($this->params) ;
+        $packageManager->performPackageRemove("Apt", $package, $this);
+    }
+
+    public function apacheRestart() {
+        $serviceFactory = new Service();
+        $serviceManager = $serviceFactory->getModel($this->params) ;
+        $serviceManager->setService("apache2");
+        $serviceManager->restart();
     }
 
 }
