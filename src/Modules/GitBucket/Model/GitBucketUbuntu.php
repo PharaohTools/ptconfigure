856fdfb383cda0707b7bcc41a54862e9a572de6d
David Amanshia <phpengine@hotmail.co.uk>
2014-03-25 11:41:10 +0000

    gitbucket updated to array based install steps but module not finished

--- src/Modules/GitBucket/Model/GitBucketUbuntu.php
+++ src/Modules/GitBucket/Model/GitBucketUbuntu.php
@@ -17,19 +17,20 @@ class GitBucketUbuntu extends BaseLinuxApp {
   public function __construct($params) {
     parent::__construct($params);
     $this->autopilotDefiner = "GitBucket";
-    $this->installCommands = array("apt-get install -y gitbucket");
+    $this->installCommands = array(
+        array("method"=> array("object" => $this, "method" => "executeDependencies", "params" => array()) ),
+    );
     $this->uninstallCommands = array("apt-get remove -y python python-docutils");
     $this->programDataFolder = "";
     $this->programNameMachine = "gitlab"; // command and app dir name
     $this->programNameFriendly = "!Git Lab!!"; // 12 chars
     $this->programNameInstaller = "Git Lab";
-    $this->registeredPreInstallFunctions = array("executeDependencies");
     $this->initialize();
   }
 
-  private function executeDependencies() {
+  public function executeDependencies() {
     $gitTools = new \Model\GitTools($this->params);
-    $gitTools->install();
+    $gitTools->ensureInstalled();
   }
 
 }
