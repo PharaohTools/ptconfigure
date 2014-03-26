f88c30c7f09da3ce1e263b9b3218e1c82488d65d
Golden Dave <dave@theboss.com>
2014-03-26 22:31:15 +0000

    install step syntax fix, wasnt using an array for multiple packagenames

--- src/Modules/MysqlServer/Model/MysqlServerUbuntu.php
+++ src/Modules/MysqlServer/Model/MysqlServerUbuntu.php
@@ -22,12 +22,10 @@ class MysqlServerUbuntu extends BaseLinuxApp {
             array("command"=> array(
                     "echo mysql-server mysql-server/root_password password $newRootPass | sudo debconf-set-selections",
                     "echo mysql-server mysql-server/root_password_again password $newRootPass | sudo debconf-set-selections" ) ),
-            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "mysql-client")) ),
-            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "mysql-server")) ),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "mysql-client", "mysql-server")) ),
         );
         $this->uninstallCommands = array(
-            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "mysql-client")) ),
-            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "mysql-server")) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", array("mysql-client", "mysql-server"))) ),
         );
         $this->programDataFolder = "/opt/MysqlServer"; // command and app dir name
         $this->programNameMachine = "mysqlserver"; // command and app dir name
