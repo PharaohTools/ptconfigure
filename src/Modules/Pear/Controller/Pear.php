c50589c9a96b24d22d199939b5f2549e74fdf5c9
Golden Dave <dave@theboss.com>
2014-03-26 22:28:13 +0000

    added channel delete to pear and fixed channel discover

--- src/Modules/Pear/Controller/Pear.php
+++ src/Modules/Pear/Controller/Pear.php
@@ -17,7 +17,7 @@ class Pear extends Base {
             $this->content["helpData"] = $helpModel->getHelpData($pageVars["route"]["control"]);
             return array ("type"=>"view", "view"=>"help", "pageVars"=>$this->content); }
 
-        if (in_array($action, array("pkg-install", "pkg-remove", "pkg-ensure", "channel-discover") )) {
+        if (in_array($action, array("pkg-install","pkg-exists", "pkg-remove", "pkg-ensure", "channel-discover", "channel-delete") )) {
             $this->content["result"] = $thisModel->askAction($action);
             $this->content["appName"] = $thisModel->programNameInstaller ;
             return array ("type"=>"view", "view"=>"pear", "pageVars"=>$this->content); }
--- src/Modules/Pear/Model/PearUbuntu.php
+++ src/Modules/Pear/Model/PearUbuntu.php
@@ -15,6 +15,16 @@ class PearUbuntu extends BasePackager {
     public $modelGroup = array("Default") ;
     protected $packagerName = "pear";
 
+    public $actionsToMethods =
+        array(
+            "pkg-install" => "performInstall",
+            "pkg-ensure" => "performInstall",
+            "pkg-remove" => "performRemove",
+            "pkg-exists" => "performExistenceCheck",
+            "channel-discover" => "channelDiscover",
+            "channel-delete" => "channelDelete",
+        ) ;
+
     public function __construct($params) {
         parent::__construct($params);
         $this->autopilotDefiner = "Pear";
@@ -26,8 +36,8 @@ class PearUbuntu extends BasePackager {
     }
 
     public function isInstalled($packageName) {
-        $out = $this->executeAndLoad("sudo pear list {$packageName} -i") ;
-        return (strpos($out, "true") != false) ? true : false ;
+        $out = $this->executeAndLoad("sudo pear list -i | grep {$packageName}") ;
+        return (strpos($out, $packageName) != false) ? true : false ;
     }
 
     public function installPackage($packageName, $autopilot = null) {
@@ -50,6 +60,47 @@ class PearUbuntu extends BasePackager {
             $console->log("Removing Package {$packageName} from the Packager {$this->programNameInstaller} did not execute correctly") ;
             return false ; }
         return true ;
+    }
+
+    public function channelDiscover() {
+        $channel = $this->setChannel();
+        $out = $this->executeAndLoad("sudo pear channel-discover $channel");
+        $consoleFactory = new \Model\Console();
+        $console = $consoleFactory->getModel($this->params);
+        // var_dump($out, 'Channel "'.$channel.'" is already initialized') ;
+        $initString = 'Channel "'.$channel.'" is already initialized'."\n" ;
+        if ($out == $initString) {
+            $console->log("Not adding Channel $channel in Packager Pear as it is already initialized") ;
+            return true ; }
+        else if (strpos($out, "Adding Channel \"".$channel."\" succeeded") == false ||
+            strpos($out, "Discovery of channel \"".$channel."\" succeeded") == false ) {
+            $console->log("Discovering Channel $channel in Packager Pear did not execute correctly") ;
+            return false ; }
+        return true ;
+    }
+
+    public function channelDelete() {
+        $channel = $this->setChannel();
+        $out = $this->executeAndLoad("sudo pear channel-del $channel");
+        $consoleFactory = new \Model\Console();
+        $console = $consoleFactory->getModel($this->params);
+        $initString = 'channel-delete: channel "'.$channel.'" does not exist'."\n" ;
+        if ($out == $initString) {
+            $console->log("Not removing Channel $channel in Packager Pear as it is already initialized") ;
+            return true ; }
+        else if (strpos($out, "Channel \"".$channel."\" deleted") == false ) {
+            $console->log("Discovering Channel $channel in Packager Pear did not execute correctly") ;
+            return false ; }
+        return true ;
+    }
+
+    protected function setChannel($channel = null) {
+        if (isset($channel)) {  }
+        else if (isset($this->params["pear-channel"])) {
+            $channel = $this->params["pear-channel"]; }
+        else {
+            $channel = self::askForInput("Enter Pear Channel:", true); }
+        return $channel ;
     }
 
 }
