8129e175e07f8b6b95ac439ce3540cde00ef48b3
David Amanshia <phpengine@hotmail.co.uk>
2014-03-27 03:24:25 +0000

    Added beginning of BoxManager Module

---
+++ src/Modules/BoxManager/Controller/BoxManager.php
@@ -0,0 +1,27 @@
+<?php
+
+Namespace Controller ;
+
+class BoxManager extends Base {
+
+    public function execute($pageVars) {
+
+        $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
+        // if we don't have an object, its an array of errors
+        if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+
+        $action = $pageVars["route"]["action"];
+
+        if ($action=="help") {
+            $helpModel = new \Model\Help();
+            $this->content["helpData"] = $helpModel->getHelpData($pageVars["route"]["control"]);
+            return array ("type"=>"view", "view"=>"help", "pageVars"=>$this->content); }
+
+        if (in_array($action, array("box-install", "box-remove", "box-ensure") )) {
+            $this->content["result"] = $thisModel->askAction($action);
+            $this->content["appName"] = $thisModel->programNameInstaller ;
+            return array ("type"=>"view", "view"=>"apt", "pageVars"=>$this->content); }
+
+    }
+
+}
