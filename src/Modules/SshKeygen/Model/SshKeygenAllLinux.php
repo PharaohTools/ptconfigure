110e62486e0c95e19730f485b8ccfa029acbe01c
David Amanshia <phpengine@hotmail.co.uk>
2014-04-03 16:15:39 +0100

    Added SSH Key Generation module

---
+++ src/Modules/SshKeygen/Model/SshKeygenAllLinux.php
@@ -0,0 +1,94 @@
+<?php
+
+Namespace Model;
+
+class SshKeygenAllLinux extends BaseLinuxApp {
+
+    // Compatibility
+    public $os = array("Linux") ;
+    public $linuxType = array("any") ;
+    public $distros = array("any") ;
+    public $versions = array("any") ;
+    public $architectures = array("any") ;
+
+    // Model Group
+    public $modelGroup = array("Default") ;
+
+    private $keygenBits;
+    private $keygenType;
+    private $keygenPath;
+    private $keygenComment;
+
+    public function __construct($params) {
+        parent::__construct($params);
+        $this->autopilotDefiner = "SshKeygen";
+        $this->installCommands = array(
+            array("method"=> array("object" => $this, "method" => "askForKeygenBits", "params" => array()) ),
+            array("method"=> array("object" => $this, "method" => "askForKeygenType", "params" => array()) ),
+            array("method"=> array("object" => $this, "method" => "askForKeygenPath", "params" => array()) ),
+            array("method"=> array("object" => $this, "method" => "askForKeygenComment", "params" => array()) ),
+            array("method"=> array("object" => $this, "method" => "doKeyGen", "params" => array()) ),
+        );
+        $this->uninstallCommands = array(
+            array("method"=> array("object" => $this, "method" => "askForKeygenPath", "params" => array()) ),
+            array("method"=> array("object" => $this, "method" => "removeKey", "params" => array()) ),
+        );
+        $this->programDataFolder = "";
+        $this->programNameMachine = "sshkeygen"; // command and app dir name
+        $this->programNameFriendly = "sshkeygen!"; // 12 chars
+        $this->programNameInstaller = "SSH Key Generation";
+        $this->initialize();
+    }
+
+    public function askForKeygenBits() {
+        if (isset($this->params["ssh-keygen-bits"]) ) {
+            $this->keygenBits = $this->params["ssh-keygen-bits"] ; }
+        else {
+            $question = "Enter number of bits for SSH Key (multiple of 1024):";
+            $this->keygenBits = self::askForInput($question, true); }
+    }
+
+    public function askForKeygenType() {
+        if (isset($this->params["ssh-keygen-type"]) ) {
+            $this->keygenType = $this->params["ssh-keygen-type"] ; }
+        else {
+            $question = "Choose Key type (rsa/dsa)";
+            $this->keygenType = self::askForArrayOption($question, array("rsa", "dsa"),  true); }
+    }
+
+    public function askForKeygenPath() {
+        if (isset($this->params["ssh-keygen-path"]) ) {
+            $this->keygenPath = $this->params["ssh-keygen-path"] ; }
+        else {
+            $question = "Enter path to store private key (public key will be same with .pub):";
+            $this->keygenPath = self::askForInput($question, true); }
+    }
+
+    public function askForKeygenComment() {
+        if (isset($this->params["ssh-keygen-comment"]) ) {
+            $this->keygenComment = $this->params["ssh-keygen-comment"] ; }
+        else {
+            $question = "Plain text comment appended to public key. None is fine";
+            $keygenComment = self::askForInput($question);
+            $this->keygenComment = (isset($keygenComment)) ? $keygenComment : "Pharoah Tools" ; }
+    }
+
+    public function removeKey() {
+        $consoleFactory = new \Model\Console();
+        $console = $consoleFactory->getModel($this->params);
+        if (file_exists($this->params["ssh-keygen-path"])) {
+            unlink($this->params["ssh-keygen-path"]) ;
+            $console->log("Removing File at {$this->params["ssh-keygen-path"]} in SSH Keygen") ;
+            unlink($this->params["ssh-keygen-path"].".pub") ;
+            $console->log("Removing File at {$this->params["ssh-keygen-path"]}.pub in SSH Keygen") ; }
+    }
+
+    public function doKeyGen() {
+        $cmd  = "ssh-keygen -b ".$this->params["ssh-keygen-bits"].' ' ;
+        $cmd .= '-t '.$this->params["ssh-keygen-type"].' ' ;
+        $cmd .= '-f '.$this->params["ssh-keygen-path"].' ' ;
+        $cmd .= '-q -N "" -C"'.$this->params["ssh-keygen-comment"].'"' ;
+        $this->executeAndOutput($cmd) ;
+    }
+
+}
---
+++ src/Modules/SshKeygen/info.SshKeygen.php
@@ -0,0 +1,59 @@
+<?php
+
+Namespace Info;
+
+class SshKeygenInfo extends Base {
+
+  public $hidden = false;
+
+  public $name = "SSH Keygen - Generate SSH Kay Pairs";
+
+  public function __construct() {
+    parent::__construct();
+  }
+
+  public function routesAvailable() {
+    return array( "SshKeygen" =>  array_merge(parent::routesAvailable(), array("install") ) );
+  }
+
+  public function routeAliases() {
+    return array("sshkeygen"=>"SshKeygen", "ssh-keygen"=>"SshKeygen");
+  }
+
+  public function autoPilotVariables() {
+    return array(
+      "SshKeygen" => array(
+        "SshKeygen" => array(
+          "programDataFolder" => "/opt/SshKeygen", // command and app dir name
+          "programNameMachine" => "mysqladmins", // command and app dir name
+          "programNameFriendly" => "Mysql Admins!", // 12 chars
+          "programNameInstaller" => "Mysql Admins",
+          "mysqlNewAdminUser" => "string",
+          "mysqlNewAdminPass" => "string",
+          "mysqlRootUser" => "string",
+          "mysqlRootPass" => "string",
+          "dbHost" => "string"
+        ),
+      )
+    );
+  }
+
+  public function helpDefinition() {
+    $help = <<<"HELPDATA"
+  This command allows you to install an SSH Key Pair.
+
+  SshKeygen, ssh-keygen, sshkeygen
+
+        - install
+        Installs a new SSH Key
+        example: cleopatra ssh-keygen install
+
+        - uninstall
+        Removes an SSH Key
+        example: cleopatra ssh-keygen uninstall
+
+HELPDATA;
+    return $help ;
+  }
+
+}
