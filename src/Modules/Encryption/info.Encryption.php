3706c0bacf4a709fab4b5d3a6aa4663ea737a08b
Golden Dave <dave@theboss.com>
2014-04-04 20:35:43 +0100

    updated the help text and completed examples

--- src/Modules/Encryption/info.Encryption.php
+++ src/Modules/Encryption/info.Encryption.php
@@ -40,8 +40,16 @@ class EncryptionInfo extends Base {
   Encryption, encrypt
 
         - install
-        Installs an encrypted file
-        example: cleopatra encryption install
+        Encrypts a file or string
+        example: sudo cleopatra encryption install --yes --unencrypted-data=/var/www/a-website/build/config/cleopatra/SSH/raw/bastion
+                 --encryption-target-file=/tmp/encrypted --encryption-key=/root/.cleopatra/SSH/key --encryption-file-permissions=""
+                 --encryption-file-owner="" --encryption-file-group=""
+
+        - uninstall
+        Decrypts an encrypted file or string
+        example: sudo cleopatra encryption uninstall --yes --encrypted-data=/tmp/encrypted
+                 --encryption-target-file=/var/www/a-website/build/config/cleopatra/SSH/raw/bastion --encryption-key=/root/.cleopatra/SSH/key
+                 --encryption-file-permissions="" --encryption-file-owner="" --encryption-file-group=""
 
 HELPDATA;
       return $help ;
