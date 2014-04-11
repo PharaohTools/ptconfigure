e26d8c4c0fa535e414bc9d41e7c31b14120869b9
David Amanshia <phpengine@hotmail.co.uk>
2014-04-11 01:49:57 +0100

    removing php notices

--- src/Modules/CleopatraRequired/Views/AppInstallView.tpl.php
+++ src/Modules/CleopatraRequired/Views/AppInstallView.tpl.php
@@ -1,9 +1,19 @@
 Single App Installer:
 --------------------------------------------
 
-<?php echo $pageVars["appName"] ; ?>: <?php
-  $result_summary = (strlen($pageVars["appInstallResult"])>0) ? "Success" : "Failure" ;
-  echo $result_summary."\n" ;
+
+<?php
+
+if (isset($pageVars["appName"]) && isset($pageVars["appInstallResult"])) {
+
+    echo  $pageVars["appName"] ; ?>: <?php
+    $result_summary = (strlen($pageVars["appInstallResult"])>0) ? "Success" : "Failure" ;
+    echo $result_summary."\n" ;
+}
+
+else {
+    echo "No Data.\n";
+}
 ?>
 
 ------------------------------
