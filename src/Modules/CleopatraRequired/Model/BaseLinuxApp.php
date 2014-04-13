2ec0adb5712d774e28be5dae0927b1029c850552
Golden Dave <dave@theboss.com>
2014-04-14 00:14:18 +0100

    removed versioning methods as they are in base now

--- src/Modules/CleopatraRequired/Model/BaseLinuxApp.php
+++ src/Modules/CleopatraRequired/Model/BaseLinuxApp.php
@@ -4,117 +4,117 @@ Namespace Model;
 
 class BaseLinuxApp extends Base {
 
-  protected $installCommands;
-  protected $uninstallCommands;
-  protected $installUserName;
-  protected $installUserHomeDir;
-  protected $programExecutorCommand;
+    protected $installCommands;
+    protected $uninstallCommands;
+    protected $installUserName;
+    protected $installUserHomeDir;
+    protected $programExecutorCommand;
 
-  public function __construct($params) {
-    parent::__construct($params);
-    $this->populateCompletion();
-  }
+    public function __construct($params) {
+        parent::__construct($params);
+        $this->populateCompletion();
+    }
 
-  public function initialize() {
-    $this->populateTitle();
-  }
+    public function initialize() {
+        $this->populateTitle();
+    }
 
-  public function askWhetherToInstallLinuxApp() {
-    return $this->performLinuxAppInstall();
-  }
+    public function askWhetherToInstallLinuxApp() {
+        return $this->performLinuxAppInstall();
+    }
 
-  public function askInstall() {
-    return $this->askWhetherToInstallLinuxApp();
-  }
+    public function askInstall() {
+        return $this->askWhetherToInstallLinuxApp();
+    }
 
-  public function askUnInstall() {
-    return $this->askWhetherToUnInstallLinuxApp();
-  }
+    public function askUnInstall() {
+        return $this->askWhetherToUnInstallLinuxApp();
+    }
 
-  public function askWhetherToUnInstallLinuxApp() {
-    return $this->performLinuxAppUnInstall();
-  }
+    public function askWhetherToUnInstallLinuxApp() {
+        return $this->performLinuxAppUnInstall();
+    }
 
-  protected function performLinuxAppInstall() {
-    $doInstall = (isset($this->params["yes"]) && $this->params["yes"]==true) ?
-      true : $this->askWhetherToInstallLinuxAppToScreen();
-    if (!$doInstall) { return false; }
-    return $this->install();
-  }
+    protected function performLinuxAppInstall() {
+        $doInstall = (isset($this->params["yes"]) && $this->params["yes"]==true) ?
+            true : $this->askWhetherToInstallLinuxAppToScreen();
+        if (!$doInstall) { return false; }
+        return $this->install();
+    }
 
-  protected function performLinuxAppUnInstall() {
-    $doUnInstall = (isset($this->params["yes"]) && $this->params["yes"]==true) ?
-      true : $this->askWhetherToUnInstallLinuxAppToScreen();
-    if (!$doUnInstall) { return false; }
-    return $this->unInstall();
-  }
+    protected function performLinuxAppUnInstall() {
+        $doUnInstall = (isset($this->params["yes"]) && $this->params["yes"]==true) ?
+            true : $this->askWhetherToUnInstallLinuxAppToScreen();
+        if (!$doUnInstall) { return false; }
+        return $this->unInstall();
+    }
 
-  public function ensureInstalled(){
-      $consoleFactory = new \Model\Console();
-      $console = $consoleFactory->getModel($this->params);
-      if ($this->askStatus() == true) {
-          $console->log("Not installing as already installed") ; }
-      else {
-          $console->log("Installing as not installed") ;
-          $this->install(); }
-      return true;
-  }
+    public function ensureInstalled(){
+        $consoleFactory = new \Model\Console();
+        $console = $consoleFactory->getModel($this->params);
+        if ($this->askStatus() == true) {
+            $console->log("Not installing as already installed") ; }
+        else {
+            $console->log("Installing as not installed") ;
+            $this->install(); }
+        return true;
+    }
 
-  public function install() {
-    if (isset($this->params["hide-title"])) { $this->populateTinyTitle() ; }
-    $this->showTitle();
-    $this->doInstallCommand();
-    if ($this->programDataFolder) {
-      $this->changePermissions($this->programDataFolder); }
-    $this->setInstallFlagStatus(true) ;
-    if (isset($this->params["hide-completion"])) { $this->populateTinyCompletion(); }
-    $this->showCompletion();
-    return true;
-  }
+    public function install() {
+        if (isset($this->params["hide-title"])) { $this->populateTinyTitle() ; }
+        $this->showTitle();
+        $this->doInstallCommand();
+        if ($this->programDataFolder) {
+            $this->changePermissions($this->programDataFolder); }
+        $this->setInstallFlagStatus(true) ;
+        if (isset($this->params["hide-completion"])) { $this->populateTinyCompletion(); }
+        $this->showCompletion();
+        return true;
+    }
 
-  public function unInstall() {
-    $this->showTitle();
-    $this->doUnInstallCommand();
-    $this->setInstallFlagStatus(false) ;
-    $this->showCompletion();
-    return true;
-  }
+    public function unInstall() {
+        $this->showTitle();
+        $this->doUnInstallCommand();
+        $this->setInstallFlagStatus(false) ;
+        $this->showCompletion();
+        return true;
+    }
 
-  protected function showTitle() {
-    print $this->titleData ;
-  }
+    protected function showTitle() {
+        print $this->titleData ;
+    }
 
-  protected function showCompletion() {
-    print $this->completionData ;
-  }
+    protected function showCompletion() {
+        print $this->completionData ;
+    }
 
-  protected function askWhetherToInstallLinuxAppToScreen(){
-    $question = "Install ".$this->programNameInstaller."?";
-    return self::askYesOrNo($question);
-  }
+    protected function askWhetherToInstallLinuxAppToScreen(){
+        $question = "Install ".$this->programNameInstaller."?";
+        return self::askYesOrNo($question);
+    }
 
-  protected function askWhetherToUnInstallLinuxAppToScreen(){
-    $question = "Uninstall ".$this->programNameInstaller."?";
-    return self::askYesOrNo($question);
-  }
+    protected function askWhetherToUnInstallLinuxAppToScreen(){
+        $question = "Uninstall ".$this->programNameInstaller."?";
+        return self::askYesOrNo($question);
+    }
 
-  protected function askForInstallUserName() {
-      $question = "Enter User To Install As:";
-      $input = (isset($this->params["install-user-name"])) ? $this->params["install-user-name"] : self::askForInput($question);
-      $this->installUserName = $input;
-  }
+    protected function askForInstallUserName() {
+        $question = "Enter User To Install As:";
+        $input = (isset($this->params["install-user-name"])) ? $this->params["install-user-name"] : self::askForInput($question);
+        $this->installUserName = $input;
+    }
 
-  protected function askForInstallUserHomeDir() {
-      $question = "Enter Install User Home Dir:";
-      $input = (isset($this->params["install-user-home"])) ? $this->params["install-user-home"] : self::askForInput($question);
-      $this->installUserHomeDir = $input ;
-  }
+    protected function askForInstallUserHomeDir() {
+        $question = "Enter Install User Home Dir:";
+        $input = (isset($this->params["install-user-home"])) ? $this->params["install-user-home"] : self::askForInput($question);
+        $this->installUserHomeDir = $input ;
+    }
 
-  protected function askForInstallDirectory(){
-      $question = "Enter Install Directory:";
-      $input = (isset($this->params["install-directory"])) ? $this->params["install-directory"] : self::askForInput($question);
-      $this->programDataFolder = $input;
-  }
+    protected function askForInstallDirectory(){
+        $question = "Enter Install Directory:";
+        $input = (isset($this->params["install-directory"])) ? $this->params["install-directory"] : self::askForInput($question);
+        $this->programDataFolder = $input;
+    }
 
     protected function doInstallCommand(){
         self::swapCommandArrayPlaceHolders($this->installCommands);
@@ -138,57 +138,30 @@ class BaseLinuxApp extends Base {
                 self::executeAsShell($uninstallCommand["command"]) ; } }
     }
 
-  protected function changePermissions($autoPilot, $target=null){
-    if ($target != null) {
-      $command = "chmod -R 775 $target";
-      self::executeAndOutput($command); }
-  }
+    protected function changePermissions($autoPilot, $target=null){
+        if ($target != null) {
+            $command = "chmod -R 775 $target";
+            self::executeAndOutput($command); }
+    }
 
-  protected function deleteExecutorIfExists(){
-    $command = 'rm -f '.$this->programExecutorFolder.DIRECTORY_SEPARATOR.$this->programNameMachine;
-    self::executeAndOutput($command, "Program Executor Deleted if existed");
-    return true;
-  }
+    protected function deleteExecutorIfExists(){
+        $command = 'rm -f '.$this->programExecutorFolder.DIRECTORY_SEPARATOR.$this->programNameMachine;
+        self::executeAndOutput($command, "Program Executor Deleted if existed");
+        return true;
+    }
 
-  protected function saveExecutorFile(){
-    $this->populateExecutorFile();
-    $executorPath = $this->programExecutorFolder.DIRECTORY_SEPARATOR.$this->programNameMachine;
-    file_put_contents($executorPath, $this->bootStrapData);
-    $this->changePermissions(null, $executorPath);
-  }
+    protected function saveExecutorFile(){
+        $this->populateExecutorFile();
+        $executorPath = $this->programExecutorFolder.DIRECTORY_SEPARATOR.$this->programNameMachine;
+        file_put_contents($executorPath, $this->bootStrapData);
+        $this->changePermissions(null, $executorPath);
+    }
 
-  protected function populateExecutorFile() {
-    $this->bootStrapData = "#!/usr/bin/php\n
+    protected function populateExecutorFile() {
+      $this->bootStrapData = "#!/usr/bin/php\n
 <?php\n
 exec('".$this->programExecutorCommand."');\n
 ?>";
-  }
-
-    /*Versioning starts here*/
-
-    public function getInstalledVersion() {
-        if (isset($this->versionCommand)) {
-            return $this->executeAndLoad($this->versionCommand); }
-        else {
-            return false; }
     }
-
-    public function getAvailableVersions() {
-        if (isset($this->availableVersions)) {
-            return $this->availableVersions ; }
-        else {
-            return false; }
-    }
-
-    public function getRecommendedVersion() {
-        return false;
-    }
-
-    public function getLatestVersion() {
-        return false;
-    }
-    /*
-    */
-
 
 }
