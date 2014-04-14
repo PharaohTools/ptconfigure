c2c851d6a3da662b6ec8696b66e3fcc3c39c4fe4
Golden Dave <dave@theboss.com>
2014-04-14 15:54:46 +0100

    added cli version view t cleo req

--- src/Modules/CleopatraRequired/Views/AppVersionView.tpl.php
+++ src/Modules/CleopatraRequired/Views/AppVersionView.tpl.php
@@ -6,7 +6,11 @@ Single App Version Check:
 
 if (isset($pageVars["appName"]) && isset($pageVars["appInstallResult"])) {
 
-    echo  $pageVars["appName"]." Version : " ;
+    echo  $pageVars["appName"];
+    if (isset($pageVars["params"]["version-type"]) &&
+        in_array($pageVars["params"]["version-type"], array("Installed", "installed", "Recommended", "recommended", "Latest", "latest"))) {
+        echo " ".ucfirst($pageVars["params"]["version-type"]) ; }
+    echo " Version : " ;
     echo $pageVars["appInstallResult"]."\n" ;
 }
 
