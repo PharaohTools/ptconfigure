e6701e2deeee783f6fd767cc569968faa7d352f5
Golden Dave <dave@theboss.com>
2014-04-15 01:53:00 +0100

    added iscompatiblewith to versioning

--- src/Modules/CleopatraRequired/Model/SoftwareVersion.php
+++ src/Modules/CleopatraRequired/Model/SoftwareVersion.php
@@ -2,18 +2,70 @@
 
 namespace Model;
 
-class RegExp {
+class SoftwareVersion {
 
-    public $regexp;
+    public $shortVersionNumber;
+    public $fullVersionNumber;
 
-    function __construct($regexp)
-    {
-        $this->regexp = $regexp;
+    public function __construct($versionNumber) {
+        $this->fullVersionNumber = $versionNumber ;
+        $this->shortVersionNumber = $this->getShortVersionNumber($versionNumber) ;
     }
 
-    function __toString()
-    {
-        return "RegExp({$this->regexp})";
+    // @todo this relies on php treating strings as arrays, which is deprecated innit?
+    private function getShortVersionNumber($versionNumber) {
+        $shortNum = "" ;
+        for ($i=0; $i<strlen($versionNumber); $i++) {
+            if (in_array($versionNumber[$i], array("1","2","3","4","5","6","7","8","9","0")) || $versionNumber[$i] == ".") {
+                $shortNum .= $versionNumber[$i]; }
+            else {
+                break ; } }
+        return $shortNum ;
+    }
+
+    //@todo this return seems wrong
+    public function isGreaterThan($compare) {
+        if (is_object($compare) && $compare instanceof SoftwareVersion) {
+            $myPieces = explode(".", $this->shortVersionNumber) ;
+            $comparePieces = explode(".", $compare->shortVersionNumber) ;
+            for ( $i=0 ; $i<count($comparePieces); $i++) {
+                if ($comparePieces[$i] > $myPieces[$i] ) {
+                    return true ; }
+                if ($comparePieces[$i] < $myPieces[$i] ) {
+                    return false ; }
+                else {
+                    continue; } } }
+        return "SoftwareVersion->isGreaterThan() Requires an instance of SoftwareVersion" ;
+    }
+
+    //@todo this return seems wrong
+    public function isLessThan($compare) {
+        if (is_object($compare) && $compare instanceof SoftwareVersion) {
+            $myPieces = explode(".", $this->shortVersionNumber) ;
+            $comparePieces = explode(".", $compare->shortVersionNumber) ;
+            for ( $i=0 ; $i<count($comparePieces); $i++) {
+                if ($comparePieces[$i] < $myPieces[$i] ) {
+                    return true ; }
+                if ($comparePieces[$i] > $myPieces[$i] ) {
+                    return false ; }
+                else {
+                    continue; } } }
+        return "SoftwareVersion->isLessThan() Requires an instance of SoftwareVersion" ;
+    }
+
+    //@todo this return seems wrong
+    public function isCompatibleWith($compare) {
+        if (is_object($compare) && $compare instanceof SoftwareVersion) {
+            $myPieces = explode(".", $this->shortVersionNumber) ;
+            $comparePieces = explode(".", $compare->shortVersionNumber) ;
+            for ( $i=0 ; $i<count($comparePieces); $i++) {
+                if ($comparePieces[$i] < $myPieces[$i] ) {
+                    return true ; }
+                if ($comparePieces[$i] > $myPieces[$i] ) {
+                    return false ; }
+                else {
+                    continue; } } }
+        return "SoftwareVersion->isCompatibleWith Requires an instance of SoftwareVersion" ;
     }
 
 
--- src/Modules/CleopatraRequired/Views/AppVersionView.tpl.php
+++ src/Modules/CleopatraRequired/Views/AppVersionView.tpl.php
@@ -4,14 +4,31 @@ Single App Version Check:
 
 <?php
 
-if (isset($pageVars["appName"]) && isset($pageVars["appInstallResult"])) {
+if (isset($pageVars["appName"]) && isset($pageVars["versionResult"])) {
 
-    echo  $pageVars["appName"];
-    if (isset($pageVars["params"]["version-type"]) &&
-        in_array($pageVars["params"]["version-type"], array("Installed", "installed", "Recommended", "recommended", "Latest", "latest"))) {
-        echo " ".ucfirst($pageVars["params"]["version-type"]) ; }
-    echo " Version : " ;
-    echo $pageVars["appInstallResult"]."\n" ;
+    if (is_object($pageVars["versionResult"]) && $pageVars["versionResult"] instanceof \Model\SoftwareVersion) {
+
+        echo  $pageVars["appName"];
+        if (isset($pageVars["params"]["version-type"]) &&
+            in_array($pageVars["params"]["version-type"], array("Installed", "installed", "Recommended", "recommended", "Latest", "latest"))) {
+            echo " ".ucfirst($pageVars["params"]["version-type"])." Version : \n" ;
+            echo "    Full Version: ".$pageVars["versionResult"]->fullVersionNumber."\n" ;
+            echo "    Short Version: ".$pageVars["versionResult"]->shortVersionNumber."\n" ; }
+        else {
+            echo " Installed Version : \n" ;
+            echo "    Full Version: ".$pageVars["versionResult"]->fullVersionNumber."\n" ;
+            echo "    Short Version: ".$pageVars["versionResult"]->shortVersionNumber."\n" ; }
+    }
+
+    else {
+        echo  $pageVars["appName"];
+        if (isset($pageVars["params"]["version-type"]) &&
+            in_array($pageVars["params"]["version-type"], array("Installed", "installed", "Recommended", "recommended", "Latest", "latest"))) {
+            echo " ".ucfirst($pageVars["params"]["version-type"]) ; }
+        echo " Version : " ;
+        echo $pageVars["versionResult"]."\n" ;
+    }
+
 }
 
 else {
