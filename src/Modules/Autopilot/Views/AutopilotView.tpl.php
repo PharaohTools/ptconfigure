78cf6bbe4ff93fb9c0b667e8004e4fbdd84888da
Golden Dave <dave@theboss.com>
2014-04-17 00:08:37 +0100

    fixed auto view was empty

--- src/Modules/Autopilot/Views/AutopilotView.tpl.php
+++ src/Modules/Autopilot/Views/AutopilotView.tpl.php
@@ -3,11 +3,7 @@ Autopilot Install - <?php echo $pageVars["package-friendly"] ; ?> Installer:
 
 <?php
 
-foreach ($pageVars["results"] as $installResult) {
-  echo $installResult["appName"] . ': ' ;
-  $result = ($installResult["installResult"] == true) ? "Success" : "Failure" ;
-  echo $result."\n" ;
-}
+echo $pageVars["autoExec"] ;
 
 ?>
 
