d9470ae5e87c9e3c43053f1e3d41447834b2a3cd
David Amanshia <phpengine@hotmail.co.uk>
2014-04-16 11:34:14 +0100

    finishing updates to splie DB Configure and Install

--- src/Modules/DBInstall/Controller/DBInstall.php
+++ src/Modules/DBInstall/Controller/DBInstall.php
@@ -14,26 +14,17 @@ class DBInstall extends Base {
 
         $action = $pageVars["route"]["action"];
 
-        $dbConfigureModel = new \Model\DBConfigure($pageVars["route"]["extraParams"]) ;
-        $dbInstallModel = new \Model\DBInstall($pageVars["route"]["extraParams"]) ;
-
-        if ($action=="configure" || $action== "config" || $action== "conf") {
-            $this->content["dbResult"] = $dbConfigureModel->askWhetherToConfigureDB();
-            return array ("type"=>"view", "view"=>"database", "pageVars"=>$this->content); }
-        else if ($action=="reset") {
-            $this->content["dbResult"] = $dbConfigureModel->askWhetherToResetDBConfiguration();
-            return array ("type"=>"view", "view"=>"database", "pageVars"=>$this->content); }
-        else if ($action=="install") {
-            $this->content["dbResult"] = $dbInstallModel->askWhetherToInstallDB();
+        if ($action=="install") {
+            $this->content["dbResult"] = $thisModel->askWhetherToInstallDB();
             return array ("type"=>"view", "view"=>"database", "pageVars"=>$this->content); }
         else if ($action=="drop") {
-            $this->content["dbResult"] = $dbInstallModel->askWhetherToDropDB();
+            $this->content["dbResult"] = $thisModel->askWhetherToDropDB();
             return array ("type"=>"view", "view"=>"database", "pageVars"=>$this->content); }
         else if ($action=="useradd") {
-            $this->content["dbResult"] = $dbInstallModel->askWhetherToAddUser();
+            $this->content["dbResult"] = $thisModel->askWhetherToAddUser();
             return array ("type"=>"view", "view"=>"database", "pageVars"=>$this->content); }
         else if ($action=="userdrop") {
-            $this->content["dbResult"] = $dbInstallModel->askWhetherToDropUser();
+            $this->content["dbResult"] = $thisModel->askWhetherToDropUser();
             return array ("type"=>"view", "view"=>"database", "pageVars"=>$this->content); }
         $this->content["messages"][] = "Invalid DB Action";
         return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content);
