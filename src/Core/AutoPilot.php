f6ddb5aff6019b8560cd6a917928b8525f6da1bd
Golden Dave <dave@theboss.com>
2014-04-21 20:50:56 +0100

    autopilot base updated to have appHomeDir property already set

--- src/Core/AutoPilot.php
+++ src/Core/AutoPilot.php
@@ -4,36 +4,14 @@ Namespace Core ;
 
 class AutoPilot {
 
-  // Git, gitk,git-cola and git-core
-  public $GitToolsInstallExecute = false; // true or false
-  public $GitToolsUnInstallExecute = false; // true or false
+    protected $appHomeDir ;
 
-  // Devhelper Install Variables
-  public $DevhelperInstallExecute = false; // true or false
-  public $DevhelperUnInstallExecute = false;
-  public $DevhelperInstallDirectory = '/opt/devhelper';
-  public $DevhelperExecutorDirectory = '/usr/bin';
+    public function __construct() {
+        $this->setProperties();
+    }
 
-  // PHP Unit 3.5 (for php 5.3) Install Variables
-  public $PHPUnit35InstallExecute = false; // true or false
-  public $PHPUnit35UnInstallExecute = false;
-  public $PHPUnit35InstallDirectory = '/opt/phpunit';
-  public $PHPUnit35ExecutorDirectory = '/usr/bin';
-
-  // PHP Unit 3.5 (for php 5.3) Install Variables
-  public $PHPCSInstallExecute = false; // true or false
-  public $PHPCSUnInstallExecute = false;
-  public $PHPCSInstallDirectory = '/opt/phpcs';
-  public $PHPCSExecutorDirectory = '/usr/bin';
-
-  // PHP Unit 3.5 (for php 5.3) Install Variables
-  public $PHPMDInstallExecute = false; // true or false
-  public $PHPMDUnInstallExecute = false;
-  public $PHPMDInstallDirectory = '/opt/phpmd';
-  public $PHPMDExecutorDirectory = '/usr/bin';
-
-  // Selenium Server
-  public $SeleniumServerInstallExecute = false; // true or false
-  public $SeleniumServerUnInstallExecute = false;
+    protected function setProperties() {
+        $this->appHomeDir = dirname(dirname(dirname(__FILE__))) ;
+    }
 
 }
