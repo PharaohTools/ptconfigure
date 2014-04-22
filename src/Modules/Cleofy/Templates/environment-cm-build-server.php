a42957d837afd04a595241e541cff9f869f8c6e2
Golden Dave <dave@theboss.com>
2014-04-22 22:24:31 +0100

    Templates updated for cm

--- src/Modules/Cleofy/Templates/environment-cm-build-server.php
+++ src/Modules/Cleofy/Templates/environment-cm-build-server.php
@@ -16,10 +16,42 @@ class AutoPilotConfigured extends AutoPilot {
         $this->steps =
             array(
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets begin Configuration of a build server on environment <%tpl.php%>env_name</%tpl.php%>"),),),
+
+                // Install Keys - Bastion Public Key
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure our Bastion Public Key is installed" ),),),
+                array ( "Copy" => array( "file" =>
+                array("from" => "build/config/cleopatra/SSH/keys/public/raw/bastion"),
+                    array("to" => "$HOME/.ssh/id_rsa"),
+                    // @todo Fix this Standalone Server
+                ),),
+
+                // SSH Hardening
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure we have some SSH Security" ),),),
+                array ( "SSHHarden" => array( "ensure" => array(),),),
+
+                // Standard Tools
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure some standard tools are installed" ),),),
-                array ( "StandardTools" => array( "install" => array(),),),
+                array ( "StandardTools" => array( "ensure" => array(),),),
+
+                // Git Tools
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure some git tools are installed" ),),),
+                array ( "GitTools" => array( "ensure" => array(),),),
+
+                // PHP Modules
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure our common PHP Modules are installed" ),),),
+                array ( "PHPModules" => array( "ensure" => array(),),),
+
+                // Apache
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure Apache Server is installed" ),),),
+                array ( "ApacheServer" => array( "ensure" => array(),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure our common Apache Modules are installed" ),),),
+                array ( "ApacheModules" => array( "ensure" => array(),),),
+
+                // Java
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure Java is installed"),),),
                 array ( "Java" => array( "ensure" => array("guess" => true ),),),
+
+                // Jenkins
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure Jenkins is installed" ),),),
                 array ( "Jenkins" => array( "install" => array(),),),
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure Jenkins PHP Plugins are installed"),),),
@@ -42,7 +74,10 @@ class AutoPilotConfigured extends AutoPilot {
                 */
 
 
-        );
+                // Dapper Reverse Proxy
+
+
+            );
 
     }
 
