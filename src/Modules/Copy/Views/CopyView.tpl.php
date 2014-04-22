fc2b38a992b0155d001c5b6f9963fa2febb5d7c4
Golden Dave <dave@theboss.com>
2014-04-22 10:02:25 +0100

    updated the view for the copy module but its reporting incorrectly

--- src/Modules/Copy/Views/CopyView.tpl.php
+++ src/Modules/Copy/Views/CopyView.tpl.php
@@ -1,4 +1,10 @@
-Copy Result: <?php echo $pageVars["result"] ; ?>
+Copy Result: <?php
+$result = ($pageVars["result"] == true) ? "Success" : "Failure" ;
+echo $result ;
+
+// @todo This is reporting incorrectly but the copy works for now
+
+?>
 
 ------------------------------
 Copy Finished
