3719c5df0674e0b029d68191f980c64c9bcec4a3
David Amanshia <phpengine@hotmail.co.uk>
2014-04-22 09:54:00 +0100

    New Templates for Cleofy

---
+++ src/Modules/Cleofy/Templates/environment-cm-standalone-server.php
@@ -0,0 +1,83 @@
+<?php
+
+Namespace Core ;
+
+class AutoPilotConfigured extends AutoPilot {
+
+    public $steps ;
+
+    public function __construct() {
+        $this->setSteps();
+    }
+
+    /* Steps */
+    private function setSteps() {
+
+        $this->steps =
+            array(
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets begin Configuration of a standalone server on environment <%tpl.php%>env_name</%tpl.php%>"),),),
+
+                // Install Keys - Bastion Public Key
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure our Bastion Public Key is installed" ),),),
+                array ( "Copy" => array( "file" =>
+                    array("from" => "build/config/cleopatra/SSH/keys/public/raw/bastion"),
+                    array("to" => "$HOME/.ssh/id_rsa"),
+                    // @todo Fix this Standalone Server
+                ),),
+
+                // SSH Hardening
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure some standard tools are installed" ),),),
+                array ( "SSHHarden" => array( "ensure" => array(),),),
+
+                // Standard Tools
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure some standard tools are installed" ),),),
+                array ( "StandardTools" => array( "ensure" => array(),),),
+
+                // Git Tools
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure some git tools are installed" ),),),
+                array ( "GitTools" => array( "ensure" => array(),),),
+
+                // PHP Modules
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure our common PHP Modules are installed" ),),),
+                array ( "PHPModules" => array( "ensure" => array(),),),
+
+                // Apache
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure Apache Server is installed" ),),),
+                array ( "ApacheServer" => array( "ensure" => array(),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure our common Apache Modules are installed" ),),),
+                array ( "ApacheModules" => array( "ensure" => array(),),),
+
+                //Mysql
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure Mysql Server is installed" ),),),
+                array ( "MysqlServer" => array( "ensure" => array(),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure a Mysql Admin User is installed"),),),
+                array ( "MysqlAdmins" => array( "install" =>
+                    array("root-user" => "root"),
+                    array("root-pass" => "cleopatra"),
+                    array("new-user" => "root"),
+                    array("new-pass" => "root"),
+                    array("mysql-host" => "127.0.0.1"),),),
+
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure the Jenkins user can use Sudo without a Password"),),),
+                array ( "JenkinsSudoNoPass" => array( "ensure" => array(),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "Configuring a standalone server on environment <%tpl.php%>env_name</%tpl.php%> complete"),),),
+
+                /*
+//                array ( "Logging" => array( "log" => array( "log-message" => "Lets block all input"), ) , ) ,
+//                array ( "Firewall" => array( "deny" => array("firewall-rule" => "ssh/tcp" ), ) , ) ,
+//                array ( "Logging" => array( "log" => array( "log-message" => "Lets block all output"), ) , ) ,
+//                array ( "Firewall" => array( "allow" => array("firewall-rule" => "ssh/https" ), ) , ) ,
+//                array ( "Logging" => array( "log" => array( "log-message" => "Lets allow SSH input"), ) , ) ,
+//                array ( "Firewall" => array( "allow" => array("firewall-rule" => "ssh/tcp" ), ) , ) ,
+//                array ( "Logging" => array( "log" => array( "log-message" => "Lets allow HTTPS input"), ) , ) ,
+//                array ( "Firewall" => array( "allow" => array("firewall-rule" => "ssh/https" ), ) , ) ,
+//                array ( "Logging" => array( "log" => array( "log-message" => "Lets allow HTTP input"), ) , ) ,
+//                array ( "Firewall" => array( "allow" => array("firewall-rule" => "ssh/http" ), ) , ) ,
+                */
+
+
+        );
+
+    }
+
+}
--- src/Modules/Cleofy/Templates/environment-invoke-build-server.php
+++ src/Modules/Cleofy/Templates/environment-invoke-build-server.php
@@ -19,22 +19,15 @@ class AutoPilotConfigured extends AutoPilot {
                     array( "log-message" => "Lets begin invoking Configuration of a build server on environment <%tpl.php%>env_name</%tpl.php%>"),
                 ) ),
                 array ( "Logging" => array( "log" =>
-                    array( "log-message" => "First lets SFTP over our PHP and Git setup"),
+                    array( "log-message" => "First lets SFTP over our Build Server CM Autopilot"),
                 ) ),
                 array ( "SFTP" => array( "copy" =>
-                    array("original-file" => "<%tpl.php%>cleo_home</%tpl.php%>/php-git-setup.php" ),
-                    array("target-file" => "<%tpl.php%>tmp_dir</%tpl.php%>/php-git-setup.php" ),
+                    array("original-file" => "build/config/cleopatra/autopilots/<%tpl.php%>env_name</%tpl.php%>-cm-build-server.php" ),
+                    array("target-file" => "/tmp/<%tpl.php%>env_name</%tpl.php%>-cm-build-server.php" ),
                     array("environment-name" => "<%tpl.php%>env_name</%tpl.php%>" ),
                 ) , ) ,
                 array ( "Logging" => array( "log" =>
-                    array( "log-message" => "Lets begin invoking Configuration of a build server on environment <%tpl.php%>env_name</%tpl.php%>"),
-                ) ),
-                array ( "Invoke" => array( "data" =>
-                    array("ssh-data" => $this->setSSHData() ),
-                    array("environment-name" => "<%tpl.php%>env_name</%tpl.php%>" ),
-                ) , ) ,
-                array ( "Logging" => array( "log" =>
-                    array( "log-message" => "Lets begin invoking Configuration of a build server on environment <%tpl.php%>env_name</%tpl.php%>"),
+                    array( "log-message" => "Lets run that autopilot"),
                 ) ),
                 array ( "Invoke" => array( "data" =>
                     array("ssh-data" => $this->setSSHData() ),
@@ -47,26 +40,9 @@ class AutoPilotConfigured extends AutoPilot {
 
     }
 
-    private function prepGitPHP() {
-        $sshData = <<<"SSHDATA"
-sudo apt-get install git -y
-sudo apt-get install php5 -y
-cd <%tpl.php%>tmp_dir</%tpl.php%>
-git clone <%tpl.php%>repo_url</%tpl.php%> <%tpl.php%>project_name</%tpl.php%>
-cd <%tpl.php%>project_name</%tpl.php%>
-sudo cleopatra autopilot execute build/config/cleopatra/autopilots/<%tpl.php%>env_name</%tpl.php%>-cm-build-server.php
-SSHDATA;
-        return $sshData ;
-    }
-
     private function setSSHData() {
         $sshData = <<<"SSHDATA"
-sudo apt-get install git -y
-sudo apt-get install php5 -y
-cd <%tpl.php%>tmp_dir</%tpl.php%>
-git clone <%tpl.php%>repo_url</%tpl.php%> <%tpl.php%>project_name</%tpl.php%>
-cd <%tpl.php%>project_name</%tpl.php%>
-sudo cleopatra autopilot execute build/config/cleopatra/autopilots/<%tpl.php%>env_name</%tpl.php%>-cm-build-server.php
+sudo cleopatra autopilot execute /tmp/<%tpl.php%>env_name</%tpl.php%>-cm-build-server.php
 SSHDATA;
         return $sshData ;
     }
