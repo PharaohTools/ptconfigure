18a765bc9f5111a7f5ccd70a6ea2d6dbdf1c3028
David Amanshia <phpengine@hotmail.co.uk>
2014-04-24 09:37:58 +0100

    SSH Encrypt will now work with default settings

--- src/Modules/SshEncrypt/Model/SshEncryptAllLinux.php
+++ src/Modules/SshEncrypt/Model/SshEncryptAllLinux.php
@@ -25,57 +25,56 @@ class SshEncryptAllLinux extends BaseLinuxApp {
         $this->autopilotDefiner = "SshEncrypt";
         $this->installCommands = array(
             array("method"=> array("object" => $this, "method" => "askForRawPrivateKey", "params" => array()) ),
-            array("method"=> array("object" => $this, "method" => "askForRawPublicKey", "params" => array()) ),
+            array("method"=> array("object" => $this, "method" => "askForEncryptionKey", "params" => array()) ),
             array("method"=> array("object" => $this, "method" => "askForTargetPrivateKey", "params" => array()) ),
-            array("method"=> array("object" => $this, "method" => "askForTargetPublicKey", "params" => array()) )
+            array("method"=> array("object" => $this, "method" => "encryptKey", "params" => array()) )
         );
         $this->uninstallCommands = array(
-            array("method"=> array("object" => $this, "method" => "askForRawPrivateKey", "params" => array()) ),
-            array("method"=> array("object" => $this, "method" => "askForRawPublicKey", "params" => array()) ),
-            array("method"=> array("object" => $this, "method" => "askForTargetPrivateKey", "params" => array()) ),
-            array("method"=> array("object" => $this, "method" => "askForTargetPublicKey", "params" => array()) )
+            // array("method"=> array("object" => $this, "method" => "askForRawPrivateKey", "params" => array()) ),
+            // array("method"=> array("object" => $this, "method" => "askForRawPublicKey", "params" => array()) ),
+            // array("method"=> array("object" => $this, "method" => "askForTargetPrivateKey", "params" => array()) ),
+            // array("method"=> array("object" => $this, "method" => "askForTargetPublicKey", "params" => array()) )
         );
         $this->programDataFolder = "";
         $this->programNameMachine = "sshencrypt"; // command and app dir name
         $this->programNameFriendly = "SSH Encrypt!"; // 12 chars
-        $this->programNameInstaller = "Encrypt an SSH Key Pair within a project";
+        $this->programNameInstaller = "Encrypt an SSH Private key into a project";
         $this->initialize();
     }
 
-    public function askForRawPrivateKey($autoPilot=null){
-        if (isset($autoPilot) &&
-            $autoPilot->{$this->autopilotDefiner."MysqlNewAdminUser"} ) {
-            $this->rawPrivKey = $autoPilot->{$this->autopilotDefiner."MysqlNewAdminUser"}; }
+    // @todo this isnt finished below
+    public function askForRawPrivateKey() {
+        if (isset($this->params["raw-private-key-data"])  ) {
+            $this->rawPrivKey = $this->params["raw-private-key-data"] ; }
+        else if (isset($this->params["raw-private-key-path"])  ) {
+            $this->rawPrivKey = file_get_contents($this->params["raw-private-key-path"]) ; }
         else {
-            $question = "Enter MySQL New Admin User:";
+            $question = "Enter Private Key Path:";
             $this->rawPrivKey = self::askForInput($question, true); }
     }
 
-    public function askForRawPublicKey($autoPilot=null){
-        if (isset($autoPilot) &&
-            $autoPilot->{$this->autopilotDefiner."MysqlNewAdminPass"} ) {
-            $this->rawPublicKey = $autoPilot->{$this->autopilotDefiner."MysqlNewAdminPass"}; }
-        else {
-            $question = "Enter MySQL New Admin Pass:";
-            $this->rawPublicKey = self::askForInput($question, true); }
-    }
-
-    public function askForTargetPrivateKey($autoPilot=null){
-        if (isset($autoPilot) &&
-            $autoPilot->{$this->autopilotDefiner."MysqlRootUser"} ) {
-            $this->targetPrivateKey = $autoPilot->{$this->autopilotDefiner."MysqlRootUser"}; }
-        else {
-            $question = "Enter MySQL Root User:";
+    public function askForTargetPrivateKey() {
+        if (isset($this->params["target-private-key"])  ) {
+            $this->targetPrivateKey = $this->params["target-private-key"] ;
+            return ; }
+        if (isset($this->params["guess"]) && isset($this->params["raw-private-key-path"])) {
+                //@todo a check if the file doesn't exist
+                $dirToStrip = dirname($this->params["raw-private-key-path"]) ;
+                $filename = str_replace($dirToStrip, "", $this->params["raw-private-key-path"]) ;
+                $encDir = "build/config/cleopatra/SSH/keys/private/encrypted/" ;
+                mkdir($encDir, 0777, true) ;
+                $this->targetPrivateKey = $encDir.$filename ;
+                return ; }
+        if (!isset($this->params["target-private-key"]) && !isset($this->params["guess"])) {
+            $question = "Enter Target Encrypted Private Key Path:";
             $this->targetPrivateKey = self::askForInput($question, true); }
     }
 
-    public function askForTargetPublicKey($autoPilot=null){
-        if (isset($autoPilot) &&
-            $autoPilot->{$this->autopilotDefiner."MysqlRootPass"} ) {
-            $this->targetPublicKey = $autoPilot->{$this->autopilotDefiner."MysqlRootPass"}; }
-        else {
-            $question = "Enter MySQL Root Pass:";
-            $this->targetPublicKey = self::askForInput($question, true); }
+    public function encryptKey() {
+        $keyData = file_get_contents($this->targetPrivateKey) ;
+        $encryptionFactory = new \Model\Encryption();
+        $encryption = $encryptionFactory->getModel($this->params) ;
+        $encryption->encrypt($keyData, $this->params["target-private-key"]) ;
     }
 
 }
