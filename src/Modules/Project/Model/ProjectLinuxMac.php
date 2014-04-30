8b75f4ea4ae039471f03271e272930540e4853d3
David Amanshia <phpengine@hotmail.co.uk>
2014-04-30 23:50:04 +0100

    build bits have gone from project to builderfy

--- src/Modules/Project/Model/ProjectLinuxMac.php
+++ src/Modules/Project/Model/ProjectLinuxMac.php
@@ -14,12 +14,7 @@ class ProjectLinuxMac extends Base  {
     // Model Group
     public $modelGroup = array("Default") ;
 
-    private $jenkinsOriginalJobFolderName;
-    private $jenkinsNewJobFolderName;
-    private $jenkinsFSFolder = "/var/lib/jenkins";
-    private $tempFolder = "/projectTemp/";
     private $projectContainerDirectory;
-    private $newJobName ;
 
     public function askWhetherToInitializeProject() {
         return $this->performProjectInitialize();
@@ -27,10 +22,6 @@ class ProjectLinuxMac extends Base  {
 
     public function askWhetherToInitializeProjectContainer() {
         return $this->performProjectContainerInitialize();
-    }
-
-    public function askWhetherToInstallBuildInProject() {
-        return $this->performInstallBuildInProject();
     }
 
     private function performProjectInitialize() {
@@ -52,29 +43,9 @@ class ProjectLinuxMac extends Base  {
         return "Seems Fine...";
     }
 
-    private function performInstallBuildInProject() {
-        $projInit = $this->askForProjModifyToScreen("To Install Build");
-        if ($projInit!=true) { return false; }
-        if ( !$this->checkIsPharoahProject() ) { return "No papyrusfile found. Try: \ndapperstrano proj init\n"; }
-        $projInit = $this->askForProjBuildInstallToScreen();
-        if (!$projInit) { return false; }
-        $this->jenkinsOriginalJobFolderName = $this->selectJenkinsFolderInProject();
-        $this->jenkinsFSFolder = $this->selectJenkinsFolderInFileSystem();
-        $this->newJobName = $this->askForTargetJobName() ;
-        $this->getNewJobFolderIfJenkinsFolderExistsInFileSystem();
-        $this->tryToCreateTempFolder();
-        $this->projectBuildInstall();
-        $this->changeNewJenkinsJobFolderPermissions();
-        $this->changeNewJenkinsJobFolderOwner();
-        $this->changeNewJenkinsJobFolderGroup();
-        $this->ctlJenkins("stop");
-        $this->ctlJenkins("start");
-        return "Seems Fine...";
-    }
-
-    private function askForProjModifyToScreen() {
+    private function askForProjModifyToScreen($extra = "") {
         if (isset($this->params["yes"]) && $this->params["yes"]==true) { return true ; }
-        $question = 'Do you want to Modify Project Settings?';
+        $question = 'Do you want to Modify Project Settings '.$extra.'?';
         return self::askYesOrNo($question);
     }
 
@@ -100,29 +71,6 @@ class ProjectLinuxMac extends Base  {
         if (isset($this->params["proj-container"])) { return $this->params["proj-container"] ; }
         $question = 'What is your Project Container directory?';
         return self::askForInput($question, true);
-    }
-
-    private function askForTargetJobName() {
-        if (isset($this->params["target-job-name"])) { return $this->params["target-job-name"] ; }
-        $question = 'What is the target Job Name?';
-        return self::askForInput($question, true);
-    }
-
-    private function selectJenkinsFolderInFileSystem(){
-        if (isset($this->params["jenkins-fs-dir"])) { return $this->params["jenkins-fs-dir"] ; }
-        $question = 'What is your Jenkins home?';
-        if ($this->detectJenkinsHomeFolderExistence()) {
-            if (isset($this->params["guess"])) { return $this->jenkinsFSFolder ; }
-            $question .= ' Found "'. $this->jenkinsFSFolder.'" - use this?';
-            $input = self::askForInput($question);
-            return ($input=="") ? $this->jenkinsFSFolder : $input ;  }
-        return self::askForInput($question, true);
-    }
-
-    private function askForProjBuildInstallToScreen() {
-        if (isset($this->params["yes"]) && $this->params["yes"]==true) { return true ; }
-        $question = 'Do you want to install the Jenkins build for this project?';
-        return self::askYesOrNo($question);
     }
 
     private function projectInitialize() {
@@ -151,79 +99,9 @@ class ProjectLinuxMac extends Base  {
           return file_exists($dir.DIRECTORY_SEPARATOR.'papyrusfile'); }
     }
 
-    private function selectJenkinsFolderInProject(){
-        if (isset($this->params["original-build-dir"])) { return $this->params["original-build-dir"] ; }
-        $projBuildsDir = getcwd().'/'."build/config/jenkins/projects" ;
-        $results = (file_exists($projBuildsDir)) ? scandir($projBuildsDir) : array() ;
-        $availableDirs = array() ;
-        $question = "Please Choose Jenkins Directory in Project:\n";
-        $i=0;
-        foreach ($results as $result) {
-            if ($result === '.' or $result === '..') continue;
-            if (is_dir("build/config/jenkins" . '/' . $result)) {
-                $availableDirs[] = $result;
-                $question .= "($i) $result\n";
-                $i++; } }
-        $validChoice = false;
-        $i=0;
-        if (count($availableDirs)>0) {
-            while ($validChoice == false) {
-                if ($i==1) { $question = "That's not a valid option, ".$question; }
-                $input = self::askForDigit($question) ;
-                if ( array_key_exists($input, $availableDirs) ){
-                    $validChoice = true;}
-                $i++; }
-            return $projBuildsDir.'/'.$availableDirs[$input] ; }
-        else {
-            echo "No options available, enter path:\n" ;
-            return $this->askForInput($question, true) ; }
-    }
-
-    private function getNewJobFolderIfJenkinsFolderExistsInFileSystem(){
-        if ( $this->detectJenkinsJobExistence() ) {
-            if (isset($this->params["new-job-dir"])) { return $this->params["new-job-dir"] ; }
-            $question = 'Job "'.$this->jenkinsOriginalJobFolderName.'" already exists. Enter new Job folder.';
-            $this->jenkinsNewJobFolderName = self::askForInput($question, true); }
-        else { $this->jenkinsNewJobFolderName = $this->jenkinsOriginalJobFolderName ; }
-    }
-
     private function tryToCreateTempFolder(){
         if (!file_exists('/tmp/'.$this->tempFolder)) {
           mkdir ('/tmp/'.$this->tempFolder, 0777, true);}
-    }
-
-    private function projectBuildInstall(){
-        $command  = 'sudo cp -r '.$this->jenkinsOriginalJobFolderName.' ' ;
-        $command .= $this->jenkinsFSFolder.'/jobs/'.$this->jenkinsNewJobFolderName;
-        self::executeAndOutput($command, "Copying Files...");
-    }
-
-    private function changeNewJenkinsJobFolderPermissions(){
-        $command  = 'sudo chmod -R 755 '.$this->jenkinsFSFolder.'/jobs/'.$this->jenkinsNewJobFolderName;
-        self::executeAndOutput($command, "Changing Folder Permissions...");
-    }
-
-    private function changeNewJenkinsJobFolderOwner(){
-        $command  = 'sudo chown -R jenkins '.$this->jenkinsFSFolder.'/jobs/'.$this->jenkinsNewJobFolderName;
-        self::executeAndOutput($command, "Changing Folder Owner...");
-    }
-
-    private function changeNewJenkinsJobFolderGroup(){
-        $command  = 'sudo chgrp -R jenkins '.$this->jenkinsFSFolder.'/jobs/'.$this->jenkinsNewJobFolderName;
-        self::executeAndOutput($command, "Changing Folder Group...");
-    }
-
-    private function ctlJenkins($action){
-        $command  = 'sudo service jenkins '.$action;
-        self::executeAndOutput($command, ucfirst($action)."ing Jenkins...");
-    }
-
-    private function detectJenkinsHomeFolderExistence(){
-        return file_exists($this->jenkinsFSFolder);
-    }
-
-    private function detectJenkinsJobExistence(){
-        return file_exists($this->jenkinsFSFolder.'/jobs/'.$this->jenkinsOriginalJobFolderName);
     }
 
 }
