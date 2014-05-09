e3c42faeb6fcbde0878bfbf1bff567775d5babae
David Amanshia <phpengine@hotmail.co.uk>
2014-05-09 19:42:59 +0100

    modified auto to correct array structure

--- src/Modules/Cleofy/Templates/environment-invoke-git.php
+++ src/Modules/Cleofy/Templates/environment-invoke-git.php
@@ -13,30 +13,30 @@ class AutoPilotConfigured extends AutoPilot {
     /* Steps */
     private function setSteps() {
 
-    $this->steps =
+        $this->steps =
         array(
-            array ( "Logging" => array( "log" =>
-                array( "log-message" => "Lets begin invoking Configuration of a Git SCM Server on environment <%tpl.php%>env_name</%tpl.php%>"),
-            ) ),
-            array ( "Logging" => array( "log" =>
-                array( "log-message" => "First lets SFTP over our Git SCM Server CM Autopilot"),
-            ) ),
+            array ( "Logging" => array( "log" => array(
+                "log-message" => "Lets begin invoking Configuration of a Git SCM Server on environment <%tpl.php%>env_name</%tpl.php%>",
+            ), ), ),
+            array ( "Logging" => array( "log" => array(
+                "log-message" => "First lets SFTP over our Git SCM Server CM Autopilot",
+            ), ), ),
             array ( "SFTP" => array( "put" =>  array(
-                array("original-file" => "build/config/cleopatra/autopilots/<%tpl.php%>env_name</%tpl.php%>-cm-git.php" ),
-                array("target-file" => "/tmp/<%tpl.php%>env_name</%tpl.php%>-cm-git.php" ),
-                array("environment-name" => "<%tpl.php%>env_name</%tpl.php%>" ),
-            ), ) , ) ,
-            array ( "Logging" => array( "log" =>
-                array( "log-message" => "Lets run that autopilot"),
-            ) ),
-            array ( "Invoke" => array( "data" =>
-                array("guess" => true ),
-                array("ssh-data" => $this->setSSHData() ),
-                array("environment-name" => "<%tpl.php%>env_name</%tpl.php%>" ),
-            ) , ) ,
-            array ( "Logging" => array( "log" =>
-                array( "log-message" => "Invoking Git SCM Server on environment <%tpl.php%>env_name</%tpl.php%> complete"),
-            ) ),
+                "source" => "build/config/cleopatra/autopilots/<%tpl.php%>env_name</%tpl.php%>-cm-git.php",
+                "target" => "/tmp/<%tpl.php%>env_name</%tpl.php%>-cm-git.php",
+                "environment-name" => "<%tpl.php%>env_name</%tpl.php%>",
+            ), ), ),
+            array ( "Logging" => array( "log" =>array(
+                "log-message" => "Lets run that autopilot"
+            ), ), ),
+            array ( "Invoke" => array( "data" => array(
+                "guess" => true,
+                "ssh-data" => $this->setSSHData(),
+                "environment-name" => "<%tpl.php%>env_name</%tpl.php%>",
+            ), ), ),
+            array ( "Logging" => array( "log" => array(
+                "log-message" => "Invoking Git SCM Server on environment <%tpl.php%>env_name</%tpl.php%> complete",
+            ), ), ),
         );
 
     }
