5bbdf1589f358c02a7da74e908ad1d87f6917152
David Amanshia <phpengine@hotmail.co.uk>
2014-05-09 15:01:50 +0100

    updated the cleofy templates

--- src/Modules/Cleofy/Templates/environment-cm-git.php
+++ src/Modules/Cleofy/Templates/environment-cm-git.php
@@ -5,7 +5,7 @@ Namespace Core ;
 class AutoPilotConfigured extends AutoPilot {
 
     public $steps ;
-    protected $myUser ;
+//    protected $myUser ;
 
     public function __construct() {
         $this->setSteps();
@@ -18,27 +18,27 @@ class AutoPilotConfigured extends AutoPilot {
             array(
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets begin Configuration of a Git SCM Server on environment <%tpl.php%>env_name</%tpl.php%>"),),),
 
-                // Install Keys - Bastion Public Key, DevOps Public Key, Bastion Private Key
-                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure our Bastion Public Key is installed" ),),),
-                array ( "SshKeyInstall" => array( "file" =>
-                    array("public-key-file" => "build/config/cleopatra/SSH/keys/public/raw/bastion"),
-                    array("user-name" => "{$this->myUser}"),),),
-                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure our DevOps Public Key is installed" ),),),
-                array ( "SshKeyInstall" => array( "file" =>
-                    array("public-key-file" => "build/config/cleopatra/SSH/keys/public/raw/bastion"),
-                    array("user-name" => "{$this->myUser}"),),),
-                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure our Bastion Private Key is installed" ),),),
-                // @todo if this is run over ssh from another machine (DevOps laptop), the encryption key never needs to be on the target
-                // box might not even need encryption... look at this
-                array ( "Encryption" => array( "uninstall" =>
-                    array("encrypted-data" => "build/config/cleopatra/SSH/keys/private/encrypted/bastion"),
-                    array("encryption-target-file" => "{$this->myUserHome}/.ssh/bastion"),
-                    // @todo the key thing
-                    array("encryption-key" => "{$this->myUser}"),
-                    array("encryption-file-permissions" => ""),
-                    array("encryption-file-owner" => ""),
-                    array("encryption-group" => ""),
-                ),),
+//                // Install Keys - Bastion Public Key, DevOps Public Key, Bastion Private Key
+//                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure our Bastion Public Key is installed" ),),),
+//                array ( "SshKeyInstall" => array( "file" =>
+//                    array("public-key-file" => "build/config/cleopatra/SSH/keys/public/raw/bastion"),
+//                    array("user-name" => "{$this->myUser}"),),),
+//                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure our DevOps Public Key is installed" ),),),
+//                array ( "SshKeyInstall" => array( "file" =>
+//                    array("public-key-file" => "build/config/cleopatra/SSH/keys/public/raw/bastion"),
+//                    array("user-name" => "{$this->myUser}"),),),
+//                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure our Bastion Private Key is installed" ),),),
+//                // @todo if this is run over ssh from another machine (DevOps laptop), the encryption key never needs to be on the target
+//                // box might not even need encryption... look at this
+//                array ( "Encryption" => array( "uninstall" =>
+//                    array("encrypted-data" => "build/config/cleopatra/SSH/keys/private/encrypted/bastion"),
+//                    array("encryption-target-file" => "{$this->myUserHome}/.ssh/bastion"),
+//                    // @todo the key thing
+//                    array("encryption-key" => "{$this->myUser}"),
+//                    array("encryption-file-permissions" => ""),
+//                    array("encryption-file-owner" => ""),
+//                    array("encryption-group" => ""),
+//                ),),
 
                 // SSH Hardening
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure we have some SSH Security" ),),),
@@ -66,17 +66,25 @@ class AutoPilotConfigured extends AutoPilot {
 
                 // Restart Apache for new modules
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets restart Apache for our PHP and Apache Modules" ),),),
-                array ( "RunCommand" => array( "restart" =>
+                array ( "RunCommand" => array( "install" =>
                     array("guess" => true),
                     array("username" => "root"),
                     array("command" => "dapperstrano ApacheCtl restart --yes"),
                     array("background" => "") ) ),
 
-                // Git Bucket
-                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure GitLab is installed" ),),),
+                // Install Git Bucket
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure GitBucket is installed" ),),),
                 array ( "GitBucket" => array( "ensure" => array(
                     array("guess" => true),
                 ),),),
+
+                // Start Git Bucket
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets start GitBucket" ),),),
+                array ( "RunCommand" => array( "install" =>
+                    array("guess" => true),
+                    array("username" => "root"),
+                    array("command" => "gitbucket"),
+                    array("background" => "true") ) ),
 
                 array ( "Logging" => array( "log" => array( "log-message" => "Configuring a Git SCM server on environment <%tpl.php%>env_name</%tpl.php%> complete"),),),
 
