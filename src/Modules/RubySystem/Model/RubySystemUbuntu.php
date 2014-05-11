5f3f989fdaa2b2ee080ab3e3ca72ba61800c1383
David Amanshia <phpengine@hotmail.co.uk>
2014-05-11 19:12:43 +0100

    Ruby system versioning

--- src/Modules/RubySystem/Model/RubySystemUbuntu.php
+++ src/Modules/RubySystem/Model/RubySystemUbuntu.php
@@ -18,16 +18,35 @@ class RubySystemUbuntu extends BaseLinuxApp {
         parent::__construct($params);
         $this->autopilotDefiner = "RubySystem";
         $this->installCommands = array(
-            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array( "Apt", array("ruby") ) ) ),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array( "Apt", "ruby1.9.1" ) ) ),
         );
         $this->uninstallCommands = array(
-            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array( "Apt", array("ruby") ) ) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array( "Apt", "ruby1.9.1" ) ) ),
         );
         $this->programDataFolder = "/opt/rubysystem";
         $this->programNameMachine = "ruby"; // command and app dir name
         $this->programNameFriendly = "Ruby System!"; // 12 chars
         $this->programNameInstaller = "Ruby, System Wide";
+        $this->statusCommand = "ruby -v" ;
+        $this->versionInstalledCommand = "sudo apt-cache policy ruby1.9.1" ;
+        $this->versionRecommendedCommand = "sudo apt-cache policy ruby1.9.1" ;
+        $this->versionLatestCommand = "sudo apt-cache policy ruby1.9.1" ;
         $this->initialize();
+    }
+
+    public function versionInstalledCommandTrimmer($text) {
+        $done = substr($text, 24, 18) ;
+        return $done ;
+    }
+
+    public function versionLatestCommandTrimmer($text) {
+        $done = substr($text, 56, 18) ;
+        return $done ;
+    }
+
+    public function versionRecommendedCommandTrimmer($text) {
+        $done = substr($text, 56, 18) ;
+        return $done ;
     }
 
 }
