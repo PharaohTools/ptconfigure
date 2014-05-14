8fcd6046e34989c05c3c72e76a9d40c753a388a5
David Amanshia <phpengine@hotmail.co.uk>
2014-05-14 21:32:22 +0100

    invoke vie

--- src/Modules/Invoke/Views/InvokeView.tpl.php
+++ src/Modules/Invoke/Views/InvokeView.tpl.php
@@ -3,19 +3,19 @@
 if ($pageVars["route"]["action"]=="data") {
 ?>
 
-Invoke SSH Data Result: <?php echo $pageVars["shlResult"] ; ?>
+Invoke SSH Data Result: <?php echo ($pageVars["shlResult"] == true) ? "Success" : "Failure" ; ?>
 
 <?php
 
 } else if ($pageVars["route"]["action"]=="cli") {
     ?>
-Shell Result: <?php echo $pageVars["shlResult"]  ; ?>
+Shell Result: <?php echo ($pageVars["shlResult"] == true) ? "Success" : "Failure" ; ?>
 
 Invoke Shell Cli
 <?php
 } else if ($pageVars["route"]["action"]=="script") {
     ?>
-Shell Result: <?php echo $pageVars["shlResult"]  ; ?>
+Shell Result: <?php echo ($pageVars["shlResult"] == true) ? "Success" : "Failure" ; ?>
 
 Invoke Script
 <?php
