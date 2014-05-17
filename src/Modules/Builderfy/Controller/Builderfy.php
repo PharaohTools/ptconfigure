884377cada6e504b6364bb07415a391ed1ddd9a3
David Amanshia <phpengine@hotmail.co.uk>
2014-05-17 19:49:09 +0100

    Builderfy Controller allows itself to be overridden by other modules which provide a route and a controller method named builderfyExecute

--- src/Modules/Builderfy/Controller/Builderfy.php
+++ src/Modules/Builderfy/Controller/Builderfy.php
@@ -6,13 +6,17 @@ class Builderfy extends Base {
 
     public function execute($pageVars) {
 
+        $action = $pageVars["route"]["action"];
+
+        $otherModuleExecutor = $this->getExecutorForAction($action);
+        if (!is_null($otherModuleExecutor)) {
+            return $otherModuleExecutor->executeBuilderfy($pageVars) ; }
+
         $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
         // if we don't have an object, its an array of errors
         if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
         $isDefaultAction = self::checkDefaultActions($pageVars, array(), $thisModel) ;
         if ( is_array($isDefaultAction) ) { return $isDefaultAction; }
-
-        $action = $pageVars["route"]["action"];
 
         if ($action == "developer") {
             $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "Developer") ;
@@ -38,8 +42,19 @@ class Builderfy extends Base {
             $this->content["result2"] = $thisModel->result;
             return array ("type"=>"view", "view"=>"builderfy", "pageVars"=>$this->content); }
 
-        $this->content["messages"][] = "Invalid Project Action";
+        $this->content["messages"][] = "Invalid Builderfy Action";
         return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content);
+    }
+
+    protected function getExecutorForAction($action) {
+        $controllers = \Core\AutoLoader::getAllControllers() ;
+        foreach ($controllers as $controller) {
+            if (method_exists($controller, "executeBuilderfy"))
+                $info = \Core\AutoLoader::getSingleInfoObject(substr(get_class($controller), 11)) ;
+                $myBuilderfyRoutes = (isset($info) && method_exists($info, "builderfyActions")) ? $info->builderfyActions() : array() ;
+                if (in_array($action, $myBuilderfyRoutes)) {
+                    return $controller ; } }
+        return null ;
     }
 
 }
