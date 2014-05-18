5fb8e7fadec7d72deb2d5f2837d89dc29899cec5
David Amanshia <phpengine@hotmail.co.uk>
2014-05-18 12:27:20 +0100

    work done to Drupal module overriding controllers and extending models of builder, dapper and db configure

---
+++ src/Modules/Drupal/Model/DBConfigureDataDrupal70.php
@@ -0,0 +1,63 @@
+<?php
+
+Namespace Model;
+
+class DBConfigureDataDrupal70 extends DBConfigureAllOS {
+
+    // Compatibility
+    public $os = array("Linux", "Darwin") ;
+    public $linuxType = array("any") ;
+    public $distros = array("any") ;
+    public $versions = array("any") ;
+    public $architectures = array("any") ;
+
+    // Model Group
+    public $modelGroup = array("Drupal7") ;
+
+    // @todo this assumes src directory
+    private $settingsFileLocation = 'src/sites/default'; // no trail slash
+    private $settingsFileName = 'settings.php';
+    private $settingsFileReplacements ;
+    private $extraConfigFileReplacements ;
+    private $extraConfigFiles = array('build/config/phpunit/bootstrap.php'); // extra files requiring db config
+
+    public function __construct(){
+        $this->setReplacements();
+        $this->setExtraConfigReplacements();
+    }
+
+    public function setPlatformVars() {
+        $this->platformVars = new \Model\DBConfigureDataGCFW2();
+        return;
+    }
+
+    public function getProperty($property) {
+        return $this->$property;
+    }
+
+    public function __call($var1, $var2){
+        return null();
+    }
+
+
+    private function setReplacements(){
+
+        $this->settingsFileReplacements = array(
+        "'database'"=>"      'database' => '****DB NAME****',",
+        "'username'"=>"      'username' => '****DB USER****',",
+        "'password'"=>"      'password' => '****DB PASS****',",
+        "'host'"=>"      'host' => '****DB HOST****'," );
+
+    }
+
+    private function setExtraConfigReplacements(){
+
+        $this->extraConfigFileReplacements = array(
+            '$bootstrapDbName =' => '$bootstrapDbName = "****DB NAME****" ; ',
+            '$bootstrapDbUser =' => '$this->dbUser = "****DB USER****" ; ',
+            '$bootstrapDbPass =' => '$this->dbPass = "****DB PASS****" ; ',
+            '$bootstrapDbHost =' => '$this->dbHost = "****DB HOST****" ; ');
+
+    }
+
+}
