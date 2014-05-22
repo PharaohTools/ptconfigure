ea46e4adb1fbb231db6fd61a00b2c19938b5bedc
Golden Dave <dave@theboss.com>
2014-05-22 20:12:17 +0100

    added clofy generic templates

---
+++ src/Modules/Cleofy/Model/CleofyGenericAutosUbuntu.php
@@ -0,0 +1,89 @@
+<?php
+
+Namespace Model;
+
+class BoxifyGenericAutosUbuntu extends BaseLinuxApp {
+
+    // Compatibility
+    public $os = array("any") ;
+    public $linuxType = array("any") ;
+    public $distros = array("any") ;
+    public $versions = array("any") ;
+    public $architectures = array("any") ;
+
+    // Model Group
+    public $modelGroup = array("GenericAutos") ;
+
+    protected $templateGroup;
+    protected $templateGroupsToDirs;
+    protected $destination;
+
+    protected $actionsToMethods =
+        array(
+            "install-generic-autopilots" => "performGenericAutopilotInstall",
+        ) ;
+
+    public function __construct($params) {
+        parent::__construct($params);
+        $this->autopilotDefiner = "Boxify";
+        $this->programNameMachine = "boxify"; // command and app dir name
+        $this->programNameFriendly = "Boxify!"; // 12 chars
+        $this->programNameInstaller = "Boxify your Environments";
+        $this->initialize();
+    }
+
+    public function performGenericAutopilotInstall() {
+        $this->setTemplateGroupsToDirs();
+        $this->setTemplateGroup();
+        $this->setDestination();
+        return $this->doCopy() ;
+    }
+
+    public function setTemplateGroupsToDirs() {
+        $dir = str_replace("Model", "", __DIR__) ;
+        $dir = $dir.'Autopilots/' ;
+        $this->templateGroupsToDirs = array(
+            "tiny" => "{$dir}Tiny",
+            "medium" => "{$dir}Medium",
+        );
+    }
+
+    public function setTemplateGroup($templateGroup = null) {
+        if (isset($templateGroup)) {
+            $this->templateGroup = $templateGroup; }
+        else if (isset($this->params["templategroup"])) {
+            $this->templateGroup = $this->params["templategroup"]; }
+        else if (isset($this->params["template-group"])) {
+            $this->templateGroup = $this->params["template-group"]; }
+        else {
+            $this->templateGroup = self::askForArrayOption("Enter Template Group:",array("tiny", "medium"), true) ; }
+    }
+
+    public function setDestination($destination = null) {
+        if (isset($destination)) {
+            $this->destination = $destination; }
+        else if (isset($this->params["destinationdir"])) {
+            $this->destination = $this->params["destinationdir"]; }
+        else if (isset($this->params["destination-dir"])) {
+            $this->destination = $this->params["destination-dir"]; }
+        else if (isset($this->params["guess"])) {
+            $defaultdir = getcwd()."/build/config/cleopatra/boxify/autopilots/" ;
+            if (!file_exists($defaultdir)) { mkdir($defaultdir, 0777, true) ; }  ;
+            $this->destination = $defaultdir ; }
+        else {
+            $this->destination = self::askForInput("Enter Destination Directory:", true); }
+    }
+
+    protected function doCopy() {
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params);
+        $source = $this->templateGroupsToDirs[$this->templateGroup] ;
+        $target = $this->destination ;
+        $logging->log("Performing file copy from $source to $target") ;
+        // @todo php cannot do a recursive copy so change the copy module to one of these
+        $result = $this->executeAndGetReturnCode("cp -r $source $target") ;
+        var_dump($result) ;
+        return $result ;
+    }
+
+}
---
+++ src/Modules/Cleofy/Templates/Generic/Medium/cleofy-cm-medium-all-parallel.php
@@ -0,0 +1,45 @@
+<?php
+
+Namespace Core ;
+
+class AutoPilotConfigured extends AutoPilot {
+
+    public $steps ;
+
+    public function __construct() {
+        $this->setSteps();
+    }
+
+    /* Steps */
+    private function setSteps() {
+
+        include("settings.php") ;
+
+        $this->steps =
+            array(
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets begin Configuration of a medium set of environments"),),),
+
+
+                // Bastion
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets add all Boxes and Environments in Parallel" ),),),
+                array ( "Parallax" => array("cli" => array(
+                    "command-1"  => "cleopatra autopilot execute {$parent}boxify-add-bastion.php",
+                    "command-2"  => "cleopatra autopilot execute {$parent}boxify-add-git.php",
+                    "command-3"  => "cleopatra autopilot execute {$parent}boxify-add-jenkins.php",
+                    "command-4"  => "cleopatra autopilot execute {$parent}boxify-add-staging-web-nodes.php",
+                    "command-5"  => "cleopatra autopilot execute {$parent}boxify-add-staging-load-balancer.php",
+                    "command-6"  => "cleopatra autopilot execute {$parent}boxify-add-staging-secondary-db.php",
+                    "command-7"  => "cleopatra autopilot execute {$parent}boxify-add-staging-primary-db.php",
+                    "command-8"  => "cleopatra autopilot execute {$parent}boxify-add-production-web-nodes.php",
+                    "command-9"  => "cleopatra autopilot execute {$parent}boxify-add-production-load-balancer.php",
+                    "command-10" => "cleopatra autopilot execute {$parent}boxify-add-production-secondary-db.php",
+                    "command-11" => "cleopatra autopilot execute {$parent}boxify-add-production-primary-db.php",
+                ),),),
+
+                array ( "Logging" => array( "log" => array( "log-message" => "Creating a medium set of environments complete"),),),
+
+            );
+
+    }
+
+}
