e393b15aaaa9305c771d498c0347ddefa6f3ac36
Golden Dave <dave@theboss.com>
2014-05-22 20:42:24 +0100

    cleofy gen autos view added

---
+++ src/Modules/Cleofy/Views/CleofyGenAutosView.tpl.php
@@ -0,0 +1,10 @@
+Cleofy Listing:
+--------------------------------------------
+
+<?php
+    $result_summary = ($pageVars["result"] == 0) ? "Success" : "Failure" ;
+    echo $result_summary."\n" ;
+?>
+
+------------------------------
+Cleofy Listing Finished
