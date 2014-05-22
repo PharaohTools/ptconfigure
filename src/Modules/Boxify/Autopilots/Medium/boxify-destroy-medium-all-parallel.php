1f1c833e85fe9e0c504045bc33b42c42e9b8644a
David Amanshia <phpengine@hotmail.co.uk>
2014-05-22 12:58:32 +0100

    begun destroy medium parallel

--- src/Modules/Boxify/Autopilots/Medium/boxify-destroy-medium-all-parallel.php
+++ src/Modules/Boxify/Autopilots/Medium/boxify-destroy-medium-all-parallel.php
@@ -15,10 +15,6 @@ class AutoPilotConfigured extends AutoPilot {
 
         include("settings.php") ;
 
-        $parent = (dirname(__FILE__))."/";
-
-        file_put_contents("/tmp/out", "cleopatra autopilot execute {$parent}boxify-add-bastion.php" )  ;
-
         $this->steps =
             array(
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets begin Configuration of a medium set of environments"),),),
@@ -27,17 +23,17 @@ class AutoPilotConfigured extends AutoPilot {
                 // Bastion
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets add all Boxes and Environments in Parallel" ),),),
                 array ( "Parallax" => array("cli" => array(
-                    "command-1"  => "cleopatra autopilot execute {$parent}boxify-add-bastion.php",
-                    "command-2"  => "cleopatra autopilot execute {$parent}boxify-add-git.php",
-                    "command-3"  => "cleopatra autopilot execute {$parent}boxify-add-jenkins.php",
-                    "command-4"  => "cleopatra autopilot execute {$parent}boxify-add-staging-web-nodes.php",
-                    "command-5"  => "cleopatra autopilot execute {$parent}boxify-add-staging-load-balancer.php",
-                    "command-6"  => "cleopatra autopilot execute {$parent}boxify-add-staging-secondary-db.php",
-                    "command-7"  => "cleopatra autopilot execute {$parent}boxify-add-staging-primary-db.php",
-                    "command-8"  => "cleopatra autopilot execute {$parent}boxify-add-production-web-nodes.php",
-                    "command-9"  => "cleopatra autopilot execute {$parent}boxify-add-production-load-balancer.php",
-                    "command-10" => "cleopatra autopilot execute {$parent}boxify-add-production-secondary-db.php",
-                    "command-11" => "cleopatra autopilot execute {$parent}boxify-add-production-primary-db.php",
+                    "command-1"  => "cleopatra autopilot execute {$parent}boxify-destroy-bastion.php",
+                    "command-2"  => "cleopatra autopilot execute {$parent}boxify-destroy-git.php",
+                    "command-3"  => "cleopatra autopilot execute {$parent}boxify-destroy-jenkins.php",
+                    "command-4"  => "cleopatra autopilot execute {$parent}boxify-destroy-staging-web-nodes.php",
+                    "command-5"  => "cleopatra autopilot execute {$parent}boxify-destroy-staging-load-balancer.php",
+                    "command-6"  => "cleopatra autopilot execute {$parent}boxify-destroy-staging-secondary-db.php",
+                    "command-7"  => "cleopatra autopilot execute {$parent}boxify-destroy-staging-primary-db.php",
+                    "command-8"  => "cleopatra autopilot execute {$parent}boxify-destroy-production-web-nodes.php",
+                    "command-9"  => "cleopatra autopilot execute {$parent}boxify-destroy-production-load-balancer.php",
+                    "command-10" => "cleopatra autopilot execute {$parent}boxify-destroy-production-secondary-db.php",
+                    "command-11" => "cleopatra autopilot execute {$parent}boxify-destroy-production-primary-db.php",
                 ),),),
 
                 array ( "Logging" => array( "log" => array( "log-message" => "Creating a medium set of environments complete"),),),
