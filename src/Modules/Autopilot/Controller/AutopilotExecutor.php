fb6b8e4d9e9e71b42bcb6026f39bc54304df1578
David Amanshia <phpengine@hotmail.co.uk>
2014-05-23 17:38:04 +0100

    autopilot executtor controller adds a newline to the output of different modules

--- src/Modules/Autopilot/Controller/AutopilotExecutor.php
+++ src/Modules/Autopilot/Controller/AutopilotExecutor.php
@@ -40,7 +40,7 @@ class AutopilotExecutor extends Base {
 //                )
 //            ) ;
 //        }
-            $dataFromThis .= $this->executeControl($currentControl, $params);
+            $dataFromThis .= $this->executeControl($currentControl, $params)."\n";
 
         }
 //        var_dump("dft: ",$dataFromThis) ;
