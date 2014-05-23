0096459ba0769790dd45f21a6a64a40024042a29
David Amanshia <phpengine@hotmail.co.uk>
2014-05-23 11:58:15 +0100

    bastion uses runcommand instead of a call to autopilot as that causes a loop. fix that or specify an error for it

--- src/Modules/Cleofy/Templates/Generic/Medium/cleofy-cm-bastion.php
+++ src/Modules/Cleofy/Templates/Generic/Medium/cleofy-cm-bastion.php
@@ -18,17 +18,41 @@ class AutoPilotConfigured extends AutoPilot {
         $this->steps =
             array(
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets Manage Configuration on the Bastion Environment" ),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "do pwd ".getcwd() ),),),
+                array ( "RunCommand" => array("install" => array(
+                    "command" => 'pwd',
+                    "run-as-user" => "",
+                    "background" => "",
+                ),),),
+//                array ( "Logging" => array( "log" => array( "log-message" => "do cd" ),),),
+//                array ( "RunCommand" => array("install" => array(
+//                    "command" => "cd $parent",
+//                    "run-as-user" => "",
+//                    "background" => "",
+//                ),),),
+//                array ( "Logging" => array( "log" => array( "log-message" => "do pwd" ),),),
+//                array ( "RunCommand" => array("install" => array(
+//                    "command" => 'pwd',
+//                    "run-as-user" => "",
+//                    "background" => "",
+//                ),),),
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets Prep Ubuntu on the Bastion Environment" ),),),
-                array ( "Autopilot" => array("execute" => array(
-                    "autopilot-file" => "{$parent}build/config/cleopatra/autopilots/medium-bastion-prep-ubuntu.php",
+                array ( "RunCommand" => array("install" => array(
+                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/autopilots/medium-bastion-prep-ubuntu.php"',
+                    "run-as-user" => "",
+                    "background" => "",
                 ),),),
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets Invoke Cleo and Dapper on the Bastion Environment" ),),),
-                array ( "Autopilot" => array("execute" => array(
-                    "autopilot-file" => "{$parent}build/config/cleopatra/autopilots/medium-bastion-invoke-cleo-dapper-new.php",
+                array ( "RunCommand" => array("install" => array(
+                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/autopilots/medium-bastion-invoke-cleo-dapper-new.php"',
+                    "run-as-user" => "",
+                    "background" => "",
                 ),),),
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets setup Bastion Box on the Bastion Environment" ),),),
-                array ( "Autopilot" => array("execute" => array(
-                    "autopilot-file" => "{$parent}build/config/cleopatra/autopilots/medium-bastion-invoke-bastion.php",
+                array ( "RunCommand" => array("install" => array(
+                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/autopilots/medium-bastion-invoke-bastion.php"',
+                    "run-as-user" => "",
+                    "background" => "",
                 ),),),
                 array ( "Logging" => array( "log" => array( "log-message" => "Managing Configuration on Bastion environment complete"),),),
             );
