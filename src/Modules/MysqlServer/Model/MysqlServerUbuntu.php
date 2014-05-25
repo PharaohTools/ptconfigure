fe056424a387062aaf5be5040cf610d31f507944
David Amanshia <phpengine@hotmail.co.uk>
2014-05-25 21:46:51 +0100

    rearranged the removed packages from mysql server, into two packageRemove statements instead of one

--- src/Modules/MysqlServer/Model/MysqlServerUbuntu.php
+++ src/Modules/MysqlServer/Model/MysqlServerUbuntu.php
@@ -26,7 +26,8 @@ class MysqlServerUbuntu extends BaseLinuxApp {
             array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "mysql-server")) ),
         );
         $this->uninstallCommands = array(
-            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", array("mysql-client", "mysql-server"))) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "mysql-client")) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "mysql-server")) ),
         );
         $this->programDataFolder = "/opt/MysqlServer"; // command and app dir name
         $this->programNameMachine = "mysqlserver"; // command and app dir name
