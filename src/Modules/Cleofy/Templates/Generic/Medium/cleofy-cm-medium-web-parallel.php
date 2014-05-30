ee66adc7d8c9528aae410d7b3eb12f99158b48a6
David Amanshia <phpengine@hotmail.co.uk>
2014-05-30 19:54:32 +0100

    added cleofy-cm-medium-web-parallel.php to generic templates for medium sized infra without dbs

---
+++ src/Modules/Cleofy/Templates/Generic/Medium/cleofy-cm-medium-web-parallel.php
@@ -0,0 +1,40 @@
+<?php
+
+Namespace Core ;
+
+class AutoPilotConfigured extends AutoPilot {
+
+    public $steps ;
+
+    public function __construct() {
+        $this->setSteps();
+    }
+
+    /* Steps */
+    private function setSteps() {
+
+        include("settings.php") ;
+
+        $this->steps =
+            array(
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets begin Configuration of a medium set of environments"),),),
+
+                // Bastion
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets add all Boxes and Environments in Parallel" ),),),
+                array ( "Parallax" => array("cli" => array(
+                    "command-1"  => "cleopatra autopilot execute --autopilot-file=\"{$parent}cleofy-cm-bastion.php\"",
+                    "command-2"  => "cleopatra autopilot execute --autopilot-file=\"{$parent}cleofy-cm-git.php\"",
+                    "command-3"  => "cleopatra autopilot execute --autopilot-file=\"{$parent}cleofy-cm-jenkins.php\"",
+                    "command-4"  => "cleopatra autopilot execute --autopilot-file=\"{$parent}cleofy-cm-staging-db.php\"",
+                    "command-5"  => "cleopatra autopilot execute --autopilot-file=\"{$parent}cleofy-cm-staging-web.php\"",
+                    "command-6"  => "cleopatra autopilot execute --autopilot-file=\"{$parent}cleofy-cm-production-db.php\"",
+                    "command-7"  => "cleopatra autopilot execute --autopilot-file=\"{$parent}cleofy-cm-production-web.php\"",
+                ),),),
+
+                array ( "Logging" => array( "log" => array( "log-message" => "Creating a medium set of environments complete"),),),
+
+            );
+
+    }
+
+}
