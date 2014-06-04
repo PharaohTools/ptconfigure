15c70e34152e0e83bf788a86c0a24df4417f0865
David Amanshia <phpengine@hotmail.co.uk>
2014-06-05 00:35:49 +0100

    cleofy generic tinies were wrong

---
+++ src/Modules/Cleofy/Templates/Generic/Tiny/cleofy-cm-git.php
@@ -0,0 +1,43 @@
+<?php
+
+Namespace Core ;
+
+class AutoPilotConfigured extends AutoPilot {
+
+    public $steps ;
+
+    public function __construct() {
+        $this->setSteps();
+    }
+
+    /* Steps */
+    private function setSteps() {
+
+        include ("settings.php") ;
+
+        $this->steps =
+            array(
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets Manage Configuration on the Git Environment" ),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets Prep Ubuntu on the Git Environment" ),),),
+                array ( "RunCommand" => array("install" => array(
+                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/medium-git-prep-ubuntu.php"',
+                    "run-as-user" => "",
+                    "background" => "",
+                ),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets Invoke Cleo and Dapper on the Git Environment" ),),),
+                array ( "RunCommand" => array("install" => array(
+                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/medium-git-invoke-cleo-dapper-new.php"',
+                    "run-as-user" => "",
+                    "background" => "",
+                ),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets setup Git Box on the Git Environment" ),),),
+                array ( "RunCommand" => array("install" => array(
+                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/medium-git-invoke-git.php"',
+                    "run-as-user" => "",
+                    "background" => "",
+                ),),),
+            );
+
+    }
+
+}
