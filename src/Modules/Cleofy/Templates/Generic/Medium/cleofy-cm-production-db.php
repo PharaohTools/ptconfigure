63f89747921855c4687793b0bf8aa6ba35933d0f
David Amanshia <phpengine@hotmail.co.uk>
2014-06-10 21:12:35 +0100

    updated secondary db to db nodes for galera

--- src/Modules/Cleofy/Templates/Generic/Medium/cleofy-cm-production-db.php
+++ src/Modules/Cleofy/Templates/Generic/Medium/cleofy-cm-production-db.php
@@ -17,43 +17,43 @@ class AutoPilotConfigured extends AutoPilot {
 
         $this->steps =
             array(
-                array ( "Logging" => array( "log" => array( "log-message" => "Lets Manage Configuration on the Production Secondary DB Environment" ),),),
-                array ( "Logging" => array( "log" => array( "log-message" => "Lets Prep Ubuntu on the Production Secondary DB Environment" ),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets Manage Configuration on the Production DB Nodes Environment" ),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets Prep Ubuntu on the Production DB Nodes Environment" ),),),
                 array ( "RunCommand" => array("install" => array(
-                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/medium-prod-secondary-db-prep-ubuntu.php"',
+                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/medium-prod-db-nodes-prep-ubuntu.php"',
                     "run-as-user" => "",
                     "background" => "",
                 ),),),
-                array ( "Logging" => array( "log" => array( "log-message" => "Lets Invoke Cleo and Dapper on the Production Secondary DB Environment" ),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets Invoke Cleo and Dapper on the Production DB Nodes Environment" ),),),
                 array ( "RunCommand" => array("install" => array(
-                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/medium-prod-secondary-db-invoke-cleo-dapper-new.php"',
+                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/medium-prod-db-nodes-invoke-cleo-dapper-new.php"',
                     "run-as-user" => "",
                     "background" => "",
                 ),),),
-                array ( "Logging" => array( "log" => array( "log-message" => "Lets setup Production Secondary DB Box on the Production Secondary DB Environment" ),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets setup Production DB Nodes Box on the Production DB Nodes Environment" ),),),
                 array ( "RunCommand" => array("install" => array(
-                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/medium-prod-secondary-db-invoke-db-node.php"',
+                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/medium-prod-db-nodes-invoke-db-node.php"',
                     "run-as-user" => "",
                     "background" => "",
                 ),),),
-                array ( "Logging" => array( "log" => array( "log-message" => "Managing Configuration on Production Secondary DB environment complete"),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "Managing Configuration on Production DB Nodes environment complete"),),),
                 // DB Nodes should be done first, so the Manager node can start the completed cluster
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets Manage Configuration on the Production Primary DB Environment" ),),),
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets Prep Ubuntu on the Production Primary DB Environment" ),),),
                 array ( "RunCommand" => array("install" => array(
-                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/medium-prod-primary-db-prep-ubuntu.php"',
+                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/medium-prod-db-balancer-prep-ubuntu.php"',
                     "run-as-user" => "",
                     "background" => "",
                 ),),),
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets Invoke Cleo and Dapper on the Production Primary DB Environment" ),),),
                 array ( "RunCommand" => array("install" => array(
-                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/medium-prod-primary-db-invoke-cleo-dapper-new.php"',
+                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/medium-prod-db-balancer-invoke-cleo-dapper-new.php"',
                     "run-as-user" => "",
                     "background" => "",
                 ),),),
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets setup Production Primary DB Box on the Production Primary DB Environment" ),),),
                 array ( "RunCommand" => array("install" => array(
-                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/medium-prod-primary-db-invoke-db-primary.php"',
+                    "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/medium-prod-db-balancer-invoke-db-primary.php"',
                     "run-as-user" => "",
                     "background" => "",
                 ),),),
