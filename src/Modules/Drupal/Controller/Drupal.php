d09f196fd56921a0aea7cd2e5a9a07a9b6262264
David Amanshia <phpengine@hotmail.co.uk>
2014-06-12 20:35:04 +0100

    drupal controller for dbconfigure

---
+++ src/Modules/Drupal/Controller/Drupal.php
@@ -0,0 +1,73 @@
+<?php
+
+Namespace Controller ;
+
+class Drupal extends Base {
+
+    public function executeBuilderfy($pageVars) {
+
+        $action = $pageVars["route"]["action"];
+
+        if ($action == "drupal-continuous") {
+            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "DrupalContinuous") ;
+            // if we don't have an object, its an array of errors
+            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+            $thisModel->params["action"] = $action ;
+            $this->content["result1"] = $thisModel->askInstall();
+            $this->content["result2"] = $thisModel->result;
+            return array ("type"=>"view", "view"=>"builderfy", "pageVars"=>$this->content); }
+
+        if ($action == "drupal-efficient") {
+            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "DrupalEfficient") ;
+            // if we don't have an object, its an array of errors
+            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+            $thisModel->params["action"] = $action ;
+            $this->content["result1"] = $thisModel->askInstall();
+            $this->content["result2"] = $thisModel->result;
+            return array ("type"=>"view", "view"=>"builderfy", "pageVars"=>$this->content); }
+
+        $this->content["messages"][] = "Invalid Builderfy Drupal Action";
+        return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content);
+    }
+
+    public function executeDapperfy($pageVars) {
+
+        $action = $pageVars["route"]["action"];
+
+        if ($action == "drupal") {
+            $thisModel = $this->getModelAndCheckDependencies("Dapperfy", $pageVars) ;
+            // if we don't have an object, its an array of errors
+            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+
+            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "DrupalEfficient") ;
+            // if we don't have an object, its an array of errors
+            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+            $thisModel->params["action"] = $action ;
+            $this->content["result1"] = $thisModel->askInstall();
+            $this->content["result2"] = $thisModel->result;
+            return array ("type"=>"view", "view"=>"dapperfy", "pageVars"=>$this->content); }
+
+        $this->content["messages"][] = "Invalid Dapperfy Drupal Action";
+        return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content);
+    }
+
+    public function executeDBConfigure($pageVars) {
+
+        $action = $pageVars["route"]["action"];
+
+        if ($action == "drupal") {
+            $thisModel = $this->getModelAndCheckDependencies("DBConfigure", $pageVars) ;
+            // if we don't have an object, its an array of errors
+            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+            $drupalPlatform = $thisModel = $this->getModelAndCheckDependencies("Drupal", $pageVars, "Drupal7Config") ;
+            $thisModel->setPlatformVars($drupalPlatform);
+            $thisModel->params["action"] = $action ;
+            $this->content["result1"] = $thisModel->askInstall();
+            $this->content["result2"] = $thisModel->result;
+            return array ("type"=>"view", "view"=>"dapperfy", "pageVars"=>$this->content); }
+
+        $this->content["messages"][] = "Invalid DBConfigure Drupal Action";
+        return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content);
+    }
+
+}
