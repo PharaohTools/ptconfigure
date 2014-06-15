8bba0b9785cbc3a2b00dd6033a560a682714fcfe
David Amanshia <phpengine@hotmail.co.uk>
2014-06-15 22:54:57 +0100

    versioning and status of citadel

--- src/Modules/Citadel/Model/CitadelUbuntu.php
+++ src/Modules/Citadel/Model/CitadelUbuntu.php
@@ -29,49 +29,29 @@ class CitadelUbuntu extends BaseLinuxApp {
                 "echo citadel-server citadel/Password_again password $newRootPass | sudo debconf-set-selections",
                 "echo citadel-server citadel/ServerIPAddress string $serverIp | sudo debconf-set-selections",
                 "echo citadel-server citadel/LoginType select Internal | sudo debconf-set-selections",
-                "echo citadel-server citadel/BadUser error | sudo debconf-set-selections",
+                // @todo below should have an extra parameter, it throws an error
+                //"echo citadel-server citadel/BadUser error | sudo debconf-set-selections",
                 "echo citadel-webcit citadel/WebcitApacheIntegration select	Internal | sudo debconf-set-selections",
                 "echo citadel-webcit citadel/WebcitHttpPort string 8504 | sudo debconf-set-selections",
                 "echo citadel-webcit citadel/WebcitHttpsPort string	-1 | sudo debconf-set-selections",
                 "echo citadel-webcit citadel/WebcitOfferLang select UNLIMITED | sudo debconf-set-selections",
             ) ),
-            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "citadel")) ),
-            // array("method"=> array("object" => $this, "method" => "addInitScript", "params" => array())),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "citadel-suite")) ),
             array("method"=> array("object" => $this, "method" => "citadelRestart", "params" => array()))
         );
         $this->uninstallCommands = array(
-            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "citadel")) ),
-            // array("method"=> array("object" => $this, "method" => "delInitScript", "params" => array())),
-            array("method"=> array("object" => $this, "method" => "citadelRestart", "params" => array()) ),
-            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "debconf-utils")) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "citadel-suite")) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "debconf-utils")) ),
         );
         $this->programDataFolder = "/opt/Citadel"; // command and app dir name
         $this->programNameMachine = "citadel"; // command and app dir name
         $this->programNameFriendly = "Citadel Server!"; // 12 chars
         $this->programNameInstaller = "Citadel Server";
-        $this->statusCommand = "sudo citadel -v" ;
-        $this->versionInstalledCommand = "sudo apt-cache policy citadel" ;
-        $this->versionRecommendedCommand = "sudo apt-cache policy citadel" ;
-        $this->versionLatestCommand = "sudo apt-cache policy citadel" ;
+        $this->statusCommand = "sudo dpkg -l citadel-server | grep -E \"^ii\" | tr -s ' ' | cut -d' ' -f3" ;
+        $this->versionInstalledCommand = "sudo apt-cache policy citadel-server" ;
+        $this->versionRecommendedCommand = "sudo apt-cache policy citadel-server" ;
+        $this->versionLatestCommand = "sudo apt-cache policy citadel-server" ;
         $this->initialize();
-    }
-
-    public function addInitScript() {
-        $templatesDir = str_replace("Model", "Templates", dirname(__FILE__) ) ;
-        $templateSource = $templatesDir.'/citadel';
-        $templatorFactory = new \Model\Templating();
-        $templator = $templatorFactory->getModel($this->params);
-        $newFileName = "/etc/default/citadel" ;
-        $templator->template(
-            file_get_contents($templateSource),
-            array(),
-            $newFileName );
-        echo "Citadel Init script config file $newFileName added\n";
-    }
-
-    public function delInitScript() {
-        unlink("/etc/default/citadel");
-        echo "Citadel Init script config file /etc/default/citadel removed\n";
     }
 
     protected function getNewRootUser() {
@@ -100,29 +80,29 @@ class CitadelUbuntu extends BaseLinuxApp {
         else if (AppConfig::getProjectVariable("citadel-default-listen-ip") != "") {
             $newRootPass = AppConfig::getProjectVariable("citadel-default-listen-ip") ; }
         else {
-            $newRootPass = "cleopatra" ; }
+            $newRootPass = "0.0.0.0" ; }
         return $newRootPass;
     }
 
     public function citadelRestart() {
         $serviceFactory = new Service();
         $serviceManager = $serviceFactory->getModel($this->params) ;
-        $serviceManager->setService("citadel-server");
+        $serviceManager->setService("citadel");
         $serviceManager->restart();
     }
 
     public function versionInstalledCommandTrimmer($text) {
-        $done = substr($text, 22, 8) ;
+        $done = substr($text, 29, 6) ;
         return $done ;
     }
 
     public function versionLatestCommandTrimmer($text) {
-        $done = substr($text, 44, 8) ;
+        $done = substr($text, 49, 6) ;
         return $done ;
     }
 
     public function versionRecommendedCommandTrimmer($text) {
-        $done = substr($text, 44, 8) ;
+        $done = substr($text, 49, 6) ;
         return $done ;
     }
 
