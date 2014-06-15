564323d9b422c1ea44412567fb0cfcd71bb961cd
David Amanshia <phpengine@hotmail.co.uk>
2014-06-15 22:54:36 +0100

    controllers which didnt check default actions that shou;ld

--- src/Modules/HAProxy/Controller/HAProxy.php
+++ src/Modules/HAProxy/Controller/HAProxy.php
@@ -10,23 +10,15 @@ class HAProxy extends Base {
         $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
         // if we don't have an object, its an array of errors
         if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+        $isDefaultAction = self::checkDefaultActions($pageVars, array(), $thisModel) ;
+        if ( is_array($isDefaultAction) ) { return $isDefaultAction; }
 
         $action = $pageVars["route"]["action"];
-
-        if ($action=="help") {
-            $helpModel = new \Model\Help();
-            $this->content["helpData"] = $helpModel->getHelpData($pageVars["route"]["control"]);
-            return array ("type"=>"view", "view"=>"help", "pageVars"=>$this->content); }
 
         if (in_array($action, array("config", "configure") )) {
             $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "Configure") ;
             // if we don't have an object, its an array of errors
             if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
-            $this->content["result"] = $thisModel->install();
-            $this->content["appName"] = $thisModel->programNameInstaller ;
-            return array ("type"=>"view", "view"=>"appInstall", "pageVars"=>$this->content); }
-
-        if (in_array($action, array("install") )) {
             $this->content["result"] = $thisModel->install();
             $this->content["appName"] = $thisModel->programNameInstaller ;
             return array ("type"=>"view", "view"=>"appInstall", "pageVars"=>$this->content); }
