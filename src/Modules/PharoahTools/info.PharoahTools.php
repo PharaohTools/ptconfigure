02a0b21ab30fdd9cc0975bd2a5297c91717d6bbe
David Amanshia <phpengine@hotmail.co.uk>
2014-06-18 12:06:00 +0100

    pharoah tools module to install all of pharoah in one command

---
+++ src/Modules/PharoahTools/info.PharoahTools.php
@@ -0,0 +1,39 @@
+<?php
+
+Namespace Info;
+
+class PharoahToolsInfo extends CleopatraBase {
+
+    public $hidden = false;
+
+    public $name = "Pharoah Tools - Tools for working with Git SCM";
+
+    public function __construct() {
+      parent::__construct();
+    }
+
+    public function routesAvailable() {
+      return array( "PharoahTools" =>  array_merge(parent::routesAvailable(), array() ) );
+    }
+
+    public function routeAliases() {
+      return array("pharoahtools"=>"PharoahTools", "pharoah-tools"=>"PharoahTools");
+    }
+
+    public function helpDefinition() {
+      $help = <<<"HELPDATA"
+  This command allows you to install the Pharoah Tools which are ready. These include
+  Cleopatra - this Configuration Management tool, Dapperstrano - the Automated Deployment tool,
+  and Testingkamen, the test configuration and automation tool.
+
+  PharoahTools, pharoahtools, pharoah-tools
+
+        - install
+        Installs the latest version of all of the Pharoah Tools
+        example: cleopatra pharoah-tools install
+
+HELPDATA;
+      return $help ;
+    }
+
+}
