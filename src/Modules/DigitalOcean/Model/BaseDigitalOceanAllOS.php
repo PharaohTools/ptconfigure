822005297eb77a68c399e44f5d6ae5584b47af62
David Amanshia <phpengine@hotmail.co.uk>
2014-06-25 15:11:15 +0100

    updated digital ocean base model to new api url

--- src/Modules/DigitalOcean/Model/BaseDigitalOceanAllOS.php
+++ src/Modules/DigitalOcean/Model/BaseDigitalOceanAllOS.php
@@ -68,13 +68,12 @@ class BaseDigitalOceanAllOS extends Base {
                 var_dump($curlParamKey, $curlParamValue) ;  }
             $postQuery .= $curlParamKey."=".$curlParamValue;
             $i++; }
-        echo $curlUrl.'?'.$postQuery ;
+        // echo $curlUrl.'?'.$postQuery ;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $curlUrl.'?'.$postQuery);
         // receive server response ...
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $server_output = curl_exec ($ch);
-        var_dump($server_output) ;
         curl_close ($ch);
         $callObject = json_decode($server_output);
         return $callObject;
