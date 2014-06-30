d667ca97647c91b3d0d65428ac750f8c0e067045
David Amanshia <phpengine@hotmail.co.uk>
2014-06-30 03:29:31 +0100

    dirname check for is int was checking full path, not just dirname, so always failed

--- src/Modules/Version/Model/VersionLinuxMac.php
+++ src/Modules/Version/Model/VersionLinuxMac.php
@@ -67,7 +67,7 @@ class VersionLinuxMac extends Base {
             foreach ($otherResults as $result) {
                 if ($result === '.' || $result === '..' || $result === 'current') continue;
                 if (!is_dir($this->appRootDirectory.'/'.$result)) continue;
-                if (!is_int($this->appRootDirectory.'/'.$result)) continue;
+                if (!is_int($result)) continue;
                 $question .= "($i1) $result\n";
                 $i1++;
                 $availableVersions[] = $result;} }
