ff9aacc60e646b2485a40fa2a43b869469cf53a4
David Amanshia <phpengine@hotmail.co.uk>
2014-06-30 03:19:01 +0100

    selectAppVersion had incorrect usage of version parameter

--- src/Modules/Version/Model/VersionLinuxMac.php
+++ src/Modules/Version/Model/VersionLinuxMac.php
@@ -56,7 +56,7 @@ class VersionLinuxMac extends Base {
     }
 
     private function selectAppVersion() {
-        if ( isset($this->params["version"])) { return $this->params["version"] ; }
+        if ( isset($this->params["version"])) {$version = $this->params["version"] ; }
         $otherResults = (is_dir($this->appRootDirectory)) ? scandir($this->appRootDirectory) : array();
         arsort($otherResults) ;
         $question = "Please Choose Version to make current (Showing newest first, Enter none for newest):\n";
