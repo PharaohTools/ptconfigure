3ac1577285b0dac8e5429018e5a5acf4966ea974
David Amanshia <phpengine@hotmail.co.uk>
2014-07-02 10:53:08 +0100

    updated first workstation template to new format

--- src/Modules/Dapperfy/Templates/environment-workstation-install-code-data.php
+++ src/Modules/Dapperfy/Templates/environment-workstation-install-code-data.php
@@ -23,65 +23,76 @@ class AutoPilotConfigured extends AutoPilot {
 
 	    $this->steps =
 	      array(
-              array ( "Project" => array(
-                  "projectInitializeExecute" => true,
-              ) , ) ,
-              array ( "HostEditor" => array(
-                  "hostEditorAdditionExecute" => true,
-                  "hostEditorAdditionIP" => "<%tpl.php%>dap_apache_vhost_ip</%tpl.php%>",
-                  "hostEditorAdditionURI" => "<%tpl.php%>dap_apache_vhost_url</%tpl.php%>.local",
-              ) , ) ,
-              array ( "ApacheVHostEditor" => array(
-                  "virtualHostEditorAdditionExecute" => true,
-                  "virtualHostEditorAdditionDocRoot" => "<%tpl.php%>dap_proj_cont_dir</%tpl.php%>",
-                  "virtualHostEditorAdditionURL" => "<%tpl.php%>dap_apache_vhost_url</%tpl.php%>.local",
-                  "virtualHostEditorAdditionIp" => "<%tpl.php%>dap_apache_vhost_ip</%tpl.php%>",
-                  "virtualHostEditorAdditionTemplateData" => "",
-                  "virtualHostEditorAdditionDirectory" => "/etc/apache2/sites-available",
-                  "virtualHostEditorAdditionFileSuffix" => "",
-                  "virtualHostEditorAdditionVHostEnable" => true,
-                  "virtualHostEditorAdditionSymLinkDirectory" => "/etc/apache2/sites-enabled",
-                  "virtualHostEditorAdditionApacheCommand" => "apache2",
-              ) , ) ,
-              array ( "DBConfigure" => array(
-                    "dbResetExecute" => true,
-                    "dbResetPlatform" => "<%tpl.php%>dap_db_platform</%tpl.php%>",
-              ) , ) ,
-              array ( "DBConfigure" => array(
-                    "dbConfigureExecute" => true,
-                    "dbConfigureDBHost" => "<%tpl.php%>dap_db_ip_address</%tpl.php%>",
-                    "dbConfigureDBUser" => "<%tpl.php%>dap_db_app_user_name</%tpl.php%>",
-                    "dbConfigureDBPass" => "<%tpl.php%>dap_db_app_user_pass</%tpl.php%>",
-                    "dbConfigureDBName" => "<%tpl.php%>dap_db_name</%tpl.php%>",
-                    "dbConfigurePlatform" => "<%tpl.php%>dap_db_platform</%tpl.php%>",
-              ) , ) ,
-              array ( "DBInstall" => array(
-                    "dbDropExecute" => true,
-                    "dbDropDBHost" => "<%tpl.php%>dap_db_ip_address</%tpl.php%>",
-                    "dbDropDBName" => "<%tpl.php%>dap_db_name</%tpl.php%>",
-                    "dbDropDBRootUser" => "****dap_db_admin_user_name****",
-                    "dbDropDBRootPass" => "****dap_db_admin_user_pass****",
-                    "dbDropUserExecute" => true,
-                    "dbDropDBUser" => "<%tpl.php%>dap_db_app_user_name</%tpl.php%>",
-              ) , ) ,
-              array ( "DBInstall" => array(
-                    "dbInstallExecute" => true,
-                    "dbInstallDBHost" => "<%tpl.php%>dap_db_ip_address</%tpl.php%>",
-                    "dbInstallDBUser" => "<%tpl.php%>dap_db_app_user_name</%tpl.php%>",
-                    "dbInstallDBPass" => "<%tpl.php%>dap_db_app_user_pass</%tpl.php%>",
-                    "dbInstallDBName" => "<%tpl.php%>dap_db_name</%tpl.php%>",
-                    "dbInstallDBRootUser" => "****dap_db_admin_user_name****",
-                    "dbInstallDBRootPass" => "****dap_db_admin_user_pass****",
-              ) , ) ,
-              array ( "Version" => array(
-                    "versionExecute" => true,
-                    "versionAppRootDirectory" => "<%tpl.php%>dap_proj_cont_dir</%tpl.php%>",
-                    "versionArrayPointToRollback" => "0",
-                    "versionLimit" => "<%tpl.php%>dap_version_num_revisions</%tpl.php%>",
-              ) , ) ,
-              array ( "ApacheControl" => array(
-                  "apacheCtlRestartExecute" => true,
-              ) , ) ,
+
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets initialize our new download directory as a dapper project"), ) ),
+                array ( "Project" => array( "init" => array(), ) , )
+                
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets create our Host File entry"), ) ),
+                array ( "HostEditor" => array( "init" => array(
+                  "host-ip" => "<%tpl.php%>dap_apache_vhost_ip</%tpl.php%>",
+                  "host-name" => "<%tpl.php%>dap_apache_vhost_url</%tpl.php%>.local",                
+                ), ) , )
+                                
+                array ( "Logging" => array( "log" => array( "log-message" => "Next create our virtual host"), ) ),
+                array ( "ApacheVHostEditor" => array( "add" => array (
+                    "guess" => true,
+                    "vhe-docroot" => "<%tpl.php%>dap_proj_cont_dir</%tpl.php%>",
+                    "vhe-url" => "<%tpl.php%>dap_apache_vhost_url</%tpl.php%>",
+                    "vhe-ip-port" => "<%tpl.php%>dap_apache_vhost_ip</%tpl.php%>",
+                    "vhe-vhost-dir" => "/etc/apache2/sites-available",
+                    "vhe-template" => $this->getTemplate(),
+                    "vhe-file-ext" => ".conf",
+                ), ), ),
+
+                array ( "Logging" => array( "log" => array( "log-message" => "Next ensure our db file configuration is reset to blank" ), ), ),
+                array ( "DBConfigure" => array( "reset" => array( "platform" => "<%tpl.php%>dap_db_platform</%tpl.php%>" ), ), ),
+
+                array ( "Logging" => array( "log" => array("log-message" => "Next configure our projects db configuration file"), ) ),
+                array ( "DBConfigure" => array( "conf" => array(
+					"mysql-admin-user" => "<%tpl.php%>dap_db_admin_user_name</%tpl.php%>",
+					"mysql-admin-pass" => "<%tpl.php%>dap_db_admin_user_pass</%tpl.php%>",
+                    "mysql-host" => "<%tpl.php%>dap_db_ip_address</%tpl.php%>",
+                    "mysql-user" => "<%tpl.php%>dap_db_app_user_name</%tpl.php%>",
+                    "mysql-pass" => "<%tpl.php%>dap_db_app_user_pass</%tpl.php%>",
+                    "mysql-db" => "<%tpl.php%>dap_db_name</%tpl.php%>",
+                    "mysql-platform" => "<%tpl.php%>dap_db_platform</%tpl.php%>",
+                ), ) , ) ,
+
+                array ( "Logging" => array( "log" => array( "log-message" => "Now lets drop our current database if it exists"), ) ),
+                array ( "DBInstall" => array( "drop" => array(
+                    "mysql-admin-user" => "<%tpl.php%>dap_db_admin_user_name</%tpl.php%>",
+                    "mysql-admin-pass" => "<%tpl.php%>dap_db_admin_user_pass</%tpl.php%>",
+                    "mysql-host" => "<%tpl.php%>dap_db_ip_address</%tpl.php%>",
+                    "mysql-user" => "<%tpl.php%>dap_db_app_user_name</%tpl.php%>",
+                    "mysql-pass" => "<%tpl.php%>dap_db_app_user_pass</%tpl.php%>",
+                    "mysql-db" => "<%tpl.php%>dap_db_name</%tpl.php%>",
+                    "mysql-platform" => "<%tpl.php%>dap_db_platform</%tpl.php%>",
+                ), ), ),
+
+                array ( "Logging" => array( "log" => array("log-message" => "Now lets install our database"), ), ),
+                array ( "DBInstall" => array( "install" => array(
+                    "mysql-admin-user" => "<%tpl.php%>dap_db_admin_user_name</%tpl.php%>",
+                    "mysql-admin-pass" => "<%tpl.php%>dap_db_admin_user_pass</%tpl.php%>",
+                    "mysql-host" => "<%tpl.php%>dap_db_ip_address</%tpl.php%>",
+                    "mysql-user" => "<%tpl.php%>dap_db_app_user_name</%tpl.php%>",
+                    "mysql-pass" => "<%tpl.php%>dap_db_app_user_pass</%tpl.php%>",
+                    "mysql-db" => "<%tpl.php%>dap_db_name</%tpl.php%>",
+                    "mysql-platform" => "<%tpl.php%>dap_db_platform</%tpl.php%>"
+                ), ), ),
+              
+				// @todo why version locally
+                array ( "Logging" => array( "log" => array( "log-message" => "The application is installed now so lets do our versioning" ), ), ),
+                array ( "Version" => array( "latest" => array(
+                    "container" => "<%tpl.php%>dap_proj_cont_dir</%tpl.php%>",
+                    "limit" => "<%tpl.php%>dap_version_num_revisions</%tpl.php%>"
+                ), ), ),
+
+                array ( "Logging" => array( "log" => array( "log-message" => "Now lets restart Apache so we are serving our new application version", ), ), ),
+                array ( "ApacheControl" => array( "restart" => array(
+                    "guess" => true,
+                ), ), ),
+
+                array ( "Logging" => array( "log" => array("log-message" => "Our deployment is done" ), ), ),
 	      );
 
 	  }
