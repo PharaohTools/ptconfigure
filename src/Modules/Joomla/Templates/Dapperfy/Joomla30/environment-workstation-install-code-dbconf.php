125308774b4d1cc768091239f2dae9d1d8347f12
David Amanshia <phpengine@hotmail.co.uk>
2014-07-02 17:53:02 +0100

    added the env-workstation-code-dbconf joomla template for data install

--- src/Modules/Joomla/Templates/Dapperfy/Joomla30/environment-workstation-install-code-dbconf.php
+++ src/Modules/Joomla/Templates/Dapperfy/Joomla30/environment-workstation-install-code-dbconf.php
@@ -23,51 +23,56 @@ class AutoPilotConfigured extends AutoPilot {
 
 	    $this->steps =
         array(
-              array ( "Project" => array(
-                    "projectInitializeExecute" => true,
-              ) , ) ,
-              array ( "HostEditor" => array(
-                    "hostEditorAdditionExecute" => true,
-                    "hostEditorAdditionIP" => "<%tpl.php%>dap_apache_vhost_ip</%tpl.php%>",
-                    "hostEditorAdditionURI" => "<%tpl.php%>dap_apache_vhost_url</%tpl.php%>.local",
-              ) , ) ,
-              array ( "ApacheVHostEditor" => array(
-                    "virtualHostEditorAdditionExecute" => true,
-                    "virtualHostEditorAdditionDocRoot" => "<%tpl.php%>dap_proj_cont_dir</%tpl.php%>",
-                    "virtualHostEditorAdditionURL" => "<%tpl.php%>dap_apache_vhost_url</%tpl.php%>.local",
-                    "virtualHostEditorAdditionIp" => "<%tpl.php%>dap_apache_vhost_ip</%tpl.php%>",
-                    "virtualHostEditorAdditionTemplateData" => "",
-                    "virtualHostEditorAdditionDirectory" => "/etc/apache2/sites-available",
-                    "virtualHostEditorAdditionFileSuffix" => "",
-                    "virtualHostEditorAdditionVHostEnable" => true,
-                    "virtualHostEditorAdditionSymLinkDirectory" => "/etc/apache2/sites-enabled",
-                    "virtualHostEditorAdditionApacheCommand" => "apache2",
-              ) , ) ,
-              array ( "DBConfigure" => array(
-                        "dbResetExecute" => true,
-                        "dbResetPlatform" => "<%tpl.php%>dap_db_platform</%tpl.php%>",
-              ) , ) ,
-              array ( "DBConfigure" => array(
-                        "dbConfigureExecute" => true,
-                        "dbConfigureDBHost" => "<%tpl.php%>dap_db_ip_address</%tpl.php%>",
-                        "dbConfigureDBUser" => "<%tpl.php%>dap_db_app_user_name</%tpl.php%>",
-                        "dbConfigureDBPass" => "<%tpl.php%>dap_db_app_user_pass</%tpl.php%>",
-                        "dbConfigureDBName" => "<%tpl.php%>dap_db_name</%tpl.php%>",
-                        "dbConfigurePlatform" => "<%tpl.php%>dap_db_platform</%tpl.php%>",
-              ) , ) ,
-            array ( "ApacheControl" => array(
-                "apacheCtlRestartExecute" => true,
-            ) , ) ,
+
+            array ( "Logging" => array( "log" => array( "log-message" => "Lets initialize our new download directory as a dapper project"), ) ),
+            array ( "Project" => array( "init" => array(), ) , ),
+
+            array ( "Logging" => array( "log" => array( "log-message" => "Lets create our Host File entry"), ) ),
+            array ( "HostEditor" => array( "add" => array(
+                "host-ip" => "<%tpl.php%>dap_apache_vhost_ip</%tpl.php%>",
+                "host-name" => "<%tpl.php%>dap_apache_vhost_url</%tpl.php%>",
+            ), ) , ),
+
+            array ( "Logging" => array( "log" => array( "log-message" => "Next create our virtual host"), ) ),
+            array ( "ApacheVHostEditor" => array( "add" => array (
+                "guess" => true,
+                "vhe-docroot" => "<%tpl.php%>dap_proj_cont_dir</%tpl.php%>",
+                "vhe-url" => "<%tpl.php%>dap_apache_vhost_url</%tpl.php%>",
+                "vhe-ip-port" => "<%tpl.php%>dap_apache_vhost_ip</%tpl.php%>",
+                "vhe-vhost-dir" => "/etc/apache2/sites-available",
+                "vhe-template" => $this->getTemplate(),
+                "vhe-file-ext" => ".conf",
+            ), ), ),
+
+            array ( "Logging" => array( "log" => array( "log-message" => "Next ensure our db file configuration is reset to blank" ), ), ),
+            array ( "DBConfigure" => array( "joomla30-reset" => array( ), ), ),
+
+            array ( "Logging" => array( "log" => array("log-message" => "Next configure our projects db configuration file"), ) ),
+            array ( "DBConfigure" => array( "joomla30-conf" => array(
+                "mysql-admin-user" => "<%tpl.php%>dap_db_admin_user_name</%tpl.php%>",
+                "mysql-admin-pass" => "<%tpl.php%>dap_db_admin_user_pass</%tpl.php%>",
+                "mysql-host" => "<%tpl.php%>dap_db_ip_address</%tpl.php%>",
+                "mysql-user" => "<%tpl.php%>dap_db_app_user_name</%tpl.php%>",
+                "mysql-pass" => "<%tpl.php%>dap_db_app_user_pass</%tpl.php%>",
+                "mysql-db" => "<%tpl.php%>dap_db_name</%tpl.php%>",
+                "mysql-platform" => "<%tpl.php%>dap_db_platform</%tpl.php%>",
+            ), ) , ) ,
+
+            array ( "Logging" => array( "log" => array( "log-message" => "Now lets restart Apache so we are serving our new application version", ), ), ),
+            array ( "ApacheControl" => array( "restart" => array(
+                "guess" => true,
+            ), ), ),
+
+            array ( "Logging" => array( "log" => array("log-message" => "Our deployment is done" ), ), ),
 	      );
 
 	  }
 
 
- // This function will set the vhost template for your Virtual Host
- // You need to call this from your constructor
- private function setVHostTemplate() {
-   $this->steps[2]["ApacheVHostEditor"]["virtualHostEditorAdditionTemplateData"] =
-  <<<'TEMPLATE'
+
+    private function getTemplate() {
+        $template =
+            <<<'TEMPLATE'
  NameVirtualHost ****IP ADDRESS****:80
  <VirtualHost ****IP ADDRESS****:80>
    ServerAdmin webmaster@localhost
@@ -88,9 +93,9 @@ class AutoPilotConfigured extends AutoPilot {
  	 ServerAdmin webmaster@localhost
  	 ServerName ****SERVER NAME****
  	 DocumentRoot ****WEB ROOT****src
-     SSLEngine on
-     SSLCertificateFile /etc/ssl/certs/snakeoil.crt
-     SSLCertificateKeyFile /etc/ssl/private/snakeoil.key
+   # SSLEngine on
+ 	 # SSLCertificateFile /etc/apache2/ssl/ssl.crt
+   # SSLCertificateKeyFile /etc/apache2/ssl/ssl.key
    # SSLCertificateChainFile /etc/apache2/ssl/bundle.crt
  	 <Directory ****WEB ROOT****src>
  		 Options Indexes FollowSymLinks MultiViews
@@ -102,7 +107,9 @@ class AutoPilotConfigured extends AutoPilot {
   CustomLog /var/log/apache2/access.log combined
   </VirtualHost>
 TEMPLATE;
-}
+
+        return $template ;
+    }
 
 
 }
