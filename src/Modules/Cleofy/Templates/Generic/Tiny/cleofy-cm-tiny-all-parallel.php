8c57258755ddcdca60f2cb0df5e545a9418ceaec
David Amanshia <phpengine@hotmail.co.uk>
2014-07-03 14:32:06 +0100

    modified the cm all tiny to have the right cm files

---
+++ src/Modules/Cleofy/Templates/Generic/Tiny/cleofy-cm-tiny-all-parallel.php
@@ -0,0 +1,38 @@
+<?php
+
+Namespace Core ;
+
+class AutoPilotConfigured extends AutoPilot {
+
+    public $steps ;
+
+    public function __construct() {
+        $this->setSteps();
+    }
+
+    /* Steps */
+    private function setSteps() {
+
+        include("settings.php") ;
+
+        $this->steps =
+            array(
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets begin Configuration of a Tiny set of environments"),),),
+
+                // Bastion
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets add all Boxes and Environments in Parallel" ),),),
+                array ( "Parallax" => array("cli" => array(
+                    "command-1"  => "cleopatra autopilot execute --autopilot-file=\"{$parent}cleofy-cm-bastion.php\"",
+                    "command-2"  => "cleopatra autopilot execute --autopilot-file=\"{$parent}cleofy-cm-git.php\"",
+                    "command-3"  => "cleopatra autopilot execute --autopilot-file=\"{$parent}cleofy-cm-jenkins.php\"",
+                    "command-4"  => "cleopatra autopilot execute --autopilot-file=\"{$parent}cleofy-cm-staging.php\"",
+                    "command-7"  => "cleopatra autopilot execute --autopilot-file=\"{$parent}cleofy-cm-production.php\"",
+                ),),),
+
+                array ( "Logging" => array( "log" => array( "log-message" => "Configuring a Tiny set of environments complete"),),),
+
+            );
+
+    }
+
+}
