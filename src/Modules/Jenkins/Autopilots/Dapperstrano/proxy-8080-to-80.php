70838177acfd24288cecbc361f3b1e39a221fae3
David Amanshia <phpengine@hotmail.co.uk>
2014-07-07 00:03:24 +0100

    proxy dapper file for 8080 to 80

--- src/Modules/Jenkins/Autopilots/Dapperstrano/proxy-8080-to-80.php
+++ src/Modules/Jenkins/Autopilots/Dapperstrano/proxy-8080-to-80.php
@@ -18,23 +18,24 @@ class AutoPilotConfigured extends AutoPilot {
 
             array ( "Logging" => array( "log" => array( "log-message" => "Lets begin Configuration of a Jenkins build server on environment tiny-jenkins"),),),
 
-            // All Pharoes
+            // Install Apache Reverse Proxy
             array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure Dapperstrano so we c" ),),),
-            array ( "Dapperstrano" => array( "ensure" => array(),),),
-
-            // BDD Testing
-
-            // SeleniumServer
-            array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure Selenium Server is installed"),),),
-            array ( "SeleniumServer" => array( "ensure" => array("guess" => true ),),),
-
-            // @todo Runcommand is, im pretty sure, not working properly for starting this background process
-            // Start the Selenium Server
-            array ( "Logging" => array( "log" => array( "log-message" => "Lets also start Selenium so we can use it"),),),
-            array ( "RunCommand" => array("install" => array(
-                "command" => 'printf "\n" | java -jar /opt/selenium/selenium-server.jar &',
-                "nohup" => true
+            array ( "ApacheVHostEditor" => array( "add-balancer" => array(
+                "guess" => true,
+                "vhe-url" => "www.jenkins.tld",
+                "vhe-ip-port" => "127.0.0.1:80",
+                "vhe-cluster-name" => "jenkins-proxy",
+                // @todo we should let it guess this, and make sure the ubuntu 14 mode provide s correct result
+                // ubuntu 14 dapper model should guess .conf whether its centos or ubuntu, past ubuntu 2.4
+                "vhe-file-ext" => ".conf",
+                "vhe-default-template-name" => "http",
+                "environment-name" => "local"
             ),),),
+
+            array ( "Logging" => array( "log" => array( "log-message" => "Now lets restart Apache so we are serving our new application version", ), ), ),
+            array ( "ApacheControl" => array( "restart" => array(
+                "guess" => true,
+            ), ), ),
 
             // End
             array ( "Logging" => array( "log" => array( "log-message" => "Configuring a build server on environment tiny-jenkins complete"),),),
