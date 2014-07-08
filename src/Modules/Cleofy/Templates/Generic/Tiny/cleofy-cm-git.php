b15cd26eeb02bb76a616d6135e5ca500bfd826e8
David Amanshia <phpengine@hotmail.co.uk>
2014-07-08 12:36:10 +0100

    updated runcommand syntax in generic cleo tiny autos to new style

--- src/Modules/Cleofy/Templates/Generic/Tiny/cleofy-cm-git.php
+++ src/Modules/Cleofy/Templates/Generic/Tiny/cleofy-cm-git.php
@@ -20,21 +20,16 @@ class AutoPilotConfigured extends AutoPilot {
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets Manage Configuration on the Git Environment" ),),),
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets Prep Ubuntu on the Git Environment" ),),),
                 array ( "RunCommand" => array("install" => array(
+                    "guess" => true,
                     "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/tiny-git-prep-ubuntu.php"',
-                    "run-as-user" => "",
-                    "background" => "",
                 ),),),
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets Invoke Cleo and Dapper on the Git Environment" ),),),
                 array ( "RunCommand" => array("install" => array(
                     "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/tiny-git-invoke-cleo-dapper-new.php"',
-                    "run-as-user" => "",
-                    "background" => "",
                 ),),),
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets setup Git Box on the Git Environment" ),),),
                 array ( "RunCommand" => array("install" => array(
                     "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/tiny-git-invoke-git.php"',
-                    "run-as-user" => "",
-                    "background" => "",
                 ),),),
             );
 
--- src/Modules/Cleofy/Templates/Generic/Tiny/cleofy-cm-staging.php
+++ src/Modules/Cleofy/Templates/Generic/Tiny/cleofy-cm-staging.php
@@ -20,21 +20,18 @@ class AutoPilotConfigured extends AutoPilot {
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets Manage Configuration on the Staging Environment" ),),),
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets Prep Ubuntu on the Staging Environment" ),),),
                 array ( "RunCommand" => array("install" => array(
+                    "guess" => true,
                     "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/tiny-staging-prep-ubuntu.php"',
-                    "run-as-user" => "",
-                    "background" => "",
                 ),),),
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets Invoke Cleo and Dapper on the Staging Environment" ),),),
                 array ( "RunCommand" => array("install" => array(
+                    "guess" => true,
                     "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/tiny-staging-invoke-cleo-dapper-new.php"',
-                    "run-as-user" => "",
-                    "background" => "",
                 ),),),
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets setup a Standalone Server Box on the Staging Environment" ),),),
                 array ( "RunCommand" => array("install" => array(
+                    "guess" => true,
                     "command" => 'cleopatra autopilot execute --autopilot-file="build/config/cleopatra/cleofy/autopilots/generated/tiny-staging-invoke-standalone-server.php"',
-                    "run-as-user" => "",
-                    "background" => "",
                 ),),),
                 array ( "Logging" => array( "log" => array( "log-message" => "Managing Configuration on Staging environment complete"),),),
             );
