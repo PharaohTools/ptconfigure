c43eceaf11b0c74ac9a1552b33498e6ac17d45ee
David Amanshia <phpengine@hotmail.co.uk>
2014-07-08 02:10:31 +0100

    cleofy default includes setting the build server first_server_target param for the reverse proxy

--- src/Modules/Cleofy/Model/CleofyUbuntu.php
+++ src/Modules/Cleofy/Model/CleofyUbuntu.php
@@ -77,6 +77,7 @@ class CleofyUbuntu extends Base {
                   array(
                       "gen_srv_array_text" => $this->getServerArrayText($environment["servers"]) ,
                       "env_name" => $environment["any-app"]["gen_env_name"],
+                      "first_server_target" => $environment["servers"][0]["target"],
                       "web_nodes_env" => $this->getEnvName("web") ,
                       "db_nodes_env" => $this->getEnvName("database") ,
                   ),
