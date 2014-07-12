2e840b2b6c8e38a871d5f9b6779194bbf643f4fa
David Amanshia <phpengine@hotmail.co.uk>
2014-07-12 18:45:40 +0100

    versioning removed from the workstation dapper code-data template

--- src/Modules/Wordpress/Templates/Dapperfy/Wordpress/environment-workstation-install-code-data.php
+++ src/Modules/Wordpress/Templates/Dapperfy/Wordpress/environment-workstation-install-code-data.php
@@ -15,7 +15,6 @@ class AutoPilotConfigured extends AutoPilot {
 
     public function __construct() {
 	    $this->setSteps();
-        $this->setVHostTemplate();
     }
 
     /* Steps */
@@ -23,81 +22,88 @@ class AutoPilotConfigured extends AutoPilot {
 
 	    $this->steps =
 	      array(
-              array ( "Project" => array(
-                  "projectInitializeExecute" => true,
-              ) , ) ,
-              array ( "HostEditor" => array(
-                  "hostEditorAdditionExecute" => true,
-                  "hostEditorAdditionIP" => "<%tpl.php%>dap_apache_vhost_ip</%tpl.php%>",
-                  "hostEditorAdditionURI" => "<%tpl.php%>dap_apache_vhost_url</%tpl.php%>.local",
-              ) , ) ,
-              array ( "ApacheVHostEditor" => array(
-                  "virtualHostEditorAdditionExecute" => true,
-                  "virtualHostEditorAdditionDocRoot" => "<%tpl.php%>dap_proj_cont_dir</%tpl.php%>",
-                  "virtualHostEditorAdditionURL" => "<%tpl.php%>dap_apache_vhost_url</%tpl.php%>.local",
-                  "virtualHostEditorAdditionIp" => "<%tpl.php%>dap_apache_vhost_ip</%tpl.php%>",
-                  "virtualHostEditorAdditionTemplateData" => "",
-                  "virtualHostEditorAdditionDirectory" => "/etc/apache2/sites-available",
-                  "virtualHostEditorAdditionFileSuffix" => "",
-                  "virtualHostEditorAdditionVHostEnable" => true,
-                  "virtualHostEditorAdditionSymLinkDirectory" => "/etc/apache2/sites-enabled",
-                  "virtualHostEditorAdditionApacheCommand" => "apache2",
-              ) , ) ,
-              array ( "DBConfigure" => array(
-                    "dbResetExecute" => true,
-                    "dbResetPlatform" => "<%tpl.php%>dap_db_platform</%tpl.php%>",
-              ) , ) ,
-              array ( "DBConfigure" => array(
-                    "dbConfigureExecute" => true,
-                    "dbConfigureDBHost" => "<%tpl.php%>dap_db_ip_address</%tpl.php%>",
-                    "dbConfigureDBUser" => "<%tpl.php%>dap_db_app_user_name</%tpl.php%>",
-                    "dbConfigureDBPass" => "<%tpl.php%>dap_db_app_user_pass</%tpl.php%>",
-                    "dbConfigureDBName" => "<%tpl.php%>dap_db_name</%tpl.php%>",
-                    "dbConfigurePlatform" => "<%tpl.php%>dap_db_platform</%tpl.php%>",
-              ) , ) ,
-              array ( "DBInstall" => array(
-                    "dbDropExecute" => true,
-                    "dbDropDBHost" => "<%tpl.php%>dap_db_ip_address</%tpl.php%>",
-                    "dbDropDBName" => "<%tpl.php%>dap_db_name</%tpl.php%>",
-                    "dbDropDBRootUser" => "****dap_db_admin_user_name****",
-                    "dbDropDBRootPass" => "****dap_db_admin_user_pass****",
-                    "dbDropUserExecute" => true,
-                    "dbDropDBUser" => "<%tpl.php%>dap_db_app_user_name</%tpl.php%>",
-              ) , ) ,
-              array ( "DBInstall" => array(
-                    "dbInstallExecute" => true,
-                    "dbInstallDBHost" => "<%tpl.php%>dap_db_ip_address</%tpl.php%>",
-                    "dbInstallDBUser" => "<%tpl.php%>dap_db_app_user_name</%tpl.php%>",
-                    "dbInstallDBPass" => "<%tpl.php%>dap_db_app_user_pass</%tpl.php%>",
-                    "dbInstallDBName" => "<%tpl.php%>dap_db_name</%tpl.php%>",
-                    "dbInstallDBRootUser" => "****dap_db_admin_user_name****",
-                    "dbInstallDBRootPass" => "****dap_db_admin_user_pass****",
-              ) , ) ,
-              array ( "Version" => array(
-                    "versionExecute" => true,
-                    "versionAppRootDirectory" => "<%tpl.php%>dap_proj_cont_dir</%tpl.php%>",
-                    "versionArrayPointToRollback" => "0",
-                    "versionLimit" => "<%tpl.php%>dap_version_num_revisions</%tpl.php%>",
-              ) , ) ,
-              array ( "ApacheControl" => array(
-                  "apacheCtlRestartExecute" => true,
-              ) , ) ,
+
+              array ( "Logging" => array( "log" => array( "log-message" => "Lets initialize our new download directory as a dapper project"), ) ),
+              array ( "Project" => array( "init" => array(), ) , ) ,
+
+              array ( "Logging" => array( "log" => array( "log-message" => "Next create our host file entry for our local URL"), ) ),
+              array ( "HostEditor" => array( "add" => array (
+                  "guess" => true,
+                  "host-name" => "<%tpl.php%>dap_apache_vhost_url</%tpl.php%>.local",
+              ), ), ),
+
+              array ( "Logging" => array( "log" => array( "log-message" => "Next create our virtual host"), ) ),
+              array ( "ApacheVHostEditor" => array( "add" => array (
+                  "guess" => true,
+                  "vhe-docroot" => "<%tpl.php%>dap_proj_cont_dir</%tpl.php%>",
+                  "vhe-url" => "<%tpl.php%>dap_apache_vhost_url</%tpl.php%>",
+                  "vhe-ip-port" => "<%tpl.php%>dap_apache_vhost_ip</%tpl.php%>",
+                  "vhe-vhost-dir" => "/etc/apache2/sites-available",
+                  "vhe-template" => $this->getTemplate(),
+                  "vhe-file-ext" => "",
+              ), ), ),
+
+              array ( "Logging" => array( "log" => array( "log-message" => "Next ensure our db file configuration is reset to blank" ), ), ),
+              array ( "DBConfigure" => array( "<%tpl.php%>dap_db_platform</%tpl.php%>-reset" => array(
+                  "parent-path" => "<%tpl.php%>dap_proj_cont_dir</%tpl.php%>",
+                  "platform" => "<%tpl.php%>dap_db_platform</%tpl.php%>",
+              ), ), ),
+
+              array ( "Logging" => array( "log" => array("log-message" => "Next configure our projects db configuration file"), ) ),
+              array ( "DBConfigure" => array( "<%tpl.php%>dap_db_platform</%tpl.php%>-conf" => array(
+                  "parent-path" => "<%tpl.php%>dap_proj_cont_dir</%tpl.php%>",
+                  "mysql-host" => "<%tpl.php%>dap_db_ip_address</%tpl.php%>",
+                  "mysql-user" => "<%tpl.php%>dap_db_app_user_name</%tpl.php%>",
+                  "mysql-pass" => "<%tpl.php%>dap_db_app_user_pass</%tpl.php%>",
+                  "mysql-db" => "<%tpl.php%>dap_db_name</%tpl.php%>",
+                  "mysql-platform" => "<%tpl.php%>dap_db_platform</%tpl.php%>",
+                  "mysql-admin-user" => "<%tpl.php%>dap_db_admin_user_name</%tpl.php%>",
+                  "mysql-admin-pass" => "<%tpl.php%>dap_db_admin_user_pass</%tpl.php%>",
+              ), ) , ) ,
+
+              array ( "Logging" => array( "log" => array( "log-message" => "Now lets drop our current database if it exists"), ) ),
+              array ( "DBInstall" => array( "drop" => array(
+                  "parent-path" => "<%tpl.php%>dap_proj_cont_dir</%tpl.php%>",
+                  "mysql-host" => "<%tpl.php%>dap_db_ip_address</%tpl.php%>",
+                  "mysql-user" => "<%tpl.php%>dap_db_app_user_name</%tpl.php%>",
+                  "mysql-pass" => "<%tpl.php%>dap_db_app_user_pass</%tpl.php%>",
+                  "mysql-db" => "<%tpl.php%>dap_db_name</%tpl.php%>",
+                  "mysql-platform" => "<%tpl.php%>dap_db_platform</%tpl.php%>",
+                  "mysql-admin-user" => "<%tpl.php%>dap_db_admin_user_name</%tpl.php%>",
+                  "mysql-admin-pass" => "<%tpl.php%>dap_db_admin_user_pass</%tpl.php%>",
+              ), ), ),
+
+              array ( "Logging" => array( "log" => array("log-message" => "Now lets install our database"), ), ),
+              array ( "DBInstall" => array( "install" => array(
+                  "parent-path" => "<%tpl.php%>dap_proj_cont_dir</%tpl.php%>",
+                  "mysql-host" => "<%tpl.php%>dap_db_ip_address</%tpl.php%>",
+                  "mysql-user" => "<%tpl.php%>dap_db_app_user_name</%tpl.php%>",
+                  "mysql-pass" => "<%tpl.php%>dap_db_app_user_pass</%tpl.php%>",
+                  "mysql-db" => "<%tpl.php%>dap_db_name</%tpl.php%>",
+                  "mysql-platform" => "<%tpl.php%>dap_db_platform</%tpl.php%>",
+                  "mysql-admin-user" => "<%tpl.php%>dap_db_admin_user_name</%tpl.php%>",
+                  "mysql-admin-pass" => "<%tpl.php%>dap_db_admin_user_pass</%tpl.php%>",
+              ), ), ),
+
+              array ( "Logging" => array( "log" => array( "log-message" => "Now lets restart Apache so we are serving our new application version", ), ), ),
+              array ( "ApacheControl" => array( "restart" => array(
+                  "guess" => true,
+              ), ), ),
+
 	      );
 
 	  }
 
 
- // This function will set the vhost template for your Virtual Host
- // You need to call this from your constructor
- private function setVHostTemplate() {
-   $this->steps[2]["ApacheVHostEditor"]["virtualHostEditorAdditionTemplateData"] =
-  <<<'TEMPLATE'
- NameVirtualHost ****IP ADDRESS****:80
+    private function getTemplate() {
+        $template =
+            <<<'TEMPLATE'
+           NameVirtualHost ****IP ADDRESS****:80
  <VirtualHost ****IP ADDRESS****:80>
    ServerAdmin webmaster@localhost
  	ServerName ****SERVER NAME****
- 	DocumentRoot ****WEB ROOT****src
- 	<Directory ****WEB ROOT****src>
+ 	DocumentRoot ****WEB ROOT****/src
+ 	<Directory ****WEB ROOT****/src>
  		Options Indexes FollowSymLinks MultiViews
  		AllowOverride All
  		Order allow,deny
@@ -111,12 +117,12 @@ class AutoPilotConfigured extends AutoPilot {
  <VirtualHost ****IP ADDRESS****:443>
  	 ServerAdmin webmaster@localhost
  	 ServerName ****SERVER NAME****
- 	 DocumentRoot ****WEB ROOT****src
-     SSLEngine on
-     SSLCertificateFile /etc/ssl/certs/ssl-cert-snakeoil.pem
-     SSLCertificateKeyFile /etc/ssl/private/ssl-cert-snakeoil.key
+ 	 DocumentRoot ****WEB ROOT****/src
+   # SSLEngine on
+ 	 # SSLCertificateFile /etc/apache2/ssl/ssl.crt
+   # SSLCertificateKeyFile /etc/apache2/ssl/ssl.key
    # SSLCertificateChainFile /etc/apache2/ssl/bundle.crt
- 	 <Directory ****WEB ROOT****src>
+ 	 <Directory ****WEB ROOT****/src>
  		 Options Indexes FollowSymLinks MultiViews
 		AllowOverride All
 		Order allow,deny
@@ -126,7 +132,10 @@ class AutoPilotConfigured extends AutoPilot {
   CustomLog /var/log/apache2/access.log combined
   </VirtualHost>
 TEMPLATE;
-}
+
+        return $template ;
+    }
+
 
 
 }
