5d60a65204e766a4073418999f4270be5b9e6c15
David Amanshia <phpengine@hotmail.co.uk>
2014-07-16 11:30:28 +0100

    autopilots are handling parameters correctly

--- src/Core/AutoPilot.php
+++ src/Core/AutoPilot.php
@@ -2,12 +2,18 @@
 
 Namespace Core ;
 
-class AutoPilot {
+use Model\Base;
+
+class AutoPilot extends Base {
 
     public $params ;
     protected $appHomeDir ;
 
-    public function __construct() {
+    public function __construct($params = array()) {
+        global $argv ;
+        $argv_or_array = (isset($argv)) ? $argv : array() ;
+        $argv_and_params = array_merge($argv_or_array, $params) ;
+        parent::__construct($argv_and_params);
         $this->setProperties();
     }
 
