b1449c443cda4f302c57873c220ae6b68b5b985a
David Amanshia <phpengine@hotmail.co.uk>
2014-07-16 15:41:49 +0100

    builder templates updated the syntax of the long builderfy command

--- src/Modules/Builderfy/Templates/Autopilots/environment-invoke-continuous-staging.php
+++ src/Modules/Builderfy/Templates/Autopilots/environment-invoke-continuous-staging.php
@@ -41,10 +41,27 @@ class AutoPilotConfigured extends AutoPilot {
     }
 
     private function setSSHData() {
+        $longCommand =
+            'sudo dapperstrano builderfy continuous-staging --yes ' .
+            '--jenkins-home="<%tpl.php%>jenkins-home</%tpl.php%>" ' .
+            '--target-job-name="<%tpl.php%>target-job-name</%tpl.php%>" ' .
+            '--project-description="<%tpl.php%>project_description</%tpl.php%>" '.
+            '--primary-scm-url="<%tpl.php%>source_scm_url</%tpl.php%>" '.
+            '--source-branch-spec="<%tpl.php%>source_branch_spec</%tpl.php%>" '.
+            '--source-scm-url="<%tpl.php%>source_scm_url</%tpl.php%>" '.
+            '--days-to-keep="<%tpl.php%>days_to_keep</%tpl.php%>" '.
+            '--amount-to-keep="<%tpl.php%>num_to_keep</%tpl.php%>" '.
+            '--autopilot-test-invoke-install-file="<%tpl.php%>autopilot-test-invoke-install-file</%tpl.php%>" '.
+            '--autopilot-test-invoke-dbconf-install-file="<%tpl.php%>autopilot-test-invoke-dbconf-install-file</%tpl.php%>" '.
+            '--autopilot-test-invoke-dbinstall-install-file="<%tpl.php%>autopilot-test-invoke-dbinstall-install-file</%tpl.php%>" '.
+            '--data-handling-type="<%tpl.php%>data-handling</%tpl.php%>" '.
+            '--error-email="<%tpl.php%>error-email</%tpl.php%>" '.
+            '--no-autopilots' ;
+
         $sshData = <<<"SSHDATA"
 cd <%tpl.php%>gen_env_tmp_dir</%tpl.php%>
 sudo rm -rf <%tpl.php%>jenkins-home</%tpl.php%>/jobs/<%tpl.php%>target-job-name</%tpl.php%>*
-sudo dapperstrano builderfy continuous-staging --yes --jenkins-home="<%tpl.php%>jenkins-home</%tpl.php%>" --target-job-name="<%tpl.php%>target-job-name</%tpl.php%>" --project-description="<%tpl.php%>project_description</%tpl.php%>" --primary-scm-url="<%tpl.php%>source_scm_url</%tpl.php%>" --source-branch-spec="<%tpl.php%>source_branch_spec</%tpl.php%>" --source-scm-url="<%tpl.php%>source_scm_url</%tpl.php%>" --days-to-keep="<%tpl.php%>days_to_keep</%tpl.php%>" --amount-to-keep="<%tpl.php%>num_to_keep</%tpl.php%>" --autopilot-test-invoke-install-file="<%tpl.php%>autopilot-test-invoke-install-file</%tpl.php%>" --data-handling-type="<%tpl.php%>data-handling</%tpl.php%>" --error-email="<%tpl.php%>error-email</%tpl.php%>"
+$longCommand
 sudo service jenkins restart
 SSHDATA;
         return $sshData ;
