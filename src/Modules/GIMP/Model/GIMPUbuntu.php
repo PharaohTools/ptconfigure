dbe3890e947a859b175fe3e0c7988f6d28705003
David Amanshia <phpengine@hotmail.co.uk>
2014-07-23 23:23:54 +0100

    updated ubuntu model for gimp to have

--- src/Modules/GIMP/Model/GIMPUbuntu.php
+++ src/Modules/GIMP/Model/GIMPUbuntu.php
@@ -2,14 +2,13 @@
 
 Namespace Model;
 
-//@todo if we can use a wget/binary method like selenium or gitbucket then we can easily use across other linux os
 class GIMPUbuntu extends BaseLinuxApp {
 
     // Compatibility
     public $os = array("Linux") ;
     public $linuxType = array("Debian") ;
     public $distros = array("Ubuntu") ;
-    public $versions = array("11.04", "11.10", "12.04", "12.10", "13.04") ;
+    public $versions = array("11.04", "11.10", "12.04", "12.10", "13.04", "13.10", "14.04", "14.10") ;
     public $architectures = array("any") ;
 
     // Model Group
@@ -18,37 +17,17 @@ class GIMPUbuntu extends BaseLinuxApp {
     public function __construct($params) {
         parent::__construct($params);
         $this->autopilotDefiner = "GIMP";
-        $this->installCommands = $this->getInstallCommands();
-        $this->uninstallCommands = array( "apt-get remove -y jenkins" );
-        $this->programDataFolder = "/var/lib/jenkins"; // command and app dir name
-        $this->programNameMachine = "jenkins"; // command and app dir name
+        $this->installCommands = array( "apt-get install -y gimp" );
+        $this->uninstallCommands = array( "apt-get remove -y gimp" );
+        $this->programDataFolder = "/var/lib/gimp"; // command and app dir name
+        $this->programNameMachine = "gimp"; // command and app dir name
         $this->programNameFriendly = " ! GIMP !"; // 12 chars
         $this->programNameInstaller = "GIMP";
-        $this->statusCommand = "sudo jenkins -v" ;
-        $this->versionInstalledCommand = "sudo apt-cache policy jenkins" ;
-        $this->versionRecommendedCommand = "sudo apt-cache policy jenkins" ;
-        $this->versionLatestCommand = "sudo apt-cache policy jenkins" ;
+        $this->statusCommand = "sudo gimp -v" ;
+        $this->versionInstalledCommand = "sudo apt-cache policy gimp" ;
+        $this->versionRecommendedCommand = "sudo apt-cache policy gimp" ;
+        $this->versionLatestCommand = "sudo apt-cache policy gimp" ;
         $this->initialize();
-    }
-
-    protected function getInstallCommands() {
-        $ray = array(
-            array("command" => array(
-                "cd /tmp" ,
-                "wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -",
-                "echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list",
-                "apt-get update -y",
-                "apt-get install -y jenkins") )
-        ) ;
-        if (isset($this->params["with-http-port-proxy"]) && $this->params["with-http-port-proxy"]==true) {
-            $dapperAuto = $this->getDapperAutoPath() ;
-            $ray[0]["command"][5] = "sudo dapperstrano autopilot execute --autopilot-file=$dapperAuto" ; }
-        return $ray ;
-    }
-
-    private function getDapperAutoPath() {
-        $path = dirname(dirname(__FILE__)).'/Autopilots/Dapperstrano/proxy-8080-to-80.php' ;
-        return $path ;
     }
 
     public function versionInstalledCommandTrimmer($text) {
