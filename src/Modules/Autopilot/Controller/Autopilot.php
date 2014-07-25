53f0a7e3358dfd137deaff6974ddb33718204f6c
David Amanshia <phpengine@hotmail.co.uk>
2014-07-25 11:54:46 +0100

    auto controller for params

--- src/Modules/Autopilot/Controller/Autopilot.php
+++ src/Modules/Autopilot/Controller/Autopilot.php
@@ -10,29 +10,29 @@ class Autopilot extends Base {
         // if we don't have an object, its an array of errors
         if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
 
-        $action = $pageVars["route"]["action"];
-        if ($action=="install" || $action=="execute" || $action=="x") {
-            if ( isset($thisModel->params["autopilot-file"]) && strlen($thisModel->params["autopilot-file"]) > 0 ) {
-                $autoPilot = $this->loadAutoPilot($thisModel->params["autopilot-file"]);
-                if ( $autoPilot!==null ) {
-                    $autoPilotExecutor = new \Controller\AutopilotExecutor();
-                    // get params from the base model to inject into the loaded autopilot object
-                    $autoPilot->params = $thisModel->params ;
-                    return $autoPilotExecutor->execute($pageVars, $autoPilot); }
+            $action = $pageVars["route"]["action"];
+            if ($action=="install" || $action=="execute" || $action=="x") {
+                if ( isset($thisModel->params["autopilot-file"]) && strlen($thisModel->params["autopilot-file"]) > 0 ) {
+                    $autoPilot = $this->loadAutoPilot($thisModel->params);
+                    if ( $autoPilot!==null ) {
+                        $autoPilotExecutor = new \Controller\AutopilotExecutor();
+                        // get params from the base model to inject into the loaded autopilot object
+                        $autoPilot->params = $thisModel->params ;
+                        return $autoPilotExecutor->execute($pageVars, $autoPilot); }
+                    else {
+                        $this->content["messages"][] = "There was a problem with the autopilot file specified"; } }
                 else {
-                    $this->content["messages"][] = "There was a problem with the autopilot file specified"; } }
-            else {
-                $this->content["messages"][] = "Parameter --autopilot-file is required"; } }
+                    $this->content["messages"][] = "Parameter --autopilot-file is required"; } }
 
-        else if ($action=="help") {
-            $helpModel = new \Model\Help();
-            $this->content["helpData"] = $helpModel->getHelpData($pageVars["route"]["control"]);
-            return array ("type"=>"view", "view"=>"help", "pageVars"=>$this->content); }
+            else if ($action=="help") {
+                $helpModel = new \Model\Help();
+                $this->content["helpData"] = $helpModel->getHelpData($pageVars["route"]["control"]);
+                return array ("type"=>"view", "view"=>"help", "pageVars"=>$this->content); }
 
         else {
             $this->content["messages"][] = "Invalid Action - Action does not Exist for Autopilot"; }
 
-      return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content);
+        return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content);
 
     }
 
@@ -40,7 +40,7 @@ class Autopilot extends Base {
         $autoPilotFileName = escapeshellcmd($params["autopilot-file"]);
         $autoPilotFilePath = getcwd().'/'.$autoPilotFileName;
         $defaultFolderToCheck = str_replace("src/Controller",
-          "build/config/dapperstrano", dirname(__FILE__));
+          "build/config/dapperstrano/autopilots", dirname(__FILE__));
         $defaultName = $defaultFolderToCheck.'/'.$autoPilotFileName.".php";
         if (file_exists($autoPilotFileName)) {
             require_once($autoPilotFileName); }
@@ -51,7 +51,7 @@ class Autopilot extends Base {
         else if (file_exists($autoPilotFilePath)) {
             require_once($autoPilotFilePath); }
         $autoPilot = (class_exists('\Core\AutoPilotConfigured')) ?
-          new \Core\AutoPilotConfigured() : null ;
+          new \Core\AutoPilotConfigured($params) : null ;
         return $autoPilot;
     }
 
