9fa5aab490ca59e553630fbfd2d0cd06b20424c8
David Amanshia <phpengine@hotmail.co.uk>
2014-07-29 14:10:36 +0100

    added run command module

---
+++ src/Modules/RunCommand/info.RunCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+Namespace Info;
+
+class RunCommandInfo extends CleopatraBase {
+
+    public $hidden = false;
+
+    public $name = "Execute a Command";
+
+    public function __construct() {
+      parent::__construct();
+    }
+
+    public function routesAvailable() {
+      return array( "RunCommand" =>  array("install", "execute", "help") );
+    }
+
+    public function routeAliases() {
+      return array("runcommand"=>"RunCommand", "run-command"=>"RunCommand");
+    }
+
+    public function helpDefinition() {
+      $help = <<<"HELPDATA"
+  This allows you to execute an Operating System command. This would primarily be used in an Autopilot.
+
+  RunCommand, runcommand, run-command
+
+        - execute
+        Execute a Command
+        example: cleopatra run-command --yes --command="ls -lah /tmp" --run-as-user="ubuntu" --background
+
+HELPDATA;
+      return $help ;
+    }
+
+}
