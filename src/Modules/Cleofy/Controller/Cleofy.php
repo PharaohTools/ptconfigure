708760bcafecd345df50efc2c20a8ea69d301d63
David Amanshia <phpengine@hotmail.co.uk>
2014-07-30 16:13:27 +0100

    calling model groups by array

--- src/Modules/Cleofy/Controller/Cleofy.php
+++ src/Modules/Cleofy/Controller/Cleofy.php
@@ -19,7 +19,6 @@ class Cleofy extends Base {
 
         if (in_array($action, array("install-generic-autopilots") )) {
             $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "GenericAutos") ;
-            // if we don't have an object, its an array of errors
             if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
             $this->content["result"] = $thisModel->askAction($action);
             return array ("type"=>"view", "view"=>"cleofyGenAutos", "pageVars"=>$this->content); }
@@ -28,42 +27,19 @@ class Cleofy extends Base {
             $this->content["result"] = $thisModel->askWhetherToCleofy();
             return array ("type"=>"view", "view"=>"cleofy", "pageVars"=>$this->content); }
 
-        if ($action=="medium") {
-            $this->content["result"] = $thisModel->askWhetherToCleofy();
-            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "Medium") ;
-            // if we don't have an object, its an array of errors
-            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
-            return array ("type"=>"view", "view"=>"cleofy", "pageVars"=>$this->content); }
+        $actionsToModelGroups = array(
+            "medium" => "Medium", "medium-web" => "MediumWeb", "db-cluster" => "DBCluster", "tiny" => "Tiny",
+            "workstation" => "Workstation" ) ;
 
-        if ($action=="medium-web") {
+        if (in_array($action, array_keys($actionsToModelGroups))) {
             $this->content["result"] = $thisModel->askWhetherToCleofy();
-            return array ("type"=>"view", "view"=>"cleofy", "pageVars"=>$this->content); }
-
-        if ($action=="db-cluster") {
-            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "DBCluster") ;
-            // if we don't have an object, its an array of errors
+            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, $actionsToModelGroups[$action]) ;
             if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
-            $this->content["result"] = $thisModel->askWhetherToCleofy();
-            return array ("type"=>"view", "view"=>"cleofy", "pageVars"=>$this->content); }
-
-        if ($action=="tiny") {
-            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "Tiny") ;
-            // if we don't have an object, its an array of errors
-            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
-            $this->content["result"] = $thisModel->askWhetherToCleofy();
-            return array ("type"=>"view", "view"=>"cleofy", "pageVars"=>$this->content); }
-
-        if ($action=="workstation") {
-            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "Workstation") ;
-            // if we don't have an object, its an array of errors
-            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
-            $this->content["result"] = $thisModel->askWhetherToCleofy();
             return array ("type"=>"view", "view"=>"cleofy", "pageVars"=>$this->content); }
 
         $this->content["messages"][] = "Invalid Cleofy Action";
         return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content);
 
     }
-
 
 }
---
+++ src/Modules/Cleofy/Model/CleofyMediumWeb.php
@@ -0,0 +1,99 @@
+<?php
+
+Namespace Model;
+
+// @todo shouldnt this extend base templater? is it missing anything?
+class CleofyMedium extends Base {
+
+    // Compatibility
+    public $os = array("Linux") ;
+    public $linuxType = array("Debian") ;
+    public $distros = array("Ubuntu") ;
+    public $versions = array("12.04", "12.10") ;
+    public $architectures = array("32", "64") ;
+
+    // Model Group
+    public $modelGroup = array("Medium") ;
+
+    private $environments ;
+    private $environmentReplacements ;
+
+    public function __construct($params) {
+      parent::__construct($params);
+    }
+
+    public function askWhetherToCleofy() {
+        if ($this->askToScreenWhetherToCleofy() != true) { return false; }
+        $this->setEnvironmentReplacements() ;
+        $this->getEnvironments() ;
+        $this->doCleofy() ;
+        return true;
+    }
+
+    public function askToScreenWhetherToCleofy() {
+        if (isset($this->params["yes"]) && $this->params["yes"]==true) { return true ; }
+        $question = 'Cleofy This?';
+        return self::askYesOrNo($question, true);
+    }
+
+    public function setEnvironmentReplacements() {
+        $this->environmentReplacements =
+            array( "cleo" => array(
+               // array("var"=>"dap_proj_cont_dir", "friendly_text"=>"Project Container directory, (inc slash)"),
+            ) );
+    }
+
+    public function getEnvironments() {
+        $environmentConfigModelFactory = new EnvironmentConfig();
+        $environmentConfigModel = $environmentConfigModelFactory->getModel($this->params);
+        $environmentConfigModel->askWhetherToEnvironmentConfig($this->environmentReplacements) ;
+        $this->environments = $environmentConfigModel->environments ;
+    }
+
+    public function getServerArrayText($serversArray) {
+        $serversText = "";
+        foreach($serversArray as $serverArray) {
+            $serversText .= 'array(';
+            $serversText .= '"target" => "'.$serverArray["target"].'", ';
+            $serversText .= '"user" => "'.$serverArray["user"].'", ';
+            $serversText .= '"pword" => "'.$serverArray["password"].'", ';
+            $serversText .= '),'."\n"; }
+        return $serversText;
+    }
+
+    private function doCleofy() {
+      $templatesDir = str_replace("Model", "Templates/EnvSpecific", dirname(__FILE__) ) ;
+      $templates = scandir($templatesDir);
+      foreach ($this->environments as $environment) {
+        foreach ($templates as $template) {
+          if (!in_array($template, array(".", ".."))) {
+              $templatorFactory = new \Model\Templating();
+              $templator = $templatorFactory->getModel($this->params);
+              $newFileName = str_replace("environment", $environment["any-app"]["gen_env_name"], $template ) ;
+              $autosDir = getcwd().'/build/config/cleopatra/cleofy/autopilots/generated';
+              $targetLocation = $autosDir.DIRECTORY_SEPARATOR.$newFileName ;
+              $templator->template(
+                  file_get_contents($templatesDir.DIRECTORY_SEPARATOR.$template),
+                  array(
+                      "gen_srv_array_text" => $this->getServerArrayText($environment["servers"]) ,
+                      "env_name" => $environment["any-app"]["gen_env_name"],
+                      "first_server_target" => $environment["servers"][0]["target"],
+                      "stage_web_nodes_env" => $this->getEnvName("stage web") ,
+                      "stage_db_nodes_env" => $this->getEnvName("stage database") ,
+                      "prod_web_nodes_env" => $this->getEnvName("prod web") ,
+                      "prod_db_nodes_env" => $this->getEnvName("prod database") ,
+                  ),
+                  $targetLocation );
+          echo $targetLocation."\n"; } } }
+    }
+
+    public function getEnvName($envType) {
+        if (isset($this->params["$envType-nodes-env"])) {
+            $this->params["$envType-nodes-environment"] = $this->params["$envType-nodes-env"] ; }
+        if (isset($this->params["$envType-nodes-environment"])) { return $this->params["$envType-nodes-environment"] ; }
+        $question = "Enter name of environment with your ".ucfirst($envType)." nodes" ;
+        $this->params["$envType-nodes-environment"] = $this->askForInput($question) ;
+        return $this->params["$envType-nodes-environment"] ;
+    }
+
+}
