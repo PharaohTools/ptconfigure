82e4f3a8ca9c73dc5a60bcef5c8a599b1f309b46
David Amanshia <phpengine@hotmail.co.uk>
2014-08-05 17:17:18 +0100

    updayted he ping module

--- src/Modules/Ping/Model/PingUbuntu.php
+++ src/Modules/Ping/Model/PingUbuntu.php
@@ -13,47 +13,112 @@ class PingUbuntu extends BaseLinuxApp {
 
     // Model Group
     public $modelGroup = array("Default") ;
-    protected $portNumber ;
-    protected $actionsToMethods = array( "is-responding" => "performPingCheck" ) ;
+    protected $target ;
+    protected $interval ;
+    protected $maxWait ;
+    protected $actionsToMethods = array(
+        "once" => "performPingOnce",
+        "ten" => "performTenPings",
+        "until-responding" => "performPingsUntil",
+    ) ;
 
     public function __construct($params) {
         parent::__construct($params);
         $this->autopilotDefiner = "Ping";
         $this->programDataFolder = "";
-        $this->programNameMachine = "port"; // command and app dir name
+        $this->programNameMachine = "ping"; // command and app dir name
         $this->programNameFriendly = "!Ping!!"; // 12 chars
         $this->programNameInstaller = "Ping";
         $this->initialize();
     }
 
-    protected function performPingCheck() {
-        $this->setPing();
-        return $this->getPingStatus();
+    protected function performPingOnce() {
+        require_once ("../Libraries/JJG/Ping.php") ;
+        $this->setTarget();
+        $this->setInterval();
+        $this->doOnePing();
     }
 
-    public function setPing($portNumber = null) {
-        if (isset($portNumber)) {
-            $this->portNumber = $portNumber; }
-        else if (isset($this->params["port-number"])) {
-            $this->portNumber = $this->params["port-number"]; }
-        else if (isset($this->params["port"])) {
-            $this->portNumber = $this->params["port"]; }
+    protected function performTenPings() {
+        require_once ("../Libraries/JJG/Ping.php") ;
+        $this->setTarget();
+        $this->setInterval();
+        $this->doTenPings();
+    }
+
+    protected function performPingsUntil() {
+        require_once ("../Libraries/JJG/Ping.php") ;
+        $this->setTarget();
+        $this->setInterval();
+        $this->setMaxWait();
+        $this->doPingsUntil();
+    }
+
+    protected function setTarget($target = null) {
+        if (isset($target)) {
+            $this->target = $target; }
+        else if (isset($this->params["target"])) {
+            $this->target = $this->params["target"]; }
         else {
-            $this->portNumber = self::askForInput("Enter Ping Number:", true); }
+            $this->target = self::askForInput("Enter Target: ", true); }
     }
 
-    private function getPingStatus() {
-        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-        if ($socket === false) { throw new Exception("Cannot create socket"); }
-        $result = @socket_connect($socket, '127.0.0.1', $this->number);
+    protected function setInterval() {
+        if (isset($this->params["guess"])) {
+            $this->interval = "2" ; }
+        else if (isset($this->params["interval"])) {
+            $this->interval = $this->params["interval"]; }
+        else {
+            $this->interval = self::askForInput("Enter Interval: ", true); }
+    }
+
+    protected function setMaxWait() {
+        if (isset($this->params["guess"])) {
+            $this->maxWait = "2" ; }
+        else if (isset($this->params["max-wait"])) {
+            $this->maxWait = $this->params["max-wait"]; }
+        else {
+            $this->maxWait = self::askForInput("Enter Max Wait: ", true); }
+    }
+
+    private function doOnePing() {
         $loggingFactory = new \Model\Logging();
         $logging = $loggingFactory->getModel($this->params);
-        if ($result !== false) {
-            $logging->log("Ping {$this->portNumber} is responding") ;
-            return true; }
+        $ping = new \JJG\Ping($this->target);
+        $latency = $ping->ping();
+        if ($latency !== false) {
+            $logging->log('Ping Latency is ' . $latency . ' ms') ; }
         else {
-            $logging->log("Ping {$this->portNumber} is not responding") ;
-            return false;}
+            $logging->log("Ping Host could not be reached.") ;}
+    }
+
+    private function doTenPings() {
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params);
+        $ping = new \JJG\Ping($this->target);
+        for ($i = 0; $i < 10; $i ++) {
+            $latency = $ping->ping();
+            if ($latency !== false) {
+                $logging->log('Ping Latency is ' . $latency . ' ms') ; }
+            else {
+                $logging->log("Ping Host could not be reached.") ; }
+            sleep($this->interval) ; }
+    }
+
+    private function doPingsUntil() {
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params);
+        $ping = new \JJG\Ping($this->target);
+        $totalTime = 0 ;
+        while ($totalTime < $this->maxWait) {
+            $latency = $ping->ping();
+            if ($latency !== false) {
+                $logging->log('Ping Latency is ' . $latency . ' ms') ;
+                break ; }
+            else {
+                $logging->log("Ping Host could not be reached.") ; }
+            sleep($this->interval) ;
+            $totalTime = $totalTime + $this->interval ; }
     }
 
 }
