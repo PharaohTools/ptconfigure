101ea919970a6a5bf0be96f1e62887c6f9493158
David Amanshia <phpengine@hotmail.co.uk>
2014-08-21 20:22:33 +0100

    phlagrant host cleofy cm uses papyrus local correctly

--- src/Modules/Cleofy/Templates/Generic/Phlagrant/cleofy-cm-phlagrant-host.php
+++ src/Modules/Cleofy/Templates/Generic/Phlagrant/cleofy-cm-phlagrant-host.php
@@ -6,8 +6,12 @@ class AutoPilotConfigured extends AutoPilot {
 
     public $steps ;
     protected $myUser ;
+    protected $papyrus ;
+    protected $sftpParams ;
 
     public function __construct() {
+        $this->loadPapyrusLocal();
+        $this->setSftpParams();
         $this->setSteps();
     }
 
@@ -22,8 +26,11 @@ class AutoPilotConfigured extends AutoPilot {
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets push over our user SSH Keys" ),),),
                 array ( "SFTP" => array( "put" => array(
                     "guess" => true,
-                    "source" => "/home/{$this->myUser}/.ssh/id_rsa",
-                    "target" => "/home/phlagrant/.ssh/id_rsa"
+                    "source" => $this->sftpParams["source"] ,
+                    "target" => $this->sftpParams["target"],
+                    "port" => $this->sftpParams["port"],
+                    "timeout" => $this->sftpParams["timeout"],
+                    "servers" => $this->sftpParams["servers"],
                 ),),),
 
                 array ( "Logging" => array( "log" => array(
@@ -32,6 +39,32 @@ class AutoPilotConfigured extends AutoPilot {
 
             );
 
+    }
+
+    protected function setSftpParams() {
+        $srv = array(
+            "user" => $this->papyrus["username"] ,
+            "password" => $this->papyrus["password"] ,
+            "target" => $this->papyrus["target"] );
+        $this->sftpParams["yes"] = true ;
+        $this->sftpParams["guess"] = true ;
+        $this->sftpParams["servers"] = serialize(array($srv)) ;
+        $this->sftpParams["source"] = "/home/{$this->myUser}/.ssh/id_rsa" ;
+        $this->sftpParams["target"] = "/home/phlagrant/.ssh/id_rsa" ;
+        $this->sftpParams["port"] = (isset($this->papyrus["port"])) ? $this->papyrus["port"] : 22 ;
+        $this->sftpParams["timeout"] = (isset($this->papyrus["timeout"])) ? $this->papyrus["timeout"] : 30 ; # your papyrus should have this from extending base class
+    }
+
+    // @todo $boxname should read from the phlagrant file, probably. it should definitely not be hardcoded
+    protected function loadPapyrusLocal() {
+        $boxname = "phlagrant-box" ;
+        $fp = getcwd()."/papyrusfilelocal" ;
+        $pl = file_get_contents($fp) ;
+        $pla = unserialize($pl);
+        if (is_array($pla) && count($pla)>0) {
+            $this->papyrus = $pla[$boxname] ; }
+        else {
+            $this->papyrus = array() ; }
     }
 
 }
