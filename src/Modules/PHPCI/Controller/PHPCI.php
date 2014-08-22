7a29f587ce6baf2e4b4036170823d41c01407aef
David Amanshia <phpengine@hotmail.co.uk>
2014-08-22 19:00:31 +0100

    phpci added correct controllers

--- src/Modules/PHPCI/Controller/PHPCI.php
+++ src/Modules/PHPCI/Controller/PHPCI.php
@@ -4,4 +4,33 @@ Namespace Controller ;
 
 class PHPCI extends Base {
 
+    public function execute($pageVars) {
+
+        $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
+        // if we don't have an object, its an array of errors
+        if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+        $isDefaultAction = self::checkDefaultActions($pageVars, array(), $thisModel) ;
+        if ( is_array($isDefaultAction) ) { return $isDefaultAction; }
+
+        $action = $pageVars["route"]["action"];
+
+        if ($action=="config-default" || $action=="default-config") {
+            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "DefaultConf") ;
+            // if we don't have an object, its an array of errors
+            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+            $this->content["result"] = $thisModel->askInstall();
+            return array ("type"=>"view", "view"=>"appInstall", "pageVars"=>$this->content); }
+
+        if ($action=="install-default-database") {
+            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "DefaultDBInstall") ;
+            // if we don't have an object, its an array of errors
+            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+            $this->content["result"] = $thisModel->askInstall();
+            return array ("type"=>"view", "view"=>"appInstall", "pageVars"=>$this->content); }
+
+        $this->content["messages"][] = "Invalid Mysql Galera Action";
+        return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content);
+
+    }
+
 }
