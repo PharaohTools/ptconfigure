11005b7df48ac7db790e796d37501bcba10820c5
David Amanshia <phpengine@hotmail.co.uk>
2014-08-23 06:48:23 +0100

    phpci info update

--- src/Modules/PHPCI/info.PHPCI.php
+++ src/Modules/PHPCI/info.PHPCI.php
@@ -28,18 +28,24 @@ class PHPCIInfo extends CleopatraBase {
   PHPCI, phpci
 
         - install
-        Installs PHPCI through apt-get
+        Installs PHPCI through git, with its dependencies
         example: cleopatra phpci install
 
         - config-default, default-config
-        Installs PHPCI through apt-get
+        Installs a default config.yml file for PHPCI
         example: cleopatra phpci config-default --yes --guess
 
         - install-default-database
-        Installs PHPCI through apt-get
+        Installs a default database and user for PHPCI
         example: cleopatra phpci install-default-database --yes --guess
             --mysql-admin-user="root" # guess will provide root
             --mysql-admin-pass="some-pass" # guess will provide cleopatra
+
+  You can install a complete local version of PHPCI with the following:
+
+  sudo cleopatra phpci install --yes --guess
+  sudo cleopatra phpci install-default-database --yes --guess
+  sudo cleopatra phpci config-default --yes --guess
 
 HELPDATA;
       return $help ;
