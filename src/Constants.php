34fb55183cbf28510abf07b36cba80433409a21e
David Amanshia <phpengine@hotmail.co.uk>
2014-09-17 19:11:22 +0100

    updated windows constants

--- src/Constants.php
+++ src/Constants.php
@@ -6,7 +6,7 @@
 
 if (PHP_OS == "Windows") {
     define('DS', "\\");
-    define('BASE_TEMP_DIR', 'C:\\tmp\\'); }
+    define('BASE_TEMP_DIR', getenv("SystemDrive").DS.'Temp'.DS); }
 else if (in_array(PHP_OS, array("Linux", "Solaris", "FreeBSD", "OpenBSD", "Darwin"))) {
     define('DS', "/");
     define('BASE_TEMP_DIR', '/tmp/'); }
