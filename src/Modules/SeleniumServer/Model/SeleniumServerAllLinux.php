7ba8f22c5b82128f545e7508ba0f8bcb72b55ccc
David Amanshia <phpengine@hotmail.co.uk>
2014-09-17 20:32:45 +0100

    selenium server guesses properly

--- src/Modules/SeleniumServer/Model/SeleniumServerAllLinux.php
+++ src/Modules/SeleniumServer/Model/SeleniumServerAllLinux.php
@@ -13,30 +13,22 @@ class SeleniumServerAllLinux extends BaseLinuxApp {
 
     // Model Group
     public $modelGroup = array("Default") ;
+    public $sv ;
 
     // @todo parameterise the selenium version
     // @todo ensure wget is installed
     public function __construct($params) {
         parent::__construct($params);
-        $sv = $this->askForSeleniumVersion();
         $this->autopilotDefiner = "SeleniumServer";
         $this->installCommands = array(
+            array("method"=> array("object" => $this, "method" => "askForSeleniumVersion", "params" => array()) ),
             array("method"=> array("object" => $this, "method" => "executeDependencies", "params" => array()) ),
-            array("command"=> array(
-                "cd /tmp" ,
-                "mkdir -p /tmp/selenium" ,
-                "cd /tmp/selenium" ,
-                "wget http://selenium-release.storage.googleapis.com/$sv/selenium-server-standalone-$sv.0.jar",
-                "mkdir -p ****PROGDIR****",
-                "mv /tmp/selenium/* ****PROGDIR****",
-                "rm -rf /tmp/selenium/",
-                "cd ****PROGDIR****",
-                "mv selenium-server-standalone-$sv.0.jar selenium-server.jar" ) ) ,
+            array("method"=> array("object" => $this, "method" => "doInstallCommands", "params" => array()) ),
             array("method"=> array("object" => $this, "method" => "deleteExecutorIfExists", "params" => array()) ),
             array("method"=> array("object" => $this, "method" => "saveExecutorFile", "params" => array()) ),
         );
         $this->uninstallCommands = array(
-            array("command"=> array("rm -rf ****PROGDIR****")));
+            array("command"=> array("rm -rf {$this->programDataFolder}")));
         $this->programDataFolder = "/opt/selenium"; // command and app dir name
         $this->programNameMachine = "selenium"; // command and app dir name
         $this->programNameFriendly = "Selenium Srv"; // 12 chars
@@ -60,14 +52,30 @@ class SeleniumServerAllLinux extends BaseLinuxApp {
         $java->ensureInstalled();
     }
 
+    public function doInstallCommands() {
+        $comms = array(
+                "cd /tmp" ,
+                "mkdir -p /tmp/selenium" ,
+                "cd /tmp/selenium" ,
+                "wget http://selenium-release.storage.googleapis.com/{$this->sv}/selenium-server-standalone-{$this->sv}.0.jar",
+                "mkdir -p {$this->programDataFolder}",
+                "mv /tmp/selenium/* {$this->programDataFolder}",
+                "rm -rf /tmp/selenium/",
+                "cd {$this->programDataFolder}",
+                "mv selenium-server-standalone-{$this->sv}.0.jar selenium-server.jar" ) ;
+        $this->executeAsShell($comms) ;
+    }
+
     protected function askForSeleniumVersion(){
         $ao = array("2.39", "2.40", "2.41", "2.42", "2.43") ;
         if (isset($this->params["version"]) && in_array($this->params["version"], $ao)) {
-            return $this->params["version"] ; }
-        if (isset($this->params["guess"])) {
-            return array_pop($ao) ; }
-        $question = 'Enter Selenium Version';
-        return self::askForArrayOption($question, $ao, true);
+            $this->sv = $this->params["version"] ; }
+        else if (isset($this->params["guess"])) {
+            $count = count($ao)-1 ;
+            $this->sv = $ao[$count] ; }
+        else {
+            $question = 'Enter Selenium Version';
+            return self::askForArrayOption($question, $ao, true); }
     }
 
 }
