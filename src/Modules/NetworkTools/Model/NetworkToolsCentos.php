6bb1944fa9d3c2390fe3f38221f03afc86a7c27b
David Amanshia <phpengine@hotmail.co.uk>
2014-09-18 17:00:34 +0100

    centos nettools

--- src/Modules/NetworkTools/Model/NetworkToolsCentos.php
+++ src/Modules/NetworkTools/Model/NetworkToolsCentos.php
@@ -2,13 +2,13 @@
 
 Namespace Model;
 
-class NetworkToolsUbuntu extends BaseLinuxApp {
+class NetworkToolsCentos extends BaseLinuxApp {
 
     // Compatibility
     public $os = array("Linux") ;
-    public $linuxType = array("Debian") ;
-    public $distros = array("Ubuntu") ;
-    public $versions = array("11.04", "11.10", "12.04", "12.10", "13.04") ;
+    public $linuxType = array("Redhat") ;
+    public $distros = array("CentOS") ;
+    public $versions = array("5", "6", "7") ;
     public $architectures = array("any") ;
 
     // Model Group
@@ -18,18 +18,18 @@ class NetworkToolsUbuntu extends BaseLinuxApp {
         parent::__construct($params);
         $this->autopilotDefiner = "NetworkTools";
         $this->installCommands = array(
-            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "traceroute")) ),
-            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "netstat")) ),
-            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "lsof")) ),
-            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "telnet")) ),
-            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "ps")) ),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Yum", "net-tools")) ),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Yum", "netstat")) ),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Yum", "lsof")) ),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Yum", "telnet")) ),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Yum", "ps")) ),
         );
         $this->uninstallCommands = array(
-            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "traceroute")) ),
-            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "netstat")) ),
-            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "lsof")) ),
-            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "telnet")) ),
-            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "ps")) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Yum", "traceroute")) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Yum", "netstat")) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Yum", "lsof")) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Yum", "telnet")) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Yum", "ps")) ),
         );
         $this->programDataFolder = "";
         $this->programNameMachine = "networktools"; // command and app dir name
