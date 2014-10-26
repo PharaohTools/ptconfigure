51b1cf62fb5ee8ff7fa35fe75da8ecf3b563c0f9
David Amanshia <phpengine@hotmail.co.uk>
2014-10-26 01:08:41 +0100

    controller has help now

--- src/Modules/Parallax/Controller/Parallax.php
+++ src/Modules/Parallax/Controller/Parallax.php
@@ -8,6 +8,11 @@ class Parallax extends Base {
 
         $action = $pageVars["route"]["action"];
 
+        if ($action=="help") {
+            $helpModel = new \Model\Help();
+            $this->content["helpData"] = $helpModel->getHelpData($pageVars["route"]["control"]);
+            return array ("type"=>"view", "view"=>"help", "pageVars"=>$this->content); }
+
         if ($action=="cli") {
             $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
             // if we don't have an object, its an array of errors
