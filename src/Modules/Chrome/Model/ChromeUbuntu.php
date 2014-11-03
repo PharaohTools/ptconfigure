ffcb846e0d18c3ccf23858e7c8a66e5bc2bf5b72
David Amanshia <phpengine@hotmail.co.uk>
2014-11-03 21:58:05 +0000

    chrome ubuntu didnt add package after adding deb

--- src/Modules/Chrome/Model/ChromeUbuntu.php
+++ src/Modules/Chrome/Model/ChromeUbuntu.php
@@ -22,7 +22,7 @@ class ChromeUbuntu extends BaseLinuxApp {
                 "wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -",
                 "sudo apt-get update -y",
                 "sudo sh -c 'echo \"deb http://dl.google.com/linux/chrome/deb/ stable main\" >> /etc/apt/sources.list.d/google.list'" ) ),
-            // array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "google-chrome-stable"))),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "google-chrome-stable"))),
         );
         $this->uninstallCommands = array(
             array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "google-chrome-stable")) ), );
