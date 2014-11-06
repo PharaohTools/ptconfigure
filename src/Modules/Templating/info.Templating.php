ef2fe4321ab6c031d960d80d0973e5834c6ec881
David Amanshia <phpengine@hotmail.co.uk>
2014-11-06 20:48:43 +0000

    templating naming small update to include file lines

--- src/Modules/Templating/info.Templating.php
+++ src/Modules/Templating/info.Templating.php
@@ -6,7 +6,7 @@ class TemplatingInfo extends CleopatraBase {
 
     public $hidden = false;
 
-    public $name = "Install files with placeholders replaced at runtime";
+    public $name = "Install files with placeholders or lines replaced at runtime";
 
     public function __construct() {
       parent::__construct();
