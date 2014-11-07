f30c5763cf9a51982611d34ddad6303cc07e25d9
David Amanshia <phpengine@hotmail.co.uk>
2014-11-07 01:46:04 +0000

    hostname model for windows

--- src/Modules/Hostname/Model/HostnameWindows.php
+++ src/Modules/Hostname/Model/HostnameWindows.php
@@ -2,12 +2,12 @@
 
 Namespace Model;
 
-class HostnameUbuntu extends BaseLinuxApp {
+class HostnameWindows extends BaseLinuxApp {
 
     // Compatibility
-    public $os = array("Linux") ;
-    public $linuxType = array("Debian") ;
-    public $distros = array("Ubuntu") ;
+    public $os = array("Windows", "WINNT") ;
+    public $linuxType = array("any") ;
+    public $distros = array("any") ;
     public $versions = array("11.04", "11.10", "12.04", "12.10", "13.04") ;
     public $architectures = array("any") ;
 
