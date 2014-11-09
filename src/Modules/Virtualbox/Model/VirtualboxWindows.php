76ebcc1e4d178d19716168e7a8716af41809c02f
David Amanshia <phpengine@hotmail.co.uk>
2014-11-09 15:07:50 +0000

    vbox windows looks correct for an install

--- src/Modules/Virtualbox/Model/VirtualboxWindows.php
+++ src/Modules/Virtualbox/Model/VirtualboxWindows.php
@@ -7,12 +7,15 @@ class VirtualboxWindows extends BaseWindowsApp {
     // Compatibility
     public $os = array("Windows") ;
     public $linuxType = array() ;
-    public $distros = array("Windows") ;
+    public $distros = array() ;
     public $versions = array("6") ;
     public $architectures = array("any") ;
 
     // Model Group
     public $modelGroup = array("Default") ;
+
+    // Virtualbox requested version
+    public $vbv ;
 
     // Windows Package
     public $packageUrl ;
@@ -36,11 +39,33 @@ class VirtualboxWindows extends BaseWindowsApp {
     // @todo this should definitely be using a package manager module
     protected function getInstallCommands() {
         $ray = array(
-            array("command" => array( "sudo apt-get install -y virtualbox") )
+            array("method"=> array("object" => $this, "method" => "askForVirtualboxVersion", "params" => array()) ),
+            array("method"=> array("object" => $this, "method" => "setPackageUrl", "params" => array()) ),
+            array("method"=> array("object" => $this, "method" => "executeDependencies", "params" => array()) ),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("WinExe", "Virtualbox")) ),
         ) ;
         if (isset($this->params["with-guest-additions"]) && $this->params["with-guest-additions"]==true) {
             array_push($ray, array("command" => array( "sudo apt-get install -y virtualbox-guest-additions-iso") ) ) ; }
         return $ray ;
+    }
+
+    protected function askForVirtualboxVersion(){
+        $ao = array("4.3.18") ;
+        if (isset($this->params["version"]) && in_array($this->params["version"], $ao)) {
+            $this->vbv = $this->params["version"] ; }
+        else if (isset($this->params["guess"])) {
+            $count = count($ao)-1 ;
+            $this->vbv = $ao[$count] ; }
+        else {
+            $question = 'Enter Virtualbox Version';
+            $this->vbv = self::askForArrayOption($question, $ao, true); }
+    }
+
+    protected function setPackageUrl(){
+        $pus = array(
+            "4.3.18" => "http://download.virtualbox.org/virtualbox/4.3.18/VirtualBox-4.3.18-96516-Win.exe" ,
+        ) ;
+        $this->packageUrl = $pus[$this->params["version"]] ;
     }
 
     public function versionInstalledCommandTrimmer($text) {
