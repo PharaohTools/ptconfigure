15ce7b82b9a7d496b183012e5619cf040b317246
David Amanshia <phpengine@hotmail.co.uk>
2014-11-09 20:00:01 +0000

    windows versioning debug

--- src/Modules/SystemDetection/Model/SystemDetectionAllOS.php
+++ src/Modules/SystemDetection/Model/SystemDetectionAllOS.php
@@ -103,14 +103,14 @@ class SystemDetectionAllOS extends Base {
             exec("ver", $output_array);
             $verString = substr(
                 $output_array[1],
-                strpos($output_array[1], "[Version ")+9,
-                strlen($output_array[1]) - strpos($output_array[1], "[Version ")+9
+                (strpos($output_array[1], "[Version ")+9),
+                (strlen($output_array[1]) - (strpos($output_array[1], "[Version ")+9))
             ) ;
             $versionObject = new \Model\SoftwareVersion($verString) ;
             var_dump(
                 $output_array[1],
-                strpos($output_array[1], "[Version ")+9,
-                strlen($output_array[1]) - strpos($output_array[1], "[Version ")+9,
+                (strpos($output_array[1], "[Version ")+9),
+                (strlen($output_array[1]) - (strpos($output_array[1], "[Version ")+9)),
                 $versionObject->fullVersionNumber,
                 $versionObject->shortVersionNumber
             ) ;
