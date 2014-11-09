27b51207e056603ef1b143b81230204fe29c5c78
David Amanshia <phpengine@hotmail.co.uk>
2014-11-09 02:50:00 +0000

    vbox windows updated statusCommand for windows

--- src/Modules/Virtualbox/Model/VirtualboxWindows.php
+++ src/Modules/Virtualbox/Model/VirtualboxWindows.php
@@ -2,25 +2,26 @@
 
 Namespace Model;
 
-class VirtualboxUbuntu extends BaseLinuxApp {
+class VirtualboxWindows extends BaseWindowsApp {
 
     // Compatibility
-    public $os = array("Linux") ;
-    public $linuxType = array("Debian") ;
-    public $distros = array("Ubuntu") ;
-    public $versions = array("11.04", "11.10", "12.04", "12.10", "13.04", "14.04") ;
+    public $os = array("Windows") ;
+    public $linuxType = array() ;
+    public $distros = array("Windows") ;
+    public $versions = array("6") ;
     public $architectures = array("any") ;
 
     // Model Group
     public $modelGroup = array("Default") ;
 
+    // Windows Package
+    public $packageUrl ;
+
     public function __construct($params) {
         parent::__construct($params);
         $this->autopilotDefiner = "Virtualbox";
         $this->installCommands = $this->getInstallCommands() ;
-        $this->uninstallCommands = array(
-            array("command" => array( "sudo apt-get remove -y virtualbox") )
-        ) ;
+        $this->uninstallCommands = array( array("command" => array( "sudo apt-get remove -y virtualbox") ) ) ;
         $this->programDataFolder = "/var/lib/virtualbox"; // command and app dir name
         $this->programNameMachine = "virtualbox"; // command and app dir name
         $this->programNameFriendly = " ! Virtualbox !"; // 12 chars
