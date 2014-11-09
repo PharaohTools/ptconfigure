41095a4449e7766c850762158964d5f85d1ba416
David Amanshia <phpengine@hotmail.co.uk>
2014-11-09 02:52:14 +0000

    basewindows app  for win is where.exe

--- src/Modules/CleopatraRequired/Model/BaseWindowsApp.php
+++ src/Modules/CleopatraRequired/Model/BaseWindowsApp.php
@@ -4,6 +4,8 @@ Namespace Model;
 
 class BaseWindowsApp extends BaseLinuxApp {
 
+    public $defaultStatusCommandPrefix = "where.exe";
+
     public function __construct($params) {
         parent::__construct($params);
     }
