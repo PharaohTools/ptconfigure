df6e08cadeccd5e4e2dc9d0af157fd3f6fa7c811
David Amanshia <phpengine@hotmail.co.uk>
2014-11-09 13:28:33 +0000

    os versions treated like software versions, can specify operators instead of strngs

--- src/Modules/SystemDetection/Model/SystemDetectionFactory.php
+++ src/Modules/SystemDetection/Model/SystemDetectionFactory.php
@@ -28,7 +28,8 @@ class SystemDetectionFactory {
                 (in_array($system->os, $model->os) || in_array("any", $model->os)) &&
                 (in_array($system->linuxType, $model->linuxType) || in_array("any", $model->linuxType)) &&
                 (in_array($system->distro, $model->distros) || in_array("any", $model->distros)) &&
-                (in_array($system->version, $model->versions) || in_array("any", $model->versions)) &&
+                //(in_array($system->version, $model->versions) || in_array("any", $model->versions)) &&
+                (self::versionsAreCompatible($system->version, $model->versions) || in_array("any", $model->versions)) &&
                 (in_array($system->architecture, $model->architectures) || in_array("any", $model->architectures))
             ) {
                 // if the everything matches, we have an exact match so return it
@@ -57,6 +58,33 @@ class SystemDetectionFactory {
                 // error_log($message);
                 return $model; } }
         return null ;
+    }
+
+    private static function versionsAreCompatible($systemVersion, $modelVersions) {
+
+        $matches = array() ;
+
+        foreach ($modelVersions as $modelVersion) {
+
+            // if string literal version
+            if (is_string($modelVersion)) {
+                if ($systemVersion == $modelVersion) {
+                    // echo "**vac 1**\n" ;
+                    $matches[] = true ; } }
+
+            // if conditions
+            if (is_array($modelVersion)) {
+                $criteriaResults = array() ;
+                $svo = new \Model\SoftwareVersion($systemVersion) ;
+                foreach ($modelVersion as $criteriaValue => $criteriaOperator) {
+                    $svo->setCondition($criteriaValue, $criteriaOperator) ; }
+                    $criteriaResults[] = $svo->isCompatible() ;
+                if (!in_array(false, $criteriaResults)) {
+                    // echo "**vac 2**\n" ;
+                    $matches[] = true ; } } }
+
+        return $matches ;
+
     }
 
 }
