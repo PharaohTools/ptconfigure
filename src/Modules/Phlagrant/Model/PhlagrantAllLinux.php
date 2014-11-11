bb2bab7d821ab294a9f5a7ac1e6c30216acd814b
David Amanshia <phpengine@hotmail.co.uk>
2014-11-11 21:58:50 +0000

    phlagrant mdl

--- src/Modules/Phlagrant/Model/PhlagrantAllLinux.php
+++ src/Modules/Phlagrant/Model/PhlagrantAllLinux.php
@@ -5,7 +5,7 @@ Namespace Model;
 class PhlagrantAllLinux extends BasePHPApp {
 
     // Compatibility
-    public $os = array("any") ;
+    public $os = array("Linux", "Darwin") ;
     public $linuxType = array("any") ;
     public $distros = array("any") ;
     public $versions = array("any") ;
