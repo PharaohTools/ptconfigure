2a1b1ace9d02e835e2b14adadd6ece86bd871874
David Amanshia <phpengine@hotmail.co.uk>
2014-11-20 19:01:14 +0000

    chrome using file module

--- src/Modules/Chrome/Model/ChromeUbuntu.php
+++ src/Modules/Chrome/Model/ChromeUbuntu.php
@@ -19,16 +19,16 @@ class ChromeUbuntu extends BaseLinuxApp {
         $this->autopilotDefiner = "Chrome";
         $this->installCommands = array(
             array("command"=> array(
-                "wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -",
-                "sudo apt-get update -y",
-                "sudo sh -c 'echo \"deb http://dl.google.com/linux/chrome/deb/ stable main\" >> /etc/apt/sources.list.d/google.list'"
-            ) ),
-            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array(
-                "Apt", "google-chrome-stable")
-            )),
+                "wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -" ) ),
+            array("command"=> array(
+                "sudo apt-get update -y" ) ),
+            array("method"=> array("object" => $this, "method" => "ensureAptSourceExists", "params" => array()) ),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array( "Apt", "google-chrome-stable") )),
         );
         $this->uninstallCommands = array(
-            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "google-chrome-stable")) ), );
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "google-chrome-stable")) ),
+            array("method"=> array("object" => $this, "method" => "ensureAptSourceRemoved", "params" => array()) ),
+        );
         $this->programDataFolder = "/opt/Chrome"; // command and app dir name
         $this->programNameMachine = "google-chrome-stable"; // command and app dir name
         $this->programNameFriendly = "GoogleChrome"; // 12 chars
@@ -65,6 +65,22 @@ class ChromeUbuntu extends BaseLinuxApp {
         $firstSpaceAfterNumber = strpos($theRest, "\n") ;
         $done = substr($text, $candidateEnds, $firstSpaceAfterNumber) ;
         return $done ;
+    }
+
+    public function ensureAptSourceExists() {
+        $fileFactory = new \Model\File() ;
+        $file = $fileFactory->getModel($this->params) ;
+        $file->setFile("/etc/apt/sources.list.d/google.list") ;
+        $file->create() ;
+        $file->setSearchLine("deb http://dl.google.com/linux/chrome/deb/ stable main") ;
+        $file->shouldHaveLine();
+    }
+
+    public function ensureAptSourceRemoved() {
+        $fileFactory = new \Model\File() ;
+        $file = $fileFactory->getModel($this->params) ;
+        $file->setFile("/etc/apt/sources.list.d/google.list") ;
+        $file->delete() ;
     }
 
 }
