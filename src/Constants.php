35ba93e110e771053bf2c619f6f04323371540c4
David Amanshia <phpengine@hotmail.co.uk>
2014-11-20 16:02:01 +0000

    constants updated the windows program dir out of default progfiles

--- src/Constants.php
+++ src/Constants.php
@@ -5,17 +5,18 @@
  */
 
 if (in_array(PHP_OS, array("Windows", "WINNT"))) {
+    $sd = getenv('SystemDrive') ;
     $pf = getenv('ProgramFiles') ;
     $pf = str_replace(" (x86)", "", $pf) ;
     $command = "where /R \"{$pf}\" *VBoxManage* " ;
     $outputArray = array();
     exec($command, $outputArray);
     define('VBOXMGCOMM', "\"{$outputArray[0]}\" ") ;
-    define('PFILESDIR', $pf) ;
+    define('PFILESDIR', $sd.'\\PharaohTools\boxes') ;
     define('CLEOCOMM', "cleopatra.cmd") ;
     define('DAPPCOMM', "dapperstrano.cmd") ;
     define('PHLCOMM', "phlagrant.cmd") ;
-    define('BOXDIR', 'C:\\PharaohTools\boxes') ;
+    define('BOXDIR', $sd.'\\PharaohTools\boxes') ;
     define("DS", "\\");
     define("BASE_TEMP_DIR", getenv("SystemDrive").'\Temp\\'); }
 else if (in_array(PHP_OS, array("Linux", "Solaris", "FreeBSD", "OpenBSD", "Darwin"))) {
