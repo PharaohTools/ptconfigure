7df74b8bddf19cebe515d47f28b530b8979de032
David Amanshia <phpengine@hotmail.co.uk>
2014-11-22 04:32:12 +0000

    added os properties to dummy modules

--- src/Extensions/DummyLinuxModule/Model/DummyLinuxModuleAllLinux.php
+++ src/Extensions/DummyLinuxModule/Model/DummyLinuxModuleAllLinux.php
@@ -4,16 +4,26 @@ Namespace Model;
 
 class DummyLinuxModuleAllLinux extends BaseLinuxApp {
 
-  public function __construct($params) {
-    parent::__construct($params);
-    $this->autopilotDefiner = "DummyLinuxModule";
-    $this->installCommands = array( "ls" );
-    $this->uninstallCommands = array( "ls" );
-    $this->programDataFolder = "/opt/DummyLinuxModule"; // command and app dir name
-    $this->programNameMachine = "DummyLinuxModule"; // command and app dir name
-    $this->programNameFriendly = "Dummy Module"; // 12 chars
-    $this->programNameInstaller = "Dummy Module";
-    $this->initialize();
-  }
+    // Compatibility
+    public $os = array("any") ;
+    public $linuxType = array("any") ;
+    public $distros = array("any") ;
+    public $versions = array("any") ;
+    public $architectures = array("any") ;
+
+    // Model Group
+    public $modelGroup = array("Default") ;
+
+    public function __construct($params) {
+        parent::__construct($params);
+        $this->autopilotDefiner = "DummyLinuxModule";
+        $this->installCommands = array( "ls" );
+        $this->uninstallCommands = array( "ls" );
+        $this->programDataFolder = "/opt/DummyLinuxModule"; // command and app dir name
+        $this->programNameMachine = "DummyLinuxModule"; // command and app dir name
+        $this->programNameFriendly = "Dummy Module"; // 12 chars
+        $this->programNameInstaller = "Dummy Module";
+        $this->initialize();
+    }
 
 }
