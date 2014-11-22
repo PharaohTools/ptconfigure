59654552eb1ab66cdad62faa3d3fd008e3a6a1e5
David Amanshia <phpengine@hotmail.co.uk>
2014-11-22 03:38:03 +0000

    fixed syntax of getmodel highlighted by test

--- src/Modules/PapyrusEditor/Model/PapyrusEditor.php
+++ src/Modules/PapyrusEditor/Model/PapyrusEditor.php
@@ -4,9 +4,9 @@ Namespace Model;
 
 class PapyrusEditor extends BaseModelFactory {
 
-    public static function getModel($params) {
+    public static function getModel($params, $modGroup = "Editor") {
         $thisModule = substr(get_called_class(), 6) ;
-        $model = \Model\SystemDetectionFactory::getCompatibleModel($thisModule, "Editor", $params);
+        $model = \Model\SystemDetectionFactory::getCompatibleModel($thisModule, $modGroup, $params);
         return $model;
     }
 
