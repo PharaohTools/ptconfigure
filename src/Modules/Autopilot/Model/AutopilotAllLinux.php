cc4dcbabc8237ce8a6cafe40b31e674256dbf7af
David Amanshia <phpengine@hotmail.co.uk>
2014-11-26 23:35:21 +0000

    updated autopilot to DS

--- src/Modules/Autopilot/Model/AutopilotAllLinux.php
+++ src/Modules/Autopilot/Model/AutopilotAllLinux.php
@@ -22,7 +22,7 @@ class AutopilotAllLinux extends BaseLinuxApp {
     protected function addAliasParams() {
         $dfd = "" ;
         if (isset($this->params["dfd"])) {
-            $dfd = getcwd()."/build/config/cleopatra/cleofy/autopilots/" ; }
+            $dfd = getcwd().DS."build".DS."config".DS."cleopatra".DS."cleofy".DS."autopilots".DS ; }
         if (isset($this->params["af"])) {
             $this->params["autopilot-file"] = $dfd.$this->params["af"] ; }
         if (isset($this->params["auto"])) {
