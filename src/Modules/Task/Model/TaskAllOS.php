cbcdf7f782d07cb66c81d9e0156c3d5089a70350
David Amanshia <phpengine@hotmail.co.uk>
2014-11-30 02:11:49 +0000

    removed some unused code and made for all OS

--- src/Modules/Task/Model/TaskAllOS.php
+++ src/Modules/Task/Model/TaskAllOS.php
@@ -20,8 +20,6 @@ class TaskAllOS extends BaseLinuxApp {
     protected $actionsToMethods =
         array(
             "ensure-domain-exists" => "ensureDNSDomainExists",
-            "ensure-domain-empty" => "ensureDNSDomainEmpty",
-            "ensure-record-exists" => "ensureDNSRecordExists",
         ) ;
 
     public function __construct($params) {
@@ -37,35 +35,6 @@ class TaskAllOS extends BaseLinuxApp {
         $this->setEnvironment($environmentName);
         return $this->removeDNS();
     }
-
-    public function performDNSDestroy($providerName = null, $environmentName = null) {
-        $this->setProvider($providerName);
-        $this->setEnvironment($environmentName);
-        return $this->destroyDNSes();
-    }
-
-    public function setEnvironment($environmentName = null) {
-        if (isset($environmentName)) {
-            $this->environmentName = $environmentName; }
-        else if (isset($this->params["environmentname"])) {
-            $this->environmentName = $this->params["environmentname"]; }
-        else if (isset($this->params["environment-name"])) {
-            $this->environmentName = $this->params["environment-name"]; }
-        else {
-            $this->environmentName = self::askForInput("Enter Environment Name:", true); }
-    }
-
-    public function setProvider($providerName = null) {
-        if (isset($providerName)) {
-            $this->providerName = $providerName; }
-        else if (isset($this->params["providername"])) {
-            $this->providerName = $this->params["providername"]; }
-        else if (isset($this->params["provider-name"])) {
-            $this->providerName = $this->params["provider-name"]; }
-        else {
-            $this->providerName = self::askForInput("Enter Provider Name:", true); }
-    }
-
 
     protected function askForDomainAddExecute() {
         if (isset($this->params["yes"]) && $this->params["yes"]==true) { return true ; }
