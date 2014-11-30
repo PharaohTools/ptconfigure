4e4097a23414dce4b64300add37d8df5bf32d420
David Amanshia <phpengine@hotmail.co.uk>
2014-11-30 02:11:26 +0000

    handle undefined taskrunners

--- src/Modules/Task/Controller/TaskExecutor.php
+++ src/Modules/Task/Controller/TaskExecutor.php
@@ -2,43 +2,52 @@
 
 Namespace Controller ;
 
-class Task extends Base {
+class TaskExecutor extends Base {
 
-    public function execute($pageVars) {
+    public function executeTFTask($pageVars, $task) {
 
-        $action = $pageVars["route"]["action"];
+        $tasks = $this->getTaskfileTasks() ;
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel(array());
+        $taskRunners = $tasks[$task] ;
+        foreach ($taskRunners as $taskRunner => $params) {
+            switch ($taskRunner) {
+                case "cleopatra" :
+                    if (is_string($params)) { $af = $params ; }
+                    else if (is_array($params) && isset($params["af"])) { $af = $params["af"] ; }
+                    else if (is_array($params) && isset($params[0])) { $af = $params[0] ; }
+                    else { $af = $params["af"] ; }
+                    $logging->log("Cleopatra Task Runner","Task") ;
+                    exec(CLEOCOMM.'autopilot execute --af="'.$af.'"') ;
+                    break ;
+                case "dapperstrano" :
+                    if (is_string($params)) { $af = $params ; }
+                    else if (is_array($params) && isset($params["af"])) { $af = $params["af"] ; }
+                    else if (is_array($params) && isset($params[0])) { $af = $params[0] ; }
+                    else { $af = $params["af"] ; }
+                    $logging->log("Dapperstrano Task Runner","Task") ;
+                    exec(DAPPCOMM.'autopilot execute --af="'.$af.'"') ;
+                    break ;
+                case "log" :
+                    $logging->log("Logging Task Runner","Task") ;
+                    if (is_string($params)) { $log = $params ; }
+                    else if (is_array($params) && isset($params["log"])) { $log = $params["log"] ; }
+                    else if (is_array($params) && isset($params[0])) { $log = $params[0] ; }
+                    else { $log = "No Log Provided" ; }
+                    $logging->log($log,"Task") ;
+                    break ;
+                default :
+                    $logging->log("Undefined Task Runner $taskRunner requested", "Task") ;
+                    break ;
+            }
+        }
 
-        $taskFileExecutor = $this->getTaskfileTaskForAction($action);
-        if (!is_null($taskFileExecutor)) {
-            return $taskFileExecutor->executeTask($pageVars) ; }
-
-        $otherModuleExecutor = $this->getModuleExecutorForAction($action);
-        if (!is_null($otherModuleExecutor)) {
-            return $otherModuleExecutor->executeTask($pageVars) ; }
-
-        if ($action=="help") {
-            $helpModel = new \Model\Help();
-            $this->content["helpData"] = $helpModel->getHelpData($pageVars["route"]["control"]);
-            return array ("type"=>"view", "view"=>"help", "pageVars"=>$this->content); }
-
-        if (in_array($action, array("list") )) {
-            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "Listing") ;
-            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
-            $this->content["result"] = $thisModel->askAction($action);
-            $this->content["appName"] = $thisModel->programNameInstaller ;
-            return array ("type"=>"view", "view"=>"TaskList", "pageVars"=>$this->content); }
-
+        return array ("type"=>"view", "view"=>"Task", "pageVars"=>$this->content);
     }
 
     protected function getTaskfileTaskForAction($action) {
         $tftasks = self::getTaskfileTasks();
-        $controllers = \Core\AutoLoader::getAllControllers() ;
-        foreach ($controllers as $controller) {
-            if (method_exists($controller, "executeDapperfy"))
-                $info = \Core\AutoLoader::getSingleInfoObject(substr(get_class($controller), 11)) ;
-            $myDapperfyRoutes = (isset($info) && method_exists($info, "dapperfyActions")) ? $info->dapperfyActions() : array() ;
-            if (in_array($action, $myDapperfyRoutes)) {
-                return $controller ; } }
+        if (in_array($action, $tftasks)) { return new \Controller\TaskExecutor(); }
         return null ;
     }
 
@@ -51,19 +60,8 @@ class Task extends Base {
         else {
             return array() ; }
         $taskObject = new \Model\Taskfile() ;
-        $tftasks = array_keys($taskObject::$tasks) ;
+        $tftasks = $taskObject::$tasks ;
         return $tftasks ;
-    }
-
-    protected function getModuleExecutorForAction($action) {
-        $controllers = \Core\AutoLoader::getAllControllers() ;
-        foreach ($controllers as $controller) {
-            if (method_exists($controller, "executeDapperfy"))
-                $info = \Core\AutoLoader::getSingleInfoObject(substr(get_class($controller), 11)) ;
-            $myDapperfyRoutes = (isset($info) && method_exists($info, "dapperfyActions")) ? $info->dapperfyActions() : array() ;
-            if (in_array($action, $myDapperfyRoutes)) {
-                return $controller ; } }
-        return null ;
     }
 
 }
