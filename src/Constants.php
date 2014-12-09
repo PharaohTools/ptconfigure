0d5d6b9a3a66f5eaa5260b206046ac8d1b604d17
David Amanshia <phpengine@hotmail.co.uk>
2014-12-09 18:13:10 +0000

    constants updated to include all pharaohs

--- src/Constants.php
+++ src/Constants.php
@@ -7,8 +7,24 @@
 define('PHARAOH_APP', "dapperstrano") ;
 
 if (in_array(PHP_OS, array("Windows", "WINNT"))) {
+    $sd = getenv('SystemDrive') ;
+    $pf = getenv('ProgramFiles') ;
+    $pf = str_replace(" (x86)", "", $pf) ;
+    $command = "where /R \"{$pf}\" *VBoxManage* " ;
+    $outputArray = array();
+    exec($command, $outputArray);
+    define('VBOXMGCOMM', "\"{$outputArray[0]}\" ") ;
+    define('PFILESDIR', $sd."\\PharaohTools\\") ;
+    define('CLEOCOMM', PFILESDIR.'cleopatra.cmd"') ;
+    define('DAPPCOMM',  PFILESDIR."dapperstrano.cmd") ;
+    define('PHLCOMM',  PFILESDIR."phlagrant.cmd") ;
     define("DS", "\\");
     define("BASE_TEMP_DIR", getenv("SystemDrive").'\Temp\\'); }
 else if (in_array(PHP_OS, array("Linux", "Solaris", "FreeBSD", "OpenBSD", "Darwin"))) {
+    define('VBOXMGCOMM', "vboxmanage ") ;
+    define('PFILESDIR', "/opt/") ;
+    define('CLEOCOMM', "cleopatra ") ;
+    define('DAPPCOMM', "dapperstrano ") ;
+    define('PHLCOMM', "phlagrant") ;
     define("DS", "/");
     define("BASE_TEMP_DIR", '/tmp/'); }
