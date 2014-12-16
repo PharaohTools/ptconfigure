31f17078afa31015fa431baffdb42e32c1db3859
David Amanshia <phpengine@hotmail.co.uk>
2014-12-16 19:10:19 +0000

    updated gitcommand help

--- src/Modules/GitCommand/info.GitCommand.php
+++ src/Modules/GitCommand/info.GitCommand.php
@@ -13,7 +13,7 @@ class GitCommandInfo extends Base {
     }
 
     public function routesAvailable() {
-      return array( "GitCommand" => array_merge(parent::routesAvailable(), array( "help",
+      return array( "GitCommand" => array_merge(parent::routesAvailable(), array( "help", "checkout-branch",
           "create-checkout-branch", "delete-branch", "ensure-branch", "ensure=delete-branch", "add", "commit", "push", "pull") ) );
     }
 
@@ -27,8 +27,12 @@ class GitCommandInfo extends Base {
 
   Git, GitCommand, git-command, gitcommand
 
+  - checkout-branch
+    checkout a branch
+    example: ".PHARAOH_APP." git checkout-branch --branch=*branchname*
+
   - create-checkout-branch
-    create a new branch
+    checkout a branch, creating a new branch if needed
     example: ".PHARAOH_APP." git create-checkout-branch --branch=*branchname*
 
   delete-branch
