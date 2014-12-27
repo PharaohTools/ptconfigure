50a7b627941de1dd3e3c11d869119d8db3502fca
David Amanshia <phpengine@hotmail.co.uk>
2014-12-27 00:18:57 +0000

    updated file watcher hel and info

--- src/Modules/FileWatcher/info.FileWatcher.php
+++ src/Modules/FileWatcher/info.FileWatcher.php
@@ -6,29 +6,32 @@ class FileWatcherInfo extends CleopatraBase {
 
     public $hidden = false;
 
-    public $name = "FileWatcher Functionality";
+    public $name = "File Watcher - Wait for changes in files, with optional actions on changes";
 
     public function _construct() {
       parent::__construct();
     }
 
     public function routesAvailable() {
-      return array( "FileWatcher" => array("path", "help") );
+      return array( "FileWatcher" => array("watch", "help") );
     }
 
     public function routeAliases() {
-      return array("chown" => "FileWatcher");
+      return array("filewatcher" => "FileWatcher", "file-watcher" => "FileWatcher");
     }
 
   public function helpDefinition() {
       $help = <<<"HELPDATA"
-  This command handles file user ownership changing functions.
+  This command watches files user ownership changing functions.
 
-  FileWatcher, chown
+  FileWatcher, filewatcher, file-watcher
 
-        - path
-        Will change the user ownership of a path
-        example: cleopatra chown path --yes --guess --recursive --path=/a/file/path --owner=golden
+        - change
+        Will watch files for changes and perform actions
+        example: cleopatra filewatcher watch --yes --guess
+                    --watchfile=/path/to/watchfile # guess will assume Watchfile
+                    --versionsource=git # git/svn/cache guess will assume git
+                    --compare=02aeb1c38dec40cede28b36ba200ec3d5b67f22c # commit id, or other comparison value
 
 HELPDATA;
       return $help ;
