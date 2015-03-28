deb2dcc1143cda82ededda4f57ce97dcfa824273
Ionut Bajescu <ionutbajescu@gmail.com>
2015-03-28 12:13:52 +0200

    bashssh driver -  fix the popen redirection, added preserved output, improved the error messages

--- src/Modules/Invoke/Model/InvokeBashSsh.php
+++ src/Modules/Invoke/Model/InvokeBashSsh.php
@@ -26,6 +26,8 @@ class InvokeBashSsh {
 
 	protected $commandsPipe;
 
+	protected $connectingDelay = 6;
+
 	/**
 	 * @param Server $server
 	 */
@@ -39,6 +41,7 @@ class InvokeBashSsh {
 		if(file_exists($this->server->password)){
 			$launcher = 'ssh -o PubkeyAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i '.escapeshellarg($this->server->password); }
         else{
+			$this->checkSshpassPresence();
 			$launcher = 'sshpass -p '.escapeshellarg($this->server->password).' ssh -o UserKnownHostsFile=/dev/null ' .
                 '-o StrictHostKeyChecking=no -o PubkeyAuthentication=no'; }
 		$this->commandsPipe = tempnam(null, 'ssh');
@@ -46,11 +49,57 @@ class InvokeBashSsh {
 		$launcher .= escapeshellarg($this->server->username.'@'.$this->server->host);
 		$pipe = "tail -f {$this->commandsPipe}";
 		if(!pcntl_fork()){
-			$fp = popen("$pipe | $launcher" ,"r");
+			if (ob_get_level() == 0)
+				ob_start();
+
+			$start = time();
+			$preserved = '';
+			$fp = popen("$pipe | $launcher 2>&1" ,"r");
 			while (!feof($fp)) {
-				echo fgets($fp, 4096); }
+				$output = fgets($fp, 4096);
+
+				if($preserved) {
+					echo $preserved;
+					$preserved = false;
+				}
+				echo $output;
+
+				if( ! $this->shouldPreserveOutput($start)){
+					ob_flush();
+					flush();
+				}
+			}
 			pclose($fp);
-			exit; }
+			exit;
+		}
+		$this->wait();
+	}
+
+	public function shouldPreserveOutput($start)
+	{
+		return (time()-$start) < $this->connectingDelay;
+	}
+
+	public function checkSshpassPresence()
+	{
+		$output = shell_exec('which sshpass');
+		if( ! $output) {
+			$sep = str_repeat('-', 40);
+			echo "\e[0;31m \n\n$sep\nWe're sorry, but 'sshpass' is a mandatory dependence for the BashSsh driver. \nYou must consider running apt-get install sshpass (ubuntu) before running this command again";
+			echo "\n$sep\e[0m\n";
+			exit;
+		}
+	}
+
+	public function wait()
+	{
+		echo 'Please wait until we\'ll connect to the server.';
+		$step = 0.3;
+		$times = $this->connectingDelay/$step;
+		for($i=0; $i<$times; $i++) {
+			echo '.';
+			usleep($step*1000000);
+		}
 	}
 
 	/**
