4a82210b751b773a82ba5171559b387148b4f61c
David Amanshia <phpengine@hotmail.co.uk>
2015-04-15 00:21:56 +0100

    sftp returning correct key rsa file

--- src/Modules/SFTP/Model/SFTPAllLinux.php
+++ src/Modules/SFTP/Model/SFTPAllLinux.php
@@ -214,8 +214,7 @@ class SFTPAllLinux extends Base {
         if (substr($pword, 0, 4) == 'KS::') {
             $ksf = new SshKeyStore();
             $ks = $ksf->getModel(array("key" => $pword, "guess" => true)) ;
-            $pword = $ks->findKey() ;
-            return $pword ; }
+            $pword = $ks->findKey() ; }
         if (substr($pword, 0, 1) == '~') {
             $home = $_SERVER['HOME'] ;
             $pword = str_replace('~', $home, $pword) ; }
