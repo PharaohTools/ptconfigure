7cf2cd16c433cc634b6739b8db783e6b85e09bab
David Amanshia <phpengine@hotmail.co.uk>
2015-06-08 02:04:38 +0100

    windows host editor line feed fix

--- src/Modules/HostEditor/Model/HostEditorWindows.php
+++ src/Modules/HostEditor/Model/HostEditorWindows.php
@@ -28,4 +28,46 @@ class HostEditorWindows extends HostEditorAllLinuxMac {
         self::executeAndOutput($command);
     }
 
+
+    protected function hostFileDataAdd($ipEntry, $uri){
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params);
+        $hostFileLines = explode(PHP_EOL , $this->hostFileData) ;
+        $newHostFileData = "";
+        $logging->log("Attempting to add Host File Entry...", $this->getModuleName()) ;
+        foreach ($hostFileLines as $line) {
+            $ipOccurs = substr_count($line, $ipEntry) ;
+            $uriOccurs = substr_count($line, $uri) ;
+            $bothOccur = ( $ipOccurs==1 && $uriOccurs==1);
+            if ( $bothOccur )  {
+                $logging->log("Host file entry already exists for Host Name {$uri}, with IP {$ipEntry} no need to edit...", $this->getModuleName()) ;
+                return true; }
+            else if ( $uriOccurs )  {
+                $logging->log("Host file entry already exists for Host Name {$uri}, with IP {$ipEntry} removing...", $this->getModuleName()) ;
+                continue ; }
+            else {
+                $newHostFileData .= $line."\r\n" ; } }
+        $logging->log("Adding requested entry {$uri}, with IP {$ipEntry} to host file data", $this->getModuleName()) ;
+        $this->hostFileData .= "$ipEntry          $uri"."\r\n";
+        $this->writeHostFileEntryToProjectFile();
+    }
+
+    protected function hostFileDataRemove($ipEntry, $uri){
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params);
+        $hostFileLines = explode(PHP_EOL , $this->hostFileData) ;
+        $newHostFileData = "";
+        foreach ($hostFileLines as $line) {
+            $ipOccurs = substr_count($line, $ipEntry) ;
+            $uriOccurs = substr_count($line, $uri) ;
+            $bothOccur = ( $ipOccurs==1 && $uriOccurs==1);
+            if (isset($this->params["guess"])) {
+                if ($uriOccurs) {
+                    $logging->log("Host file entry exists, attempting to remove...", $this->getModuleName()) ;
+                    continue ; } }
+            if ( !$bothOccur )  { $newHostFileData .= $line."\r\n" ; } }
+        $this->hostFileData = $newHostFileData;
+        $this->deleteHostFileEntryFromProjectFile();
+    }
+
 }
