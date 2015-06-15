bcc92dade814012f4ab29063e647d9b6400ec20e
David Amanshia <phpengine@hotmail.co.uk>
2015-06-15 02:51:10 +0100

    default suto check before use vars

--- src/Modules/Autopilot/Views/DefaultAUTOView.tpl.php
+++ src/Modules/Autopilot/Views/DefaultAUTOView.tpl.php
@@ -4,6 +4,8 @@
       echo $pageVars["appName"] ; ?>: <?php }
   $result_summary = ($pageVars["result"] == true) ? "Success" : "Failure" ;
   echo $result_summary."\n" ;
-  if (is_string($pageVars["result"])) {
+if (isset($pageVars["result"]) && is_string($pageVars["result"])) {
     echo $pageVars["result"]."\n" ; }
+if (isset($pageVars["result"]) && !is_string($pageVars["result"])) {
+    var_dump($pageVars["result"]) ; }
 ?>
