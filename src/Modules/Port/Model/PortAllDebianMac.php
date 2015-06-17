5159bf1841378d45ff80b49f0616d031a325f309
David Amanshia <phpengine@hotmail.co.uk>
2015-06-17 09:26:22 +0100

    array to strnig before strpos

--- src/Modules/Port/Model/PortAllDebianMac.php
+++ src/Modules/Port/Model/PortAllDebianMac.php
@@ -83,7 +83,7 @@ class PortAllDebianMac extends BaseLinuxApp {
         if ($this->installDependencies() == false) { return false ;}
         $comm = SUDOPREFIX.'lsof -i :'.$this->portNumber.' | grep LISTEN';
         $out = self::executeAndGetReturnCode($comm, false, true) ;
-        $process = substr($out["output"], 0, strpos($out["output"], " ")) ;
+        $process = substr(implode("\n", $out["output"]), 0, strpos(implode("\n", $out["output"]), " ")) ;
         if ($out["rc"] != "0") {
             \Core\BootStrap::setExitCode(1);
             $logging->log("Port process command execution failed.", $this->getModuleName()) ;
