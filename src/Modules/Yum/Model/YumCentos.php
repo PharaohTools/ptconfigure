7f3e3469440f883633b86002956014a8682de734
David Amanshia <phpengine@hotmail.co.uk>
2015-06-17 10:10:48 +0100

    yum returns

--- src/Modules/Yum/Model/YumCentos.php
+++ src/Modules/Yum/Model/YumCentos.php
@@ -48,17 +48,20 @@ class YumCentos extends BasePackager {
             return false ; }
         foreach ($packageName as $package) {
             $out = $this->executeAndOutput(SUDOPREFIX."yum install $package -y");
-            if (strpos($out, "Complete!") != false) {
+            if (strpos($out, "Complete!") != false ) {
                 $logging->log("Added Package $package from the Packager {$this->programNameInstaller} successfully", $this->getModuleName()) ;
-                return false ; }
-            else if (strpos($out, "Complete!") == false && strpos($out, "already installed and latest version.") == false) {
-                $logging->log("Adding Package $package from the Packager {$this->programNameInstaller} did not execute correctly", $this->getModuleName()) ;
-                return false ; }
+                return true ; }
+            else if (strpos($out, "Nothing to do") != false) {
+                $logging->log("Package $package from the Packager {$this->programNameInstaller} already installed", $this->getModuleName()) ;
+                return true; }
             else if (strpos($out, "already installed and latest version.") != false) {
                 $ltext  = "Package $package from the Packager {$this->programNameInstaller} is " ;
                 $ltext .= "already installed, so not installing." ;
                 $logging->log($ltext, $this->getModuleName()) ;
-                return true ; } }
+                return true ; }
+            else {
+                $logging->log("Adding Package $package from the Packager {$this->programNameInstaller} did not execute correctly", $this->getModuleName()) ;
+                return false ; } }
         return true ;
     }
 
