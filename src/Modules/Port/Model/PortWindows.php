747b6d7b4e8214ada9e03fa6ed20b6119054fec1
David Amanshia <phpengine@hotmail.co.uk>
2015-06-23 23:52:31 +0100

    port for windows should work

---
+++ src/Modules/Port/Model/PortWindows.php
@@ -0,0 +1,27 @@
+<?php
+
+Namespace Model;
+
+class PortCentOS extends PortAllDebianMac {
+
+    // Compatibility
+    public $os = array("Linux") ;
+    public $linuxType = array("Redhat") ;
+    public $distros = array("any") ;
+    public $versions = array("any") ;
+    public $architectures = array("any") ;
+
+    protected function installDependencies() {
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params);
+        $logging->log("Ensuring Redhat based dependency for Port Process check, package lsof", $this->getModuleName()) ;
+        $yumFactory = new \Model\Yum();
+        $yum = $yumFactory->getModel($this->params);
+        if ($yum->isInstalled("lsof")==true) {
+            $logging->log("Package lsof is already installed", $this->getModuleName()) ;
+            return true; }
+        $logging->log("Package lsof not installed, installing...", $this->getModuleName()) ;
+        return $yum->installPackage("lsof") ;
+    }
+
+}
