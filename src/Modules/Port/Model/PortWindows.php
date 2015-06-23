3bf26060d0aeed983e4f9a0369c271c17460a6f2
David Amanshia <phpengine@hotmail.co.uk>
2015-06-23 23:52:47 +0100

    port for windows should work

--- src/Modules/Port/Model/PortWindows.php
+++ src/Modules/Port/Model/PortWindows.php
@@ -2,26 +2,64 @@
 
 Namespace Model;
 
-class PortCentOS extends PortAllDebianMac {
+class PortWindows extends PortAllDebianMac {
 
     // Compatibility
-    public $os = array("Linux") ;
-    public $linuxType = array("Redhat") ;
+    public $os = array("Windows", "WINNT") ;
+    public $linuxType = array("any") ;
     public $distros = array("any") ;
     public $versions = array("any") ;
     public $architectures = array("any") ;
 
-    protected function installDependencies() {
+    protected function getPortService() {
         $loggingFactory = new \Model\Logging();
         $logging = $loggingFactory->getModel($this->params);
-        $logging->log("Ensuring Redhat based dependency for Port Process check, package lsof", $this->getModuleName()) ;
-        $yumFactory = new \Model\Yum();
-        $yum = $yumFactory->getModel($this->params);
-        if ($yum->isInstalled("lsof")==true) {
-            $logging->log("Package lsof is already installed", $this->getModuleName()) ;
+        if ($this->installDependencies() == false) { return false ;}
+        $comm = 'netstat -anb  | find "'.$this->portNumber.'"';
+        $out = self::executeAndGetReturnCode($comm, false, true) ;
+
+        $process = $this->findProcess($out);
+
+        if ($out["rc"] != "0") {
+            \Core\BootStrap::setExitCode(1);
+            $logging->log("Port process command execution failed.", $this->getModuleName()) ;
             return true; }
-        $logging->log("Package lsof not installed, installing...", $this->getModuleName()) ;
-        return $yum->installPackage("lsof") ;
+        if (isset($process)) {
+            $logging->log("Port {$this->portNumber} is being used by the process {$process}", $this->getModuleName()) ;
+            if (isset($this->params["expect"])) {
+                $logging->log("Expecting process to be {$this->params["expect"]}", $this->getModuleName()) ;
+                if ($this->params["expect"] != $process ) {
+                    $logging->log("Wrong process on Port {$this->portNumber}", $this->getModuleName()) ;
+                    \Core\BootStrap::setExitCode(1);
+                    return false ; } }
+            $logging->log("Expected process found", $this->getModuleName()) ;
+            return true; }
+        else {
+            $logging->log("Port {$this->portNumber} is not being used by a process", $this->getModuleName()) ;
+            return false; }
+    }
+
+    protected function findProcess($out) {
+        $startChar=0;
+        $sys = new \Model\SystemDetectionAllOS() ;
+        for ($i=0; $i<count($out["output"]); $i++) {
+            $hasChar = strpos($out["output"][$i], "Local Address") ;
+            if ($hasChar !==false) {
+                $startChar = $hasChar ; }
+            else {
+                $endStr = substr($out["output"][$i], $startChar) ;
+                $possible = substr($out["output"][$i], $startChar, strpos($endStr, " ")) ;
+                foreach ($sys->ipAddresses as $ip) {
+                    if ($possible == "{$ip}:{$this->portNumber}") {
+                        return $this->getServiceNameFromOutput($out["output"][$i+1]); } } } }
+        return null ;
+    }
+
+    protected function getServiceNameFromOutput($out) {
+        $start = strpos($out, "[") ;
+        $end = strpos($out, "]") ;
+        $str = substr($out, $start, $end) ;
+        return $str ;
     }
 
 }
