a6a09bf62fec3459312fa8c71936ee030d897b99
David Amanshia <phpengine@hotmail.co.uk>
2015-06-30 00:37:09 +0100

    templated gen  and env specific in one

--- src/Modules/Cleofy/Model/CleofyTinyAllOS.php
+++ src/Modules/Cleofy/Model/CleofyTinyAllOS.php
@@ -19,7 +19,7 @@ class CleofyTinyAllOS extends Base {
     private $environmentReplacements ;
 
     public function __construct($params) {
-      parent::__construct($params);
+        parent::__construct($params);
     }
 
     public function askWhetherToCleofy() {
@@ -54,33 +54,71 @@ class CleofyTinyAllOS extends Base {
         $serversText = "";
         foreach($serversArray as $serverArray) {
             $serversText .= 'array(';
-            $serversText .= '"target" => "'.$serverArray["target"].'", ';
-            $serversText .= '"user" => "'.$serverArray["user"].'", ';
-            $serversText .= '"pword" => "'.$serverArray["password"].'", ';
+            $serversText .= ' "target" => "'.$serverArray["target"].'", ';
+            $serversText .= ' "user" => "'.$serverArray["user"].'", ';
+            $serversText .= ' "pword" => "'.$serverArray["password"].'", ';
             $serversText .= '),'."\n"; }
         return $serversText;
     }
 
     private function doCleofy() {
-      $templatesDir = str_replace("Model", "Templates/EnvSpecific", dirname(__FILE__) ) ;
-      $templates = scandir($templatesDir);
-      foreach ($this->environments as $environment) {
-        foreach ($templates as $template) {
-          if (!in_array($template, array(".", ".."))) {
-              $templatorFactory = new \Model\Templating();
-              $templator = $templatorFactory->getModel($this->params);
-              $newFileName = str_replace("environment", $environment["any-app"]["gen_env_name"], $template ) ;
-              $autosDir = getcwd().'build'.DS.'config'.DS.'ptconfigure'.DS.'cleofy'.DS.'autopilots'.DS.'generated';
-              $targetLocation = $autosDir.DIRECTORY_SEPARATOR.$newFileName ;
-              $templator->template(
-                  file_get_contents($templatesDir.DIRECTORY_SEPARATOR.$template),
-                  array(
-                      "gen_srv_array_text" => $this->getServerArrayText($environment["servers"]) ,
-                      "env_name" => $environment["any-app"]["gen_env_name"],
-                      "first_server_target" => $environment["servers"][0]["target"],
-                  ),
-                  $targetLocation );
-          echo $targetLocation."\n"; } } }
+        $templatesDir = str_replace("Model", "Templates".DS."EnvSpecific", dirname(__FILE__) ) ;
+        $templates = scandir($templatesDir);
+        $results = array();
+        foreach ($this->environments as $environment) {
+            foreach ($templates as $template) {
+                if (!in_array($template, array(".", ".."))) {
+                    $templatorFactory = new \Model\Templating();
+                    $templator = $templatorFactory->getModel($this->params);
+                    $newFileName = str_replace("environment", $environment["any-app"]["gen_env_name"], $template ) ;
+                    $autosDir = getcwd().'build'.DS.'config'.DS.'ptconfigure'.DS.'cleofy';
+                    $targetLocation = $autosDir.DIRECTORY_SEPARATOR.$newFileName ;
+                    $results[] = $templator->template(
+                        file_get_contents($templatesDir.DIRECTORY_SEPARATOR.$template),
+                        array(
+                            "gen_srv_array_text" => $this->getServerArrayText($environment["servers"]) ,
+                            "env_name" => $environment["any-app"]["gen_env_name"],
+                            "first_server_target" => $environment["servers"][0]["target"],
+                        ),
+                        $targetLocation ); } } }
+        $result = (in_array($results, false)) ? false : true ;
+        return $result ;
+    }
+
+    private function doCleofyGuess() {
+        $templatesDir = str_replace("Model", "Templates".DS."EnvSpecific", dirname(__FILE__) ) ;
+        $templates = scandir($templatesDir);
+        $results = array();
+
+
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params);
+
+        if ($this->params["guess"]==true) {
+            $logging->log("Attempting to guess settings file", $this->getModuleName()) ;
+            $default_settings = getcwd().DS.'build'.DS.'config'.DS.'ptconfigure'.DS.'settings.php' ;
+            $exists = file_exists($default_settings) ;
+            if ($exists==true) {
+                $logging->log("Found settings file $default_settings", $this->getModuleName()) ;
+                require_once($default_settings); } }
+
+        if (!isset($bastion_env)) $bastion_env = $this->getEnvName("bastion") ;
+        if (!isset($build_env)) $build_env = $this->getEnvName("build") ;
+        if (!isset($git_env)) $git_env = $this->getEnvName("git") ;
+        if (!isset($staging_env)) $staging_env = $this->getEnvName("staging") ;
+        if (!isset($production_env)) $production_env = $this->getEnvName("production") ;
+
+
+        $result = (in_array($results, false)) ? false : true ;
+        return $result ;
+    }
+
+
+
+    private function getEnvName($env) {
+        if (isset($this->params["{$env}-env"])) { return $this->params["{$env}-env"] ; }
+        $question = 'Env name for '.ucfirst($env).'?';
+        return self::askYesOrNo($question, true);
     }
 
 }
