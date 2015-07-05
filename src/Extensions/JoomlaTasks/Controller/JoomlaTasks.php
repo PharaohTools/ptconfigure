166c649ed5315b5ec56e6909df8f37e406d926fa
David Amanshia <phpengine@hotmail.co.uk>
2015-07-05 19:06:52 +0100

    joomla tasks module started

---
+++ src/Extensions/JoomlaTasks/Controller/JoomlaTasks.php
@@ -0,0 +1,35 @@
+<?php
+
+Namespace Controller ;
+
+class Boxify extends Base {
+
+    public function execute($pageVars) {
+        $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
+        // if we don't have an object, its an array of errors
+        if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+        $action = $pageVars["route"]["action"];
+        if ($action=="help") {
+            $helpModel = new \Model\Help();
+            $this->content["helpData"] = $helpModel->getHelpData($pageVars["route"]["control"]);
+            return array ("type"=>"view", "view"=>"help", "pageVars"=>$this->content); }
+    }
+
+    public function executeTask($pageVars) {
+        $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
+        // if we don't have an object, its an array of errors
+        if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+        $action = $pageVars["route"]["action"];
+        if ($action=="help") {
+            $helpModel = new \Model\Help();
+            $this->content["helpData"] = $helpModel->getHelpData($pageVars["route"]["control"]);
+            return array ("type"=>"view", "view"=>"help", "pageVars"=>$this->content); }
+        if (in_array($action, array("install-generic-autopilots", "gen") )) {
+            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "GenericAutos") ;
+            // if we don't have an object, its an array of errors
+            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+            $this->content["result"] = $thisModel->askAction($action);
+            return array ("type"=>"view", "view"=>"boxifyGenAutos", "pageVars"=>$this->content); }
+    }
+
+}
