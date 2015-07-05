804c240969030df3b5799cd59966e82c78cd7d4a
David Amanshia <phpengine@hotmail.co.uk>
2015-07-06 00:25:25 +0100

    minor formatting

--- src/Modules/Joomla/Controller/Joomla.php
+++ src/Modules/Joomla/Controller/Joomla.php
@@ -117,13 +117,11 @@ class Joomla extends Base {
     public function executeDBInstall($pageVars) {
         $action = $pageVars["route"]["action"];
         if ($action == "joomla-save") {
-
             $confModel = $this->getModelAndCheckDependencies("DBConfigure", $pageVars) ;
             // if we don't have an object, its an array of errors
             if (is_array($confModel)) { return $this->failDependencies($pageVars, $this->content, $confModel) ; }
             $joomlaPlatform = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "Joomla30Config") ;
             $confModel->setPlatformVars($joomlaPlatform);
-
             $thisModel = $this->getModelAndCheckDependencies("DBInstall", $pageVars) ;
             if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
             $this->content["dbResult"] = $thisModel->askWhetherToSaveDB($confModel);
