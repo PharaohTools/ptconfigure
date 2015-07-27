291b88757a3b3830b27afba611ca1ca57d36797b
David Amanshia <phpengine@hotmail.co.uk>
2015-07-27 22:18:09 +0100

    pharaoh buid module sets almost everything for correct initial installation

--- src/Modules/PTBuild/Model/PTBuildLinuxMac.php
+++ src/Modules/PTBuild/Model/PTBuildLinuxMac.php
@@ -35,25 +35,34 @@ class PTBuildLinuxMac extends BasePHPApp {
     public function getPostInstallCommands() {
         $ray = array( ) ;
         if (isset($this->params["with-webfaces"]) && $this->params["with-webfaces"]==true) {
-            $dapperAuto = $this->getDapperAutoPath() ;
             $ray[]["command"][] = SUDOPREFIX.PTBCOMM." assetpublisher publish --yes --guess" ;
-            $ray[]["command"][] = SUDOPREFIX.PTDCOMM." auto x --af=$dapperAuto" ; }
-
-
+            $ray[]["command"][] = SUDOPREFIX.PTDCOMM." auto x --af=".$this->getDeployAutoPath() ;
+            $ray[]["command"][] = SUDOPREFIX."sh ".$this->getUserShellAutoPath() ;
+            $ray[]["command"][] = SUDOPREFIX.PTCCOMM." auto x --af=".$this->getConfigureAutoPath() ; }
         /*
-         * @todo sudo chmod 777 /opt/ptbuild/ptbuild/ptbuildvars
-         * @todo sudo mkdir /opt/ptbuild/pipes
-         * @todo sudo chmod 777 /opt/ptbuild/pipes
-         * @todo create switching user
-         * @todo create default setting for switching user
-         *
+         * @todo create switching user -- user shell
+         * @todo user can sudo without password -- first cm file step
+         * @todo create default setting for switching user - how to do this? can we api settings changes
+         * @todo sudo chmod 777 /opt/ptbuild/ptbuild/ptbuildvars -- second CM file step
+         * @todo sudo mkdir /opt/ptbuild/pipes -- third CM file step
+         * @todo sudo chmod 777 /opt/ptbuild/pipes-- fourth CM file step
          */
-
         return $ray ;
     }
 
-    public function getDapperAutoPath() {
+    public function getDeployAutoPath() {
         $path = dirname(dirname(__FILE__)).DS.'Autopilots'.DS.'PTDeploy'.DS.'create-vhost.php' ;
+        return $path ;
+    }
+
+    public function getConfigureAutoPath() {
+        $path = dirname(dirname(__FILE__)).DS.'Autopilots'.DS.'PTConfigure'.DS.'users-and-permissions.php' ;
+        return $path ;
+    }
+
+    public function getUserShellAutoPath() {
+        $path = dirname(dirname(__FILE__)).DS.'Scripts'.DS.'create-user.sh' ;
+        $this->executeAsShell("sh $path");
         return $path ;
     }
 
