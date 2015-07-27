477bb3f50f44fccc06f662287c3883260f23ec5b
David Amanshia <phpengine@hotmail.co.uk>
2015-07-27 13:13:52 +0100

    ptd autopilot for vhost creation in pharaoh build install

---
+++ src/Modules/PTBuild/Autopilots/PTDeploy/create-vhost.php
@@ -0,0 +1,64 @@
+<?php
+
+Namespace Core ;
+
+class AutoPilotConfigured extends AutoPilot {
+
+    public $steps ;
+
+    public function __construct($params = null) {
+        parent::__construct($params);
+        $this->setSteps();
+    }
+
+    /* Steps */
+    private function setSteps() {
+
+        $vhe_url = (isset($this->params['vhe-url'])) ? $this->params['vhe-url'] : 'www.jenkins.tld' ;
+
+        $this->steps =
+            array(
+
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets begin Configuration of a Reverse Proxy from 8080 to 80"),),),
+
+                array ( "Logging" => array( "log" => array( "log-message" => "Make a default local environment to load balance to", ), ), ),
+                array ( "ApacheVHostEditor" => array( "disable-default" => array( "guess" => true, ), ), ),
+
+                // Install Apache Reverse Proxy
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets Add our reverse proxy Apache VHost" ),),),
+                array ( "ApacheVHostEditor" => array( "add" => array(
+                    "guess" => true,
+                    "vhe-url" => $vhe_url,
+                    "vhe-ip-port" => "0.0.0.0",
+                    "vhe-template" => $this->getProxyTemplate(),
+                ),),),
+
+                array ( "Logging" => array( "log" => array( "log-message" => "Now lets restart Apache so we are serving our new proxy", ), ), ),
+                array ( "ApacheControl" => array( "restart" => array( "guess" => true, ), ), ),
+
+                // End
+                array ( "Logging" => array( "log" => array( "log-message" => "Configuration of a Reverse Proxy from 8080 to 80 complete"),),),
+
+            );
+
+    }
+
+
+    private function getProxyTemplate() {
+        $template =
+            <<<'TEMPLATE'
+     NameVirtualHost ****IP ADDRESS****:80
+     <VirtualHost ****IP ADDRESS****:80>
+        ServerAdmin webmaster@localhost
+        ProxyPreserveHost On
+        ProxyPass / http://127.0.0.1:8080/
+        ProxyPassReverse / http://127.0.0.1:8080/
+     </VirtualHost>
+
+TEMPLATE;
+
+        return $template ;
+    }
+
+
+}
