804f77e80d35989bcbc88ba9af6fb3fc5d7724b0
David Amanshia <phpengine@hotmail.co.uk>
2015-07-27 13:15:09 +0100

    ptbuild intall uses new post install hook for php app to install webface

--- src/Modules/PTBuild/Model/PTBuildLinuxMac.php
+++ src/Modules/PTBuild/Model/PTBuildLinuxMac.php
@@ -24,11 +24,38 @@ class PTBuildLinuxMac extends BasePHPApp {
               null // can be null for none
           )
         );
+        $this->postinstallCommands = $this->getPostInstallCommands();
         $this->programNameMachine = "ptbuild"; // command and app dir name
         $this->programNameFriendly = " PTBuild! "; // 12 chars
         $this->programNameInstaller = "PTBuild - Update to latest version";
         $this->programExecutorTargetPath = 'ptbuild/src/Bootstrap.php';
         $this->initialize();
     }
+
+    public function getPostInstallCommands() {
+        $ray = array( ) ;
+        if (isset($this->params["with-webfaces"]) && $this->params["with-webfaces"]==true) {
+            $dapperAuto = $this->getDapperAutoPath() ;
+            $ray[]["command"][] = SUDOPREFIX.PTBCOMM." assetpublisher publish --yes --guess" ;
+            $ray[]["command"][] = SUDOPREFIX.PTDCOMM." auto x --af=$dapperAuto" ; }
+
+
+        /*
+         * @todo sudo chmod 777 /opt/ptbuild/ptbuild/ptbuildvars
+         * @todo sudo mkdir /opt/ptbuild/pipes
+         * @todo sudo chmod 777 /opt/ptbuild/pipes
+         * @todo create switching user
+         * @todo create default setting for switching user
+         *
+         */
+
+        return $ray ;
+    }
+
+    public function getDapperAutoPath() {
+        $path = dirname(dirname(__FILE__)).DS.'Autopilots'.DS.'PTDeploy'.DS.'create-vhost.php' ;
+        return $path ;
+    }
+
 
 }
