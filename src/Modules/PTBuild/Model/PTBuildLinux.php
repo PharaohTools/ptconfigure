d1391cb78b58c305aa9614a3490c4c58a3a23518
David Amanshia <phpengine@pharaohtools.com>
2015-08-03 19:05:22 +0100

    formatting change

--- src/Modules/PTBuild/Model/PTBuildLinux.php
+++ src/Modules/PTBuild/Model/PTBuildLinux.php
@@ -5,7 +5,7 @@ Namespace Model;
 class PTBuildLinux extends BasePHPApp {
 
     // Compatibility
-    public $os = array("Linux", "Darwin") ;
+    public $os = array("Linux") ;
     public $linuxType = array("any") ;
     public $distros = array("any") ;
     public $versions = array("any") ;
