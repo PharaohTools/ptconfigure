f1cb6e70b9199cefcb60a8e19d40bd6118a43331
David Amanshia <phpengine@pharaohtools.com>
2015-08-06 16:20:22 +0100

    changing shell of user for debian apche

--- src/Modules/PTBuild/Scripts/create-linux-user.sh
+++ src/Modules/PTBuild/Scripts/create-linux-user.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# Set the www-data users shell to bash
+chsh -s /bin/bash www-data
+# add the ptbuild user
 useradd -m -d /home/ptbuild -s /bin/bash -c "The Pharaoh Build User" -U ptbuild
