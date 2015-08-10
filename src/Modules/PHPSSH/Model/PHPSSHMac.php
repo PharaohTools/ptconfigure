7247cd6e6d59bf75e7bf2d6fddf008058b476804
David Amanshia <phpengine@pharaohtools.com>
2015-08-10 03:23:03 +0100

    php ssh for mac is now updates ini files

--- src/Modules/PHPSSH/Model/PHPSSHMac.php
+++ src/Modules/PHPSSH/Model/PHPSSHMac.php
@@ -18,16 +18,16 @@ class PHPSSHMac extends PHPSSHUbuntu {
         parent::__construct($params);
         $this->installCommands = array(
             array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("MacPorts", array("php55-ssh2"))) ),
-            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("PECL", array("ssh2-beta"))) ),
+            array("method"=> array("object" => $this, "method" => "addPHPIniExtension", "params" => array()) ),
         );
         $this->uninstallCommands = array(
-//            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", array("libssh2-1-dev"))) ),
-            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("PECL", array("ssh2-beta"))) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("MacPorts", array("php55-ssh2"))) ),
+            array("method"=> array("object" => $this, "method" => "removePHPIniExtension", "params" => array()) ),
         );
         $this->initialize();
     }
 
-    public function updatePHPIni() {
+    public function addPHPIniExtension() {
         $loggingFactory = new \Model\Logging();
         $logging = $loggingFactory->getModel($this->params);
         $logging->log("Removing any old extension line from PHP Ini", $this->getModuleName()) ;
@@ -44,6 +44,19 @@ class PHPSSHMac extends PHPSSHUbuntu {
         $params1["search"] = 'extension=/opt/local/lib/php55/extensions/no-debug-non-zts-20121212/ssh2.so' ;
         $file2 = $fileFactory->getModel($params2) ;
         $file2->performShouldHaveLine() ;
+    }
+
+    public function removePHPIniExtension() {
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params);
+        $logging->log("Removing extension line from PHP Ini", $this->getModuleName()) ;
+        $iniFileLocation = '/private/etc/php.ini' ;
+        $params1 = $params2 = $this->params ;
+        $params1["file"] = $iniFileLocation ;
+        $params1["search"] = 'extension=/opt/local/lib/php55/extensions/no-debug-non-zts-20121212/ssh2.so' ;
+        $fileFactory = new \Model\File();
+        $file1 = $fileFactory->getModel($params1) ;
+        $file1->performShouldNotHaveLine() ;
     }
 
     public function askStatus() {
