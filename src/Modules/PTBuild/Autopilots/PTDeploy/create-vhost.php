e03c755f9d473a973c2dd120af9684d207885272
David Amanshia <phpengine@pharaohtools.com>
2015-09-09 04:05:11 +0100

    port 6041 for pharaoh build

--- src/Modules/PTBuild/Autopilots/PTDeploy/create-vhost.php
+++ src/Modules/PTBuild/Autopilots/PTDeploy/create-vhost.php
@@ -72,7 +72,7 @@ class AutoPilotConfigured extends AutoPilot {
    CustomLog /var/log/apache2/access.log combined
 
    <IfModule mod_fastcgi.c>
-    ProxyPassMatch ^/(.*\.php(/.*)?)$ fcgi://127.0.0.1:6040/opt/ptbuild/ptbuild/src/Modules/PostInput/$1
+    ProxyPassMatch ^/(.*\.php(/.*)?)$ fcgi://127.0.0.1:6041/opt/ptbuild/ptbuild/src/Modules/PostInput/$1
    </IfModule>
 
  </VirtualHost>
