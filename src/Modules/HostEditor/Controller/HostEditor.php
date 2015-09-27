b80cf21e61d8b6c1035b621dfb1fd7140bf61c91
David Amanshia <phpengine@pharaohtools.com>
2015-09-28 00:18:34 +0100

    host editor needs default actions

--- src/Modules/HostEditor/Controller/HostEditor.php
+++ src/Modules/HostEditor/Controller/HostEditor.php
@@ -9,8 +9,8 @@ class HostEditor extends Base {
         $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
         // if we don't have an object, its an array of errors
         if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
-        // $isDefaultAction = self::checkDefaultActions($pageVars, array(), $thisModel) ;
-        // if ( is_array($isDefaultAction) ) { return $isDefaultAction; }
+         $isDefaultAction = self::checkDefaultActions($pageVars, array(), $thisModel) ;
+         if ( is_array($isDefaultAction) ) { return $isDefaultAction; }
         $action = $pageVars["route"]["action"];
 
         if ($action=="add") {
