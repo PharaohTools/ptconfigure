9f027cd4e5b254956b2b22a27cd3b1538356dcd3
David Amanshia <phpengine@pharaohtools.com>
2015-10-04 20:19:11 +0100

    pt build for mac add macports auto

--- src/Modules/PTBuild/Model/PTBuildMac.php
+++ src/Modules/PTBuild/Model/PTBuildMac.php
@@ -14,9 +14,34 @@ class PTBuildMac extends PTBuildLinux {
     // Model Group
     public $modelGroup = array("Default") ;
 
+
+    public function getPostInstallCommands() {
+        $ray = array( ) ;
+
+        if (isset($this->params["with-webfaces"]) && $this->params["with-webfaces"]==true) {
+
+            $vhestring = '';
+            $vheipport = '';
+            if (isset($this->params["vhe-url"])) { $vhestring = '--vhe-url='.$this->params["vhe-url"] ; }
+            if (isset($this->params["vhe-ip-port"])) { $vheipport = '--vhe-ip-port='.$this->params["vhe-ip-port"] ; }
+
+            $ray[]["command"][] = SUDOPREFIX.PTBCOMM." assetpublisher publish --yes --guess" ;
+            $ray[]["command"][] = SUDOPREFIX."sh ".$this->getUserShellAutoPath() ;
+            $ray[]["command"][] = SUDOPREFIX.PTCCOMM." auto x --af=".$this->getMacPortsAutoPath() ;
+            $ray[]["command"][] = SUDOPREFIX.PTCCOMM." auto x --af=".$this->getConfigureAutoPath() ;
+            $ray[]["command"][] = SUDOPREFIX.PTDCOMM." auto x --af=".$this->getDeployAutoPath(). " $vhestring $vheipport" ; }
+
+        return $ray ;
+    }
+
     public function getUserShellAutoPath() {
         $path = dirname(dirname(__FILE__)).DS.'Scripts'.DS.'create-mac-user.sh' ;
         $this->executeAsShell("sh $path");
+        return $path ;
+    }
+
+    public function getConfigureAutoPath() {
+        $path = dirname(dirname(__FILE__)).DS.'Autopilots'.DS.'PTConfigure'.DS.'macports.php' ;
         return $path ;
     }
 
