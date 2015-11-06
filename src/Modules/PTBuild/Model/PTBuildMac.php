f894c76ac754a22dddecc2685b2f303691c65273
David Amanshia <phpengine@hotmail.co.uk>
2015-11-06 00:09:58 +0000

    mac build constructor

--- src/Modules/PTBuild/Model/PTBuildMac.php
+++ src/Modules/PTBuild/Model/PTBuildMac.php
@@ -14,23 +14,22 @@ class PTBuildMac extends PTBuildLinux {
     // Model Group
     public $modelGroup = array("Default") ;
 
+    public function __construct($params) {
+        parent::__construct($params);
+    }
 
     public function getPostInstallCommands() {
         $ray = array( ) ;
-
         if (isset($this->params["with-webfaces"]) && $this->params["with-webfaces"]==true) {
-
             $vhestring = '';
             $vheipport = '';
             if (isset($this->params["vhe-url"])) { $vhestring = '--vhe-url='.$this->params["vhe-url"] ; }
             if (isset($this->params["vhe-ip-port"])) { $vheipport = '--vhe-ip-port='.$this->params["vhe-ip-port"] ; }
-
             $ray[]["command"][] = SUDOPREFIX.PTBCOMM." assetpublisher publish --yes --guess" ;
             $ray[]["command"][] = SUDOPREFIX."sh ".$this->getUserShellAutoPath() ;
             $ray[]["command"][] = SUDOPREFIX.PTCCOMM." auto x --af=".$this->getMacPortsAutoPath() ;
             $ray[]["command"][] = SUDOPREFIX.PTCCOMM." auto x --af=".$this->getConfigureAutoPath() ;
             $ray[]["command"][] = SUDOPREFIX.PTDCOMM." auto x --af=".$this->getDeployAutoPath(). " $vhestring $vheipport" ; }
-
         return $ray ;
     }
 
