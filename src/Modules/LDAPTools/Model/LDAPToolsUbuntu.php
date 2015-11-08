5b3e309aaab5e4dab2e1094138066cf1939477a2
David Amanshia <phpengine@hotmail.co.uk>
2015-11-08 01:43:21 +0000

    ldap tools module for ubuntu

---
+++ src/Modules/LDAPTools/Model/LDAPToolsUbuntu.php
@@ -0,0 +1,43 @@
+<?php
+
+Namespace Model;
+
+class GitToolsUbuntu extends BaseLinuxApp {
+
+    // Compatibility
+    public $os = array("Linux") ;
+    public $linuxType = array("Debian") ;
+    public $distros = array("Ubuntu") ;
+    public $versions = array("11.04", "11.10", "12.04", "12.10", "13.04") ;
+    public $architectures = array("any") ;
+
+    // Model Group
+    public $modelGroup = array("Default") ;
+
+    public function __construct($params) {
+        parent::__construct($params);
+        $this->autopilotDefiner = "GitTools";
+        $this->installCommands = array(
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "git")) ),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "git-core")) ),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "gitk")) ),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "git-cola")) ),
+        );
+        $this->uninstallCommands = array(
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "git")) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "git-core")) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "gitk")) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "git-cola")) ),
+        );
+        $this->programDataFolder = "";
+        $this->programNameMachine = "gittools"; // command and app dir name
+        $this->programNameFriendly = "!Git Tools!!"; // 12 chars
+        $this->programNameInstaller = "Git Tools";
+        $this->initialize();
+    }
+
+    public function askStatus() {
+        return $this->askStatusByArray(array( "git", "gitk", "git-cola" )) ;
+    }
+
+}
---
+++ src/Modules/LDAPTools/info.LDAPTools.php
@@ -0,0 +1,39 @@
+<?php
+
+Namespace Info;
+
+class GitToolsInfo extends PTConfigureBase {
+
+    public $hidden = false;
+
+    public $name = "Git Tools - Tools for working with Git SCM";
+
+    public function __construct() {
+      parent::__construct();
+    }
+
+    public function routesAvailable() {
+      return array( "GitTools" =>  array_merge(parent::routesAvailable(), array("install") ) );
+    }
+
+    public function routeAliases() {
+      return array("gittools"=>"GitTools", "git-tools"=>"GitTools");
+    }
+
+    public function helpDefinition() {
+      $help = <<<"HELPDATA"
+  This module allows you to install Git and a set of common Git Tools. These include
+  Git - the distributed source control manager, git Core a supplement to Git, Gitk
+  which is a GUI for git, and git-cola, which is also a Git GUI.
+
+  GitTools, gittools, git-tools
+
+        - install
+        Installs the latest version of Git Tools
+        example: ptconfigure gittools install
+
+HELPDATA;
+      return $help ;
+    }
+
+}
