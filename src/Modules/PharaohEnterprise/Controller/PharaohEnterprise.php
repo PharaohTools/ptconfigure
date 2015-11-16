bc916f235980e47c7770b01ba5132887eeffd6d9
Pharaoh Tools <phpengine@hotmail.co.uk>
2015-11-16 01:55:53 +0000

    save credential method complete for pharaoh enterprise

--- src/Modules/PharaohEnterprise/Controller/PharaohEnterprise.php
+++ src/Modules/PharaohEnterprise/Controller/PharaohEnterprise.php
@@ -20,6 +20,13 @@ class PharaohEnterprise extends Base {
             $this->content["result"] = $thisModel->install();
             return array ("type"=>"view", "view"=>"appInstall", "pageVars"=>$this->content); }
 
+        if (in_array($action, array("save-credentials", "save-creds"))) {
+            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "SaveCredentials") ;
+            // if we don't have an object, its an array of errors
+            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+            $this->content["result"] = $thisModel->install();
+            return array ("type"=>"view", "view"=>"appInstall", "pageVars"=>$this->content); }
+
         \Core\BootStrap::setExitCode(1);
         $this->content["messages"][] = "Action $action is not supported by ".get_class($this)." Module";
         return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content);
