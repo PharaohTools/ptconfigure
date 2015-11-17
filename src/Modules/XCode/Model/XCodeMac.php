cf6a861bf7c815984e7b5e1115e7d5baae0eaaff
Pharaoh Tools <phpengine@hotmail.co.uk>
2015-11-17 12:14:42 +0000

    xcode installs through gem app first draft

--- src/Modules/XCode/Model/XCodeMac.php
+++ src/Modules/XCode/Model/XCodeMac.php
@@ -17,77 +17,33 @@ class XCodeMac extends BaseLinuxApp {
     public function __construct($params) {
         parent::__construct($params);
         $this->installCommands = array(
-            array("method"=> array("object" => $this, "method" => "xcodeInstall", "params" => array()) ),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Gem", "xcode-install" ) ) ),
+            array("method"=> array("object" => $this, "method" => "xcodeCliToolsInstall", "params" => array()) ),
         );
         $this->uninstallCommands = array( );
         $this->programDataFolder = "/opt/XCode"; // command and app dir name
         $this->programNameMachine = "xcode"; // command and app dir name
         $this->programNameFriendly = "XCode on OSx"; // 12 chars
         $this->programNameInstaller = "XCode for OSx";
+        $this->statusCommand = "xcversion list" ;
         $this->initialize();
     }
 
-    public function askStatus() {
-        $modsTextCmd = 'httpd -M';
-        $modsText = $this->executeAndLoad($modsTextCmd) ;
-        $modsToCheck = array("fastcgi_module" ) ;
-        $loggingFactory = new \Model\Logging();
-        $logging = $loggingFactory->getModel($this->params);
-        $passing = true ;
-        foreach ($modsToCheck as $modToCheck) {
-            if (!strstr($modsText, $modToCheck)) {
-                $logging->log("Apache Module {$modToCheck} does not exist.", $this->getModuleName()) ;
-                $passing = false ; } }
-        return $passing ;
-    }
-
-//    public function ensureXCode() {
-//        $loggingFactory = new \Model\Logging();
-//        $logging = $loggingFactory->getModel($this->params);
-//        $logging->log("Ensuring XCode", $this->getModuleName()) ;
-//        $params = $this->params ;
-//        $params["file"] = "/tmp/mod_fastcgi-2.4.6/Makefile" ;
-//        $params["search"] = "= /usr/local/apache2" ;
-//        $params["replace"] = "= /usr/share/httpd" ;
-//        $fileFactory = new \Model\File() ;
-//        $file = $fileFactory->getModel($params) ;
-//        $res[] = $file->performReplaceText();
-//        $params = $this->params ;
-//        $params["file"] = "/tmp/mod_fastcgi-2.4.6/Makefile" ;
-//        $params["search"] = "CFLAGS=-arch x86_64." ;
-//        $file = $fileFactory->getModel($params) ;
-//        $res[] = $file->performShouldHaveLine();
-//        return in_array(false, $res)==false ;
-//    }
-//
-//    public function fastCgiMake() {
-//        $loggingFactory = new \Model\Logging();
-//        $logging = $loggingFactory->getModel($this->params);
-//        $logging->log("Fast CGI Make", $this->getModuleName()) ;
-//        chdir("/tmp/mod_fastcgi-2.4.6/");
-//        $sys = new \Model\SystemDetectionAllOS(); // $sysFac->getModel($this->params) ;
-//        $version = $sys->version ;
-//        if (substr_count($sys->version, ".")==2) {
-//            $rpos = strrpos($sys->version, ".") ;
-//            $version = substr($sys->version, 0, $rpos) ; }
-//        var_dump("syssvers", $version) ;
-//        $comm = SUDOPREFIX."ln -s XcodeDefault.xctoolchain /Applications/Xcode.app/Contents/Developer/Toolchains/OSX{$version}.xctoolchain" ;
-//        $rc = $this->executeAndGetReturnCode($comm, true, true) ;
-//        $res[] = ($rc["rc"] == true) ? true : false ;
-//        $comm = "pwd" ;
-//        $rc = $this->executeAndGetReturnCode($comm, true, true) ;
-//        $res[] = ($rc["rc"] == true) ? true : false ;
-//        $comm = "make" ;
-//        $rc = $this->executeAndGetReturnCode($comm, true, true) ;
-//        $res[] = ($rc["rc"] == true) ? true : false ;
-//        return (!in_array(false, $res)) ;
-//    }
-
-    public function xcodeInstall() {
+    public function xcodeCliToolsInstall() {
         $loggingFactory = new \Model\Logging();
         $logging = $loggingFactory->getModel($this->params);
         $logging->log("Installing XCode CLI Tools", $this->getModuleName()) ;
-        $comm = dirname(dirname(__FILE__))."/Templates/xcode-cli-tools-install.sh" ;
+        $comm = SUDOPREFIX." xcversion install-cli-tools" ;
+        $rc = $this->executeAndGetReturnCode($comm, true, true) ;
+        $res = ($rc["rc"] == true) ? true : false ;
+        return $res ;
+    }
+
+    public function runXCodeInstaller() {
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params);
+        $logging->log("Installing XCode Application", $this->getModuleName()) ;
+        $comm = SUDOPREFIX." xcversion install" ;
         $rc = $this->executeAndGetReturnCode($comm, true, true) ;
         $res = ($rc["rc"] == true) ? true : false ;
         return $res ;
