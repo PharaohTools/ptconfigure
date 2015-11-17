76692a7f5c6f834563d7bdf4f12c003147de51cf
Pharaoh Tools <phpengine@hotmail.co.uk>
2015-11-17 14:02:39 +0000

    initialising xcode credentials

--- src/Modules/XCode/Model/XCodeMac.php
+++ src/Modules/XCode/Model/XCodeMac.php
@@ -14,11 +14,17 @@ class XCodeMac extends BaseLinuxApp {
     // Model Group
     public $modelGroup = array("Default") ;
 
+    protected $username ;
+    protected $pass ;
+
     public function __construct($params) {
         parent::__construct($params);
         $this->installCommands = array(
-            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Gem", "xcode-install" ) ) ),
-            array("method"=> array("object" => $this, "method" => "xcodeCliToolsInstall", "params" => array()) ),
+            array("method"=> array("object" => $this, "method" => "xEcho", "params" => array())),
+            array("method"=> array("object" => $this, "method" => "initialiseEnterprise", "params" => array()) ),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Gem", "xcode-install" ))),
+            array("method"=> array("object" => $this, "method" => "xcodeCliToolsInstall", "params" => array())),
+            array("method"=> array("object" => $this, "method" => "runXCodeInstaller", "params" => array()))
         );
         $this->uninstallCommands = array( );
         $this->programDataFolder = "/opt/XCode"; // command and app dir name
@@ -27,6 +33,40 @@ class XCodeMac extends BaseLinuxApp {
         $this->programNameInstaller = "XCode for OSx";
         $this->statusCommand = "xcversion list" ;
         $this->initialize();
+    }
+
+
+    public function initialiseXCode() {
+        $this->username = $this->askForAppleXCodeUsername();
+        $this->apiKey = $this->askForAppleXCodeAPIKey();
+    }
+
+    protected function askForAppleXCodeAPIKey(){
+        if (isset($this->params["api-key"])) { return $this->params["api-key"] ; }
+        $appVar = \Model\AppConfig::getAppVariable("apple-developer-api-key") ;
+        if ($appVar != null) {
+            $question = 'Use Application saved Apple Developer API Key?';
+            if (self::askYesOrNo($question, true) == true) { return $appVar ; } }
+        $question = 'Enter Apple Developer API Key';
+        return self::askForInput($question, true);
+    }
+
+    protected function askForAppleXCodeUsername(){
+        if (isset($this->params["username"])) { return $this->params["username"] ; }
+        $appVar = \Model\AppConfig::getAppVariable("apple-developer-username") ;
+        if ($appVar != null) {
+            $question = 'Use Application saved Apple Developer User Name?';
+            if (self::askYesOrNo($question, true) == true) {
+                return $appVar ; } }
+        $question = 'Enter Apple Developer User Name';
+        return self::askForInput($question, true);
+    }
+
+    public function xEcho() {
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params);
+        $logging->log("Echoing something", $this->getModuleName()) ;
+        return true ;
     }
 
     public function xcodeCliToolsInstall() {
