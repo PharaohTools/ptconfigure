e0e8bcbcbb157c37f3b74c31f13b1b5764776c25
Pharaoh Tools <phpengine@hotmail.co.uk>
2015-11-29 05:13:39 +0000

    handling install action

--- src/Modules/PharaohEnterprise/Controller/PharaohEnterprise.php
+++ src/Modules/PharaohEnterprise/Controller/PharaohEnterprise.php
@@ -13,6 +13,13 @@ class PharaohEnterprise extends Base {
             $this->content["helpData"] = $helpModel->getHelpData($pageVars["route"]["control"]);
             return array ("type"=>"view", "view"=>"help", "pageVars"=>$this->content); }
 
+        if (in_array($action, array("install"))) {
+            $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
+            // if we don't have an object, its an array of errors
+            if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+            $this->content["result"] = $thisModel->install();
+            return array ("type"=>"view", "view"=>"appInstall", "pageVars"=>$this->content); }
+
         if (in_array($action, array("test-credentials", "test-creds"))) {
             $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "TestCredentials") ;
             // if we don't have an object, its an array of errors
