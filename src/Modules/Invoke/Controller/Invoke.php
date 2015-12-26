c1bf45c4885ad9223e3df9412cd467f5541754ee
Pharaoh Tools <phpengine@hotmail.co.uk>
2015-12-26 05:12:20 +0000

    added default result return from controller

--- src/Modules/Invoke/Controller/Invoke.php
+++ src/Modules/Invoke/Controller/Invoke.php
@@ -9,6 +9,8 @@ class Invoke extends Base {
         $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
         // if we don't have an object, its an array of errors
         if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+	    $isDefaultAction = self::checkDefaultActions($pageVars, array(), $thisModel) ;
+        if ( is_array($isDefaultAction) ) { return $isDefaultAction; }
 
         $action = $pageVars["route"]["action"];
         $this->content["route"] = $pageVars["route"] ;
@@ -19,13 +21,15 @@ class Invoke extends Base {
             return array ("type"=>"view", "view"=>"help", "pageVars"=>$this->content); }
 
         if ($action=="cli") {
-            $this->content["shlResult"] = $thisModel->askWhetherToInvokeSSHShell();
+            $this->content["shlResult"] = $this->content["result"] = $thisModel->askWhetherToInvokeSSHShell();
             return array ("type"=>"view", "view"=>"invoke", "pageVars"=>$this->content); }
+
         if ($action=="script") {
-            $this->content["shlResult"] = $thisModel->askWhetherToInvokeSSHScript();
+            $this->content["shlResult"] = $this->content["result"] = $thisModel->askWhetherToInvokeSSHScript();
             return array ("type"=>"view", "view"=>"invoke", "pageVars"=>$this->content); }
+
         if ($action=="data") {
-            $this->content["shlResult"] = $thisModel->askWhetherToInvokeSSHData();
+            $this->content["shlResult"] = $this->content["result"] = $thisModel->askWhetherToInvokeSSHData();
             return array ("type"=>"view", "view"=>"invoke", "pageVars"=>$this->content); }
 
         \Core\BootStrap::setExitCode(1);
