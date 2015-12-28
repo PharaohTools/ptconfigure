926fbe67ae21a392427b21912da74d1f05d1aaa8
Pharaoh Tools <phpengine@hotmail.co.uk>
2015-12-28 12:45:14 +0000

    app install streamlined

--- src/Modules/PTConfigureRequired/Views/AppInstallView.tpl.php
+++ src/Modules/PTConfigureRequired/Views/AppInstallView.tpl.php
@@ -1,17 +1,3 @@
-Step Begun
-------------------------------
 <?php
-
-if (isset($pageVars["appName"]) && isset($pageVars["result"])) {
-
-    echo  $pageVars["appName"] ; ?>: <?php
-    $result_summary = (strlen($pageVars["result"])>0) ? "Success" : "Failure" ;
-    echo $result_summary."\n" ;
-}
-
-else {
-//    echo "No Data.\n";
-}
-?>
-------------------------------
-Step Complete
+$result_summary = ($pageVars["result"]!==false) ? "Success" : "Failure" ;
+echo $result_summary ;
