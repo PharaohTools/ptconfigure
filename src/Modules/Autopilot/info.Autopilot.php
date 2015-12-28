9e7183ef33534c039a94144b6a08634ba6ba859e
Pharaoh Tools <phpengine@hotmail.co.uk>
2015-12-28 12:56:18 +0000

    test action added

--- src/Modules/Autopilot/info.Autopilot.php
+++ src/Modules/Autopilot/info.Autopilot.php
@@ -9,19 +9,19 @@ class AutopilotInfo extends PTConfigureBase {
     public $name = "PTConfigure Autopilot - User Defined Installations";
 
     public function __construct() {
-      parent::__construct();
+        parent::__construct();
     }
 
     public function routesAvailable() {
-      return array( "Autopilot" =>  array_merge(parent::routesAvailable(), array("install", "execute", "x") ) );
+        return array( "Autopilot" =>  array_merge(parent::routesAvailable(), array("install", "execute", "x", "test") ) );
     }
 
     public function routeAliases() {
-      return array("auto"=>"Autopilot", "autopilot"=>"Autopilot");
+        return array("auto"=>"Autopilot", "autopilot"=>"Autopilot");
     }
 
     public function helpDefinition() {
-      $help = <<<"HELPDATA"
+        $help = <<<"HELPDATA"
   This command is part of a default Module and provides you with a method by which you can perform user defined
   executions of any PTDeploy Modules, in any order, and with your own predefined settings.
 
@@ -29,11 +29,15 @@ class AutopilotInfo extends PTConfigureBase {
 
     - install, execute, x
     execute all of the defined modules in your Autopilot file
-    example: ptdeploy autopilot install --autopilot-file=*path-to-file*
-    example: ptdeploy auto x --af=*path-to-file*
+    example: ptconfigure autopilot install --autopilot-file=*path-to-file*
+    example: ptconfigure auto x --af=*path-to-file* *
+
+    - test
+    execute all of the steps defined as tests in your Autopilot file
+    example: ptconfigure auto test --af=*path-to-file*
 
 HELPDATA;
-      return $help ;
+        return $help ;
     }
 
 }
