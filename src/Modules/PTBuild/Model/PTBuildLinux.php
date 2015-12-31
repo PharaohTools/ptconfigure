6583b193694f8713484bd9cd8637216121022ef7
Pharaoh Tools <phpengine@hotmail.co.uk>
2015-12-31 01:54:47 +0000

    build install upgrade started

--- src/Modules/PTBuild/Model/PTBuildLinux.php
+++ src/Modules/PTBuild/Model/PTBuildLinux.php
@@ -46,6 +46,39 @@ class PTBuildLinux extends BasePHPApp {
         $this->postinstallCommands = $ray ;
     }
 
+    public function setupgradeinstallCommands() {
+        $ray = array( ) ;
+        if (isset($this->params["force"]) && $this->params["force"]==true) {
+            $vhestring = '';
+            $vheipport = '';
+            if (isset($this->params["vhe-url"])) { $vhestring = '--vhe-url='.$this->params["vhe-url"] ; }
+            if (isset($this->params["vhe-ip-port"])) { $vheipport = '--vhe-ip-port='.$this->params["vhe-ip-port"] ; }
+            $ray[]["command"][] = SUDOPREFIX.PTBCOMM." assetpublisher publish --yes --guess" ;
+            $ray[]["command"][] = SUDOPREFIX."sh ".$this->getLinuxUserShellAutoPath() ;
+            $ray[]["command"][] = SUDOPREFIX.PTCCOMM." auto x --af=".$this->getConfigureAutoPath() ;
+            $ray[]["command"][] = SUDOPREFIX.PTDCOMM." auto x --af=".$this->getDeployAutoPath(). " $vhestring $vheipport" ;
+            $ptb_user = PTBCOMM ;
+            $ray[]["command"][] = SUDOPREFIX."mkdir -p /tmp/ptbuild-pipes/" ;
+            $ray[]["command"][] = SUDOPREFIX."mkdir -p /tmp/ptbuild-settings/" ;
+            $ray[]["command"][] = SUDOPREFIX."mkdir -p /tmp/ptbuild-keys/" ;
+            $ray[]["command"][] = SUDOPREFIX."cp -r /opt/ptbuild/pipes /tmp/ptbuild-pipes/" ;
+            $ray[]["command"][] = SUDOPREFIX."cp -r /opt/ptbuild/keys /tmp/ptbuild-keys/" ;
+            $ray[]["command"][] = SUDOPREFIX."cp /opt/ptbuild/ptbuild/ptbuildvars /tmp/ptbuild-settings/" ;
+            $ray[]["command"][] = SUDOPREFIX."cp /opt/ptbuild/ptbuild/src/Modules/Signup/Data/users.txt /tmp/ptbuild-settings/" ;
+//            $ray[]["method"] = array("object" => $this, "method" => "install", "params" => array("params", "jenkins") ) ;
+            $ray[]["command"][] = SUDOPREFIX."ptconfigure ptbuild install -yg --with-webfaces --vhe-url=ptbuild.pharaohtools.com --vhe-ip-port=198.211.118.37:80" ;
+            $ray[]["command"][] = SUDOPREFIX."cp -r /tmp/ptbuild-pipes/pipes/* /opt/ptbuild/pipes/" ;
+            $ray[]["command"][] = SUDOPREFIX."cp -r /tmp/ptbuild-keys/* /opt/ptbuild/keys/" ;
+            $ray[]["command"][] = SUDOPREFIX."chmod -R 0600 /opt/ptbuild/keys/*" ;
+            $ray[]["command"][] = SUDOPREFIX."cp /tmp/ptbuild-settings/users.txt /opt/ptbuild/ptbuild/src/Modules/Signup/Data/users.txt" ;
+            $ray[]["command"][] = SUDOPREFIX."cp /tmp/ptbuild-settings/ptbuildvars /opt/ptbuild/ptbuild/ptbuildvars" ;
+            $ray[]["command"][] = SUDOPREFIX."chown -R ".$ptb_user.":".$ptb_user." /opt/ptbuild/" ;
+
+        }
+
+        $this->upgradeinstallCommands = $ray ;
+    }
+
     public function getDeployAutoPath() {
         $path = dirname(dirname(__FILE__)).DS.'Autopilots'.DS.'PTDeploy'.DS.'create-vhost.php' ;
         return $path ;
