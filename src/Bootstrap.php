c3b96596fb2fa5042f22c369b4ea3d6db7c03d98
Pharaoh Tools <phpengine@hotmail.co.uk>
2016-01-02 18:23:11 +0000

    default exit code

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -8,6 +8,7 @@ $argv_or_null = (isset($argv)) ? $argv : null ;
 // @todo document this feature that allows a user to provide a json envoronment variable of parameters
 
 $bootStrapParams = (isset($_ENV['ptdeploy_bootstrap'])) ? json_decode($_ENV['ptdeploy_bootstrap']) : $argv_or_null ;
+$bootStrap->setExitCode(0) ;
 $bootStrap->main($bootStrapParams);
 
 class BootStrap {
