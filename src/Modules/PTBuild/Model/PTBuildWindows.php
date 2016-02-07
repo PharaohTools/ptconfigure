e7c0cda7bb1852ee261c784922edfe6cafb1dcdf
Pharaoh Tools <phpengine@hotmail.co.uk>
2016-02-07 13:59:03 +0000

    ptbuild for window ha webfaces

--- src/Modules/PTBuild/Model/PTBuildWindows.php
+++ src/Modules/PTBuild/Model/PTBuildWindows.php
@@ -31,4 +31,62 @@ class PTBuildWindows extends BasePHPWindowsApp {
         $this->initialize();
     }
 
+    public function setpostinstallCommands() {
+        $ray = array( ) ;
+        if (isset($this->params["with-webfaces"]) && $this->params["with-webfaces"]==true) {
+            $vhestring = '';
+            $vheipport = '';
+            if (isset($this->params["vhe-url"])) { $vhestring = '--vhe-url='.$this->params["vhe-url"] ; }
+            if (isset($this->params["vhe-ip-port"])) { $vheipport = '--vhe-ip-port='.$this->params["vhe-ip-port"] ; }
+            $ray[]["command"][] = PTBCOMM." assetpublisher publish --yes --guess" ;
+            $ray[]["command"][] = "net user ptbuild ptbuild /ADD" ;
+            $ray[]["command"][] = PTCCOMM." auto x --af=".$this->getConfigureAutoPath() ;
+            $ray[]["command"][] = PTDCOMM." auto x --af=".$this->getDeployAutoPath(). " $vhestring $vheipport" ; }
+        $this->postinstallCommands = $ray ;
+    }
+
+    public function setupgradeinstallCommands() {
+        $ray = array( ) ;
+        if (isset($this->params["force"]) && $this->params["force"]==true) {
+            $vhestring = '';
+            $vheipport = '';
+            if (isset($this->params["vhe-url"])) { $vhestring = '--vhe-url='.$this->params["vhe-url"] ; }
+            if (isset($this->params["vhe-ip-port"])) { $vheipport = '--vhe-ip-port='.$this->params["vhe-ip-port"] ; }
+            $ray[]["command"][] = PTBCOMM." assetpublisher publish --yes --guess" ;
+            $ray[]["command"][] = "net user ptbuild ptbuild /ADD" ;
+            $ray[]["command"][] = PTCCOMM." auto x --af=".$this->getConfigureAutoPath() ;
+            $ray[]["command"][] = PTDCOMM." auto x --af=".$this->getDeployAutoPath(). " $vhestring $vheipport" ;
+            $ptb_user = PTBCOMM ;
+            $temp_dir = getenv('TEMP') ;
+            $ray[]["command"][] = "if not exist \"".$temp_dir."ptbuild-pipes".DS."\" mkdir \"".$temp_dir."ptbuild-pipes".DS."\"" ;
+            $ray[]["command"][] = "if not exist \"".$temp_dir."ptbuild-keys".DS."\" mkdir \"".$temp_dir."ptbuild-keys".DS."\"" ; //"mkdir -p ".$temp_dir."ptbuild-settings".DS ;
+            $ray[]["command"][] = "if not exist \"".$temp_dir."ptbuild-settings".DS."\" mkdir \"".$temp_dir."ptbuild-settings".DS."\"" ; //"mkdir -p ".$temp_dir."ptbuild-keys".DS ;
+            $ray[]["command"][] = "xcopy /q /s /e /y ".PIPEDIR." ".$temp_dir."ptbuild-pipes".DS ;
+            $ray[]["command"][] = "xcopy /q /s /e /y ".PFILESDIR."keys ".$temp_dir."ptbuild-keys".DS ;
+            $ray[]["command"][] = "copy ".PFILESDIR."ptbuild".DS."ptbuild".DS."ptbuildvars ".$temp_dir."ptbuild-settings".DS."ptbuildvars" ;
+            $ray[]["command"][] = "copy ".PFILESDIR."ptbuild".DS."ptbuild".DS."src\\Modules\\Signup\\Data\\users.txt ".$temp_dir."ptbuild-settings".DS."users.txt" ;
+//            $ray[]["method"] = array("object" => $this, "method" => "install", "params" => array("params", "jenkins") ) ;
+            $ray[]["command"][] = "ptconfigure ptbuild install -yg --with-webfaces --vhe-url=ptbuild.local --vhe-ip-port=127.0.0.1" ;
+            $ray[]["command"][] = "xcopy /q /s /e /y ".$temp_dir."ptbuild-pipes".DS."pipes".DS."* ".PIPEDIR ;
+            $ray[]["command"][] = "xcopy /q /s /e /y ".$temp_dir."ptbuild-keys".DS."* ".PFILESDIR."keys".DS ;
+//            $ray[]["command"][] = "chmod -R 0600 /opt/ptbuild/keys/*" ;
+            $ray[]["command"][] = "copy ".$temp_dir."ptbuild-settings".DS."users.txt ".PFILESDIR."ptbuild".DS."ptbuild".DS."src\\Modules\\Signup\\Data\\users.txt" ;
+            $ray[]["command"][] = "copy ".$temp_dir."ptbuild-settings".DS."ptbuildvars ".PFILESDIR."ptbuild".DS."ptbuild".DS."ptbuildvars" ;
+//            $ray[]["command"][] = "chown -R ".$ptb_user.":".$ptb_user." /opt/ptbuild".DS ;
+
+        }
+
+        $this->upgradeinstallCommands = $ray ;
+    }
+
+    public function getDeployAutoPath() {
+        $path = dirname(dirname(__FILE__)).DS.'Autopilots'.DS.'PTDeploy'.DS.'create-vhost.php' ;
+        return $path ;
+    }
+
+    public function getConfigureAutoPath() {
+        $path = dirname(dirname(__FILE__)).DS.'Autopilots'.DS.'PTConfigure'.DS.'ptbconf.php' ;
+        return $path ;
+    }
+
 }
