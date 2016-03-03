634ab1a67e38a7e3dc8725fffdf10630e3001110
Pharaoh Tools <phpengine@hotmail.co.uk>
2016-03-03 12:16:27 +0000

    autopilot controller supports autopilot dsl module

--- src/Modules/Autopilot/Controller/Autopilot.php
+++ src/Modules/Autopilot/Controller/Autopilot.php
@@ -13,7 +13,7 @@ class Autopilot extends Base {
         $action = $pageVars["route"]["action"];
         if ( in_array($action, array("install", "execute", "x", "test") ) ) {
             if ( isset($thisModel->params["autopilot-file"]) && strlen($thisModel->params["autopilot-file"]) > 0 ) {
-                $autoPilot = $this->loadAutoPilot($thisModel->params);
+                $autoPilot = $this->loadAutoPilot($thisModel->params, $pageVars);
                 if ( $autoPilot!==null ) {
 //                    echo "1" ;
                     $autoPilotExecutor = new \Controller\AutopilotExecutor();
@@ -46,14 +46,25 @@ class Autopilot extends Base {
 
     }
 
-    private function loadAutoPilot($params){
+    private function loadAutoPilot($params, $pageVars){
         $autoPilotFileName = escapeshellcmd($params["autopilot-file"]);
         $autoPilotFilePath = getcwd().DS.$autoPilotFileName;
         $defaultFolderToCheck = str_replace("src".DS."Controller",
             "build".DS."config".DS.PHARAOH_APP, dirname(__FILE__));
         $defaultName = $defaultFolderToCheck.DS.$autoPilotFileName.".php";
         if (file_exists($autoPilotFileName)) {
+            $dsl_ext = substr($autoPilotFileName, -7) ;
+            if ($dsl_ext=="dsl.php") {
+                $dsl_au = $this->loadDSLAutoPilot($autoPilotFileName, $pageVars) ;
+                if (is_object($dsl_au)) {
+                    return $dsl_au ; }
+                else {
+                    $loggingFactory = new \Model\Logging();
+                    $logging = $loggingFactory->getModel($params);
+                    $logging->log("Unable to build object from DSL", "AutopilotDSL", LOG_FAILURE_EXIT_CODE) ;
+                    return false ; } }
             require_once($autoPilotFileName); }
+
         else if (file_exists($defaultName)) {
             include_once($defaultName); }
         else if (file_exists("autopilot-".$defaultName)) {
@@ -71,6 +82,26 @@ class Autopilot extends Base {
         $autoPilot = (class_exists('\Core\AutoPilotConfigured')) ?
             new \Core\AutoPilotConfigured($params) : null ;
         return $autoPilot;
+    }
+
+    private function loadDSLAutoPilot($filename, $pageVars){
+        $dslModel = $this->getModelAndCheckDependencies("AutopilotDSL", $pageVars) ;
+        $autoPilotData = $dslModel->loopOurDSLFile($filename) ;
+        $autoPilotData = $this->transformData($autoPilotData);
+        $auto = new \StdClass() ;
+        $auto->steps = $autoPilotData ;
+        return $auto ;
+    }
+
+    public function transformData($array_to_transform) {
+        $all_steps = array() ;
+        foreach ($array_to_transform as $single_step) {
+            if (isset($single_step["params"]) && is_array($single_step["params"])) {
+                $all_steps[] = array( "{$single_step["module"]}" =>
+                array( "{$single_step["action"]}" => $single_step["params"] )) ; }
+            else {
+                $all_steps[] = array( "{$single_step["module"]}" => array( "{$single_step["action"]}")) ; } }
+        return $all_steps ;
     }
 
 }
