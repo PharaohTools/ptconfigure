a514dd97de5285ea61d6b1d78d6ec8e8808fecae
Pharaoh Tools <phpengine@hotmail.co.uk>
2016-03-08 00:03:31 +0000

    module manager handling module name better

--- src/Modules/ModuleManager/Model/ModuleManagerAnyOS.php
+++ src/Modules/ModuleManager/Model/ModuleManagerAnyOS.php
@@ -16,7 +16,6 @@ class ModuleManagerAnyOS extends BasePHPApp {
 
     public function __construct($params) {
         parent::__construct($params);
-        $this->setParameterOverrides() ;
         $this->autopilotDefiner = "ModuleManager";
         $this->fileSources = $this->getFileSources() ;
         $this->programNameMachine = "modulemanager"; // command and app dir name
@@ -28,28 +27,50 @@ class ModuleManagerAnyOS extends BasePHPApp {
     protected function setParameterOverrides() {
         $this->params["module-manager"] = true ;
         $this->params["no-executor"] = true ;
-        $ext_dir = dirname(dirname(dirname(__FILE__))) ;
-        $ext_dir .= DIRECTORY_SEPARATOR."Extensions" ;
+        $name = $this->getNameOfModuleToManage();
+        $ext_dir = dirname(dirname(dirname(dirname(__FILE__)))) ;
+        $ext_dir .= DS."Extensions".DS.$name ;
         $this->params["program-data-directory"] = $ext_dir ;
     }
 
     protected function getFileSources() {
-        $fileSources =
+        $this->setParameterOverrides() ;
+        $ms = $this->getModuleSource() ;
+        $name = substr($ms, strrpos($ms, "/"));
+        $fileSources[] =
             array(
-                array(
-                    $this->getModuleSource(),
-                    "module",
-                    null // can be null for none
-                )
+                $ms,
+                $name,
+                null // can be null for none
             );
         return $fileSources ;
     }
 
+    protected function getNameOfModuleToManage() {
+        if (isset($this->params['module-name'])) { return $this->params['module-name'] ; }
+    }
+
     protected function getModuleSource() {
-//        if (isset($this->params['module-source'])) { return $this->params['module-source'] ; }
-//        $question = "Enter the Git Repository URL of your module:";
-//        return self::askForInput($question, true);
-        return "" ;
+        if (isset($this->params['module-source'])) { return $this->params['module-source'] ; }
+        // @todo this breaks webface
+        // $question = "Enter the Git Repository URL of your module:";
+        // return self::askForInput($question, true);
+    }
+
+    public function disableModule() {
+        $appConfig = new \Model\AppConfig() ;
+        $disabled_modules = $appConfig->getAppVariable("disabled_modules") ;
+        if (!in_array($this->params['module-disable'], $disabled_modules)) {
+            $appConfig->setAppVariable("disabled_modules", $this->params['module-disable'], true) ; }
+        return true ;
+    }
+
+    public function enableModule() {
+        $appConfig = new \Model\AppConfig() ;
+        $disabled_modules = $appConfig->getAppVariable("disabled_modules") ;
+        $new_disabled_modules = array_diff($disabled_modules, array($this->params['module-enable'])) ;
+        $appConfig->setAppVariable("disabled_modules", $new_disabled_modules) ;
+        return true ;
     }
 
 }
