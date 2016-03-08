8f1af6948eb961146de0e0e69885e8f6a543c6fa
Pharaoh Tools <phpengine@hotmail.co.uk>
2016-03-08 04:07:53 +0000

    removed formatting

--- src/Modules/MacPorts/Views/MacPortsView.tpl.php
+++ src/Modules/MacPorts/Views/MacPortsView.tpl.php
@@ -1,12 +1,6 @@
-MacPorts Modifications:
---------------------------------------------
-
 <?php echo $pageVars["appName"] ; ?>: <?php
   $result_summary = ($pageVars["result"] == true) ? "Success" : "Failure" ;
   echo $result_summary."\n" ;
   if (is_string($pageVars["result"])) {
     echo $pageVars["result"]."\n" ; }
 ?>
-
-------------------------------
-MacPorts Mods Finished
