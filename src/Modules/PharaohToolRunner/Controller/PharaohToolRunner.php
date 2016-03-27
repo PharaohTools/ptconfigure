4c2c8b7ec88d5098e3971f19dd81535341f9751d
Pharaoh Tools <phpengine@hotmail.co.uk>
2016-03-27 22:42:41 +0100

    pharoah tools runner added to deploy

---
+++ src/Modules/PharaohToolRunner/Controller/PharaohToolRunner.php
@@ -0,0 +1,28 @@
+<?php
+
+Namespace Controller ;
+
+class PharaohToolRunner extends Base {
+
+    public function execute($pageVars) {
+
+        $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
+        // if we don't have an object, its an array of errors
+        if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+        $isDefaultAction = self::checkDefaultActions($pageVars, array(), $thisModel) ;
+        if ( is_array($isDefaultAction) ) { return $isDefaultAction; }
+
+        $action = $pageVars["route"]["action"];
+        $this->content["route"] = $pageVars["route"] ;
+
+        if ($action=="run") {
+            $this->content["result"] = $thisModel->askWhetherToPharaohToolRunnerPut();
+            return array ("type"=>"view", "view"=>"PharaohToolRunner", "pageVars"=>$this->content); }
+
+        \Core\BootStrap::setExitCode(1);
+        $this->content["messages"][] = "Action $action is not supported by ".get_class($this)." Module";
+        return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content);
+
+    }
+
+}
