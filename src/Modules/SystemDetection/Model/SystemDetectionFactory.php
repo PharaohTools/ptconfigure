6e0734b966b5726c436a41d2088e8ad938f6d863
David Amanshia <phpengine@pharaohtools.com>
2016-04-28 02:05:59 +0100

    debugging

--- src/Modules/SystemDetection/Model/SystemDetectionFactory.php
+++ src/Modules/SystemDetection/Model/SystemDetectionFactory.php
@@ -73,6 +73,11 @@ class SystemDetectionFactory {
                     return true ; } }
             // if conditions
             if (is_array($modelVersions[$i])) {
+
+                if (!isset($modelVersions[$i][0])) {
+                    var_dump("mv: ", $modelVersions[$i]) ;
+                }
+
                 $svo->setCondition($modelVersions[$i][0], $modelVersions[$i][1]) ;
                 $matches[] = $svo->isCompatible() ; } }
         $res = (in_array(false, $matches)) ? false : true ;
