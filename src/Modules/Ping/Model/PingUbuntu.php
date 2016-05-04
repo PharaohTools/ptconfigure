39848803e9ec43cefa37d5b4ec032c7a3d126cc5
David Amanshia <phpengine@pharaohtools.com>
2016-05-05 00:24:20 +0100

    ping supports getting targets from environments

--- src/Modules/Ping/Model/PingUbuntu.php
+++ src/Modules/Ping/Model/PingUbuntu.php
@@ -13,7 +13,7 @@ class PingUbuntu extends BaseLinuxApp {
 
     // Model Group
     public $modelGroup = array("Default") ;
-    protected $target ;
+    protected $targets ;
     protected $interval ;
     protected $maxWait ;
     protected $actionsToMethods = array(
@@ -34,7 +34,7 @@ class PingUbuntu extends BaseLinuxApp {
 
     protected function performPingOnce() {
         $libDir = str_replace("Model", "Libraries", dirname(__FILE__) ) ;
-        require_once ("{$libDir}/JJG/Ping.php") ;
+        require_once ("{$libDir}".DS."JJG".DS."Ping.php") ;
         $this->setTarget();
         $this->setInterval();
         return $this->doOnePing();
@@ -42,7 +42,7 @@ class PingUbuntu extends BaseLinuxApp {
 
     protected function performTenPings() {
         $libDir = str_replace("Model", "Libraries", dirname(__FILE__) ) ;
-        require_once ("{$libDir}/JJG/Ping.php") ;
+        require_once ("{$libDir}".DS."JJG".DS."Ping.php") ;
         $this->setTarget();
         $this->setInterval();
         return $this->doTenPings();
@@ -50,7 +50,7 @@ class PingUbuntu extends BaseLinuxApp {
 
     protected function performPingsUntil() {
         $libDir = str_replace("Model", "Libraries", dirname(__FILE__) ) ;
-        require_once ("{$libDir}/JJG/Ping.php") ;
+        require_once ("{$libDir}".DS."JJG".DS."Ping.php") ;
         $this->setTarget();
         $this->setInterval();
         $this->setMaxWait();
@@ -58,12 +58,45 @@ class PingUbuntu extends BaseLinuxApp {
     }
 
     protected function setTarget($target = null) {
-        if (isset($target)) {
-            $this->target = $target; }
+        $ges = $this->getEnvironmentIfSet() ;
+        if ($ges !== false) {
+            $this->targets = $this->getEnvironmentServerTargets($ges); }
+        else if (isset($target) && is_array($target)) {
+            $this->targets = $target; }
+        else if (isset($target) && !is_array($target)) {
+            $this->targets = array($target); }
+        else if (isset($this->params["targets"])) {
+            $this->targets = explode(',', $this->params["targets"] ) ; }
         else if (isset($this->params["target"])) {
-            $this->target = $this->params["target"]; }
+            $this->targets = explode(',', $this->params["target"] ) ; }
         else {
-            $this->target = self::askForInput("Enter Target: ", true); }
+            $this->targets = self::askForInput("Enter Target: ", true); }
+    }
+
+    protected function getEnvironmentIfSet() {
+        if (isset($this->params["environment-name"])) {
+            return $this->getEnvironment($this->params["environment-name"]); }
+        else if (isset($this->params["env"])) {
+            $this->params["environment-name"] = $this->params["env"] ;
+            return $this->getEnvironment($this->params["environment-name"]) ; }
+        else {
+            return false; }
+    }
+
+    protected function getEnvironment($envToFind) {
+        $envs = \Model\AppConfig::getProjectVariable("environments");
+        foreach ($envs as $env) {
+            if ($env["any-app"]["gen_env_name"] == $envToFind) {
+                return $env ; } }
+        return false ;
+    }
+
+    protected function getEnvironmentServerTargets($env) {
+        $stargets = array() ;
+        foreach ($env["servers"] as $server) {
+//            var_dump($server) ;
+            $stargets[] = $server["target"] ; }
+        return (count($stargets) > 0 ) ? $stargets : false ;
     }
 
     protected function setInterval() {
@@ -87,37 +120,54 @@ class PingUbuntu extends BaseLinuxApp {
     private function doOnePing() {
         $loggingFactory = new \Model\Logging();
         $logging = $loggingFactory->getModel($this->params);
-        $ping = new \JJG\Ping($this->target);
-        $latency = $ping->ping();
-        if ($latency !== false) {
-            $logging->log('Ping Latency is ' . $latency . ' ms') ;
-            return true; }
-        else {
-            $logging->log("Ping Host {$this->target} could not be reached.") ;
-            return false; }
+        $statuses = array() ;
+        $c = count($this->targets) ;
+        $logging->log("Attempting to Ping {$c} Host/s", $this->getModuleName()) ;
+        for ($target_count = 0; $target_count < count($this->targets) ; $target_count++) {
+            $logging->log("Attempting to Ping Host {$this->targets[$target_count]} once", $this->getModuleName()) ;
+            $ping = new \JJG\Ping($this->targets[$target_count]);
+            try {
+                $latency = $ping->ping();
+                if ($latency !== false) {
+                    $logging->log('Ping Latency is ' . $latency . ' ms', $this->getModuleName()) ;
+                    $statuses[$target_count] = true; }
+                else {
+                    $logging->log("Ping Host {$this->targets[$target_count]} could not be reached.", $this->getModuleName()) ;
+                    $statuses[$target_count] = false; } }
+            catch (\Exception $e) {
+                $logging->log('Failed to initialize the Ping Library', $this->getModuleName()) ; } }
+        return (in_array(false, $statuses)) ? false : true ;
     }
 
     private function doTenPings() {
         $loggingFactory = new \Model\Logging();
         $logging = $loggingFactory->getModel($this->params);
-        $ping = new \JJG\Ping($this->target);
-        for ($i = 0; $i < 10; $i ++) {
-            $latency = $ping->ping();
-            if ($latency !== false) {
-                $logging->log('Ping Latency is ' . $latency . ' ms') ;
-            $foundSuccess = true; }
+        $statuses = array() ;
+        $c = count($this->targets) ;
+        $logging->log("Attempting to Ping {$c} Host/s", $this->getModuleName()) ;
+        for ($target_count = 0; $target_count < count($this->targets) ; $target_count++) {
+            $logging->log("Attempting to Ping Host {$this->targets[$target_count]} ten times", $this->getModuleName()) ;
+            for ($i = 0; $i < 10; $i ++) {
+                $ping = new \JJG\Ping($this->targets[$target_count]);
+                $latency = $ping->ping();
+                if ($latency !== false) {
+                    $logging->log('Ping Latency is ' . $latency . ' ms', $this->getModuleName()) ;
+                    $foundSuccess = true; }
+                else {
+                    $time = $i * $this->interval ;
+                    $logging->log("Ping Host {$this->targets[$target_count]} could not be reached after $i iterations and $time seconds", $this->getModuleName()) ; }
+                sleep($this->interval) ; }
+            if (isset ($foundSuccess) && $foundSuccess==true) {
+                $statuses[$target_count] = true ; }
             else {
-                $time = $i * $this->interval ;
-                $logging->log("Ping Host {$this->target} could not be reached after $i iterations and $time seconds") ; }
-            sleep($this->interval) ; }
-        if (isset ($foundSuccess) && $foundSuccess==true) { return true ; }
-        return false ;
+                $statuses[$target_count] = false ; } }
+        return (in_array(false, $statuses)) ? false : true ;
     }
 
     private function doPingsUntil() {
         $loggingFactory = new \Model\Logging();
         $logging = $loggingFactory->getModel($this->params);
-        $ping = new \JJG\Ping($this->target);
+        $ping = new \JJG\Ping($this->targets);
         $totalTime = 0 ;
         $i = 0;
         while ($totalTime < $this->maxWait) {
@@ -126,7 +176,7 @@ class PingUbuntu extends BaseLinuxApp {
                 $logging->log('Ping Latency is ' . $latency . ' ms') ;
                 return true ; }
             else {
-                $logging->log("Ping Host {$this->target} could not be reached after $i iterations and $totalTime seconds") ; }
+                $logging->log("Ping Host {$this->targets} could not be reached after $i iterations and $totalTime seconds") ; }
             sleep($this->interval) ;
             $totalTime = $totalTime + $this->interval ;
             $i++; }
