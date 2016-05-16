7482373beb9db32906fab5e67a73bf6d88952897
David Amanshia <phpengine@pharaohtools.com>
2016-05-16 04:10:50 +0100

    removed messy newline from output

--- src/Modules/Firewall/Views/FirewallView.tpl.php
+++ src/Modules/Firewall/Views/FirewallView.tpl.php
@@ -2,5 +2,5 @@
   $result_summary = ($pageVars["result"] == true) ? "Success" : "Failure" ;
   echo $result_summary."\n" ;
   if (is_string($pageVars["result"])) {
-    echo $pageVars["result"]."\n" ; }
+    echo $pageVars["result"] ; }
 ?>
