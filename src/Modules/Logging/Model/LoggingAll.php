e4fe61c4eca24be47dab5410a37ed19cfcdc6c54
David Amanshia <phpengine@pharaohtools.com>
2016-06-10 01:55:22 +0100

    fixed major bug, must forcibly clear value of static property for php 7+

--- src/Modules/Logging/Model/LoggingAll.php
+++ src/Modules/Logging/Model/LoggingAll.php
@@ -41,24 +41,30 @@ class LoggingAll extends BaseLinuxApp {
     }
 
     public function log($message = null, $source = null, $log_exit_code = null) {
-
         if (isset($this->params["log-message"])) {
             self::$logMessage = $this->params["log-message"] ; }
         if (isset($this->params["message"])) {
             self::$logMessage = $this->params["message"] ; }
-
         if (is_null($source) && isset($this->params["source"])) {
             $source = $this->params["source"] ; }
-        if (isset(self::$logMessage)) { $message = self::$logMessage ; }
+        if (isset(self::$logMessage)) {
+            $message = self::$logMessage ; }
+
+        if (!isset($message) || is_null($message)) {
+            debug_print_backtrace() ;
+            return true ; }
+
         if (!is_null($log_exit_code)) {
             \Core\BootStrap::setExitCode($log_exit_code) ; }
         $stx = (strlen($source)>0) ? "[$source] " : "" ;
         $fullMessage = "[Pharaoh Logging] " . $stx . $message . "\n" ;
+
         $res = file_put_contents("php://stderr", $fullMessage );
         if ($res==false) { return false ;}
         if (isset($this->params["php-log"])) {
             $res = error_log($fullMessage) ;
             if ($res==false) { return false ;} }
+        self::$logMessage = null ;
         return true ;
     }
 
