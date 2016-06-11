cba3933d785ab78b944a5ca4f59226b68e95086c
David Amanshia <phpengine@pharaohtools.com>
2016-06-11 20:35:01 +0100

    suppressing ERR: for stderr

--- src/Core/Base/Model/Base.php
+++ src/Core/Base/Model/Base.php
@@ -175,8 +175,10 @@ COMPLETION;
                     echo $buf ; }
                 if ( (isset($buf2) && $buf2 !== false) || $buf2 = fread($pipes[2], 32768) ) {
 //                    $buf2 = "ERR: ".$buf2;
-                    $data .= "ERR: ".$buf2;
-                    echo "ERR: ".$buf2 ;
+//                    $data .= "ERR: ";
+                    $data .= $buf2;
+//                    echo "ERR: " ;
+                    echo $buf2 ;
                     unset($buf2) ;} } }
 
         $status = proc_get_status($proc);
