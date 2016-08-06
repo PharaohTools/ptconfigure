a10614daef556e1f6dd46ddb9e6af40f9f2d5414
David Amanshia <phpengine@pharaohtools.com>
2016-08-06 01:06:58 +0100

    cli index view updated old content

--- src/Modules/Index/Views/IndexView.tpl.php
+++ src/Modules/Index/Views/IndexView.tpl.php
@@ -1,23 +1,28 @@
-PTDeploy by Golden Contact Computing
+Pharaoh Deploy
 -------------------
 
 About:
 -----------------
-This tool helps with setting up projects. It's really cool for
-cloning/installing/spinning up webs apps easily and quickly.
+Automated Deployment, Web App Versioning and Infrastructure by Code in PHP. PTDeploy deploys PHP Applications in a
+really simple way, and does it all through code configuration. That's what it's about.
 
-Very cool for CI, you can install your web app in one line like:
+This tool is for provisioning applications and builds to your boxes. You can set up simple or complex application
+deployment patterns to your systems with one or two PHP files, or quickly set up cloud friendly deployment patterns.
 
-ptdeploy install autopilot *autopilot-file*
+PTDeploy is modular. object oriented and extendible, you can pretty easily write your own module if you want
+functionality we haven't yet covered. Feel free to submit us pull requests.
 
-With code configured settings for (at least the core parts, and
-more if want to extend) Project Checkout / Download, Cucumber
-Configuration, Database Management Functions, Host File
-Management Functions, Installation Functions, SSH Invocation
-Functions, PTDeploy Project Management Functions, Apache
-Virtual Host Functions and Versioning - On 1 or 100 servers, so
-you now have a fully code controlled deploy system, from
-Personal or Development machine to Large Enterprise.
+This is part of the Pharaoh Tools suite, which covers Configuration Management, Test Automation Management, Automated
+Deployment, Build and Release Management and more, all implemented in code, and all in PHP.
+
+Its easy to write modules for any Operating System but we've begun with Ubuntu and adding more as soon as possible.
+Currently, all of the Modules work on Ubuntu 12, most on 13, and a few on Centos.
+
+If you've heard of the Ruby tool Capistrano, then you can probably guess why this is called PTDeploy. It performs a
+similar function (app deployment), but does it in PHP (Because PHP is way cooler). This tool helps just as well with
+setting up projects locally or on 50 remote servers. It's really cool for cloning / installing / spinning up web
+apps easily and quickly - to one or multiple servers using one or two config files. Just as Capistrano is a must for
+your Ruby CI setup, PTDeploy is a must for your PHP CI.
 
 -------------------------------------------------------------
 
