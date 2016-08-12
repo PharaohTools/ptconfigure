43788867e840b5262f92ac79e7ed0189ad5a3b8b
David Amanshia <phpengine@pharaohtools.com>
2016-08-12 12:54:02 +0100

    fixed post install commands

--- src/Modules/PTBuild/Model/PTBuildMac.php
+++ src/Modules/PTBuild/Model/PTBuildMac.php
@@ -20,17 +20,31 @@ class PTBuildMac extends PTBuildLinux {
 
     public function setpostinstallCommands() {
         $ray = array( ) ;
-        if (isset($this->params["with-webfaces"]) && $this->params["with-webfaces"]==true) {
-            $vhestring = '';
-            $vheipport = '';
+//        $ray[]["method"] = array("object" => $this, "method" => "ensureApplicationUser", "params" => array() ) ;
+        if ( (isset($this->params["with-webfaces"]) && $this->params["with-webfaces"]==true) ||
+            (isset($this->params["guess"]) && $this->params["guess"]==true) ) {
+            $vhestring = '--vhe-url=build.pharaoh.tld';
+            $vheipport = '--vhe-ip-port=127.0.0.1:80';
             if (isset($this->params["vhe-url"])) { $vhestring = '--vhe-url='.$this->params["vhe-url"] ; }
             if (isset($this->params["vhe-ip-port"])) { $vheipport = '--vhe-ip-port='.$this->params["vhe-ip-port"] ; }
             $ray[]["command"][] = SUDOPREFIX.PTBCOMM." assetpublisher publish --yes --guess" ;
             $ray[]["command"][] = SUDOPREFIX."sh ".$this->getMacUserShellAutoPath() ;
             $ray[]["command"][] = SUDOPREFIX.PTCCOMM." auto x --af=".$this->getMacPortsAutoPath() ;
-            $ray[]["command"][] = SUDOPREFIX.PTCCOMM." auto x --af=".$this->getWebappConfigureAutoPath() ;
-            $ray[]["command"][] = SUDOPREFIX.PTDCOMM." auto x --af=".$this->getDeployAutoPath(). " $vhestring $vheipport" ; }
+            $ray[]["command"][] = SUDOPREFIX.PTCCOMM." auto x --af=".$this->getModuleConfigureAutoPath().' --app-slug=ptbuild --fpm-port=6041' ;
+            $ray[]["command"][] = SUDOPREFIX.PTCCOMM." auto x --af=".$this->getWebappConfigureAutoPath().' --app-slug=ptbuild --fpm-port=6041' ;
+            $ray[]["command"][] = SUDOPREFIX.PTDCOMM." auto x --af=".$this->getDeployAutoPath(). " $vhestring $vheipport".' --app-slug=build --fpm-port=6041' ;
+            $ray[]["command"][] = SUDOPREFIX."mkdir -p /opt/ptbuild/pipes/" ; }
+        if (is_array($this->preinstallCommands) && count($this->preinstallCommands)>0) {
+            $ray[]["command"][] = "echo 'Copy from temp ptbuild directories'" ;
+            $ray[]["command"][] = SUDOPREFIX."cp -r /tmp/ptbuild-pipes/pipes/* /opt/ptbuild/pipes/" ;
+            $ray[]["command"][] = SUDOPREFIX."cp -r /tmp/ptbuild-keys/* /opt/ptbuild/keys/" ;
+            $ray[]["command"][] = SUDOPREFIX."chmod -R 0600 /opt/ptbuild/keys/*" ;
+            $ray[]["command"][] = SUDOPREFIX."cp /tmp/ptbuild-settings/users.txt /opt/ptbuild/ptbuild/src/Modules/Signup/Data/users.txt" ;
+            $ray[]["command"][] = SUDOPREFIX."cp /tmp/ptbuild-settings/ptbuildvars /opt/ptbuild/ptbuild/ptbuildvars" ; }
+        $ray[]["command"][] = SUDOPREFIX."chown -R ptbuild:ptbuild /opt/ptbuild/" ;
+        $ray[]["command"][] = SUDOPREFIX."chmod -R 775 /opt/ptbuild/" ;
         $this->postinstallCommands = $ray ;
+        return $ray ;
     }
 
     public function getMacUserShellAutoPath() {
