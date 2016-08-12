d733210d47b806fac717c98709b8f7a4fd75fba3
David Amanshia <phpengine@pharaohtools.com>
2016-08-12 01:47:39 +0100

    user for mac

--- src/Modules/User/Model/UserMac.php
+++ src/Modules/User/Model/UserMac.php
@@ -2,13 +2,13 @@
 
 Namespace Model;
 
-class UserUbuntu extends BaseLinuxApp {
+class UserMac extends BaseLinuxApp {
 
     // Compatibility
-    public $os = array("Linux") ;
-    public $linuxType = array("Debian") ;
-    public $distros = array("Ubuntu") ;
-    public $versions = array( array("11.04", "+")) ;
+    public $os = array("Darwin") ;
+    public $linuxType = array("any") ;
+    public $distros = array("any") ;
+    public $versions = array("any") ;
     public $architectures = array("any") ;
 
     // Model Group
