085fbc88bacaa206309f14fcb2569378d8bd5d1f
David Amanshia <phpengine@pharaohtools.com>
2016-08-12 01:33:41 +0100

    state conf facts

--- src/Modules/PTWebApplication/Autopilots/PTConfigure/app-state-conf.dsl.php
+++ src/Modules/PTWebApplication/Autopilots/PTConfigure/app-state-conf.dsl.php
@@ -1,7 +1,6 @@
 Logging log
   log-message "Lets configure PHP and Files for Pharaoh Web Application"
 
-
 User ensure-exists
   when "{{ PTWebApplication::~::isNotOSX }}"
   label "Ensure {{{ Parameter::app-slug }}} user exists"
@@ -13,28 +12,22 @@ User ensure-exists
 PHPDefaults install
   label "Ensure PHP Default Settings are okay"
 
-
 ApacheDefaults install
   label "Ensure Apache Default Settings are okay"
-
 
 PHPFPM ensure
   label "Ensure PHP FPM is installed"
 
-
 PTDeploy ensure
   label "Ensure PHP Pharaoh Deploy is installed"
 
-
 ApacheFastCGIModules ensure
   label "Ensure Apache Fast CGI is installed"
-
 
 Chmod path
   label "Make the PT Web Application Settings file writable"
   path '{{{ Facts::Runtime::factGetConstant::PFILESDIR }}}{{{ Parameter::app-slug }}}/{{{ Parameter::app-slug }}}/{{{ Parameter::app-slug }}}vars'
   mode "0755"
-
 
 Chown path
   label "Ensure the Pharaoh Web Application user owns the Program Files"
@@ -42,13 +35,11 @@ Chown path
   recursive true
   user '{{{ Parameter::app-slug }}}'
 
-
 Chgrp path
   label "Ensure the Pharaoh Group user owns the Program Files"
   path '{{{ Facts::Runtime::factGetConstant::PFILESDIR }}}{{{ Parameter::app-slug }}}/'
   recursive true
   group '{{{ Parameter::app-slug }}}'
-
 
 Templating install
   label "{{{ Parameter::app-slug }}} PHP FPM Pool Config"
@@ -57,10 +48,8 @@ Templating install
   template_app-slug "{{{ Parameter::app-slug }}}"
   template_fpm-port "{{{ Parameter::fpm-port }}}"
 
-
 PHPFPM restart
   label "{{{ Parameter::app-slug }}} PHP FPM Restart"
-
 
 Logging log
   log-message "Configuration Management for Pharaoh Web Application Complete"
