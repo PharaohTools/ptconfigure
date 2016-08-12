b39ba5ffd2b84a7d8ca1a09851873e66d74d79fd
David Amanshia <phpengine@pharaohtools.com>
2016-08-12 01:13:48 +0100

    app state conf is a dsl now

---
+++ src/Modules/PTWebApplication/Autopilots/PTConfigure/app-state-conf.dsl.php
@@ -0,0 +1,67 @@
+Logging log
+  log-message "Lets configure PHP and Files for Pharaoh Web Application"
+
+
+User ensure-exists
+  when "{{ PTWebApplication::~::isNotOSX }}"
+  label "Ensure {{{ Parameter::app-slug }}} user exists"
+  username "{{{ Parameter::app-slug }}}"
+  fullname "{{{ Parameter::app-slug }}}"
+  home-directory ""
+  shell "/bin/bash"
+
+
+PHPDefaults install
+  label "Ensure PHP Default Settings are okay"
+
+
+ApacheDefaults install
+  label "Ensure Apache Default Settings are okay"
+
+
+PHPFPM ensure
+  label "Ensure PHP FPM is installed"
+
+
+PTDeploy ensure
+  label "Ensure PHP FPM is installed"
+
+
+ApacheFastCGIModules ensure
+  label "Ensure Apache Fast CGI is installed"
+
+
+Chmod path
+  label "Make the PT Web Application Settings file writable"
+  path '{{{ Facts::Runtime::factGetConstant::PFILESDIR }}}{{{ Parameter::app-slug }}}/{{{ Parameter::app-slug }}}/{{{ Parameter::app-slug }}}vars'
+  mode '755'
+
+
+Chown path
+  label "Ensure the Pharaoh Web Application user owns the Program Files"
+  path PFILESDIR.'{{{ Parameter::app-slug }}}'.DS
+  recursive true
+  user '{{{ Parameter::app-slug }}}'
+
+
+Chgrp path
+  label "Ensure the Pharaoh Group user owns the Program Files"
+  path PFILESDIR.'{{{ Parameter::app-slug }}}'.DS
+  recursive true
+  group '{{{ Parameter::app-slug }}}'
+
+
+Templating install
+  label "{{{ Parameter::app-slug }}} PHP FPM Pool Config"
+  source dirname(dirname(__DIR__)).DS.'Templates'.DS.'ptapplication_pool.tpl.php'
+  target "{{{ PTWebApplication::~::getApachePoolDir }}}/{{{ Parameter::app-slug }}}.conf"
+  template_app-slug "{{{ Parameter::app-slug }}}"
+  template_fpm-port "{{{ Parameter::fpm-port }}}"
+
+
+PHPFPM restart
+  label "{{{ Parameter::app-slug }}} PHP FPM Restart"
+
+
+Logging log
+  log-message "Configuration Management for Pharaoh Web Application Complete"
