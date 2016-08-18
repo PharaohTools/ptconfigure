41a8b1744f8d7f68e710957a495b151100a83d6e
David Amanshia <phpengine@pharaohtools.com>
2016-08-18 16:44:32 +0100

    fixed app-state conf path to include dsl

--- src/Modules/PTSource/Model/PTSourceLinux.php
+++ src/Modules/PTSource/Model/PTSourceLinux.php
@@ -82,7 +82,7 @@ class PTSourceLinux extends BasePHPApp {
     }
 
     public function getWebappConfigureAutoPath() {
-        $path = dirname(dirname(dirname(__FILE__))).DS.'PTWebApplication'.DS.'Autopilots'.DS.'PTConfigure'.DS.'app-state-conf.php' ;
+        $path = dirname(dirname(dirname(__FILE__))).DS.'PTWebApplication'.DS.'Autopilots'.DS.'PTConfigure'.DS.'app-state-conf.dsl.php' ;
         return $path ;
     }
 
