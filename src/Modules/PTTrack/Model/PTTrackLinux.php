bba30656868d550db76db467d303d08cfd4fa7da
David Amanshia <phpengine@pharaohtools.com>
2016-10-08 08:12:58 +0100

    calling enable ssl param in install

--- src/Modules/PTTrack/Model/PTTrackLinux.php
+++ src/Modules/PTTrack/Model/PTTrackLinux.php
@@ -37,13 +37,15 @@ class PTTrackLinux extends BasePHPApp {
             (isset($this->params["guess"]) && $this->params["guess"]==true) ) {
             $vhestring = '--vhe-url=track.pharaoh.tld';
             $vheipport = '--vhe-ip-port=127.0.0.1:80';
+            $sslstring = '';
             if (isset($this->params["vhe-url"])) { $vhestring = '--vhe-url='.$this->params["vhe-url"] ; }
             if (isset($this->params["vhe-ip-port"])) { $vheipport = '--vhe-ip-port='.$this->params["vhe-ip-port"] ; }
+            if (isset($this->params["enable-ssl"])) { $sslstring = ' --enable-ssl' ; }
             $ray[]["command"][] = SUDOPREFIX.PTTRCOMM." assetpublisher publish --yes --guess" ;
 //            $ray[]["command"][] = SUDOPREFIX."sh ".$this->getLinuxUserShellAutoPath() ;
             $ray[]["command"][] = SUDOPREFIX.PTCCOMM." auto x --af=".$this->getModuleConfigureAutoPath().' --app-slug=pttrack --fpm-port=6042' ;
             $ray[]["command"][] = SUDOPREFIX.PTCCOMM." auto x --af=".$this->getWebappConfigureAutoPath().' --app-slug=pttrack --fpm-port=6042' ;
-            $ray[]["command"][] = SUDOPREFIX.PTDCOMM." auto x --af=".$this->getDeployAutoPath(). " $vhestring $vheipport".' --app-slug=track --fpm-port=6042' ; }
+            $ray[]["command"][] = SUDOPREFIX.PTDCOMM." auto x --af=".$this->getDeployAutoPath(). " $vhestring $vheipport".' --app-slug=track --fpm-port=6042'.$sslstring ; }
         if (is_array($this->preinstallCommands) && count($this->preinstallCommands)>0) {
             $ray[]["command"][] = "echo 'Copy from temp pttrack directories'" ;
             $ray[]["command"][] = SUDOPREFIX."cp -r /tmp/pttrack-data/* /opt/pttrack/data/" ;
