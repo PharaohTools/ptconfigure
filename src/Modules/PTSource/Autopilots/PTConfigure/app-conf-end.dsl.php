1590211013ae3271a4e46cee8d3fd65bcc96bc08
David Amanshia <phpengine@pharaohtools.com>
2016-10-15 22:54:35 +0100

    fixed typo {{{{ to {{{

--- src/Modules/PTSource/Autopilots/PTConfigure/app-conf-end.dsl.php
+++ src/Modules/PTSource/Autopilots/PTConfigure/app-conf-end.dsl.php
@@ -4,17 +4,17 @@ Logging log
 RunCommand install
   guess
   command "git config --global http.https://{{{ Param::vhe-url }}}.sslCAInfo '/etc/ssl/certificates/fullchain.pem'"
-  when "{{{{ Param::enable-ssl }}}"
+  when "{{{ Param::enable-ssl }}}"
 
 RunCommand install
   guess
   command "git config --global http.https://{{{ Param::vhe-url }}}.sslCert '/etc/ssl/certificates/cert.pem'"
-  when "{{{{ Param::enable-ssl }}}"
+  when "{{{ Param::enable-ssl }}}"
 
 RunCommand install
   guess
   command "git config --global http.https://{{{ Param::vhe-url }}}.sslKey '/etc/ssl/certificates/private.pem'"
-  when "{{{{ Param::enable-ssl }}}"
+  when "{{{ Param::enable-ssl }}}"
 
 Logging log
   log-message "Configuration Management for Pharaoh Source Complete"
