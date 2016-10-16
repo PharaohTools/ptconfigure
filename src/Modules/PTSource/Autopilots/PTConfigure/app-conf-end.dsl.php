16e221a6de5d074428539109c42daf77cb0649be
David Amanshia <phpengine@pharaohtools.com>
2016-10-16 03:32:48 +0100

    parsing so no double slash

--- src/Modules/PTSource/Autopilots/PTConfigure/app-conf-end.dsl.php
+++ src/Modules/PTSource/Autopilots/PTConfigure/app-conf-end.dsl.php
@@ -3,17 +3,17 @@ Logging log
 
 File should-have-line
   label "{{{ Parameter::app-slug }}} PHP Config for Memory Limit"
-  file "{{{ PTWebApplication::~::getFPMPoolDir }}}/{{{ Parameter::app-slug }}}.conf"
+  file "{{{ PTWebApplication::~::getFPMPoolDir }}}{{{ Parameter::app-slug }}}.conf"
   search "php_admin_flag[memory_limit] = 128M "
 
 File should-have-line
   label "{{{ Parameter::app-slug }}} PHP Config for Upload Filesize Limit"
-  file "{{{ PTWebApplication::~::getFPMPoolDir }}}/{{{ Parameter::app-slug }}}.conf"
+  file "{{{ PTWebApplication::~::getFPMPoolDir }}}{{{ Parameter::app-slug }}}.conf"
   search "php_admin_flag[upload_max_filesize] = 128M "
 
 File should-have-line
   label "{{{ Parameter::app-slug }}} PHP Config for Post Size Limit"
-  file "{{{ PTWebApplication::~::getFPMPoolDir }}}/{{{ Parameter::app-slug }}}.conf"
+  file "{{{ PTWebApplication::~::getFPMPoolDir }}}{{{ Parameter::app-slug }}}.conf"
   search "php_admin_flag[post_max_size] = 128M "
 
 Logging log
