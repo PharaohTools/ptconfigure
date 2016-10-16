df2e1a54e0721120dbcdf1f259cf79ab3779d572
David Amanshia <phpengine@pharaohtools.com>
2016-10-16 04:51:49 +0100

    1024M by default for pharaoh source

--- src/Modules/PTSource/Autopilots/PTConfigure/app-conf-end.dsl.php
+++ src/Modules/PTSource/Autopilots/PTConfigure/app-conf-end.dsl.php
@@ -4,17 +4,17 @@ Logging log
 File should-have-line
   label "{{{ Parameter::app-slug }}} PHP Config for Memory Limit"
   file "{{{ PTWebApplication::~::getFPMPoolDir }}}{{{ Parameter::app-slug }}}.conf"
-  search "php_admin_flag[memory_limit] = 128M "
+  search "php_admin_flag[memory_limit] = 1024M "
 
 File should-have-line
   label "{{{ Parameter::app-slug }}} PHP Config for Upload Filesize Limit"
   file "{{{ PTWebApplication::~::getFPMPoolDir }}}{{{ Parameter::app-slug }}}.conf"
-  search "php_admin_flag[upload_max_filesize] = 128M "
+  search "php_admin_flag[upload_max_filesize] = 1024M "
 
 File should-have-line
   label "{{{ Parameter::app-slug }}} PHP Config for Post Size Limit"
   file "{{{ PTWebApplication::~::getFPMPoolDir }}}{{{ Parameter::app-slug }}}.conf"
-  search "php_admin_flag[post_max_size] = 128M "
+  search "php_admin_flag[post_max_size] = 1024M "
 
 Logging log
   log-message "PHP Configuration for Pharaoh Source Complete"
