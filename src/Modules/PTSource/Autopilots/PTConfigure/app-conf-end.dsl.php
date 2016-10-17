483388ea16a393cb1707189bc1f12115337699d9
David Amanshia <phpengine@pharaohtools.com>
2016-10-17 01:59:58 +0100

    value not flag

--- src/Modules/PTSource/Autopilots/PTConfigure/app-conf-end.dsl.php
+++ src/Modules/PTSource/Autopilots/PTConfigure/app-conf-end.dsl.php
@@ -4,27 +4,27 @@ Logging log
 File should-have-line
   label "{{{ Parameter::app-slug }}} PHP Config for Memory Limit"
   file "{{{ PTWebApplication::~::getFPMPoolDir }}}{{{ Parameter::app-slug }}}.conf"
-  search "php_admin_flag[memory_limit] = 1024M "
+  search "php_admin_value[memory_limit] = 1024M "
 
 File should-have-line
   label "{{{ Parameter::app-slug }}} PHP Config for Upload Filesize Limit"
   file "{{{ PTWebApplication::~::getFPMPoolDir }}}{{{ Parameter::app-slug }}}.conf"
-  search "php_admin_flag[upload_max_filesize] = 1024M "
+  search "php_admin_value[upload_max_filesize] = 1024M "
 
 File should-have-line
   label "{{{ Parameter::app-slug }}} PHP Config for Post Size Limit"
   file "{{{ PTWebApplication::~::getFPMPoolDir }}}{{{ Parameter::app-slug }}}.conf"
-  search "php_admin_flag[post_max_size] = 1024M "
+  search "php_admin_value[post_max_size] = 1024M "
 
 File should-have-line
   label "{{{ Parameter::app-slug }}} PHP Config for Post Size Limit"
   file "{{{ PTWebApplication::~::getFPMPoolDir }}}{{{ Parameter::app-slug }}}.conf"
-  search "php_admin_flag[post_max_size] = 1024M "
+  search "php_admin_value[post_max_size] = 1024M "
 
 File should-have-line
   label "{{{ Parameter::app-slug }}} Apache Config for Post Size Limit"
   file "{{{ PTWebApplication::~::getFPMPoolDir }}}{{{ Parameter::app-slug }}}.conf"
-  search "php_admin_flag[post_max_size] = 1024M "
+  search "php_admin_value[post_max_size] = 1024M "
 
 PHPFPM restart
   label "{{{ Parameter::app-slug }}} PHP FPM Restart"
