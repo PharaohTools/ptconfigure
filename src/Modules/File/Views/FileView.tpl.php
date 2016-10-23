799d21d4699213f13cacbf6eb161c6ca0cc572f4
David Amanshia <phpengine@pharaohtools.com>
2016-10-23 02:15:05 +0100

    removed unused newline

--- src/Modules/File/Views/FileView.tpl.php
+++ src/Modules/File/Views/FileView.tpl.php
@@ -1,4 +1,4 @@
 File Modifications: <?php echo $pageVars["appName"] ; ?>: <?php
   $result_summary = ($pageVars["result"] == true) ? "Success" : "Failure" ;
-  echo $result_summary."\n" ;
+  echo $result_summary ;
   if (is_string($pageVars["result"])) { echo $pageVars["result"] ; } ?>
