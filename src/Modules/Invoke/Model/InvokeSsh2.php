be28622a45e32d91d9a968ad401022047e22f16e
David Amanshia <phpengine@pharaohtools.com>
2016-10-23 02:25:13 +0100

    check var before using

--- src/Modules/Invoke/Model/InvokeSsh2.php
+++ src/Modules/Invoke/Model/InvokeSsh2.php
@@ -114,15 +114,15 @@ class InvokeSsh2 extends Base {
     }
 
     function improvedExec( $command ) {
-        $result = $this->rawExec( '('.$command.'); echo -en "\n$?" ;' );
-        $pres =preg_match( "/^(.*)\n(0|-?[1-9][0-9]*)$/s", $result[0], $matches ) ;
-        if( $pres==false ) {
+        $result = $this->rawExec( ''.$command.'; echo -en "\n$?" ;' );
+        $pres = preg_match( "/^(.*)\n(0|-?[1-9][0-9]*)$/s", $result[0], $matches ) ;
+        if( $pres == false ) {
             $loggingFactory = new \Model\Logging();
             $logging = $loggingFactory->getModel($this->params) ;
             $logging->log("No return status found from command", LOG_FAILURE_EXIT_CODE) ;  }
         $res = array() ;
-        $res["rc"] = $matches[2] ;
-        $res["data"] = $matches[1] ;
+        $res["rc"] = (isset($matches[2])) ? $matches[2] : "0" ;
+        $res["data"] = (isset($matches[1])) ? $matches[1] : $result ;
         return $res;
     }
 
@@ -137,12 +137,12 @@ class InvokeSsh2 extends Base {
         stream_set_blocking( $error_stream, TRUE );
         $data = "" ;
         $error_output = "" ;
-        while ($buf = fread($this->stream, 131072)) {
-            $eo = stream_get_contents( $error_stream ) ;
-            if (strlen($eo)>0) { $error_output .= $eo ; }
+        while ($buf = fread($this->stream, 4096)) {
             $data .= $buf;
             //echo $buf ;
         }
+        $eo = stream_get_contents( $error_stream ) ;
+        if (strlen($eo)>0) { $error_output .= $eo ; }
         fclose( $this->stream );
         fclose( $error_stream );
         return array( $data, $error_output );
@@ -157,7 +157,7 @@ class InvokeSsh2 extends Base {
         stream_set_blocking( $this->stream, TRUE );
         $data = "" ;
         $error_output = "" ;
-        while ($buf = fread($this->stream, 16384)) { $data .= $buf; }
+        while ($buf = fread($this->stream, 4096)) { $data .= $buf; }
         fclose( $this->stream );
         return $data ;
     }
