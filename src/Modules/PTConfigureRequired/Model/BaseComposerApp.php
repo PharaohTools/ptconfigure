26572a81b6efc27d21d95dfc6955b1ec8f3c34e5
David Amanshia <phpengine@pharaohtools.com>
2016-10-27 05:25:29 +0100

    base composer app, using composer module

--- src/Modules/PTConfigureRequired/Model/BaseComposerApp.php
+++ src/Modules/PTConfigureRequired/Model/BaseComposerApp.php
@@ -4,70 +4,78 @@ Namespace Model;
 
 class BaseComposerApp extends BasePHPApp {
 
-  protected $fileSources;
+    protected $fileSources;
 
-  public function __construct($params) {
-    parent::__construct($params);
-  }
+    public function __construct($params) {
+        parent::__construct($params);
+    }
 
-  public function install($autoPilot = null) {
-    if (isset($this->params["hide-title"])) { $this->populateTinyTitle() ; }
-    $this->showTitle();
-    $this->programDataFolder = ($autoPilot)
-      ? $autoPilot->{$this->autopilotDefiner."InstallDirectory"}
-      : $this->askForProgramDataFolder();
-    $this->programExecutorFolder = ($autoPilot)
-      ? $autoPilot->{$this->autopilotDefiner."ExecutorDirectory"}
-      : $this->askForProgramExecutorFolder();
-    $this->deleteProgramDataFolderAsRootIfExists();
-    $this->makeProgramDataFolderIfNeeded();
-    $this->copyComposerJsonToProgramDataFolder();
-    $this->doComposerCommand();
-    $this->deleteExecutorIfExists();
-    $this->populateExecutorFile();
-    $this->saveExecutorFile();
-    $this->deleteInstallationFiles();
-    $this->changePermissions();
-    // $this->setInstallFlagStatus(true) ; @todo we can deprecate this now as status is dynamic, and install is used by everything not just installers
-    if (isset($this->params["hide-completion"])) { $this->populateTinyCompletion(); }
-    $this->showCompletion();
-  }
+    public function install($autoPilot = null) {
+        if (isset($this->params["hide-title"])) { $this->populateTinyTitle() ; }
+        $this->showTitle();
+        $this->programDataFolder = ($autoPilot)
+          ? $autoPilot->{$this->autopilotDefiner."InstallDirectory"}
+          : $this->askForProgramDataFolder();
+        $this->programExecutorFolder = ($autoPilot)
+          ? $autoPilot->{$this->autopilotDefiner."ExecutorDirectory"}
+          : $this->askForProgramExecutorFolder();
+        $this->deleteProgramDataFolderAsRootIfExists();
+        $this->makeProgramDataFolderIfNeeded();
+        $this->copyComposerJsonToProgramDataFolder();
+        $this->doComposerCommand();
+        $this->deleteExecutorIfExists();
+        $this->populateExecutorFile();
+        $this->saveExecutorFile();
+        $this->deleteInstallationFiles();
+        $this->changePermissions();
+        // $this->setInstallFlagStatus(true) ; @todo we can deprecate this now as status is dynamic, and install is used by everything not just installers
+        if (isset($this->params["hide-completion"])) { $this->populateTinyCompletion(); }
+        $this->showCompletion();
+    }
 
-  public function unInstall($autoPilot = null) {
-    $this->showTitle();
-    $this->programDataFolder = ($autoPilot)
-      ? $autoPilot->{$this->autopilotDefiner}
-      : $this->askForProgramDataFolder();
-    $this->programExecutorFolder = $this->askForProgramExecutorFolder();
-    $this->deleteProgramDataFolderAsRootIfExists();
-    $this->deleteExecutorIfExists();
-    // $this->setInstallFlagStatus(false) ; @todo we can deprecate this now as status is dynamic, and install is used by everything not just installers
-    $this->showCompletion();
-  }
+    public function unInstall($autoPilot = null) {
+        $this->showTitle();
+        $this->programDataFolder = ($autoPilot)
+          ? $autoPilot->{$this->autopilotDefiner}
+          : $this->askForProgramDataFolder();
+        $this->programExecutorFolder = $this->askForProgramExecutorFolder();
+        $this->deleteProgramDataFolderAsRootIfExists();
+        $this->deleteExecutorIfExists();
+        // $this->setInstallFlagStatus(false) ; @todo we can deprecate this now as status is dynamic, and install is used by everything not just installers
+        $this->showCompletion();
+    }
 
-  // @todo removed hardcoded composer path
-  protected function copyComposerJsonToProgramDataFolder() {
-      $templatorFactory = new \Model\Templating();
-      $templator = $templatorFactory->getModel($this->params);
-      $original = "/opt/ptconfigure/ptconfigure/src/Modules/".$this->getModuleName()."/Templates/composer.json" ;
-      $targetLocation = $this->programDataFolder.DIRECTORY_SEPARATOR.$this->programNameMachine.DIRECTORY_SEPARATOR."composer.json" ;
-      $templator->template(
-          $original,
-          array(),
-          $targetLocation );
-  }
+    // @todo removed hardcoded composer path
+    protected function copyComposerJsonToProgramDataFolder() {
+        $templatorFactory = new \Model\Templating();
+        $templator = $templatorFactory->getModel($this->params);
+        $original = PFILESDIR."ptconfigure".DS."ptconfigure".DS."src".DS."Modules".DS.$this->getModuleName().DS."Templates".DS."composer.json" ;
+        $targetLocation = $this->programDataFolder.DS.$this->programNameMachine.DS."composer.json" ;
+        $templator->template(
+            $original,
+            array(),
+            $targetLocation );
+    }
 
-  protected function doComposerCommand() {
-      $sys = new \Model\SystemDetectionAllOS() ;
-      if ($sys->os == "Darwin") { $dlcomm = "curl https://getcomposer.org/installer --insecure -o installer" ; }
-      else { $dlcomm = "wget https://getcomposer.org/installer --no-check-certificate" ; }
-      $command = array(
-          "cd $this->programDataFolder".DIRECTORY_SEPARATOR.$this->programNameMachine ,
-          $dlcomm,
-          "php installer",
-          SUDOPREFIX."php composer.phar install -vvv --prefer-dist" );
-      self::executeAsShell($command);
-    return true;
-  }
+    protected function doComposerCommand() {
+
+
+        $composerFactory = new \Model\Composer();
+        $composer = $composerFactory->getModel($this->params);
+        if ($composer->status() !== true) {
+            $composer->install() ;
+        }
+
+//
+//        $sys = new \Model\SystemDetectionAllOS() ;
+//        if ($sys->os == "Darwin") { $dlcomm = "curl https://getcomposer.org/installer --insecure -o installer" ; }
+//        else { $dlcomm = "wget https://getcomposer.org/installer --no-check-certificate" ; }
+
+        $command = array(
+
+            SUDOPREFIX."php composer.phar install -vvv --prefer-dist" );
+        $res = self::executeAndGetReturnCode($command, true, true);
+        return ($res["rc"]==0) ? true : false ;
+    }
 
 }
