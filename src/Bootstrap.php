c4106501b7f65642084b5c624a8faac0daaaa714
David Amanshia <phpengine@pharaohtools.com>
2016-11-12 02:12:34 +0000

    refactored the Bootstrap class instantiator so that tests can include the bootstrap class without immaediate instantion

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,72 +1,7 @@
 <?php
 
-Namespace Core;
-
-$bootStrap = new BootStrap();
-
+require_once("BootstrapCore.php") ;
+$bootStrap = new \Core\BootStrap();
 $argv_or_null = (isset($argv)) ? $argv : null ;
 $bootStrapParams = (isset($_ENV['ptconfigure_bootstrap'])) ? unserialize($_ENV['ptconfigure_bootstrap']) : $argv_or_null ;
 $bootStrap->main($bootStrapParams);
-
-class BootStrap {
-
-    private static $exitCode ;
-    private $start ;
-
-    public function __construct() {
-        require_once("Constants.php");
-        require_once("AutoLoad.php");
-        $autoLoader = new autoLoader();
-        $autoLoader->launch();
-    }
-
-    public static function setExitCode($exitCode){
-        self::$exitCode = $exitCode ;
-    }
-
-    public static function getExitCode(){
-        return (is_null(self::$exitCode)) ? 0 : self::$exitCode ;
-    }
-
-    public function main($argv_or_boot_params_null) {
-        $this->start = time() ;
-        $routeObject = new \Core\Router();
-        $route = $routeObject->run($argv_or_boot_params_null);
-        $emptyPageVars = array("messages"=>array(), "route"=>$route);
-        $this->executeControl($route["control"], $emptyPageVars);
-        $this->exitGracefully();
-    }
-
-    public function executeControl($controlToExecute, $pageVars=null) {
-        $control = new \Core\Control();
-        $controlResult = $control->executeControl($controlToExecute, $pageVars);
-        try {
-            if ($controlResult["type"]=="view") {
-                $this->executeView( $controlResult["view"], $controlResult["pageVars"] ); }
-            else if ($controlResult["type"]=="control") {
-                $this->executeControl( $controlResult["control"], $controlResult["pageVars"] ); } }
-        catch (\Exception $e) {
-            throw new \Exception( 'No controller result type specified', 0, $e); }
-    }
-
-    private function executeView($viewTemplate, $viewVars) {
-        $view = new \Core\View();
-        $view->executeView($viewTemplate, $viewVars);
-    }
-
-    private function exitGracefully() {
-        // @note this must be the last executed line as it sets exit code
-        $cur = time() ;
-        $finish = $cur - $this->start ;
-        echo "[Pharaoh Exit] Execution finished after ".$finish." seconds ".PHP_EOL;
-        if (self::$exitCode == null) {
-            exit(0) ; }
-        else if (!is_int(self::$exitCode)) {
-            echo "[Pharaoh Exit] Non Integer Exit Code Attempted".PHP_EOL; ;
-            exit(1) ; }
-        else {
-            echo "[Pharaoh Exit] Exiting with exit code: ".self::$exitCode.PHP_EOL; ;
-            exit(self::$exitCode) ; }
-    }
-
-}
