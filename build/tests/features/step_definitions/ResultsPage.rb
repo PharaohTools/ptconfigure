8a993c9d8fd9fc2a2d60beb19af5b6ff8ab4f8ba
David Amanshia <phpengine@pharaohtools.com>
2016-11-12 18:23:54 +0000

    removed unused code

--- build/tests/features/step_definitions/ResultsPage.rb
+++
@@ -1,3 +0,0 @@
-Given /^I visit the results page$/ do
-  visit "/index.php?control=resultsPage&action=results"
-end
--- build/tests/features/step_definitions/UserPage.rb
+++
@@ -1,3 +0,0 @@
-Given /^I visit the user page$/ do
-  visit "/index.php?control=userPage&action=user"
-end
