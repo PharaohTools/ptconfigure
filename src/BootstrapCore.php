88d7395f16948aff6eb4e9e408e7811902002147
David Amanshia <phpengine@pharaohtools.com>
2016-11-12 18:21:19 +0000

    newly split bootstrap class from instantiator

---
+++ src/BootstrapCore.php
@@ -0,0 +1,73 @@
+<?php
+
+Namespace Core;
+
+$bootStrap = new BootStrap();
+
+$argv_or_null = (isset($argv)) ? $argv : null ;
+// @todo document this feature that allows a user to provide a json envoronment variable of parameters
+
+$bootStrapParams = (isset($_ENV['ptdeploy_bootstrap'])) ? json_decode($_ENV['ptdeploy_bootstrap']) : $argv_or_null ;
+$bootStrap->setExitCode(0) ;
+$bootStrap->main($bootStrapParams);
+
+class BootStrap {
+
+    private static $exitCode ;
+    private $start ;
+
+    public function __construct() {
+        require_once("Constants.php");
+        require_once("AutoLoad.php");
+        $autoLoader = new autoLoader();
+        $autoLoader->launch();
+    }
+
+    public static function setExitCode($exitCode){
+        self::$exitCode = $exitCode ;
+    }
+
+    public static function getExitCode(){
+        return (is_null(self::$exitCode)) ? 0 : self::$exitCode ;
+    }
+
+    public function main($argv_or_boot_params_null) {
+        $routeObject = new \Core\Router();
+        $route = $routeObject->run($argv_or_boot_params_null);
+        $emptyPageVars = array("messages"=>array(), "route"=>$route);
+        $this->executeControl($route["control"], $emptyPageVars);
+        $this->exitGracefully();
+    }
+
+    public function executeControl($controlToExecute, $pageVars=null) {
+        $control = new \Core\Control();
+        $controlResult = $control->executeControl($controlToExecute, $pageVars);
+        try {
+            if ($controlResult["type"]=="view") {
+                $this->executeView( $controlResult["view"], $controlResult["pageVars"] ); }
+            else if ($controlResult["type"]=="control") {
+                $this->executeControl( $controlResult["control"], $controlResult["pageVars"] ); }
+        } catch (\Exception $e) {
+            throw new \Exception( 'No controller result type specified', 0, $e); }
+    }
+
+    private function executeView($viewTemplate, $viewVars) {
+        $view = new \Core\View();
+        $view->executeView($viewTemplate, $viewVars);
+    }
+
+    private function exitGracefully() {
+        // @note this must be the last executed line as it sets exit code
+        $finish = microtime(true) - $this->start ;
+        echo "[Pharaoh Exit] Execution finished after ".$finish." seconds ".PHP_EOL;
+        if (self::$exitCode == null) {
+            exit(0) ; }
+        else if (!is_int(self::$exitCode)) {
+            echo "[Pharaoh Exit] Non Integer Exit Code Attempted".PHP_EOL; ;
+            exit(1) ; }
+        else {
+            echo "[Pharaoh Exit] Exiting with exit code: ".self::$exitCode.PHP_EOL; ;
+            exit(self::$exitCode) ; }
+    }
+
+}
