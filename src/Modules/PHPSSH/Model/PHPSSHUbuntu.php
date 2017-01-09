38e5e16036d5a7238e27de09bc2bee33c587b658
David Amanshia <phpengine@pharaotools.com>
2017-01-09 22:38:16 +0000

    supports php7

--- src/Modules/PHPSSH/Model/PHPSSHUbuntu.php
+++ src/Modules/PHPSSH/Model/PHPSSHUbuntu.php
@@ -17,14 +17,18 @@ class PHPSSHUbuntu extends BaseLinuxApp {
     public function __construct($params) {
         parent::__construct($params);
         $this->autopilotDefiner = "PHPSSH";
-        $this->installCommands = array(                                                                                          //libssh2-1-dev libssh2-php
-            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", array("libssh2-1-dev", "libssh2-php"))) ),
-//            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("PECL", array("ssh2-beta"))) ),
-            array("method"=> array("object" => $this, "method" => "askStatus", "params" => array()) ),
+        if (PHP_MAJOR_VERSION >= 7) {
+            $APT_PARAMS = array("Apt", array("php-ssh2") ) ; }
+        else {
+            $APT_PARAMS = array("Apt", array("libssh2-1-dev", "libssh2-php") ) ; }
+        $this->installCommands = array(
+            array('method'=> array("object" => $this, 'method' => "packageAdd", "params" => $APT_PARAMS) ),
+//            array('method'=> array("object" => $this, 'method' => "packageAdd", "params" => array("PECL", array("ssh2-beta"))) ),
+            array('method'=> array("object" => $this, 'method' => "askStatus", "params" => array()) ),
         );
         $this->uninstallCommands = array(
-            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", array("libssh2-1-dev", "libssh2-php"))) ),
-//            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("PECL", array("ssh2-beta"))) ),
+            array('method'=> array("object" => $this, 'method' => "packageRemove", "params" => $APT_PARAMS) ),
+//            array('method'=> array("object" => $this, 'method' => "packageRemove", "params" => array("PECL", array("ssh2-beta"))) ),
         );
         $this->programDataFolder = "/opt/PHPSSH"; // command and app dir name
         $this->programNameMachine = "phpssh"; // command and app dir name
@@ -34,7 +38,7 @@ class PHPSSHUbuntu extends BaseLinuxApp {
     }
 
     public function askStatus() {
-        $modsTextCmd = SUDOPREFIX.'php -m';
+        $modsTextCmd = SUDOPREFIX.' php -m';
         $modsText = $this->executeAndLoad($modsTextCmd) ;
         $modsToCheck = array("ssh2") ;
         $loggingFactory = new \Model\Logging();
