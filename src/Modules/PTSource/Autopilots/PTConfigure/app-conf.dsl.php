ab2a1cd929362a3d3c6bc86aa13e79c9701ee23e
David Amanshia <phpengine@pharaotools.com>
2017-01-31 01:05:12 +0000

    fixed brackets

--- src/Modules/PTSource/Autopilots/PTConfigure/app-conf.dsl.php
+++ src/Modules/PTSource/Autopilots/PTConfigure/app-conf.dsl.php
@@ -13,17 +13,17 @@ Chmod path
 
 RunCommand install
   guess
-  command "git config --global http.https://{{ Param::vhe-url }}.sslCAInfo '/etc/ssl/certificates/fullchain.pem'"
+  command "git config --global http.https://{{{ Param::vhe-url }}}.sslCAInfo '/etc/ssl/certificates/fullchain.pem'"
   when "{{{ Param::enable-ssl }}}"
 
 RunCommand install
   guess
-  command "git config --global http.https://{{ Param::vhe-url }}.sslCert '/etc/ssl/certificates/cert.pem'"
+  command "git config --global http.https://{{{ Param::vhe-url }}}.sslCert '/etc/ssl/certificates/cert.pem'"
   when "{{{ Param::enable-ssl }}}"
 
 RunCommand install
   guess
-  command "git config --global http.https://{{ Param::vhe-url }}.sslKey '/etc/ssl/certificates/private.pem'"
+  command "git config --global http.https://{{{ Param::vhe-url }}}.sslKey '/etc/ssl/certificates/private.pem'"
   when "{{{ Param::enable-ssl }}}"
 
 Logging log
