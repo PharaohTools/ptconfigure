34d01a5dad7df9eae7fab8eb315cf7af1216de97
David Amanshia <phpengine@pharaotools.com>
2017-02-16 16:53:44 +0000

    fixed brackets

--- src/Modules/PTSource/Autopilots/PTConfigure/app-conf-start.dsl.php
+++ src/Modules/PTSource/Autopilots/PTConfigure/app-conf-start.dsl.php
@@ -16,31 +16,31 @@ PackageManager pkg-install
   label "Install apache Mod Auth External for Debian"
   package-name libapache2-mod-authnz-external
   packager Apt
-  when "{{ Param::is_debian }}"
+  when "{{{ Param::is_debian }}}"
 
 RunCommand install
   label "Enable apache Mod Auth External for Debian"
   guess
   command "a2enmod authnz_external"
-  when "{{ Param::is_debian }}"
+  when "{{{ Param::is_debian }}}"
 
 PackageManager pkg-install
   label "Install apache Mod Auth External for Redhat"
   package-name mod_authnz_external
   packager Yum
-  when "{{ Param::is_redhat }}"
+  when "{{{ Param::is_redhat }}}"
 
 PackageManager pkg-install
   label "Install apache PWAuth for Redhat"
   package-name pwauth
   packager Yum
-  when "{{ Param::is_redhat }}"
+  when "{{{ Param::is_redhat }}}"
 
 RunCommand install
   label "Enable apache Mod Auth External for Redhat"
   guess
   command "a2enmod authnz_external"
-  when "{{ Param::is_redhat }}"
+  when "{{{ Param::is_redhat }}}"
 
 Copy put
   label "{{{ Parameter::app-slug }}} Apache Custom Authentication method Conf file"
