4945b7a377994597882993abe6ea35d5c0e6767f
David Amanshia <phpengine@pharaotools.com>
2017-02-17 19:40:14 +0000

    hide unused steps

--- src/Modules/PTSource/Autopilots/PTConfigure/git-http.dsl.php
+++ src/Modules/PTSource/Autopilots/PTConfigure/git-http.dsl.php
@@ -19,27 +19,29 @@ PackageManager pkg-install
   packager Yum
   when "{{{ Param::is_redhat }}}"
 
-PackageManager pkg-install
-  label "Install apache PWAuth for Redhat"
-  package-name pwauth
-  packager Yum
-  when "{{{ Param::is_redhat }}}"
-
-RunCommand install
-  label "Enable apache Mod Auth External for Redhat"
-  guess
-  command "a2enmod authnz_external"
-  when "{{{ Param::is_redhat }}}"
+#PackageManager pkg-install
+#  label "Install apache PWAuth for Redhat"
+#  package-name pwauth
+#  packager Yum
+#  when "{{{ Param::is_redhat }}}"
+#
+#RunCommand install
+#  label "Enable apache Mod Auth External for Redhat"
+#  guess
+#  command "a2enmod authnz_external"
+#  when "{{{ Param::is_redhat }}}"
 
 Copy put
   label "{{{ Parameter::app-slug }}} Apache Custom Authentication method Conf file"
   source "{{{ Facts::Runtime::factGetConstant::PFILESDIR }}}ptconfigure/ptconfigure/src/Modules/PTSource/Templates/{{{ Parameter::app-slug }}}_auth.conf"
   target "/etc/apache2/conf-available/{{{ Parameter::app-slug }}}_auth.conf"
+  when "{{{ Param::is_debian }}}"
 
 RunCommand install
   label "Enable apache Mod Auth External"
   guess
   command "a2enconf {{{ Parameter::app-slug }}}_auth"
+  when "{{{ Param::is_debian }}}"
 
 Logging log
   log-message "Configuration Management for HTTP Git Settings for Pharaoh Source Complete"
