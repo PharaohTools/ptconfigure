4276fb44147c82c0a33aab8e7b6f310261fd72a7
David Amanshia <phpengine@pharaotools.com>
2017-02-24 23:29:46 +0000

    linux compiler module work

---
+++ src/Modules/LinuxCompiler/Model/LinuxCompilerCentos.php
@@ -0,0 +1,40 @@
+<?php
+
+Namespace Model;
+
+class LinuxCompilerUbuntu extends BaseLinuxApp {
+
+    // Compatibility
+    public $os = array("Linux") ;
+    public $linuxType = array("Debian") ;
+    public $distros = array("Ubuntu") ;
+    public $versions = array("11.04", "11.10", "12.04", "12.10", "13.04") ;
+    public $architectures = array("any") ;
+
+    // Model Group
+    public $modelGroup = array("Default") ;
+
+    public function __construct($params) {
+        parent::__construct($params);
+        $this->autopilotDefiner = "LinuxCompiler";
+        $this->installCommands = array(
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", "c++", "build-essential", "make")) ),
+        );
+        $this->uninstallCommands = array(
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", "c++", "build-essential", "make")) ),
+        );
+        $this->programDataFolder = "/opt/LinuxCompiler"; // command and app dir name
+        $this->programNameMachine = "ubuntucompiler"; // command and app dir name
+        $this->programNameFriendly = "Linux Comp!"; // 12 chars
+        $this->programNameInstaller = "Linux Compiler";
+        $this->initialize();
+    }
+
+    public function askStatus() {
+        $stat1 = $this->askStatusByArray( array("make") ) ;
+        $aptFactory = new Apt();
+        $aptModel = $aptFactory->getModel($this->params) ;
+        $stat2 = $aptModel->isInstalled("build-essential") ;
+        return ($stat1==true && $stat2==true) ? true : false ;
+    }
+}
---
+++ src/Modules/LinuxCompiler/Views/linuxCompilerView.tpl.php
@@ -0,0 +1,8 @@
+Compiler Result: <?php
+
+var_dump($pageVars["result"]) ;
+
+$result = ($pageVars["result"] == true) ? "Success" : "Failure" ;
+echo $result ;
+
+?>
