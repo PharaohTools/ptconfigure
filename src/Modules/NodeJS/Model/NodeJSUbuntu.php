0940aba29ae263dd31d45a155251affcdbc8ec08
David Amanshia <phpengine@pharaotools.com>
2017-03-11 23:12:53 +0000

    node js packages fix

--- src/Modules/NodeJS/Model/NodeJSUbuntu.php
+++ src/Modules/NodeJS/Model/NodeJSUbuntu.php
@@ -8,7 +8,7 @@ class NodeJSUbuntu extends BaseLinuxApp {
     public $os = array("Linux") ;
     public $linuxType = array("Debian") ;
     public $distros = array("Ubuntu") ;
-    public $versions = array("11.04", "11.10", "12.04", "12.10", "13.04") ;
+    public $versions = array(array("11.04", "+")) ;
     public $architectures = array("any") ;
 
     // Model Group
@@ -18,10 +18,10 @@ class NodeJSUbuntu extends BaseLinuxApp {
         parent::__construct($params);
         $this->autopilotDefiner = "NodeJS";
         $this->installCommands = array(
-            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", array("node", "nodejs"))) ),
+            array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("Apt", array("nodejs"))) ),
         );
         $this->uninstallCommands = array(
-            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", array("node", "nodejs"))) ),
+            array("method"=> array("object" => $this, "method" => "packageRemove", "params" => array("Apt", array("nodejs"))) ),
         );
         $this->programDataFolder = "/opt/NodeJS"; // command and app dir name
         $this->programNameMachine = "nodejs"; // command and app dir name
