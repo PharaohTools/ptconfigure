7b5f79206cfed2d6e4fec4505a63debfa8c81a4b
Pharaoh Tools <phpengine@pharaohtools.com>
2017-04-09 23:45:52 +0100

    php modules mongo fix on php7

--- src/Modules/PHPModules/Model/PHPModulesUbuntu.php
+++ src/Modules/PHPModules/Model/PHPModulesUbuntu.php
@@ -52,7 +52,7 @@ class PHPModulesUbuntu extends BaseLinuxApp {
         "{$ps2}-sqlite {$ps2}-ldap php-pear" ;
 
         if (PHP_MAJOR_VERSION > 6) {
-            $pstr .= " {$ps2}-xml openssl" ; }  // openssl is required for mongo in php7
+            $pstr .= " {$ps2}-xml openssl pkg-config" ; }  // openssl, pkg-config are required for mongo in php7
 
         $this->packages = array( $pstr ) ;
     }
