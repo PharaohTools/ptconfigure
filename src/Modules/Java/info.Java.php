80d6350fc95d32c97e2c84a612eb0a701d356412
David Amanshia <phpengine@pharaohtools.com>
2017-08-06 20:13:57 +0100

    java jdk version 1.8

--- src/Modules/Java/info.Java.php
+++ src/Modules/Java/info.Java.php
@@ -9,34 +9,33 @@ class JavaInfo extends PTConfigureBase {
     public $name = "Java JDK";
 
     public function __construct() {
-        parent::__construct();
+      parent::__construct();
     }
 
     public function routesAvailable() {
-        return array( "Java" =>  array_merge(parent::routesAvailable(), array("install") ) );
+      return array( "Java" =>  array_merge(parent::routesAvailable(), array("install") ) );
     }
 
     public function routeAliases() {
-        return array("java"=>"Java");
+      return array("java"=>"Java");
     }
 
     public function helpDefinition() {
-        $help = <<<"HELPDATA"
+      $help = <<<"HELPDATA"
   This module allows you to install Java JDK 1.7 or 1.8 .
 
   Java, java
 
         - install
         Installs a version of Oracle Java JDK 1.7 or 1.8. It will also configure java,
-        javac and javaws to be provided by the newly installed Oracle version.
-        
+        javac and javaws to be provided by the new Oracle version.
         example: ptconfigure java install # will install 1.7
         example: ptconfigure java install --java-install-version=1.7
         example: ptconfigure java install --java-install-version=1.8
 
 
 HELPDATA;
-        return $help ;
+      return $help ;
     }
 
 }
