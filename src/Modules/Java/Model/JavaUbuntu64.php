1997d67de66372e2a8e9bf2efd08530487b4f02a
David Amanshia <phpengine@pharaohtools.com>
2017-08-06 22:00:13 +0100

    Java Ubuntu latest

--- src/Modules/Java/Model/JavaUbuntu64.php
+++ src/Modules/Java/Model/JavaUbuntu64.php
@@ -20,42 +20,18 @@ class JavaUbuntu64 extends BaseLinuxApp {
         parent::__construct($params);
         $this->autopilotDefiner = "Java";
         $this->installCommands = array(
+            array("method"=> array("object" => $this, "method" => "askForJavaInstallVersion", "params" => array()) ),
             array("method"=> array("object" => $this, "method" => "askForJavaInstallDirectory", "params" => array()) ),
-            array("command" => array(
-                "curl -o /tmp/oraclejdk.zip http://46f95a86014936ec1625-77a12a9c8b6f69dd83500dbd082befcc.r16.cf3.rackcdn.com/jdk-17.zip" ,
-                // "mv /tmp/6c383e2868bd.zip /tmp/oraclejdk.zip",
-                "unzip /tmp/oraclejdk.zip -d /tmp/oraclejdk",
-                "rm -f /tmp/oraclejdk.zip",
-                "mv /tmp/oraclejdk/phpengine-cleo-jdk-64-6c383e2868bd/jdk-7u60-linux-x64.tar.gz /tmp/oraclejdk" ,
-                "cd /tmp/oraclejdk && tar -xf jdk-7u60-linux-x64.tar.gz" ,
-                "mkdir -p ****PROGDIR****" ,
-                "cp -r /tmp/oraclejdk/jdk1.7.0_60/* ****PROGDIR****" ,
-                "rm -rf /tmp/oraclejdk" ,
-                "cd ****PROGDIR****",
-                "chmod a+x ****PROGDIR****",
-                'echo \'JAVA_HOME=****PROGDIR****\' >> /etc/profile',
-                'echo \'PATH=$PATH:$HOME/bin:$JAVA_HOME/bin\' >> /etc/profile',
-                'echo \'export JAVA_HOME\' >> /etc/profile',
-                'echo \'export PATH\' >> /etc/profile',
-                'echo \'JAVA_HOME=****PROGDIR****\' >> /etc/bash.bashrc',
-                'echo \'PATH=$PATH:$HOME/bin:$JAVA_HOME/bin\' >> /etc/bash.bashrc',
-                'echo \'export JAVA_HOME\' >> /etc/bash.bashrc',
-                'echo \'export PATH\' >> /etc/bash.bashrc',
-                SUDOPREFIX.'update-alternatives --install "/usr/bin/java" "java" "****PROGDIR****/bin/java" 1 ',
-                SUDOPREFIX.'update-alternatives --install "/usr/bin/javac" "javac" "****PROGDIR****/bin/javac" 1 ',
-                SUDOPREFIX.'update-alternatives --install "/usr/bin/javaws" "javaws" "****PROGDIR****/bin/javaws" 1 ',
-                SUDOPREFIX.'update-alternatives --set java ****PROGDIR****/bin/java ',
-                SUDOPREFIX.'update-alternatives --set javac ****PROGDIR****/bin/javac ',
-                SUDOPREFIX.'update-alternatives --set javaws ****PROGDIR****/bin/javaws ',
-                '. /etc/profile' ) )
-            );
+            array("method"=> array("object" => $this, "method" => "runJavaInstall", "params" => array()) ),
+        );
+
         //@todo uninstall commands of java
         $this->uninstallCommands = array(
             array("method"=> array("object" => $this, "method" => "askForJavaInstallDirectory", "params" => array()) ),);
-        $this->programDataFolder = "/var/lib/jvm/jdk1.7";
+        $this->programDataFolder = "/var/lib/jvm/jdk".$this->params["java-install-version"];
         $this->programNameMachine = "java"; // command and app dir name
         $this->programNameFriendly = "!!Java JDK!!"; // 12 chars
-        $this->programNameInstaller = "The Oracle Java JDK 1.7";
+        $this->programNameInstaller = "The Oracle Java JDK";
         $this->statusCommand = 'java -version' ;
         $this->versionInstalledCommand = 'java -version 2>&1' ;
         $this->versionRecommendedCommand = SUDOPREFIX."apt-cache policy java" ;
@@ -64,25 +40,62 @@ class JavaUbuntu64 extends BaseLinuxApp {
     }
 
     protected function askForJavaInstallVersion() {
-        if (isset($this->params["guess"]) && $this->params["guess"]==true) {
+        if (isset($this->params["java-install-version"])) {
+            $this->javaDetails = $this->getJavaDetails($this->params["java-install-version"]); }
+        else if (isset($this->params["guess"]) && $this->params["guess"]==true) {
+            $this->params["java-install-version"] = "1.7" ;
             $this->javaDetails = $this->getJavaDetails("1.7") ; }
-        else if (isset($this->params["java-install-version"])) {
-            $this->javaDetails = $this->getJavaDetails("java-install-version"); }
         else {
             $question = "Enter Java Install Version (1.7 or 1.8):";
             $jd = self::askForInput($question, true);
+            $this->params["java-install-version"] = $jd ;
             $this->javaDetails = $this->getJavaDetails($jd); }
+    }
+
+    protected function runJavaInstall() {
+        $ray =
+            array(
+                array("command" => array(
+                    "curl -o /tmp/oraclejdk.tar.gz {$this->javaDetails['jdk_url']}" ,
+                     "mv /tmp/6c383e2868bd.zip /tmp/oraclejdk.zip",
+                    "mkdir -p /tmp/oraclejdk",
+                    "tar -xzf /tmp/oraclejdk.tar.gz -C /tmp/oraclejdk",
+                    "rm -f /tmp/oraclejdk.zip",
+                    "mkdir -p ****PROGDIR****" ,
+                    "cp -r /tmp/oraclejdk/{$this->javaDetails['extracted_dir']}/* ****PROGDIR****" ,
+                    "rm -rf /tmp/oraclejdk" ,
+                    "cd ****PROGDIR****",
+                    "chmod a+x ****PROGDIR****",
+                    'echo \'JAVA_HOME=****PROGDIR****\' >> /etc/profile',
+                    'echo \'PATH=$PATH:$HOME/bin:$JAVA_HOME/bin\' >> /etc/profile',
+                    'echo \'export JAVA_HOME\' >> /etc/profile',
+                    'echo \'export PATH\' >> /etc/profile',
+                    'echo \'JAVA_HOME=****PROGDIR****\' >> /etc/bash.bashrc',
+                    'echo \'PATH=$PATH:$HOME/bin:$JAVA_HOME/bin\' >> /etc/bash.bashrc',
+                    'echo \'export JAVA_HOME\' >> /etc/bash.bashrc',
+                    'echo \'export PATH\' >> /etc/bash.bashrc',
+                    SUDOPREFIX.'update-alternatives --install "/usr/bin/java" "java" "****PROGDIR****/bin/java" 1 ',
+                    SUDOPREFIX.'update-alternatives --install "/usr/bin/javac" "javac" "****PROGDIR****/bin/javac" 1 ',
+                    SUDOPREFIX.'update-alternatives --install "/usr/bin/javaws" "javaws" "****PROGDIR****/bin/javaws" 1 ',
+                    SUDOPREFIX.'update-alternatives --set java ****PROGDIR****/bin/java ',
+                    SUDOPREFIX.'update-alternatives --set javac ****PROGDIR****/bin/javac ',
+                    SUDOPREFIX.'update-alternatives --set javaws ****PROGDIR****/bin/javaws ',
+                    '. /etc/profile' )
+                )
+            ) ;
+        $this->installCommands = $ray ;
+        return $this->doInstallCommand() ;
     }
 
     public function getJavaDetails($version) {
         if ($version == "1.8") {
-            $details['jdk_url'] = "http://46f95a86014936ec1625-77a12a9c8b6f69dd83500dbd082befcc.r16.cf3.rackcdn.com/jdk.1.8.zip" ;
+            $details['jdk_url'] = "http://46f95a86014936ec1625-77a12a9c8b6f69dd83500dbd082befcc.r16.cf3.rackcdn.com/jdk1.8.tar.gz" ;
             $details['path_in_repo'] = "phpengine-cleo-jdk-64-6c383e2868bd/jdk-7u60-linux-x64.tar.gz" ;
             $details['fname_in_repo'] = "jdk-7u60-linux-x64.tar.gz" ;
-            $details['extracted_dir'] = "jdk1.7.0_60" ;
+            $details['extracted_dir'] = "jdk1.8.0_131" ;
         } else {
-            $details['jdk_url'] = "http://46f95a86014936ec1625-77a12a9c8b6f69dd83500dbd082befcc.r16.cf3.rackcdn.com/jdk1.7.zip" ;
-            $details['path_in_repo'] = "phpengine-cleo-jdk-64-6c383e2868bd/jdk-7u60-linux-x64.tar.gz" ;
+            $details['jdk_url'] = "http://46f95a86014936ec1625-77a12a9c8b6f69dd83500dbd082befcc.r16.cf3.rackcdn.com/jdk1.7.tar.gz" ;
+            $details['path_in_repo'] = "jdk-7u60-linux-x64.tar.gz" ;
             $details['fname_in_repo'] = "jdk-7u60-linux-x64.tar.gz" ;
             $details['extracted_dir'] = "jdk1.7.0_60" ;
         }
