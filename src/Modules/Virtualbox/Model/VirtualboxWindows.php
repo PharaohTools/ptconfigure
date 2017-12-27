0bfb7d8f043e8d98b797ce7a285174a38fda0b48
David Amanshia <phpengine@pharaohtools.com>
2017-12-27 06:32:21 +0000

    virtualbox updated to wrk on windows with latest package manager

--- src/Modules/Virtualbox/Model/VirtualboxWindows.php
+++ src/Modules/Virtualbox/Model/VirtualboxWindows.php
@@ -6,16 +6,16 @@ class VirtualboxWindows extends BaseWindowsApp {
 
     // Compatibility
     public $os = array("Windows", "WINNT") ;
-    public $linuxType = array("None") ;
-    public $distros = array("None") ;
-    public $versions = array("6", "7", "8", "9", '10') ;
+    public $linuxType = array('any') ;
+    public $distros = array('any') ;
+    public $versions = array('any') ;
     public $architectures = array("any") ;
 
     // Model Group
     public $modelGroup = array("Default") ;
 
     // Virtualbox requested version
-    public $vbv ;
+    public $exeInstallFlags = ' --silent -msiparams ALLUSERS=1' ;
 
     // Windows Package
     public $packageUrl ;
@@ -25,14 +25,12 @@ class VirtualboxWindows extends BaseWindowsApp {
         $this->autopilotDefiner = "Virtualbox";
         $this->installCommands = $this->getInstallCommands() ;
         $this->uninstallCommands = array( ) ;
-        $this->programDataFolder = "/var/lib/virtualbox"; // command and app dir name
+        $this->programDataFolder = $_ENV['ProgramFiles']."\\Virtualbox\\"; // command and app dir name
         $this->programNameMachine = "virtualbox"; // command and app dir name
-        $this->programNameFriendly = " ! Virtualbox !"; // 12 chars
+        $this->programNameFriendly = "! VirtualBox !"; // 12 chars
         $this->programNameInstaller = "Virtualbox";
+        $this->packageSearchString = "Oracle VM VirtualBox";
         $this->statusCommand = "where.exe VBoxManage" ;
-        $this->versionInstalledCommand = SUDOPREFIX."apt-cache policy virtualbox" ;
-        $this->versionRecommendedCommand = SUDOPREFIX."apt-cache policy virtualbox" ;
-        $this->versionLatestCommand = SUDOPREFIX."apt-cache policy virtualbox" ;
         $this->initialize();
     }
 
@@ -41,9 +39,9 @@ class VirtualboxWindows extends BaseWindowsApp {
         $loggingFactory = new \Model\Logging();
         $logging = $loggingFactory->getModel($this->params);
         $ray = array(
-            array("method"=> array("object" => $this, "method" => "askForVirtualboxVersion", "params" => array()) ),
+            array("method"=> array("object" => $this, "method" => "askForVersion", "params" => array()) ),
             array("method"=> array("object" => $this, "method" => "setPackageUrl", "params" => array()) ),
-            array("method"=> array("object" => $this, "method" => "executeDependencies", "params" => array()) ),
+            // array("method"=> array("object" => $this, "method" => "packageDownload", "params" => array()) ),
             array("method"=> array("object" => $this, "method" => "packageAdd", "params" => array("WinExe", "Virtualbox")) ),
         ) ;
         if (isset($this->params["with-guest-additions"]) && $this->params["with-guest-additions"]==true) {
@@ -53,19 +51,7 @@ class VirtualboxWindows extends BaseWindowsApp {
         return $ray ;
     }
 
-    protected function askForVirtualboxVersion(){
-        $ao = array("5.2.0") ;
-        if (isset($this->params["version"]) && in_array($this->params["version"], $ao)) {
-            $this->vbv = $this->params["version"] ; }
-        else if (isset($this->params["guess"])) {
-            $index = count($ao)-1 ;
-            $this->vbv = $ao[$index] ; }
-        else {
-            $question = 'Enter Virtualbox Version';
-            $this->vbv = self::askForArrayOption($question, $ao, true); }
-    }
-
-    protected function setPackageUrl(){
+    public function setPackageUrl(){
         $pus = array(
             "5.2.0" => "http://download.virtualbox.org/virtualbox/5.2.0/VirtualBox-5.2.0-118431-Win.exe" ,
         ) ;
