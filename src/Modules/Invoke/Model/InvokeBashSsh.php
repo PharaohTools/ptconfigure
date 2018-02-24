e3ff90769436d230423b5d2deb20f41b4f7ae3f5
David Amanshia <phpengine@pharaohtools.com>
2018-02-24 13:01:39 +0000

    bash ssh doesnt extend so module name as string

--- src/Modules/Invoke/Model/InvokeBashSsh.php
+++ src/Modules/Invoke/Model/InvokeBashSsh.php
@@ -43,7 +43,7 @@ class InvokeBashSsh {
         if (!function_exists("pcntl_fork")) {
             $loggingFactory = new \Model\Logging();
             $logging = $loggingFactory->getModel($this->params);
-            $logging->log("Unable to use pcntl_fork, ending", $this->getModuleName()) ;
+            $logging->log("Unable to use pcntl_fork, ending", 'BashSSH') ;
             return false ; }
         $pcomm = "$launcher 'echo Pharaoh Tools'" ;
         passthru($pcomm, $res) ;
