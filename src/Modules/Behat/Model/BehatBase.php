c1c4f400514725a824f546397c142ac50b4e992a
David Amanshia <phpengine@pharaohtools.com>
2018-03-24 09:00:28 +0000

    behat fixes for different oses

---
+++ src/Modules/Behat/Model/BehatBase.php
@@ -0,0 +1,28 @@
+<?php
+
+Namespace Model;
+
+class BehatBase extends BaseComposerApp {
+
+    // Compatibility
+    public $os = array("any") ;
+    public $linuxType = array("any") ;
+    public $distros = array("any") ;
+    public $versions = array("any") ;
+    public $architectures = array("any") ;
+
+    // Model Group
+    public $modelGroup = array("Default") ;
+
+    public function __construct($params) {
+        parent::__construct($params);
+        $this->autopilotDefiner = "Behat";
+        $this->programNameMachine = "behat"; // command and app dir name
+        $this->programNameFriendly = " Behat "; // 12 chars
+        $this->programNameInstaller = "Behat";
+        $this->programExecutorTargetPath = 'behat/bin/behat';
+        $this->statusCommand = "behat -h" ;
+        $this->initialize();
+    }
+
+}
--- src/Modules/Behat/Model/BehatWindows.php
+++ src/Modules/Behat/Model/BehatWindows.php
@@ -2,13 +2,13 @@
 
 Namespace Model;
 
-class BehatAllOS extends BaseComposerApp {
+class BehatOSX extends BehatBase {
 
     // Compatibility
-    public $os = array("any") ;
+    public $os = array("Darwin") ;
     public $linuxType = array("any") ;
     public $distros = array("any") ;
-    public $versions = array("any") ;
+    public $versions = array('any') ;
     public $architectures = array("any") ;
 
     // Model Group
