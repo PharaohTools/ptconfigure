2c56ffaf6eb759102c9d698ccc7e0d13f3f7eefd
David Amanshia <phpengine@pharaohtools.com>
2019-03-19 16:16:48 +0000

    bakery routes

--- src/Modules/Bakery/info.Bakery.php
+++ src/Modules/Bakery/info.Bakery.php
@@ -28,13 +28,20 @@ class BakeryInfo extends PTConfigureBase {
   Bakery, bakery
 
         - osinstall
-        Installs an Operating System from its base ISO Image, into a format of your choice
-        example: ptconfigure bakery osinstall # will install 1.7
-        example: ptconfigure bakery osinstall --bakery-install-version=1.7
-        example: ptconfigure bakery osinstall --bakery-install-version=1.8
+        Installs an Operating System from its base ISO Image, into a Box package usable by Virtualize
+        example: ptconfigure bakery osinstall # will ask for below mentioned parameters
+        example: ptconfigure bakery osinstall --iso="ubuntu.iso" --yes --guess
+        example: ptconfigure bakery osinstall
+                                      --iso="ubuntu.iso"
+                                      --name="ptv_bakery_temp_vm" # Optional
+                                      --ostype="Ubuntu_64" # Optional
+                                      --memory="512" # Optional
+                                      --vram="33" # Optional
+                                      --cpus="1" # Optional
+                                      --ssh_forwarding_port="9988" # Optional
 
         - bake
-        Create a new Image, including specified configurations, into format of your choice
+        Create a new Box Package from a Base Box, including specified configurations, into format of your choice
         example: ptconfigure bakery bake # will install 1.7
         example: ptconfigure bakery bake --bakery-install-version=1.7
         example: ptconfigure bakery bake --bakery-install-version=1.8
