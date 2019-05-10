af7c374a2c3f05b38ff99a91c250e8a0a2972c8b
David Amanshia <phpengine@pharaohtools.com>
2019-05-10 19:26:30 +0100

    fixed packages for vbox

--- src/Modules/Bakery/Templates/Ubuntu/postinstall.sh
+++ src/Modules/Bakery/Templates/Ubuntu/postinstall.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 apt-get update -y
-apt-get install -y virtualbox-guest-additions-iso
+apt-get install -y virtualbox-guest-x11 virtualbox-guest-dkms virtualbox-guest-additions-iso
 mkdir -p /mnt/guestadditionsiso/
 mount -o loop /usr/share/virtualbox/VBoxGuestAdditions.iso /mnt/guestadditionsiso/
 /mnt/guestadditionsiso/VBoxLinuxAdditions.run
-unmount /mnt/guestadditionsiso/
+umount /mnt/guestadditionsiso/
 shutdown now
