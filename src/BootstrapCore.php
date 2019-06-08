784bc936b055307618ee872934526c5ebf073fea
David Amanshia <phpengine@pharaohtools.com>
2019-06-09 00:20:19 +0100

    supress startup and end logs for not cli

--- src/BootstrapCore.php
+++ src/BootstrapCore.php
@@ -23,17 +23,22 @@ class BootStrap {
     }
 
     public function main($argv_or_boot_params_null) {
+        ini_set('display_errors', 0) ;
+        ini_set('display_startup_errors', 0) ;
         date_default_timezone_set('Europe/London');
         $this->start = time() ;
         $date_format = date('H:i:s, d/m/Y', $this->start) ;
         $friendly = substr(PHARAOH_APP, 2) ;
         $friendly = ucfirst($friendly) ;
-        echo "[Pharaoh {$friendly}] [Start] Execution begun at ".$date_format.PHP_EOL;
+        $is_cli = $this->isCLI($argv_or_boot_params_null) ;
+        if ($is_cli === true) {
+            echo "[Pharaoh {$friendly}] [Start] Execution begun at ".$date_format.PHP_EOL;
+        }
         $routeObject = new \Core\Router();
         $route = $routeObject->run($argv_or_boot_params_null);
         $emptyPageVars = array("messages"=>array(), "route"=>$route);
         $this->executeControl($route["control"], $emptyPageVars);
-        $this->exitGracefully();
+        $this->exitGracefully($is_cli);
     }
 
     public function executeControl($controlToExecute, $pageVars=null) {
@@ -54,22 +59,40 @@ class BootStrap {
         $view->executeView($viewTemplate, $viewVars);
     }
 
-    private function exitGracefully() {
+    private function exitGracefully($is_cli = false) {
         // @note this must be the last executed line as it sets exit code
         $cur = time() ;
         $finish = $cur - $this->start ;
         $date_format = date('H:i:s, d/m/Y', $cur) ;
         $friendly = substr(PHARAOH_APP, 2) ;
         $friendly = ucfirst($friendly) ;
-        echo "[Pharaoh {$friendly}] [Exit] Execution finished at {$date_format}, after ".$finish." seconds ".PHP_EOL;
+        if ($is_cli === true) {
+            echo "[Pharaoh {$friendly}] [Exit] Execution finished at {$date_format}, after ".$finish." seconds ".PHP_EOL; }
         if (self::$exitCode == null) {
             exit(0) ; }
         else if (!is_int(self::$exitCode)) {
-            echo "[Pharaoh {$friendly}] [Exit] Non Integer Exit Code Attempted".PHP_EOL; ;
+            if ($is_cli === true) {
+                echo "[Pharaoh {$friendly}] [Exit] Non Integer Exit Code Attempted".PHP_EOL; }
             exit(1) ; }
         else {
-            echo "[Pharaoh {$friendly}] [Exit] Exiting with exit code: ".self::$exitCode.PHP_EOL; ;
+            if ($is_cli === true) {
+                echo "[Pharaoh {$friendly}] [Exit] Exiting with exit code: ".self::$exitCode.PHP_EOL; }
             exit(self::$exitCode) ; }
+    }
+
+    private function isCLI($argv_or_boot_params_null) {
+        foreach ($argv_or_boot_params_null as $param) {
+            $output_prefix = substr($param, 0, 16) ;
+            $output_format = substr($param, 16) ;
+            if ($output_prefix === '--output-format=') {
+                if ($output_format === 'cli') {
+                    return true ;
+                } else {
+                    return false ;
+                }
+            }
+        }
+        return false ;
     }
 
 }
