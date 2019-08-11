49f6a9c87b2329141912aa779775d81cdb8c7b81
Dave da Rave <dave@pharaohtools.com>
2019-08-11 17:58:02 +0100

    autopilots for bakery at latest stage

---
+++ src/Modules/Bakery/Autopilots/PTConfigure/readme.md
@@ -0,0 +1,3 @@
+Run it like this
+
+ptconfigure auto x --af=multi-os-install.dsl.yml --vars=vars_ubuntu_14.04.6.php,vars_all.php --step-times --step-numbers
--- src/Modules/Bakery/Autopilots/PTConfigure/vars_ubuntu_14.04.6.php
+++ src/Modules/Bakery/Autopilots/PTConfigure/vars_ubuntu_14.04.6.php
@@ -10,11 +10,19 @@ if (isset($params)) {
 
 $variables['os'] = 'Ubuntu' ;
 $variables['os_type'] = 'Ubuntu_64' ;
-$variables['os_major_version'] = '16' ;
+$variables['os_major_version'] = '14' ;
 $variables['os_minor_version'] = '04' ;
 $variables['os_build_version'] = '6' ;
+$variables['os_slugvar'] = 'ubuntu_14.04.6' ;
 $variables['os_full_version'] = $variables['os_major_version'].'.'.$variables['os_minor_version'].'.'.$variables['os_build_version'] ;
 $variables['os_vbox_version'] = $variables['os_major_version'].'.'.$variables['os_minor_version'] ;
 $variables['os_group'] = "server-64bit" ;
-$variables['iso_file_remote_location'] = "";
-$variables['iso_path'] = "{{{ Facts::Runtime::factGetEnvVar::HOME }}}/Downloads/ubuntu-16.04.6-server-amd64.iso";
+$variables['iso_file_remote_location']  = "http://releases.ubuntu.com/";
+$variables['iso_file_remote_location'] .= "{$variables['os_major_version']}.{$variables['os_minor_version']}/";
+$variables['iso_file_remote_location'] .= "ubuntu-{$variables['os_major_version']}.{$variables['os_minor_version']}";
+$variables['iso_file_remote_location'] .= ".{$variables['os_build_version']}";
+$variables['iso_file_remote_location'] .= "-server-amd64.iso";
+$variables['iso_path']  = "{{{ Facts::Runtime::factGetEnvVar::HOME }}}/Downloads/ubuntu-";
+$variables['iso_path'] .= "{$variables['os_major_version']}.{$variables['os_minor_version']}";
+$variables['iso_path'] .= ".{$variables['os_build_version']}";
+$variables['iso_path'] .= "-server-amd64.iso";
