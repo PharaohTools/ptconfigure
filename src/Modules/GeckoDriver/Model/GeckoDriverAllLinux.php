141560755aaff9df74259934492dbbb590893598
Dave da Rave <dave@pharaohtools.com>
2020-06-07 02:54:29 +0100

    gecko installler additions

--- src/Modules/GeckoDriver/Model/GeckoDriverAllLinux.php
+++ src/Modules/GeckoDriver/Model/GeckoDriverAllLinux.php
@@ -54,12 +54,12 @@ class GeckoDriverAllLinux extends BaseLinuxApp {
                 "cd /tmp" ,
                 "mkdir -p /tmp/geckodriver" ,
                 "cd /tmp/geckodriver" ,
-                "wget http://geckodriver.storage.googleapis.com/{$this->sv}/geckodriver_linux{$arch}.zip",
+                "wget https://github.com/mozilla/geckodriver/releases/download/v{$this->sv}/geckodriver-v{$this->sv}-linux{$arch}.tar.gz",
                 "mkdir -p {$this->programDataFolder}",
                 "mv /tmp/geckodriver/* {$this->programDataFolder}",
                 "rm -rf /tmp/geckodriver/",
                 "cd {$this->programDataFolder}",
-                "unzip -o geckodriver_linux{$arch}.zip",
+                "tar -xvf geckodriver-v{$this->sv}-linux{$arch}.tar.gz",
                 SUDOPREFIX."chmod -R u+x {$this->programDataFolder}",
                 SUDOPREFIX."chmod -R 777 {$this->programDataFolder}",
                 'echo \'PATH=$PATH:/opt/geckodriver\' >> /etc/profile',
@@ -72,8 +72,9 @@ class GeckoDriverAllLinux extends BaseLinuxApp {
 
     protected function askForGeckoDriverVersion(){
         $ao = array (
-            "2.0", "2.10", "2.1", "2.11", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7",
-            "2.8", "2.9", "80.0.3987.106"
+            "0.26.0","0.25.0","0.24.0","0.23.0","0.22.0","0.21.0","0.20.0",
+            "0.19.0","0.18.0","0.17.0","0.16.0","0.15.0","0.14.0","0.13.0",
+            "0.12.0","0.11.0","0.10.0"
         ) ;
         if (isset($this->params["version"]) && in_array($this->params["version"], $ao)) {
             $this->sv = $this->params["version"] ; }
